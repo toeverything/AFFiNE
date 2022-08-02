a73651817e273c494e2ec5be44f3bc303d124e8f
alt0 <wxlxinglong@outlook.com>
2022-08-02 18:50:49 +0800

    feat(draw): when first time transfer to board mode, use page width as editor shape's width

--- libs/components/affine-board/src/hooks/use-shapes.ts
+++ libs/components/affine-board/src/hooks/use-shapes.ts
@@ -3,8 +3,12 @@ import { services } from '@toeverything/datasource/db-service';
 import type { ReturnEditorBlock } from '@toeverything/datasource/db-service';
 import type { TDShape } from '@toeverything/components/board-types';
 import { Editor } from '@toeverything/components/board-shapes';
+import { usePageClientWidth } from '@toeverything/datasource/state';
 
 export const useShapes = (workspace: string, rootBlockId: string) => {
+    const { pageClientWidth } = usePageClientWidth();
+    // page padding left and right total 300px
+    const editorShapeInitSize = pageClientWidth - 300;
     const [blocks, setBlocks] = useState<ReturnEditorBlock[]>();
     useEffect(() => {
         services.api.editorBlock
@@ -58,8 +62,9 @@ export const useShapes = (workspace: string, rootBlockId: string) => {
             acc[block.id] = { ...shapeProps, id: block.id };
         } else {
             acc[block.id] = Editor.getShape({
-                point: [groupCount * 740, 200],
+                point: [groupCount * editorShapeInitSize + 200, 200],
                 id: block.id,
+                size: [editorShapeInitSize, 200],
                 ...shapeProps,
                 affineId: shapeProps.affineId ?? block.id,
                 workspace: block.workspace,
