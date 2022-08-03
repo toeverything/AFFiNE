2f03304961d2aa69bcc393de234e98483cc2300d
xiaodong zuo <zuoxd02@gmail.com>
2022-08-04 06:11:07 +0800

    fix: variable naming

--- libs/datasource/jwt/src/adapter/yjs/binary.ts
+++ libs/datasource/jwt/src/adapter/yjs/binary.ts
@@ -3,29 +3,29 @@ import { Array as YArray, Map as YMap } from 'yjs';
 import { RemoteKvService } from '@toeverything/datasource/remote-kv';
 
 export class YjsRemoteBinaries {
-    readonly #binaries: YMap<YArray<ArrayBuffer>>; // binary instance
-    readonly #remote_storage?: RemoteKvService;
+    readonly _binaries: YMap<YArray<ArrayBuffer>>; // binary instance
+    readonly _remoteStorage?: RemoteKvService;
 
     constructor(binaries: YMap<YArray<ArrayBuffer>>, remote_token?: string) {
-        this.#binaries = binaries;
+        this._binaries = binaries;
         if (remote_token) {
-            this.#remote_storage = new RemoteKvService(remote_token);
+            this._remoteStorage = new RemoteKvService(remote_token);
         } else {
             console.warn(`Remote storage is not ready`);
         }
     }
 
     has(name: string): boolean {
-        return this.#binaries.has(name);
+        return this._binaries.has(name);
     }
 
     async get(name: string): Promise<YArray<ArrayBuffer> | undefined> {
-        if (this.#binaries.has(name)) {
-            return this.#binaries.get(name);
+        if (this._binaries.has(name)) {
+            return this._binaries.get(name);
         } else {
             // TODO: Remote Load
             try {
-                const file = await this.#remote_storage?.instance.getBuffData(
+                const file = await this._remoteStorage?.instance.getBuffData(
                     name
                 );
                 console.log(file);
@@ -38,16 +38,16 @@ export class YjsRemoteBinaries {
     }
 
     async set(name: string, binary: YArray<ArrayBuffer>) {
-        if (!this.#binaries.has(name)) {
+        if (!this._binaries.has(name)) {
             console.log(name, 'name');
             if (binary.length === 1) {
-                this.#binaries.set(name, binary);
-                if (this.#remote_storage) {
+                this._binaries.set(name, binary);
+                if (this._remoteStorage) {
                     // TODO: Remote Save, if there is an object with the same name remotely, the upload is skipped, because the file name is the hash of the file content
-                    const has_file = this.#remote_storage.instance.exist(name);
+                    const has_file = this._remoteStorage.instance.exist(name);
                     if (!has_file) {
                         const upload_file = new File(binary.toArray(), name);
-                        await this.#remote_storage.instance
+                        await this._remoteStorage.instance
                             .upload(upload_file)
                             .catch(err => {
                                 throw new Error(`${err} upload error`);
