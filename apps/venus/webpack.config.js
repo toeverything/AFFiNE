1ee53790220816524bf58c86f1400d4c03a21d4b
CarlosZoft <carlosk2gunn@hotmail.com>
2022-08-06 09:54:18 -0300

    refactor : remove redundant if

--- apps/venus/webpack.config.js
+++ apps/venus/webpack.config.js
@@ -35,11 +35,6 @@ module.exports = function (webpackConfig) {
 
     if (isProd) {
         config.module.rules.unshift(style9);
-    } else {
-        config.module.rules.push(style9);
-    }
-
-    if (isProd) {
         config.entry = {
             main: [...config.entry.main, ...config.entry.polyfills],
         };
@@ -124,6 +119,7 @@ module.exports = function (webpackConfig) {
         });
         config.module.rules.splice(6);
     } else {
+        config.module.rules.push(style9);
         config.output = {
             ...config.output,
             publicPath: '/',
