3274d3aaff7c48ffeb989661ff24c76d5495d889
DarkSky <25152247+darkskygit@users.noreply.github.com>
2022-08-09 10:55:02 +0800

    Update notify.mjs

--- tools/notify.mjs
+++ tools/notify.mjs
@@ -1,7 +1,7 @@
 import got from 'got';
 
 const STAGE_HOST = 'https://nightly.affine.pro/';
-if (process.env.CF_PAGES_BRANCH === 'master') {
+if (['master', 'develop'].includes(process.env.CF_PAGES_BRANCH)) {
     const message = `Daily builds: New deployment of Ligo-Virgo version [${process.env.CF_PAGES_COMMIT_SHA}](${STAGE_HOST}), this [${STAGE_HOST}](${STAGE_HOST}) link always points to the latest version deployment, to access the old version, please go to Github to view the deployment record.`;
     const url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`;
 
