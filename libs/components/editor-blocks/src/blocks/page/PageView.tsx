7c076ea567a26ca1f7b5c9a814c7f465f79a34d8
DiamondThree <diamond.shx@gmail.com>
2022-08-10 18:35:31 +0800

    fix: style ui

--- libs/components/editor-blocks/src/blocks/page/PageView.tsx
+++ libs/components/editor-blocks/src/blocks/page/PageView.tsx
@@ -109,12 +109,15 @@ export const PageView: FC<CreateView> = ({ block, editor }) => {
     );
 };
 
-const PageTitleBlock = styled('div')({
-    '.title': {
-        fontSize: Theme.typography.page.fontSize,
-        lineHeight: Theme.typography.page.lineHeight,
-    },
-    '.content': {
-        outline: 'none',
-    },
+const PageTitleBlock = styled('div')(({ theme }) => {
+    return {
+        '.title': {
+            fontSize: theme.affine.typography.page.fontSize,
+            lineHeight: theme.affine.typography.page.lineHeight,
+            fontWeight: theme.affine.typography.page.fontWeight,
+        },
+        '.content': {
+            outline: 'none',
+        },
+    };
 });
