feda6d390e4f523c4b4534cac68bbfc14821b4e8
alt0 <wxlxinglong@outlook.com>
2022-08-12 18:24:32 +0800

    fix: update e2e

--- apps/ligo-virgo-e2e/src/integration/app.spec.ts
+++ apps/ligo-virgo-e2e/src/integration/app.spec.ts
@@ -4,7 +4,7 @@ describe('ligo-virgo', () => {
     beforeEach(() => cy.visit('/'));
 
     it('basic load check', () => {
-        getTitle().contains('Get Started with AFFiNE');
+        getTitle().contains('👋 Get Started with AFFINE');
 
         cy.get('.block_container').contains('The Essentials');
 
