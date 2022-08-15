2dc11231d315cc04623b41114c349ff0efa0f789
DiamondThree <diamond.shx@gmail.com>
2022-08-15 21:09:09 +0800

    fix:while bord group error

--- libs/components/affine-board/src/hooks/use-shapes.ts
+++ libs/components/affine-board/src/hooks/use-shapes.ts
@@ -57,27 +57,23 @@ export const useShapes = (workspace: string, rootBlockId: string) => {
     return blocks?.reduce((acc, block) => {
         const shapeProps = block.properties.shapeProps?.value
             ? JSON.parse(block.properties.shapeProps.value)
-            : null;
+            : {};
         if (block.type === 'shape') {
-            if (shapeProps) {
-                acc[block.id] = { ...shapeProps, id: block.id };
-            }
+            acc[block.id] = { ...shapeProps, id: block.id };
         } else {
             acc[block.id] = Editor.getShape({
                 point: [groupCount * editorShapeInitSize + 200, 200],
                 id: block.id,
                 size: [editorShapeInitSize, 200],
-                ...(shapeProps || {}),
-
-                affineId: shapeProps
-                    ? shapeProps.affineId ?? block.id
-                    : block.id,
+                ...shapeProps,
+                affineId: shapeProps.affineId ?? block.id,
                 workspace: block.workspace,
                 rootBlockId: block.id,
             });
             groupCount = groupCount + 1;
         }
 
+        console.log('acc: ', acc);
         return acc;
     }, {} as Record<string, TDShape>);
 };
