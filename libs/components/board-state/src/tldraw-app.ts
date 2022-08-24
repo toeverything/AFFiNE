e13c1947c45f94f71bebbacd156da49edcc055ac
DiamondThree <diamond.shx@gmail.com>
2022-08-24 15:22:54 +0800

    delete log

--- libs/components/board-state/src/tldraw-app.ts
+++ libs/components/board-state/src/tldraw-app.ts
@@ -15,13 +15,13 @@ import {
     TLPointerEventHandler,
     TLShapeCloneHandler,
     TLWheelEventHandler,
-    Utils,
+    Utils
 } from '@tldraw/core';
 import { Vec } from '@tldraw/vec';
 import {
     clearPrevSize,
     defaultStyle,
-    shapeUtils,
+    shapeUtils
 } from '@toeverything/components/board-shapes';
 import {
     AlignType,
@@ -54,7 +54,7 @@ import {
     TDUser,
     TldrawCommand,
     USER_COLORS,
-    VIDEO_EXTENSIONS,
+    VIDEO_EXTENSIONS
 } from '@toeverything/components/board-types';
 import { MIN_PAGE_WIDTH } from '@toeverything/components/editor-core';
 import {
@@ -67,7 +67,7 @@ import {
     migrate,
     openAssetFromFileSystem,
     openFromFileSystem,
-    saveToFileSystem,
+    saveToFileSystem
 } from './data';
 import { getClipboard, setClipboard } from './idb-clipboard';
 import { StateManager } from './manager/state-manager';
@@ -2899,7 +2899,6 @@ export class TldrawApp extends StateManager<TDSnapshot> {
      * @param center The point to zoom toward.
      */
     zoomBy = Utils.throttle((delta: number, center?: number[]): this => {
-        console.log(123123);
         const { zoom } = this.camera;
         const nextZoom = TLDR.get_camera_zoom(zoom - delta * zoom);
         return this.zoomTo(nextZoom, center);
