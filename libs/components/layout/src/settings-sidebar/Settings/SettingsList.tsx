e737c86692ffaba50ccfc70d078da6374231271a
JimmFly <yangjinfei001@gmail.com>
2022-09-01 17:38:48 +0800

    update

--- libs/components/layout/src/settings-sidebar/Settings/SettingsList.tsx
+++ libs/components/layout/src/settings-sidebar/Settings/SettingsList.tsx
@@ -49,7 +49,7 @@ export const SettingsList = () => {
                         {item.key === 'Language' ? (
                             <div style={{ marginLeft: '12em' }}>
                                 <Select
-                                    defaultValue={opetion[0].value}
+                                    defaultValue={options[0].value}
                                     onChange={changeLanguage}
                                 >
                                     {options.map(option => (
