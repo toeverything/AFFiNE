857e2d82d1bcc996801affc659d919e066f68610
DarkSky <darksky2048@gmail.com>
2022-09-06 17:00:51 +0800

    feat: fix style for dark mode

---
+++ apps/venus/src/app/common/index.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { styled } from '@mui/joy/styles';
+
+export const AFFiNEImage = styled('img')({
+    maxWidth: '100%',
+    objectFit: 'contain',
+});
+
+export { AFFiNEFooter } from './Footer';
+export { AFFiNEHeader } from './Header';
