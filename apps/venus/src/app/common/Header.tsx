715b235fea14c80536e4bc9549cbaa58611f317f
zuomeng wang <wxlxinglong@outlook.com>
2022-09-08 16:24:49 +0800

    Update landing page (#387)
    * feat: update landing page
    
    * feat(code): improve code editing experience
    
    * feat: update landing page
    
    * fix: react warning
    
    * fix: update logo and i18n
    
    Co-authored-by: tzhangchi <terry.zhangchi@outlook.com>
    Co-authored-by: Yifeng Wang <doodlewind@qq.com>

--- apps/venus/src/app/common/Header.tsx
+++ apps/venus/src/app/common/Header.tsx
@@ -6,82 +6,141 @@ import { Button, Grid } from '@mui/joy';
 import Option from '@mui/joy/Option';
 import Select from '@mui/joy/Select';
 // eslint-disable-next-line no-restricted-imports
-import { useMediaQuery } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+// eslint-disable-next-line no-restricted-imports
+import { styled, useMediaQuery } from '@mui/material';
+import AFFiNETextLogo from './affine-text-logo.png';
+import { HoverMenu } from './HoverMenu';
 
 import { options } from '../i18n';
-import { GitHub } from '../Icons';
 
 export const AFFiNEHeader = () => {
     const matches = useMediaQuery('(max-width: 1024px)');
     const navigate = useNavigate();
-    const { i18n } = useTranslation();
+    const { i18n, t } = useTranslation();
 
     const changeLanguage = (event: any) => {
         i18n.changeLanguage(event);
     };
     const matchesIPAD = useMediaQuery('(max-width: 768px)');
     return (
-        <Grid
-            container
-            spacing={2}
-            sx={{
-                maxWidth: '1280px',
-                margin: 'auto',
-            }}
-        >
-            <Grid xs={6}>
-                <Button
-                    size="lg"
-                    variant="plain"
-                    sx={{
-                        padding: matches ? '0' : '0 0.5em',
-                        ':hover': { backgroundColor: 'unset' },
-                        fontSize: '24px',
-                        '@media (max-width: 1024px)': {
-                            fontSize: '16px',
-                        },
-                    }}
+        <Container container spacing={2}>
+            <Grid xs={6} sx={{ display: 'flex', alignItems: 'center' }}>
+                <StyledImage
+                    src={AFFiNETextLogo}
+                    alt="affine"
                     onClick={() => navigate('/')}
-                >
-                    AFFiNE
-                </Button>
-            </Grid>
-            <Grid xs={6} sx={{ display: 'flex', justifyContent: 'right' }}>
-                <GitHub flat />
-                <Button
-                    onClick={() => window.open('https://blog.affine.pro')}
-                    variant="plain"
-                    sx={{
-                        padding: matches ? '0' : '0 0.5em',
-                        ':hover': { backgroundColor: 'unset' },
-                        fontSize: '24px',
-                        '@media (max-width: 1024px)': {
-                            fontSize: '16px',
-                        },
-                    }}
-                    size="lg"
-                >
-                    Blog
-                </Button>
+                />
                 <Button
                     onClick={() => navigate('/aboutus')}
                     variant="plain"
+                    color="neutral"
                     sx={{
                         padding: matches ? '0' : '0 0.5em',
-                        ':hover': { backgroundColor: 'unset' },
-                        fontSize: '24px',
-                        '@media (max-width: 1024px)': {
-                            fontSize: '16px',
-                        },
+                        fontSize: '16px',
                     }}
-                    size="lg"
+                    size="md"
                 >
-                    About Us
+                    {t('AboutUs')}
                 </Button>
+                <Button
+                    onClick={() => window.open('https://blog.affine.pro')}
+                    variant="plain"
+                    color="neutral"
+                    sx={{
+                        padding: matches ? '0' : '0 0.5em',
+                        fontSize: '16px',
+                    }}
+                    size="md"
+                >
+                    {t('Blog')}
+                </Button>
+                <Button
+                    onClick={() => window.open('https://docs.affine.pro/')}
+                    variant="plain"
+                    color="neutral"
+                    sx={{
+                        padding: matches ? '0' : '0 0.5em',
+                        fontSize: '16px',
+                    }}
+                    size="md"
+                >
+                    {t('Docs')}
+                </Button>
+                <Button
+                    onClick={() => window.open('https://feedback.affine.pro/')}
+                    variant="plain"
+                    color="neutral"
+                    sx={{
+                        padding: matches ? '0' : '0 0.5em',
+                        fontSize: '16px',
+                    }}
+                    size="md"
+                >
+                    {t('Feedback')}
+                </Button>
+            </Grid>
+            <Grid xs={6} sx={{ display: 'flex', justifyContent: 'right' }}>
+                <Button
+                    variant="plain"
+                    color="neutral"
+                    onClick={() =>
+                        window.open('https://github.com/toeverything/AFFiNE')
+                    }
+                    sx={{
+                        padding: matches ? '0' : '0 0.5em',
+                        fontSize: '16px',
+                    }}
+                    size="md"
+                >
+                    <GitHubIcon />
+                </Button>
+                <Button
+                    onClick={() => window.open('https://livedemo.affine.pro/')}
+                    variant="plain"
+                    color="neutral"
+                    sx={{
+                        padding: matches ? '0' : '0 0.5em',
+                        fontSize: '16px',
+                    }}
+                    size="md"
+                >
+                    {t('Try it Online')}
+                </Button>
+                <HoverMenu
+                    title={t('ContactUs')}
+                    options={[
+                        {
+                            title: 'Discord',
+                            value: 'https://discord.gg/Arn7TqJBvG',
+                        },
+                        {
+                            title: 'Telegram',
+                            value: 'https://t.me/affineworkos',
+                        },
+                        {
+                            title: 'Reddit',
+                            value: 'https://www.reddit.com/r/Affine/',
+                        },
+                        {
+                            title: 'Medium',
+                            value: 'https://medium.com/@affineworkos',
+                        },
+                        {
+                            title: 'Email',
+                            value: 'mailto:contact@toeverything.info',
+                        },
+                    ]}
+                    onSelect={href => {
+                        window.open(href);
+                    }}
+                />
                 <Select
                     defaultValue="en"
                     sx={{ display: matchesIPAD ? 'none' : 'intial' }}
                     onChange={changeLanguage}
+                    size="md"
+                    variant="plain"
                 >
                     {options.map(option => (
                         <Option key={option.value} value={option.value}>
@@ -90,6 +149,26 @@ export const AFFiNEHeader = () => {
                     ))}
                 </Select>
             </Grid>
-        </Grid>
+        </Container>
     );
 };
+
+const Container = styled(Grid)({
+    position: 'fixed',
+    top: 0,
+    left: '50%',
+    transform: 'translateX(-50%)',
+    width: '100%',
+    paddingTop: '1em',
+    backgroundColor: '#fff',
+    zIndex: 1500,
+    maxWidth: '1440px',
+    margin: 'auto',
+    marginTop: '0 !important',
+});
+
+const StyledImage = styled('img')({
+    height: '24px',
+    marginRight: '16px',
+    cursor: 'pointer',
+});
