6d89c7f9a0db76d31b0576f45f53060ae11d3230
alt0 <wxlxinglong@outlook.com>
2022-12-05 15:54:11 +0000

    docs: update demo page content

--- packages/app/src/pages-content/DemoPage/index.tsx
+++ packages/app/src/pages-content/DemoPage/index.tsx
@@ -2,7 +2,11 @@ export const DemoPageContent = () => {
   return (
     <div>
       <h1>Demo</h1>
-      <p>Demo content.</p>
+      <p>
+        We agreed to implement the complex page this way, but since it's in a
+        different branch, we wrote this demo page first to prevent merge
+        conflicts.
+      </p>
     </div>
   );
 };
