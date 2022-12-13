d4a8189ced3a4af5709b13a58539f56fc35233b1
QiShaoXuan <qishaoxuan777@gmail.com>
2022-12-13 17:47:41 +0800

    fix: lint error

--- packages/app/src/components/quick-search/content/searchResult/NotFound.tsx
+++ packages/app/src/components/quick-search/content/searchResult/NotFound.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Command, useCommandState } from 'cmdk';
 
 const noResult = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const search = useCommandState(state => state.search);
+  // eslint-disable-next-line react/no-unescaped-entities
   return <Command.Empty>No results found for "{search}".</Command.Empty>;
 };
 
