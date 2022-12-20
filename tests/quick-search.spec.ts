a7f167968f70b3d77c75bb49d063f6a9aebf2467
JimmFly <yangjinfei001@gmail.com>
2022-12-20 09:59:20 +0800

    test: update test case for quick search

--- tests/quick-search.spec.ts
+++ tests/quick-search.spec.ts
@@ -15,10 +15,21 @@ async function assertTitleTexts(
   texts: string[],
   option?: { delay: number }
 ) {
-  await page.mouse.move(100, 100); // move mouse for focus
+  await page.mouse.move(100, 100);
   const actual = await page
     .locator('[class=affine-default-page-block-title]')
     .allInnerTexts();
+  setTimeout(() => {
+    expect(actual).toEqual(texts);
+  }, option?.delay);
+}
+async function assertResultList(
+  page: Page,
+  texts: string[],
+  option?: { delay: number }
+) {
+  await page.mouse.move(100, 100);
+  const actual = await page.locator('[cmdk-item]').allInnerTexts();
   setTimeout(() => {
     expect(actual).toEqual(texts);
   }, option?.delay);
@@ -31,7 +42,7 @@ test.describe('Open quick search', () => {
     );
     await quickSearchButton.click();
     const quickSearch = page.locator('[data-testid=quickSearch]');
-    expect(quickSearch.isVisible()).toEqual(true);
+    await expect(quickSearch).toBeVisible();
   });
   test('Click arrowDown icon after title', async ({ page }) => {
     //header-quickSearchButton
@@ -40,12 +51,12 @@ test.describe('Open quick search', () => {
     );
     await quickSearchButton.click();
     const quickSearch = page.locator('[data-testid=quickSearch]');
-    expect(quickSearch.isVisible()).toEqual(true);
+    await expect(quickSearch).toBeVisible();
   });
   test('Press the shortcut key cmd+k', async ({ page }) => {
     await openQuickSearchByShortcut(page);
     const quickSearch = page.locator('[data-testid=quickSearch]');
-    expect(quickSearch.isVisible()).toEqual(true);
+    await expect(quickSearch).toBeVisible();
   });
 });
 
@@ -62,5 +73,30 @@ test.describe('Add new page in quick search', () => {
     const addNewPage = page.locator('[data-testid=quickSearch-addNewPage]');
     await addNewPage.click();
     await assertTitleTexts(page, ['test'], { delay: 50 });
+  });
+});
+
+test.describe('Search and select', () => {
+  test('Search and get results', async ({ page }) => {
+    await openQuickSearchByShortcut(page);
+    await page.keyboard.insertText('Welcome');
+    await assertResultList(page, ['Welcome to the AFFiNE Alpha'], {
+      delay: 50,
+    });
+  });
+  test('Create a new page and search this page', async ({ page }) => {
+    await openQuickSearchByShortcut(page);
+    await page.keyboard.insertText('Welcome');
+    const addNewPage = page.locator('[data-testid=quickSearch-addNewPage]');
+    await addNewPage.click();
+    await page.waitForTimeout(500);
+    await openQuickSearchByShortcut(page);
+    await page.keyboard.insertText('Welcome');
+    await assertResultList(page, ['Welcome to the AFFiNE Alpha', 'Welcome']);
+    await page.keyboard.press('ArrowDown', { delay: 50 });
+    await page.keyboard.press('Enter', { delay: 50 });
+    await assertTitleTexts(page, ['Welcome'], {
+      delay: 50,
+    });
   });
 });
