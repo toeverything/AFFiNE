16b79e2824ed5a593e9d46bb4a10f45d77207d98
tzhangchi <terry.zhangchi@outlook.com>
2022-12-23 16:58:38 +0800

    feat: add empty cover for PageList

--- packages/app/src/components/page-list/empty.tsx
+++ packages/app/src/components/page-list/empty.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-
-export const Empty = () => {
+import { Empty } from '@/ui/empty';
+export const PageListEmpty = () => {
   return (
     <div style={{ textAlign: 'center' }}>
+      <Empty
+        width={800}
+        height={300}
+        sx={{ marginTop: '100px', marginBottom: '30px' }}
+      />
       <p>Tips: Click Add to Favourites/Trash and the page will appear here.</p>
       <p>(Designer is grappling with designing)</p>
     </div>
   );
 };
 
-export default Empty;
+export default PageListEmpty;
