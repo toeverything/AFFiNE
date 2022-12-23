07e158970667fdec81fc4447a1ae40b24820205b
tzhangchi <terry.zhangchi@outlook.com>
2022-12-23 22:00:58 +0800

    test: make change-page-mode test stable

--- tests/change-page-mode.spec.ts
+++ tests/change-page-mode.spec.ts
@@ -10,7 +10,9 @@ test.describe('Change page mode(Paper or Edgeless)', () => {
     await expect(box?.x).not.toBeUndefined();
 
     // mouse hover trigger animation for showing full switcher
-    await page.mouse.move((box?.x ?? 0) + 5, (box?.y ?? 0) + 5);
+    // await page.mouse.move((box?.x ?? 0) + 5, (box?.y ?? 0) + 5);
+    await page.mouse.move((box?.x ?? 0) + 10, (box?.y ?? 0) + 10);
+
     // await page.waitForTimeout(1000);
     const edgelessButton = page.getByTestId('edgeless-item'); // page.getByText('Edgeless').click()
 
