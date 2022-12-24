90bd753a6ccd51406da74140996437fe5a32ef48
tzhangchi <terry.zhangchi@outlook.com>
2022-12-24 22:39:13 +0800

    test: test case is added to firebase.app redirect

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -14,4 +14,24 @@ test.describe('Login Flow', () => {
       .getByRole('heading', { name: 'Currently not logged in' })
       .click();
   });
+
+  test('Open google firebase page', async ({ page }) => {
+    await page.getByTestId('current-workspace').click();
+    await page.waitForTimeout(800);
+    // why don't we use waitForSelector, It seems that waitForSelector not stable?
+    await page.getByTestId('open-login-modal').click();
+    await page.waitForTimeout(800);
+    const [firebasePage] = await Promise.all([
+      page.waitForEvent('popup'),
+      page
+        .getByRole('button', {
+          name: 'Google Continue with Google Set up an AFFINE account to sync data',
+        })
+        .click(),
+    ]);
+
+    expect(await firebasePage.url()).toContain(
+      '.firebaseapp.com/__/auth/handler'
+    );
+  });
 });
