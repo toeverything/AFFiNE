11d8cc83ef6e5a7b2adfb07a77c6b081de9e4c5d
Himself65 <himself65@outlook.com>
2022-12-24 16:22:58 +0800

    feat: support debug with blocksuite (#582)
    Co-authored-by: Yifeng Wang <doodlewind@toeverything.info>

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,5 +1,6 @@
 const { getGitVersion, getCommitHash } = require('./scripts/gitInfo');
-const { dependencies } = require('./package.json')
+const { dependencies } = require('./package.json');
+const path = require('node:path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,7 +14,38 @@ const nextConfig = {
     CI: process.env.CI || null,
     VERSION: getGitVersion(),
     COMMIT_HASH: getCommitHash(),
-    EDITOR_VERSION: dependencies['@blocksuite/editor']
+    EDITOR_VERSION: dependencies['@blocksuite/editor'],
+  },
+  transpilePackages: process.env.LOCAL_BLOCK_SUITE
+    ? ['@blocksuite/editor', '@blocksuite/blocks', '@blocksuite/store']
+    : [],
+  webpack: config => {
+    if (
+      process.env.LOCAL_BLOCK_SUITE &&
+      path.isAbsolute(process.env.LOCAL_BLOCK_SUITE)
+    ) {
+      config.resolve.alias['yjs'] = require.resolve('yjs');
+      config.resolve.extensionAlias = {
+        '.js': ['.js', '.ts', '.tsx'],
+      };
+      const baseDir = process.env.LOCAL_BLOCK_SUITE;
+      config.resolve.alias['@blocksuite/editor'] = path.resolve(
+        baseDir,
+        'packages',
+        'editor'
+      );
+      config.resolve.alias['@blocksuite/blocks'] = path.resolve(
+        baseDir,
+        'packages',
+        'blocks'
+      );
+      config.resolve.alias['@blocksuite/store'] = path.resolve(
+        baseDir,
+        'packages',
+        'store'
+      );
+    }
+    return config;
   },
   images: {
     unoptimized: true,
