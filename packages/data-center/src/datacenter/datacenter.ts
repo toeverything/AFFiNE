e6e4c775a8031f9e30d84eb938ae469ee32f52c6
DarkSky <darksky2048@gmail.com>
2022-12-29 18:40:43 +0800

    feat: data center part0

--- packages/data-center/src/datacenter/datacenter.ts
+++ packages/data-center/src/datacenter/datacenter.ts
@@ -1,9 +1,18 @@
+import { DCProvider, MemoryProvider } from './provider.js';
+
 export class DataCenter {
+  private readonly _providers = new Map<string, DCProvider>();
+
   static async init() {
-    return new DataCenter();
+    const dc = new DataCenter();
+    dc.addProvider(MemoryProvider);
   }
 
   private constructor() {
     // TODO
+  }
+
+  addProvider<P extends DCProvider>(provider: P) {
+    this._providers.set(provider.id, provider);
   }
 }
