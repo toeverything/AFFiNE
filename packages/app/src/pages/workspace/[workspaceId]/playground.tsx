233dcc88176da122a8be6efec438f5f97ba4427e
tzhangchi <terry.zhangchi@outlook.com>
2023-01-03 14:29:32 +0800

    feat: add playground

--- packages/app/src/pages/workspace/[workspaceId]/playground.tsx
+++ packages/app/src/pages/workspace/[workspaceId]/playground.tsx
@@ -1,29 +1,48 @@
 import { styled } from '@/styles';
 
-import usePageMetaList from '@/hooks/use-page-meta-list';
-import { ReactElement } from 'react';
+import { ReactElement, ReactNode } from 'react';
 import WorkspaceLayout from '@/components/workspace-layout';
 import { Button } from '@/ui/button';
 
-export const FeatureCardDiv = styled('div')({
+export const FeatureCardDiv = styled('section')({
   width: '800px',
   border: '1px #eee solid',
   margin: '20px auto',
   minHeight: '200px',
   padding: '15px',
 });
+const FeatureCard = (props: {
+  name: string;
+  children: ReactNode | ReactNode[];
+}) => {
+  return (
+    <FeatureCardDiv>
+      <h1>Feature - {props.name}</h1>
+      {props.children}
+    </FeatureCardDiv>
+  );
+};
 
 export const Playground = () => {
   return (
     <>
-      <FeatureCardDiv>
-        <h1>Feature - Sign In</h1>
-        <Button>Login</Button>
-      </FeatureCardDiv>
+      <FeatureCard name="Account">
+        <Button>Sign In</Button>
+        <Button>Sign Out</Button>
+      </FeatureCard>
 
-      <FeatureCardDiv>
-        <h1>Feature - Workspace List</h1>
-      </FeatureCardDiv>
+      <FeatureCard name="Workspace List">
+        <ul>
+          <li>AFFiNE Demo</li>
+        </ul>
+      </FeatureCard>
+
+      <FeatureCard name="Active Workspace">
+        <Button>Upate Workspace Name</Button>
+        <Button>Upate Workspace Aavartor</Button>
+        <Button>Leave Workspace</Button>
+        <Button>Delete Workspace </Button>
+      </FeatureCard>
     </>
   );
 };
