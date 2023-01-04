c73252018285797b6b8c86571ed7177eb959ef7e
Lin Onetwo <linonetwo012@gmail.com>
2023-01-04 21:04:06 +0800

    feat: directly open affine page on production

--- client-app/src-tauri/src/main.rs
+++ client-app/src-tauri/src/main.rs
@@ -5,14 +5,23 @@
 
 mod commands;
 mod state;
+use dotenvy::dotenv;
 use state::AppState;
-use tokio::sync::Mutex;
+use std::env;
 use tauri::TitleBarStyle;
+use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() {
   tauri::async_runtime::set(tokio::runtime::Handle::current());
+  dotenv().ok();
   let preload = include_str!("../../public/preload/index.js");
+  let is_dev = env::var("NODE_ENV").unwrap_or_default() == "development";
+  let initial_path = if is_dev {
+    "index.html"
+  } else {
+    "affine-out/index.html"
+  };
   tauri::Builder::default()
     .manage(AppState(Mutex::new(
       state::AppStateRaw::new().await.unwrap(),
@@ -20,7 +29,7 @@ async fn main() {
     // manually create window here, instead of in the tauri.conf.json, to add `initialization_script` here
     .setup(move |app| {
       let _window =
-        tauri::WindowBuilder::new(app, "label", tauri::WindowUrl::App("index.html".into()))
+        tauri::WindowBuilder::new(app, "label", tauri::WindowUrl::App(initial_path.into()))
           .title("AFFiNE")
           .inner_size(1000.0, 800.0)
           .title_bar_style(TitleBarStyle::Overlay)
