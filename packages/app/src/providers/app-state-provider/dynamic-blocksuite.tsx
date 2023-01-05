27b7c4e8fb5ecb1e996bd2aab098fa4914668e13
QiShaoXuan <qishaoxuan777@gmail.com>
2023-01-05 18:33:17 +0800

    chore: remove useless file & code

--- packages/app/src/providers/app-state-provider/dynamic-blocksuite.tsx
+++ packages/app/src/providers/app-state-provider/dynamic-blocksuite.tsx
@@ -2,31 +2,13 @@ import { useEffect } from 'react';
 import type { Page } from '@blocksuite/store';
 import '@blocksuite/blocks';
 import { EditorContainer } from '@blocksuite/editor';
-import type { LoadWorkspaceHandler, CreateEditorHandler } from './context';
-import { getDataCenter } from '@affine/datacenter';
+import type { CreateEditorHandler } from './context';
 
 interface Props {
-  setLoadWorkspaceHandler: (handler: LoadWorkspaceHandler) => void;
   setCreateEditorHandler: (handler: CreateEditorHandler) => void;
 }
 
-const DynamicBlocksuite = ({
-  setLoadWorkspaceHandler,
-  setCreateEditorHandler,
-}: Props) => {
-  useEffect(() => {
-    const openWorkspace: LoadWorkspaceHandler = async (workspaceId: string) => {
-      if (workspaceId) {
-        const dc = await getDataCenter();
-        return dc.load(workspaceId, { providerId: 'affine' });
-      } else {
-        return null;
-      }
-    };
-
-    setLoadWorkspaceHandler(openWorkspace);
-  }, [setLoadWorkspaceHandler]);
-
+const DynamicBlocksuite = ({ setCreateEditorHandler }: Props) => {
   useEffect(() => {
     const createEditorHandler: CreateEditorHandler = (page: Page) => {
       const editor = new EditorContainer();
