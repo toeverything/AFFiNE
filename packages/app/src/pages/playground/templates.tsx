3745b8f6d40b77b7fa7ea96fd6493c9659be9ec9
x1a0t <405028157@qq.com>
2023-01-06 15:03:42 +0800

    feat: compatiable with async importMarkdown()

--- packages/app/src/pages/playground/templates.tsx
+++ packages/app/src/pages/playground/templates.tsx
@@ -43,13 +43,16 @@ const All = () => {
     if (page) {
       currentWorkspace?.setPageMeta(page.id, { title });
       if (page && page.root === null) {
-        setTimeout(() => {
+        setTimeout(async () => {
           const editor = document.querySelector('editor-container');
           if (editor) {
             page.addBlock({ flavour: 'affine:surface' }, null);
             const frameId = page.addBlock({ flavour: 'affine:frame' }, pageId);
             // TODO blocksuite should offer a method to import markdown from store
-            editor.clipboard.importMarkdown(template.source, `${frameId}`);
+            await editor.clipboard.importMarkdown(
+              template.source,
+              `${frameId}`
+            );
             page.resetHistory();
             editor.requestUpdate();
           }
