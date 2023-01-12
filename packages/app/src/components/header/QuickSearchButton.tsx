03a827e75952d7002e752221a22be4ce3c923ad1
JimmFly <yangjinfei001@gmail.com>
2023-01-12 17:28:18 +0800

    chore: update searchArrowButton style

--- packages/app/src/components/header/QuickSearchButton.tsx
+++ packages/app/src/components/header/QuickSearchButton.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { IconButton, IconButtonProps } from '@/ui/button';
-import { Tooltip } from '@/ui/tooltip';
 import { ArrowDownIcon } from '@blocksuite/icons';
 import { useModal } from '@/providers/GlobalModalProvider';
-import { useTranslation } from '@affine/i18n';
 import { styled } from '@/styles';
 
-const StyledIconButtonWithAnimate = styled(IconButton)(() => {
+const StyledIconButtonWithAnimate = styled(IconButton)(({ theme }) => {
   return {
     svg: {
       transition: 'transform 0.15s ease-in-out',
@@ -14,6 +12,9 @@ const StyledIconButtonWithAnimate = styled(IconButton)(() => {
     ':hover': {
       svg: {
         transform: 'translateY(3px)',
+      },
+      '::after': {
+        background: theme.colors.pageBackground,
       },
     },
   };
@@ -23,20 +24,17 @@ export const QuickSearchButton = ({
   ...props
 }: Omit<IconButtonProps, 'children'>) => {
   const { triggerQuickSearchModal } = useModal();
-  const { t } = useTranslation();
   return (
-    <Tooltip content={t('Jump to')} placement="bottom">
-      <StyledIconButtonWithAnimate
-        data-testid="header-quickSearchButton"
-        {...props}
-        onClick={e => {
-          onClick?.(e);
-          triggerQuickSearchModal();
-        }}
-      >
-        <ArrowDownIcon />
-      </StyledIconButtonWithAnimate>
-    </Tooltip>
+    <StyledIconButtonWithAnimate
+      data-testid="header-quickSearchButton"
+      {...props}
+      onClick={e => {
+        onClick?.(e);
+        triggerQuickSearchModal();
+      }}
+    >
+      <ArrowDownIcon />
+    </StyledIconButtonWithAnimate>
   );
 };
 
