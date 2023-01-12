a45040a95649fa56c9ecc6ddedcd868c7dc07037
x1a0t <405028157@qq.com>
2023-01-12 16:13:26 +0800

    fix: deleting bash script, using npm script to be cross platform compatible

--- update-dep.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd packages/app
-pnpm i @blocksuite/store@nightly @blocksuite/blocks@nightly @blocksuite/editor@nightly
-cd ../data-center
-pnpm i @blocksuite/store@nightly @blocksuite/blocks@nightly @blocksuite/editor@nightly
