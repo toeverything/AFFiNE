26c7e4d82a2a7bc51cf2e2ffce6c1ded5be97e88
alt0 <wxlxinglong@outlook.com>
2023-02-01 12:06:28 +0800

    fix: after enabled cloud, should migrate indexeddb

---
+++ packages/data-center/src/provider/affine/idb-kv.ts
@@ -0,0 +1,22 @@
+import { createStore, keys, clear, setMany, getMany } from 'idb-keyval';
+
+type IDBInstance<T = ArrayBufferLike> = {
+  keys: () => Promise<string[]>;
+  clear: () => Promise<void>;
+  setMany: (entries: [string, T][]) => Promise<void>;
+  getMany: (keys: string[]) => Promise<T[]>;
+};
+
+export function getDatabase<T = ArrayBufferLike>(
+  type: string,
+  database: string
+): IDBInstance<T> {
+  const name = `${database}_${type}`;
+  const db = createStore(name, type);
+  return {
+    keys: () => keys(db),
+    clear: () => clear(db),
+    setMany: entries => setMany(entries, db),
+    getMany: keys => getMany(keys, db),
+  };
+}
