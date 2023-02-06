759ec6707ac598a38562a9be2778ca83d54868ba
lawvs <18554747+lawvs@users.noreply.github.com>
2023-02-06 17:37:26 +0800

    feat: add @emotion/babel-plugin

--- packages/app/.babelrc.js
+++ packages/app/.babelrc.js
@@ -7,6 +7,21 @@ if (process.env.NODE_ENV === 'development' || process.env.COVERAGE === 'true') {
   plugins.push('istanbul');
 }
 
+plugins.push([
+  '@emotion',
+  {
+    // See https://emotion.sh/docs/@emotion/babel-plugin
+    importMap: {
+      '@/styles': {
+        styled: {
+          canonicalImport: ['@emotion/styled', 'default'],
+          styledBaseImport: ['@/styles', 'styled'],
+        },
+      },
+    },
+  },
+]);
+
 module.exports = {
   presets: ['next/babel'],
   plugins,
