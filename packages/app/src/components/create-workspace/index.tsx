3c5a98ff7c753c314175eeccbdb0e24ec16ae4fc
QiShaoXuan <qishaoxuan777@gmail.com>
2023-02-07 14:00:33 +0800

    fix: uniform workspace name length limit, fixed #810

--- packages/app/src/components/create-workspace/index.tsx
+++ packages/app/src/components/create-workspace/index.tsx
@@ -57,8 +57,10 @@ export const CreateWorkspaceModal = ({ open, onClose }: ModalProps) => {
             <Input
               onKeyDown={handleKeyDown}
               placeholder={t('Set a Workspace name')}
+              maxLength={15}
+              minLength={0}
               onChange={value => {
-                setWorkspaceName(value.slice(0, 15));
+                setWorkspaceName(value);
               }}
             ></Input>
             <Button
