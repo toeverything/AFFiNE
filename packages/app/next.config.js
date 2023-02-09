eb47794e984a76e60ba72ed47cbfd268085c9274
DarkSky <darksky2048@gmail.com>
2023-02-09 15:24:25 +0800

    chore: renew dev server

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -11,8 +11,8 @@ const EDITOR_VERSION = enableDebugLocal
 
 const profileTarget = {
   ac: '100.85.73.88:12001',
-  dev: '100.77.180.48:11001',
-  test: '100.77.180.48:11001',
+  dev: '100.84.105.99:11001',
+  test: '100.84.105.99:11001',
   stage: '',
   pro: 'http://pathfinder.affine.pro',
   local: '127.0.0.1:3000',
