86fd49eb1cc59a84cfcb4f4807b314db9a1edb91
himself65 <himself65@outlook.com>
2023-02-09 13:33:04 -0600

    chore: bump version

--- scripts/upgrade-blocksuite.sh
+++ scripts/upgrade-blocksuite.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # Usage:
-#   ./scripts/upgrade-blocksuite.sh
 #   ./scripts/upgrade-blocksuite.sh --latest
+#   ./scripts/upgrade-blocksuite.sh 0.4.0-20230209191848-0a912e3
 
-pnpm up "@blocksuite/*" "!@blocksuite/icons" -r -i "$@"
+if [ "$1" == "--latest" ]; then
+  pnpm up "@blocksuite/*" "!@blocksuite/icons" -r -i --latest
+else
+  pnpm up "@blocksuite/*@${1}" "!@blocksuite/icons" -r
+fi
+
