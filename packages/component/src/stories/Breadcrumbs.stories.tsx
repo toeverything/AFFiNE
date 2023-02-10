084d091bdec8f261ec715d16d24c89fb6bd5b5a5
himself65 <himself65@outlook.com>
2023-02-09 23:20:39 -0600

    feat(component): enhance storybook preview

--- packages/component/src/stories/Breadcrumbs.stories.tsx
+++ packages/component/src/stories/Breadcrumbs.stories.tsx
@@ -1,19 +1,24 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Breadcrumbs, getLightTheme, ThemeProvider } from '..';
+import { Breadcrumbs } from '..';
+import { Link, Typography } from '@mui/material';
 
 export default {
   title: 'AFFiNE/Breadcrumbs',
   component: Breadcrumbs,
 } as Meta;
 
-const Template: Story = args => (
-  <ThemeProvider theme={useMemo(() => getLightTheme('page'), [])}>
-    <Breadcrumbs {...args} />
-  </ThemeProvider>
-);
+const Template: Story = args => <Breadcrumbs {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  children: [<span>1</span>, <span>2</span>, <span>3</span>],
+  children: [
+    <Link underline="hover" color="inherit" href="/">
+      AFFiNE
+    </Link>,
+    <Link underline="hover" color="inherit" href="/Docs/">
+      Docs
+    </Link>,
+    <Typography color="text.primary">Introduction</Typography>,
+  ],
 };
--- packages/component/src/stories/Button.stories.tsx
+++ packages/component/src/stories/Button.stories.tsx
@@ -1,20 +1,45 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Button, getLightTheme, ThemeProvider } from '..';
+import { Button } from '..';
+import { ButtonProps } from '../ui/button/interface';
 
 export default {
   title: 'AFFiNE/Button',
   component: Button,
-} as Meta;
+  argTypes: {
+    hoverBackground: { control: 'color' },
+    hoverColor: { control: 'color' },
+  },
+} as Meta<ButtonProps>;
 
-const Template: Story = args => (
-  <ThemeProvider theme={useMemo(() => getLightTheme('page'), [])}>
-    <Button {...args} />
-  </ThemeProvider>
-);
+const Template: Story<ButtonProps> = args => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
   type: 'primary',
-  children: 'This is a button',
+  children: 'This is a primary button',
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  type: 'default',
+  children: 'This is a default button',
+};
+
+export const Light = Template.bind({});
+Light.args = {
+  type: 'light',
+  children: 'This is a light button',
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  type: 'warning',
+  children: 'This is a warning button',
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  type: 'danger',
+  children: 'This is a danger button',
 };
