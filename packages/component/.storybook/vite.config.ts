116f5d13d0f26c10fd19828e25b62f07ad037044
himself65 <himself65@outlook.com>
2023-02-10 15:47:48 -0600

    fix: build

--- packages/component/.storybook/vite.config.ts
+++ packages/component/.storybook/vite.config.ts
@@ -1,9 +1,20 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
 
 const config: Pick<StorybookConfig, 'viteFinal'> = {
   async viteFinal(config, { configType }) {
-    return mergeConfig(config, {});
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          '@': path.resolve(fileURLToPath(new URL('../src', import.meta.url))),
+          '@affine/i18n': path.resolve(
+            fileURLToPath(new URL('../../i18n/src', import.meta.url))
+          ),
+        },
+      },
+    });
   },
 };
 
