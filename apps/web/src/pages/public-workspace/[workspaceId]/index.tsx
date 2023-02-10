69740865bcbe2757b0ebb4101c243dccb36a8356
Peng Xiao <pengxiao@freewheel.tv>
2023-02-10 17:29:13 +0800

    fix: should show workspace avatar using blob url

--- apps/web/src/pages/public-workspace/[workspaceId]/index.tsx
+++ apps/web/src/pages/public-workspace/[workspaceId]/index.tsx
@@ -1,5 +1,5 @@
 import { PageList } from '@/components/page-list';
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement, useEffect, useMemo, useState } from 'react';
 import { PageMeta, useAppState } from '@/providers/app-state-provider';
 import { useRouter } from 'next/router';
 import {
@@ -12,23 +12,36 @@ import { Breadcrumbs } from '@affine/component';
 import { WorkspaceUnitAvatar } from '@/components/workspace-avatar';
 import { SearchIcon } from '@blocksuite/icons';
 import { useModal } from '@/store/globalModal';
+import { WorkspaceUnit } from '@affine/datacenter';
 const All = () => {
   const { dataCenter } = useAppState();
   const router = useRouter();
-  const [pageList, setPageList] = useState<PageMeta[]>([]);
-  const [workspaceName, setWorkspaceName] = useState('');
   const { triggerQuickSearchModal } = useModal();
+  const [workspace, setWorkspace] = useState<WorkspaceUnit>();
+
+  const pageList = useMemo(() => {
+    return (workspace?.blocksuiteWorkspace?.meta.pageMetas ?? []) as PageMeta[];
+  }, [workspace]);
+
+  const workspaceName = workspace?.blocksuiteWorkspace?.meta.name;
+
   useEffect(() => {
-    dataCenter
-      .loadPublicWorkspace(router.query.workspaceId as string)
-      .then(data => {
-        setPageList(data.blocksuiteWorkspace?.meta.pageMetas as PageMeta[]);
-        setWorkspaceName(data.blocksuiteWorkspace?.meta.name as string);
-      })
-      .catch(() => {
-        router.push('/404');
-      });
-  }, [router, dataCenter]);
+    const workspaceId = router.query.workspaceId as string;
+    if (workspaceId) {
+      dataCenter
+        .loadPublicWorkspace(router.query.workspaceId as string)
+        .then(data => {
+          if (workspaceId === router.query.workspaceId) {
+            setWorkspace(data);
+          }
+        })
+        .catch(() => {
+          if (workspaceId === router.query.workspaceId) {
+            router.push('/404');
+          }
+        });
+    }
+  }, [router, router.query, dataCenter]);
 
   return (
     <PageContainer>
@@ -37,7 +50,11 @@ const All = () => {
           <StyledBreadcrumbs
             href={`/public-workspace/${router.query.workspaceId}`}
           >
-            <WorkspaceUnitAvatar size={24} name={workspaceName} />
+            <WorkspaceUnitAvatar
+              size={24}
+              name={workspaceName}
+              workspaceUnit={workspace}
+            />
             <span>{workspaceName}</span>
           </StyledBreadcrumbs>
         </Breadcrumbs>
