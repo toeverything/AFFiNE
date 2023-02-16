eb3a749e151baf998cf4b0b48f71bf59a1d4c2c7
zqran <uuxnet@gmail.com>
2023-02-16 21:53:45 +0800

    feat: optimize type

--- apps/web/src/components/editor/index.tsx
+++ apps/web/src/components/editor/index.tsx
@@ -38,7 +38,7 @@ export const Editor = ({
 
     const editor = new EditorContainer();
     editor.page = page;
-    editor.mode = page.meta.mode as 'page' | 'edgeless';
+    editor.mode = page.meta.mode as typeof editor.mode;
 
     editorContainer.current?.appendChild(editor);
     if (page.isEmpty) {
