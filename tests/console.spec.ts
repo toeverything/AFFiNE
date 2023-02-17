d93d6494d47739b19cd3382b5b2d9a4dbad397e3
himself65 <himself65@outlook.com>
2023-02-16 18:46:50 -0600

    fix: remove url

--- tests/console.spec.ts
+++ tests/console.spec.ts
@@ -3,10 +3,7 @@ import path from 'path';
 import { expect } from '@playwright/test';
 import { test } from './libs/playwright.js';
 import { loadPage } from './libs/load-page.js';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const pkgPath = path.join(__dirname, '../apps/web/package.json');
 const record = fs.readFileSync(pkgPath, 'utf8');
 const temp = JSON.parse(record);
