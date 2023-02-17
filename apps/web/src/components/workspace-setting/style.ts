1e298c61075cc7f68b05aaa085f4d8945365b2c9
QiShaoXuan <qishaoxuan777@gmail.com>
2023-02-17 17:04:29 +0800

    feat: modify collaboration page

--- apps/web/src/components/workspace-setting/style.ts
+++ apps/web/src/components/workspace-setting/style.ts
@@ -4,9 +4,8 @@ export const StyledSettingContainer = styled('div')(() => {
   return {
     display: 'flex',
     flexDirection: 'column',
-    padding: '0 0 20px 48px',
-    height: '100vh',
-    marginTop: '48px',
+    padding: '48px 0 20px 48px',
+    height: 'calc(100vh - 60px)',
   };
 });
 
