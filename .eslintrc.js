0c8bb198e39fb33c26497f53f8246cd9169e9678
himself65 <himself65@outlook.com>
2023-02-17 22:47:47 -0600

    fix: remove

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,7 +40,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    'no-unused-vars': 'off',
     'unused-imports/no-unused-imports': 'error',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
