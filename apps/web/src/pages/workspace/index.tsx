371f9fa69e042c3e6d96d67c5f800e6e69284c57
himself65 <himself65@outlook.com>
2023-02-20 01:26:38 -0600

    feat: update code

--- apps/web/src/pages/workspace/index.tsx
+++ apps/web/src/pages/workspace/index.tsx
@@ -1,43 +1,19 @@
-import { useDataCenter, useDataCenterWorkspace } from '@affine/store';
 import { useRouter } from 'next/router';
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 
 import { PageLoading } from '@/components/loading';
-
-// refactor(himself65): move this to top level hook
-function useCurrentWorkspace() {
-  const router = useRouter();
-  const dataCenter = useDataCenter();
-  const workspaceId =
-    typeof router.query.workspaceId === 'string'
-      ? router.query.workspaceId
-      : dataCenter.workspaces.at(0)?.id ?? null;
-  const currentWorkspace = useDataCenterWorkspace(workspaceId);
-  const notExist = useMemo(
-    () =>
-      workspaceId &&
-      dataCenter.workspaces.length &&
-      dataCenter.workspaces.findIndex(
-        meta => meta.id.toString() === workspaceId
-      ) === -1,
-    [dataCenter.workspaces, workspaceId]
-  );
-  return {
-    currentWorkspace,
-    exist: !notExist,
-  };
-}
+import { useRouterTargetWorkspace } from '@/hooks/use-router-target-workspace';
 
 export const WorkspaceIndex = () => {
   const router = useRouter();
-  const { currentWorkspace, exist } = useCurrentWorkspace();
+  const { targetWorkspace, exist } = useRouterTargetWorkspace();
   useEffect(() => {
     if (!exist) {
       router.push('/404');
-    } else if (currentWorkspace) {
-      router.push(`/workspace/${currentWorkspace.id}`);
+    } else if (targetWorkspace) {
+      router.push(`/workspace/${targetWorkspace.id}`);
     }
-  }, [currentWorkspace, exist, router]);
+  }, [targetWorkspace, exist, router]);
   return <PageLoading />;
 };
 
