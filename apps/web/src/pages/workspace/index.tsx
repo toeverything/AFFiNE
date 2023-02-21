ab2e857f56ac97b37a98537ff6919d2f0e17c1dc
himself65 <himself65@outlook.com>
2023-02-20 23:06:38 -0600

    fix: code

--- apps/web/src/pages/workspace/index.tsx
+++ apps/web/src/pages/workspace/index.tsx
@@ -1,19 +1,27 @@
+import { useGlobalStateApi } from '@affine/store';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { PageLoading } from '@/components/loading';
 import { useRouterTargetWorkspace } from '@/hooks/use-router-target-workspace';
 
 export const WorkspaceIndex = () => {
   const router = useRouter();
+  const api = useGlobalStateApi();
   const { targetWorkspace, exist } = useRouterTargetWorkspace();
+  const onceRef = useRef(true);
   useEffect(() => {
+    if (!onceRef.current) {
+      return;
+    }
+    onceRef.current = true;
     if (!exist) {
       router.push('/404');
     } else if (targetWorkspace) {
+      api.getState().loadWorkspace(targetWorkspace.id);
       router.push(`/workspace/${targetWorkspace.id}`);
     }
-  }, [targetWorkspace, exist, router]);
+  }, [targetWorkspace, exist, router, api]);
   return <PageLoading />;
 };
 
