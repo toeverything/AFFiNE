0a1f6c0841de085289de7b5c72e157f4bee7fb6a
himself65 <himself65@outlook.com>
2023-02-22 22:27:04 -0600

    fix: enable indexed db provider

--- apps/rem/src/blocksuite/index.ts
+++ apps/rem/src/blocksuite/index.ts
@@ -6,7 +6,7 @@ export const createAffineProviders = (
 ): Provider[] => {
   return [
     createWebSocketProvider(blockSuiteWorkspace),
-    // createIndexedDBProvider(blockSuiteWorkspace),
+    createIndexedDBProvider(blockSuiteWorkspace),
   ];
 };
 
