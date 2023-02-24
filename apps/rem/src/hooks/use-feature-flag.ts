95a2166c7eccf1d5dacb0a7369d101ec88e72e08
himself65 <himself65@outlook.com>
2023-02-23 21:04:24 -0600

    feat: add feature flag

---
+++ apps/rem/src/hooks/use-feature-flag.ts
@@ -0,0 +1,46 @@
+import { useCallback, useSyncExternalStore } from 'react';
+
+interface FeatureFlag {
+  enableIndexedDBProvider: boolean;
+}
+
+export const defaultRecord: FeatureFlag = {
+  enableIndexedDBProvider: true,
+} as const;
+
+const featureFlag = {
+  record: {
+    enableIndexedDBProvider: true,
+  } as FeatureFlag,
+  callback: new Set<() => void>(),
+};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var featureFlag: {
+    record: FeatureFlag;
+    callback: Set<() => void>;
+  };
+}
+
+if (!globalThis.featureFlag) {
+  globalThis.featureFlag = featureFlag;
+}
+
+export const getFeatureFlag = <Key extends keyof FeatureFlag>(key: Key) =>
+  featureFlag.record[key];
+
+export function useFeatureFlag<Key extends keyof FeatureFlag>(
+  key: Key
+): FeatureFlag[Key] {
+  return useSyncExternalStore(
+    useCallback(onStoreChange => {
+      featureFlag.callback.add(onStoreChange);
+      return () => {
+        featureFlag.callback.delete(onStoreChange);
+      };
+    }, []),
+    useCallback(() => featureFlag.record[key], [key]),
+    useCallback(() => defaultRecord[key], [key])
+  );
+}
