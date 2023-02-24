d2417a7b28a24e68ae31d05b505552137479dc19
himself65 <himself65@outlook.com>
2023-02-23 21:32:23 -0600

    docs: add comments

--- apps/rem/src/atoms/index.ts
+++ apps/rem/src/atoms/index.ts
@@ -1,8 +1,10 @@
 import { atom } from 'jotai';
 
+// workspace necessary atoms
 export const currentWorkspaceIdAtom = atom<string | null>(null);
 export const currentPageIdAtom = atom<string | null>(null);
 
+// modal atoms
 export const openWorkspacesModalAtom = atom(false);
 export const openCreateWorkspaceModalAtom = atom(false);
 export const openQuickSearchModalAtom = atom(false);
