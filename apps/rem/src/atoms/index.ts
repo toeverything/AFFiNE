f6010925f95530f77f300789d3a84303cbfd0bca
himself65 <himself65@outlook.com>
2023-02-26 12:10:38 -0600

    fix: update lock

--- apps/rem/src/atoms/index.ts
+++ apps/rem/src/atoms/index.ts
@@ -3,7 +3,9 @@ import { atom } from 'jotai';
 // workspace necessary atoms
 export const currentWorkspaceIdAtom = atom<string | null>(null);
 export const currentPageIdAtom = atom<string | null>(null);
-export const readonlyAtom = atom(false);
+// If the workspace is locked, it means that the user maybe updating the workspace
+//  from local to remote or vice versa
+export const workspaceLockAtom = atom(false);
 
 // modal atoms
 export const openWorkspacesModalAtom = atom(false);
