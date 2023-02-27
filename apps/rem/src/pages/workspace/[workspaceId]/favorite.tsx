ed7cc98bbbd3a164f65eb003c9dd2d7a912cb34d
himself65 <himself65@outlook.com>
2023-02-27 11:59:23 -0600

    feat: favorite page

--- apps/rem/src/pages/workspace/[workspaceId]/favorite.tsx
+++ apps/rem/src/pages/workspace/[workspaceId]/favorite.tsx
@@ -1,9 +1,11 @@
 import { useTranslation } from '@affine/i18n';
 import { FavoriteIcon } from '@blocksuite/icons';
+import { assertExists } from '@blocksuite/store';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Helmet } from 'react-helmet-async';
 
+import PageList from '../../../components/blocksuite/block-suite-page-list/page-list';
 import { PageLoading } from '../../../components/pure/loading';
 import { WorkspaceTitle } from '../../../components/pure/workspace-title';
 import { useCurrentWorkspace } from '../../../hooks/current/use-current-workspace';
@@ -21,16 +23,39 @@ const FavouritePage: NextPageWithLayout = () => {
   const { t } = useTranslation();
   useLoadWorkspace(currentWorkspace);
   useSyncRouterWithCurrentWorkspace(router);
+  const onClickPage = useCallback(
+    (pageId: string, newTab?: boolean) => {
+      assertExists(currentWorkspace);
+      if (newTab) {
+        window.open(`/workspace/${currentWorkspace?.id}/${pageId}`, '_blank');
+      } else {
+        router.push({
+          pathname: '/workspace/[workspaceId]/[pageId]',
+          query: {
+            workspaceId: currentWorkspace.id,
+            pageId,
+          },
+        });
+      }
+    },
+    [currentWorkspace, router]
+  );
   if (currentWorkspace === null) {
     return <PageLoading />;
   }
+  const blockSuiteWorkspace = currentWorkspace.blockSuiteWorkspace;
+  assertExists(blockSuiteWorkspace);
   return (
     <>
       <Helmet>
         <title>{t('Favorites')} - AFFiNE</title>
       </Helmet>
       <WorkspaceTitle icon={<FavoriteIcon />}>{t('Favorites')}</WorkspaceTitle>
-      <div>Favourite Page</div>
+      <PageList
+        blockSuiteWorkspace={blockSuiteWorkspace}
+        onClickPage={onClickPage}
+        listType="favorite"
+      />
     </>
   );
 };
