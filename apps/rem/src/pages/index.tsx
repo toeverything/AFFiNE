1af8b5bcdee715602a8114e4d144a319851a4bcf
himself65 <himself65@outlook.com>
2023-02-26 23:56:23 -0600

    fix: code

--- apps/rem/src/pages/index.tsx
+++ apps/rem/src/pages/index.tsx
@@ -1,20 +1,15 @@
 import { useAtom } from 'jotai';
-import { useHydrateAtoms } from 'jotai/utils';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 
-import {
-  currentWorkspaceAtomFromServer,
-  currentWorkspaceIdAtom,
-} from '../atoms';
+import { currentWorkspaceIdAtom } from '../atoms';
 import { PageLoading } from '../components/pure/loading';
 import { prefetchNecessaryData, useWorkspaces } from '../hooks/use-workspaces';
 
 prefetchNecessaryData();
 const IndexPage: NextPage = () => {
   const router = useRouter();
-  useHydrateAtoms([[currentWorkspaceIdAtom, currentWorkspaceAtomFromServer]]);
   const [workspaceId] = useAtom(currentWorkspaceIdAtom);
   const workspaces = useWorkspaces();
   useEffect(() => {
