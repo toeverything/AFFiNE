cf5b902e311fec0db648a2110210d69d0f3f15a1
himself65 <himself65@outlook.com>
2023-02-27 00:34:54 -0600

    feat: add title

--- apps/rem/src/pages/public-workspace/[workspaceId]/[pageId].tsx
+++ apps/rem/src/pages/public-workspace/[workspaceId]/[pageId].tsx
@@ -1,4 +1,13 @@
+import {
+  Breadcrumbs,
+  displayFlex,
+  IconButton,
+  styled,
+} from '@affine/component';
+import { useTranslation } from '@affine/i18n';
+import { PaperIcon } from '@blocksuite/icons';
 import { useAtomValue, useSetAtom } from 'jotai';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { Suspense, useEffect } from 'react';
 
@@ -8,25 +17,91 @@ import {
 } from '../../../atoms/public-workspace';
 import { QueryParamError } from '../../../components/affine/affine-error-eoundary';
 import { PageDetailEditor } from '../../../components/page-detail-editor';
+import { WorkspaceAvatar } from '../../../components/pure/footer';
 import { PageLoading } from '../../../components/pure/loading';
 import { WorkspaceLayout } from '../../../layouts';
 import { NextPageWithLayout } from '../../../shared';
+
+export const NavContainer = styled.div(({ theme }) => {
+  return {
+    width: '100vw',
+    padding: '0 12px',
+    height: '60px',
+    ...displayFlex('start', 'center'),
+    backgroundColor: theme.colors.pageBackground,
+  };
+});
+
+export const StyledBreadcrumbs = styled(Link)(({ theme }) => {
+  return {
+    flex: 1,
+    ...displayFlex('center', 'center'),
+    paddingLeft: '12px',
+    span: {
+      padding: '0 12px',
+      fontSize: theme.font.base,
+      lineHeight: theme.font.lineHeight,
+    },
+    ':hover': { color: theme.colors.primaryColor },
+    transition: 'all .15s',
+    ':visited': {
+      color: theme.colors.popoverColor,
+      ':hover': { color: theme.colors.primaryColor },
+    },
+  };
+});
+
+export const SearchButton = styled(IconButton)(({ theme }) => {
+  return {
+    color: theme.colors.iconColor,
+    fontSize: '24px',
+    marginLeft: 'auto',
+    padding: '0 24px',
+  };
+});
 
 const PublicWorkspaceDetailPageInner: React.FC<{
   pageId: string;
 }> = ({ pageId }) => {
   const blockSuiteWorkspace = useAtomValue(publicBlockSuiteAtom);
   if (!blockSuiteWorkspace) {
-    return <PageLoading />;
+    throw new Error('cannot find workspace');
   }
+  const { t } = useTranslation();
+  const name = blockSuiteWorkspace.meta.name;
+  const pageTitle = blockSuiteWorkspace.meta.getPageMeta(pageId)?.title;
   return (
-    <PageDetailEditor
-      pageId={pageId}
-      blockSuiteWorkspace={blockSuiteWorkspace}
-      onLoad={(_, editor) => {
-        editor.readonly = true;
-      }}
-    />
+    <>
+      <NavContainer>
+        <Breadcrumbs>
+          <StyledBreadcrumbs
+            href={`/public-workspace/${blockSuiteWorkspace.room}`}
+          >
+            <WorkspaceAvatar
+              size={24}
+              name={name}
+              avatar={blockSuiteWorkspace.meta.avatar}
+            />
+            <span>{name}</span>
+          </StyledBreadcrumbs>
+          <StyledBreadcrumbs
+            href={`/public-workspace/${
+              blockSuiteWorkspace.room as string
+            }/${pageId}`}
+          >
+            <PaperIcon fontSize={24} />
+            <span>{pageTitle ? pageTitle : t('Untitled')}</span>
+          </StyledBreadcrumbs>
+        </Breadcrumbs>
+      </NavContainer>
+      <PageDetailEditor
+        pageId={pageId}
+        blockSuiteWorkspace={blockSuiteWorkspace}
+        onLoad={(_, editor) => {
+          editor.readonly = true;
+        }}
+      />
+    </>
   );
 };
 
