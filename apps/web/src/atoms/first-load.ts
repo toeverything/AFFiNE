c92c99a0eb7af69545557d7704aa63532f98ddda
JimmFly <yangjinfei001@gmail.com>
2023-03-08 13:11:52 +0800

    chore: update first load hook

--- apps/web/src/atoms/first-load.ts
+++ apps/web/src/atoms/first-load.ts
@@ -2,4 +2,4 @@ import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 
 export const isFirstLoadAtom = atomWithStorage<boolean>('isFirstLoad', true);
-export const openTipsAtom = atom(false);
+export const openTipsAtom = atom<boolean>(false);
--- apps/web/src/hooks/affine/use-is-first-load.ts
+++ apps/web/src/hooks/affine/use-is-first-load.ts
@@ -1,8 +1,12 @@
 import { useAtom } from 'jotai';
 
-import { isFirstLoadAtom } from '../../atoms/first-load';
+import { isFirstLoadAtom, openTipsAtom } from '../../atoms/first-load';
 
 export function useIsFirstLoad() {
   const [isFirstLoad, setIsFirstLoad] = useAtom(isFirstLoadAtom);
   return [isFirstLoad, setIsFirstLoad] as const;
+}
+export function useOpenTips() {
+  const [openTips, setOpenTips] = useAtom(openTipsAtom);
+  return [openTips, setOpenTips] as const;
 }
