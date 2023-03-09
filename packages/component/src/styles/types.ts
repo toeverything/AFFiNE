e5178c40b8debb23738d4c92eb3f01c3de3de4e5
QiShaoXuan <qishaoxuan777@gmail.com>
2023-03-09 16:09:14 +0800

    fix: type error

--- packages/component/src/styles/types.ts
+++ packages/component/src/styles/types.ts
@@ -1,4 +1,5 @@
 import { EditorContainer } from '@blocksuite/editor';
+import { CSSProperties } from 'react';
 
 export type Theme = 'light' | 'dark';
 export type ThemeMode = Theme | 'auto';
@@ -149,9 +150,9 @@ export interface AffineThemeCSSVariables {
 
   '--affine-popover-radius': AffineTheme['radius']['popover'];
   // use for blocksuite
-  '--affine-editor-width': 'string';
-  '--affine-zoom': 'string';
-  '--affine-scale': 'string';
+  '--affine-editor-width': CSSProperties['width'];
+  '--affine-zoom': CSSProperties['zoom'];
+  '--affine-scale': string;
 }
 
 declare module '@emotion/react' {
