039e549dae8380b7f82de28221bc38a2bf65100d
himself65 <himself65@outlook.com>
2023-03-10 11:48:50 -0600

    fix: lottie import logic

--- apps/web/src/components/blocksuite/header/editor-mode-switch/switch-items.tsx
+++ apps/web/src/components/blocksuite/header/editor-mode-switch/switch-items.tsx
@@ -1,8 +1,8 @@
 import React, { cloneElement, HTMLAttributes, useState } from 'react';
 import Lottie from 'react-lottie';
 
-import * as edgelessHoverAnimationData from './animation-data/edgeless-hover.json';
-import * as pageHoverAnimationData from './animation-data/page-hover.json';
+import edgelessHoverAnimationData from './animation-data/edgeless-hover.json';
+import pageHoverAnimationData from './animation-data/page-hover.json';
 import { StyledSwitchItem } from './style';
 
 type HoverAnimateControllerProps = {
