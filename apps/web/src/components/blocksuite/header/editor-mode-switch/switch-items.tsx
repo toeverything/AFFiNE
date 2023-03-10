1239957446d2583d9258bd1d3d2587affdb75f99
Himself65 <himself65@outlook.com>
2023-03-10 12:06:27 -0600

    fix: lottie import logic (#1526)

--- apps/web/src/components/blocksuite/header/editor-mode-switch/switch-items.tsx
+++ apps/web/src/components/blocksuite/header/editor-mode-switch/switch-items.tsx
@@ -1,8 +1,6 @@
 import React, { cloneElement, HTMLAttributes, useState } from 'react';
 import Lottie from 'react-lottie';
 
-import * as edgelessHoverAnimationData from './animation-data/edgeless-hover.json';
-import * as pageHoverAnimationData from './animation-data/page-hover.json';
 import { StyledSwitchItem } from './style';
 
 type HoverAnimateControllerProps = {
@@ -49,7 +47,7 @@ export const PageSwitchItem = (
         options={{
           loop: false,
           autoplay: false,
-          animationData: pageHoverAnimationData,
+          animationData: require('./animation-data/page-hover.json'),
           rendererSettings: {
             preserveAspectRatio: 'xMidYMid slice',
           },
@@ -68,7 +66,7 @@ export const EdgelessSwitchItem = (
         options={{
           loop: false,
           autoplay: false,
-          animationData: edgelessHoverAnimationData,
+          animationData: require('./animation-data/edgeless-hover.json'),
           rendererSettings: {
             preserveAspectRatio: 'xMidYMid slice',
           },
