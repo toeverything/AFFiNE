594f76c3ebdb90d6b9125c958e0b491d758b2b0c
himself65 <himself65@outlook.com>
2023-03-11 00:29:54 -0600

    fix: build

--- packages/component/.storybook/vite.config.ts
+++ packages/component/.storybook/vite.config.ts
@@ -2,10 +2,19 @@ import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
+import istanbul from 'vite-plugin-istanbul';
 
 const config: Pick<StorybookConfig, 'viteFinal'> = {
   async viteFinal(config, { configType }) {
     return mergeConfig(config, {
+      plugins: [
+        istanbul({
+          include: ['src/*'],
+          exclude: ['node_modules', 'test', 'stories'],
+          extension: ['.ts', '.tsx'],
+          forceBuildInstrument: process.env.COVERAGE === 'true',
+        }),
+      ],
       resolve: {
         alias: {
           '@': path.resolve(fileURLToPath(new URL('../src', import.meta.url))),
