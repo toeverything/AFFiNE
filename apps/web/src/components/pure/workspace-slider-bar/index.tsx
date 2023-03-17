37500a4c9bdec4c4ded4e6483f994c755590a20b
himself65 <himself65@outlook.com>
2023-03-17 11:46:45 -0400

    fix: remove unused useRef

--- apps/web/src/components/pure/workspace-slider-bar/index.tsx
+++ apps/web/src/components/pure/workspace-slider-bar/index.tsx
@@ -14,7 +14,7 @@ import type { PageMeta } from '@blocksuite/store';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import type React from 'react';
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 import { useSidebarStatus } from '../../../hooks/affine/use-sidebar-status';
 import { usePageMeta } from '../../../hooks/use-page-meta';
@@ -49,12 +49,7 @@ const FavoriteList: React.FC<FavoriteListProps> = ({
     () => pageMeta.filter(p => p.favorite && !p.trash),
     [pageMeta]
   );
-  const activeItemRef = useRef<HTMLButtonElement>(null);
-  useEffect(() => {
-    if (activeItemRef.current && activeItemRef.current.scrollIntoView) {
-      activeItemRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [activeItemRef.current]);
+
   return (
     <MuiCollapse
       in={showList}
@@ -70,7 +65,11 @@ const FavoriteList: React.FC<FavoriteListProps> = ({
             <StyledSubListItem
               data-testid={`favorite-list-item-${pageMeta.id}`}
               active={active}
-              ref={active ? activeItemRef : null}
+              ref={ref => {
+                if (ref) {
+                  ref.scrollIntoView({ behavior: 'smooth' });
+                }
+              }}
               onClick={() => {
                 if (active) {
                   return;
