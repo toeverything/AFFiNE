97d8f37ba573af2bfcabaf986332e59c9116bad4
himself65 <himself65@outlook.com>
2023-03-17 23:48:06 -0500

    fix: warning on bypass visible value in switch button

--- apps/web/src/components/affine/sidebar-switch/style.ts
+++ apps/web/src/components/affine/sidebar-switch/style.ts
@@ -1,10 +1,12 @@
 import { IconButton, styled } from '@affine/component';
 
-export const StyledSidebarSwitch = styled(IconButton)<{ visible: boolean }>(
-  ({ visible }) => {
-    return {
-      opacity: visible ? 1 : 0,
-      transition: 'all 0.2s ease-in-out',
-    };
-  }
-);
+export const StyledSidebarSwitch = styled(IconButton, {
+  shouldForwardProp(propName: PropertyKey) {
+    return propName !== 'visible';
+  },
+})<{ visible: boolean }>(({ visible }) => {
+  return {
+    opacity: visible ? 1 : 0,
+    transition: 'all 0.2s ease-in-out',
+  };
+});
