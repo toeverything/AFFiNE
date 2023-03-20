8695702b8cd02ebc63042ab1408955f88d51e960
himself65 <himself65@outlook.com>
2023-03-20 02:46:41 -0500

    build: lazy load `lottie-web`

--- apps/web/src/components/blocksuite/header/editor-mode-switch/CustomLottie.tsx
+++ apps/web/src/components/blocksuite/header/editor-mode-switch/CustomLottie.tsx
@@ -1,6 +1,8 @@
-import lottie from 'lottie-web';
+import { atom, useAtomValue } from 'jotai';
 import type { FC } from 'react';
 import { useEffect, useRef } from 'react';
+
+const lottieAtom = atom(async () => import('lottie-web').then(m => m.default));
 
 type CustomLottieProps = {
   options: {
@@ -26,6 +28,7 @@ const CustomLottie: FC<CustomLottieProps> = ({
 }) => {
   const element = useRef<HTMLDivElement>(null);
   const lottieInstance = useRef<any>();
+  const lottie = useAtomValue(lottieAtom);
 
   useEffect(() => {
     if (element.current) {
@@ -37,7 +40,7 @@ const CustomLottie: FC<CustomLottieProps> = ({
     return () => {
       lottieInstance.current?.destroy();
     };
-  }, [options]);
+  }, [lottie, options]);
 
   useEffect(() => {
     if (speed) {
