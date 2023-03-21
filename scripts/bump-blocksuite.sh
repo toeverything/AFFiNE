2c78bc86767f0f56f846c6b071acdb1b9fd6461d
Yifeng Wang <doodlewind@toeverything.info>
2023-03-21 16:05:44 +0800

    build: add `bump:nightly` script

---
+++ scripts/bump-blocksuite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+LATEST_NIGHTLY=$(npm view @blocksuite/editor@nightly version | cut -d ':' -f 2 | tr -d '[:space:]')
+echo "Bump to latest BlockSuite nightly version: $LATEST_NIGHTLY"
+
+yarn up "@blocksuite/store@${LATEST_NIGHTLY}"
+yarn up "@blocksuite/blocks@${LATEST_NIGHTLY}"
+yarn up "@blocksuite/editor@${LATEST_NIGHTLY}"
