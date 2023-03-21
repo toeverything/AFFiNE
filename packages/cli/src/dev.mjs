b4e71dadada3c1429a926c5a6cd80e08ba58d540
himself65 <himself65@outlook.com>
2023-03-21 17:21:14 -0500

    fix: lint

--- packages/cli/src/dev.mjs
+++ packages/cli/src/dev.mjs
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import { spawn } from 'node:child_process';
 import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import * as p from '@clack/prompts';
-import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
