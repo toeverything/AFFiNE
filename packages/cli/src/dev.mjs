180382daf5ae6943fa3048d2a5d4bcd8feff32aa
himself65 <himself65@outlook.com>
2023-03-21 23:31:29 -0500

    fix(cli): remove unused variables

--- packages/cli/src/dev.mjs
+++ packages/cli/src/dev.mjs
@@ -4,7 +4,6 @@ import * as path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import * as p from '@clack/prompts';
-import os from 'os';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
