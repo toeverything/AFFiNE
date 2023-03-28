241595a45146cde56efe68da5b73daade12dd1ea
himself65 <himself65@outlook.com>
2023-03-27 19:19:40 -0500

    fix: publicBlockSuiteAtom

--- apps/web/src/atoms/public-workspace/index.ts
+++ apps/web/src/atoms/public-workspace/index.ts
@@ -13,18 +13,11 @@ export const publicBlockSuiteAtom = atom<Promise<BlockSuiteWorkspace>>(
       throw new Error('No workspace id');
     }
     const binary = await affineApis.downloadWorkspace(workspaceId, true);
-    // fixme: this is a hack
-    const params = new URLSearchParams(window.location.search);
-    const prefixUrl = params.get('prefixUrl')
-      ? (params.get('prefixUrl') as string)
-      : '/';
     const blockSuiteWorkspace = createEmptyBlockSuiteWorkspace(
       workspaceId,
       (k: string) =>
         // fixme: token could be expired
-        ({ api: `${prefixUrl}api/workspace`, token: getLoginStorage()?.token }[
-          k
-        ])
+        ({ api: `api/workspace`, token: getLoginStorage()?.token }[k])
     );
     BlockSuiteWorkspace.Y.applyUpdate(
       blockSuiteWorkspace.doc,
@@ -39,19 +32,6 @@ export const publicBlockSuiteAtom = atom<Promise<BlockSuiteWorkspace>>(
     );
     blockSuiteWorkspace.awarenessStore.setFlag('enable_slash_menu', false);
     blockSuiteWorkspace.awarenessStore.setFlag('enable_drag_handle', false);
-    return new Promise(resolve => {
-      setTimeout(() => {
-        // const workspace: LocalWorkspace = {
-        //   id: workspaceId,
-        //   blockSuiteWorkspace,
-        //   flavour: RemWorkspaceFlavour.LOCAL,
-        //   providers: [],
-        // };
-        // fixme: quick search won't work, ASAP
-        // dataCenter.workspaces.push(workspace);
-        // dataCenter.callbacks.forEach(cb => cb());
-        resolve(blockSuiteWorkspace);
-      }, 0);
-    });
+    return blockSuiteWorkspace;
   }
 );
