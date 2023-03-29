92487790da0afb54ae933655bb1972118edf7f0f
JimmFly <yangjinfei001@gmail.com>
2023-03-29 11:08:35 +0800

    feat: update first load hooks

--- apps/web/src/atoms/first-load.ts
+++ apps/web/src/atoms/first-load.ts
@@ -1,5 +1,47 @@
 import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 
-export const isFirstLoadAtom = atomWithStorage<boolean>('isFirstLoad', true);
-export const openTipsAtom = atom<boolean>(false);
+export type FirstLoad = {
+  version: string;
+  openTips: boolean;
+  showWhatsNew: boolean;
+};
+
+export const isFirstLoadAtom = atomWithStorage<FirstLoad>('isFirstLoad', {
+  version: '0.0.0',
+  openTips: false,
+  showWhatsNew: false,
+});
+
+export const versionAtom = atom(
+  get => {
+    const firstLoad = get(isFirstLoadAtom);
+    return firstLoad.version;
+  },
+  (get, set, value: string) => {
+    const firstLoad = get(isFirstLoadAtom);
+    set(isFirstLoadAtom, { ...firstLoad, version: value });
+  }
+);
+
+export const openTipsAtom = atom(
+  get => {
+    const firstLoad = get(isFirstLoadAtom);
+    return firstLoad.openTips;
+  },
+  (get, set, value: boolean) => {
+    const firstLoad = get(isFirstLoadAtom);
+    set(isFirstLoadAtom, { ...firstLoad, openTips: value });
+  }
+);
+
+export const showWhatsNewAtom = atom(
+  get => {
+    const firstLoad = get(isFirstLoadAtom);
+    return firstLoad.showWhatsNew;
+  },
+  (get, set, value: boolean) => {
+    const firstLoad = get(isFirstLoadAtom);
+    set(isFirstLoadAtom, { ...firstLoad, showWhatsNew: value });
+  }
+);
--- apps/web/src/hooks/affine/use-is-first-load.ts
+++ apps/web/src/hooks/affine/use-is-first-load.ts
@@ -1,12 +1,25 @@
 import { useAtom } from 'jotai';
 
-import { isFirstLoadAtom, openTipsAtom } from '../../atoms/first-load';
+import {
+  isFirstLoadAtom,
+  openTipsAtom,
+  showWhatsNewAtom,
+  versionAtom,
+} from '../../atoms/first-load';
 
 export function useIsFirstLoad() {
   const [isFirstLoad, setIsFirstLoad] = useAtom(isFirstLoadAtom);
   return [isFirstLoad, setIsFirstLoad] as const;
 }
+export function useFirstLoadVersion() {
+  const [version, setVersion] = useAtom(versionAtom);
+  return [version, setVersion] as const;
+}
 export function useOpenTips() {
   const [openTips, setOpenTips] = useAtom(openTipsAtom);
   return [openTips, setOpenTips] as const;
+}
+export function useShowWhatsNew() {
+  const [showWhatsNew, setShowWhatsNews] = useAtom(showWhatsNewAtom);
+  return [showWhatsNew, setShowWhatsNews] as const;
 }
