ed7cad3b0f1387e855705f67ab4ecc3115bccc20
JimmFly <yangjinfei001@gmail.com>
2023-03-29 17:36:40 +0800

    fix: always return initial value on first render

--- apps/web/src/atoms/first-load.ts
+++ apps/web/src/atoms/first-load.ts
@@ -2,7 +2,16 @@ import { atomWithStorage } from 'jotai/utils';
 
 export type Visibility = Record<string, boolean>;
 
-export const lastVersionAtom = atomWithStorage('lastVersion', '0.0.0');
+const DEFAULT_VALUE = '0.0.0';
+//atomWithStorage always uses initial value when first render
+//https://github.com/pmndrs/jotai/discussions/1737
+export const lastVersionAtom = atomWithStorage(
+  'lastVersion',
+  JSON.parse(
+    globalThis.localStorage?.getItem('lastVersion') ??
+      JSON.stringify(DEFAULT_VALUE)
+  )
+);
 
 export const guideHiddenAtom = atomWithStorage<Visibility>('guideHidden', {});
 
