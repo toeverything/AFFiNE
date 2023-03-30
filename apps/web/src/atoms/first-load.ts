e59fe889fab7795b4009314afb96055b4e946fe2
JimmFly <yangjinfei001@gmail.com>
2023-03-30 18:15:33 +0800

    fix: handle SSR

--- apps/web/src/atoms/first-load.ts
+++ apps/web/src/atoms/first-load.ts
@@ -5,14 +5,21 @@ export type Visibility = Record<string, boolean>;
 const DEFAULT_VALUE = '0.0.0';
 //atomWithStorage always uses initial value when first render
 //https://github.com/pmndrs/jotai/discussions/1737
+
+function getInitialValue() {
+  if (typeof window !== 'undefined') {
+    const storedValue = window.localStorage.getItem('lastVersion');
+    if (storedValue) {
+      return JSON.parse(storedValue);
+    }
+  }
+  return DEFAULT_VALUE;
+}
+
 export const lastVersionAtom = atomWithStorage(
   'lastVersion',
-  JSON.parse(
-    globalThis.localStorage?.getItem('lastVersion') ??
-      JSON.stringify(DEFAULT_VALUE)
-  )
+  getInitialValue()
 );
-
 export const guideHiddenAtom = atomWithStorage<Visibility>('guideHidden', {});
 
 export const guideHiddenUntilNextUpdateAtom = atomWithStorage<Visibility>(
