f817d4b162dfb8b11bde8cb2e87125a5bac97b8d
Peng Xiao <pengxiao@outlook.com>
2023-03-31 16:58:58 +0800

    fix: first workspace create logic

--- apps/web/src/hooks/use-create-first-workspace.ts
+++ apps/web/src/hooks/use-create-first-workspace.ts
@@ -1,22 +1,25 @@
+import { DebugLogger } from '@affine/debug';
 import { DEFAULT_WORKSPACE_NAME } from '@affine/env';
 import { jotaiWorkspacesAtom } from '@affine/workspace/atom';
 import { WorkspaceFlavour } from '@affine/workspace/type';
 import { createEmptyBlockSuiteWorkspace } from '@affine/workspace/utils';
 import { assertEquals, assertExists, nanoid } from '@blocksuite/store';
 import { useAtom } from 'jotai';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { LocalPlugin } from '../plugins/local';
 
+const logger = new DebugLogger('use-create-first-workspace');
+
 export function useCreateFirstWorkspace() {
   const [jotaiWorkspaces, set] = useAtom(jotaiWorkspacesAtom);
+  const creatingFirst = useRef(false);
   useEffect(() => {
-    const controller = new AbortController();
-
     /**
      * Create a first workspace, only just once for a browser
      */
     async function createFirst() {
+      creatingFirst.current = true;
       const blockSuiteWorkspace = createEmptyBlockSuiteWorkspace(
         nanoid(),
         (_: string) => undefined
@@ -32,16 +35,11 @@ export function useCreateFirstWorkspace() {
           flavour: WorkspaceFlavour.LOCAL,
         },
       ]);
+      logger.info('created local workspace', id);
+      creatingFirst.current = false;
     }
-    if (
-      jotaiWorkspaces.length === 0 &&
-      localStorage.getItem('first') !== 'true'
-    ) {
-      localStorage.setItem('first', 'true');
+    if (jotaiWorkspaces.length === 0 && !creatingFirst.current) {
       createFirst();
     }
-    return () => {
-      controller.abort();
-    };
   }, [jotaiWorkspaces.length, set]);
 }
