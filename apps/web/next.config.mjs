5ad04bdfdf854b1e2052ea85a2b3ae642f1ce9f6
himself65 <himself65@outlook.com>
2023-04-01 23:35:32 -0500

    feat: add plugin

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -4,4 +4,26 @@ export default {
   experimental: {
     appDir: true,
   },
+  webpack: (config, { dev, isServer }) => {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    config.module.rules.push({
+      test: /\.md$/i,
+      loader: 'raw-loader',
+    });
+    config.resolve.alias['yjs'] = require.resolve('yjs');
+
+    if (!isServer && !dev && process.env.PERFSEE_TOKEN) {
+      config.devtool = 'hidden-nosources-source-map';
+      const perfsee = new PerfseePlugin({
+        project: 'affine-toeverything',
+      });
+      if (Array.isArray(config.plugins)) {
+        config.plugins.push(perfsee);
+      } else {
+        config.plugins = [perfsee];
+      }
+    }
+
+    return config;
+  },
 };
