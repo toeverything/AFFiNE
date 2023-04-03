e3d29a95ee24c2e6ed2dfdacccb6d49e63fe38ea
himself65 <himself65@outlook.com>
2023-04-03 15:18:39 -0500

    build(component): fix storybook config

--- packages/component/.storybook/main.ts
+++ packages/component/.storybook/main.ts
@@ -1,19 +1,33 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { fileURLToPath } from 'node:url';
+import { mergeConfig } from 'vite';
 
 export default {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../../../apps/web/public'],
   addons: [
     '@storybook/addon-links',
-    'storybook-dark-mode-v7',
+    'storybook-dark-mode',
     '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/react-vite',
-    options: {
-      builder: {
-        viteConfigPath: '.storybook/vite.config.ts',
+  },
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      define: {
+        'process.env': {},
       },
-    },
+      resolve: {
+        alias: {
+          'next/config': fileURLToPath(
+            new URL(
+              '../../../scripts/vitest/next-config-mock.ts',
+              import.meta.url
+            )
+          ),
+        },
+      },
+    });
   },
 } as StorybookConfig;
