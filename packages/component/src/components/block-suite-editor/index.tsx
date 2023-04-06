704834bc7729550cb4dadd04a1ccb3d792e4f0cf
himself65 <himself65@outlook.com>
2023-04-06 10:55:51 -0500

    refactor: improve

--- packages/component/src/components/block-suite-editor/index.tsx
+++ packages/component/src/components/block-suite-editor/index.tsx
@@ -1,12 +1,13 @@
 import type { BlockHub } from '@blocksuite/blocks';
 import { EditorContainer } from '@blocksuite/editor';
 import { assertExists } from '@blocksuite/global/utils';
-import type { Page, Workspace } from '@blocksuite/store';
-import type { CSSProperties } from 'react';
-import { useEffect, useRef } from 'react';
+import type { Page } from '@blocksuite/store';
+import type { CSSProperties, ReactElement } from 'react';
+import { memo, useCallback, useEffect, useRef } from 'react';
+import type { FallbackProps } from 'react-error-boundary';
+import { ErrorBoundary } from 'react-error-boundary';
 
 export type EditorProps = {
-  blockSuiteWorkspace: Workspace;
   page: Page;
   mode: 'page' | 'edgeless';
   onInit: (page: Page, editor: Readonly<EditorContainer>) => void;
@@ -14,17 +15,20 @@ export type EditorProps = {
   style?: CSSProperties;
 };
 
+export type ErrorBoundaryProps = {
+  onReset?: () => void;
+};
+
 declare global {
-  // eslint-disable-next-line no-var
-  var currentBlockSuiteWorkspace: Workspace | undefined;
   // eslint-disable-next-line no-var
   var currentPage: Page | undefined;
   // eslint-disable-next-line no-var
   var currentEditor: EditorContainer | undefined;
 }
 
-export const BlockSuiteEditor = (props: EditorProps) => {
+const BlockSuiteEditorImpl = (props: EditorProps): ReactElement => {
   const page = props.page;
+  assertExists(page, 'page should not be null');
   const editorRef = useRef<EditorContainer | null>(null);
   const blockHubRef = useRef<BlockHub | null>(null);
   if (editorRef.current === null) {
@@ -77,10 +81,49 @@ export const BlockSuiteEditor = (props: EditorProps) => {
   }, [page]);
   return (
     <div
-      data-testid={`editor-${props.blockSuiteWorkspace.id}-${props.page.id}`}
+      data-testid={`editor-${props.page.id}`}
       className="editor-wrapper"
       style={props.style}
       ref={ref}
     />
   );
 };
+
+const BlockSuiteErrorFallback = (
+  props: FallbackProps & ErrorBoundaryProps
+): ReactElement => {
+  return (
+    <div>
+      <h1>Sorry.. there was an error</h1>
+      <div>{props.error.message}</div>
+      <button
+        data-testid="error-fallback-reset-button"
+        onClick={() => {
+          props.onReset?.();
+          props.resetErrorBoundary();
+        }}
+      >
+        Try again
+      </button>
+    </div>
+  );
+};
+
+export const BlockSuiteEditor = memo(function BlockSuiteEditor(
+  props: EditorProps & ErrorBoundaryProps
+): ReactElement {
+  return (
+    <ErrorBoundary
+      fallbackRender={useCallback(
+        (fallbackProps: FallbackProps) => (
+          <BlockSuiteErrorFallback {...fallbackProps} onReset={props.onReset} />
+        ),
+        [props.onReset]
+      )}
+    >
+      <BlockSuiteEditorImpl {...props} />
+    </ErrorBoundary>
+  );
+});
+
+BlockSuiteEditor.displayName = 'BlockSuiteEditor';
