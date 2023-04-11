57888af4cb4c56438b01c292d2a6e92a6ccefdd2
himself65 <himself65@outlook.com>
2023-04-10 19:06:40 -0500

    feat: improve share workspace

--- packages/component/src/components/share-menu/ShareWorkspace.tsx
+++ packages/component/src/components/share-menu/ShareWorkspace.tsx
@@ -6,39 +6,36 @@ import { Button } from '../..';
 import type { ShareMenuProps } from '.';
 import { buttonStyle, descriptionStyle, menuItemStyle } from './index.css';
 
-export const ShareWorkspace: FC<ShareMenuProps> = props => {
-  const isAffineWorkspace = props.workspace.flavour === WorkspaceFlavour.AFFINE;
-  if (!isAffineWorkspace) {
-    return (
-      <div className={menuItemStyle}>
-        <div className={descriptionStyle}>
-          Sharing page publicly requires AFFiNE Cloud service.
-        </div>
-        <Button
-          data-testid="share-menu-enable-affine-cloud-button"
-          className={buttonStyle}
-          type="light"
-          shape="circle"
-          onClick={() => {
-            props.onEnableAffineCloud(props.workspace as LocalWorkspace);
-          }}
-        >
-          Enable AFFiNE Cloud
-        </Button>
-      </div>
-    );
-  }
-  const isPublicWorkspace = (props.workspace as AffineWorkspace).public;
+const ShareLocalWorkspace: FC<ShareMenuProps<LocalWorkspace>> = props => {
   return (
-    <div>
-      {isPublicWorkspace ? (
-        <div>
-          Current workspace has been published to the web as a public workspace.
-        </div>
-      ) : (
-        <div>Invite others to join the Workspace or publish it to web</div>
-      )}
+    <div className={menuItemStyle}>
+      <div className={descriptionStyle}>
+        Sharing page publicly requires AFFiNE Cloud service.
+      </div>
+      <Button
+        data-testid="share-menu-enable-affine-cloud-button"
+        className={buttonStyle}
+        type="light"
+        shape="circle"
+        onClick={() => {
+          props.onEnableAffineCloud(props.workspace as LocalWorkspace);
+        }}
+      >
+        Enable AFFiNE Cloud
+      </Button>
+    </div>
+  );
+};
 
+const ShareAffineWorkspace: FC<ShareMenuProps<AffineWorkspace>> = props => {
+  const isPublicWorkspace = props.workspace.public;
+  return (
+    <div className={menuItemStyle}>
+      <div className={descriptionStyle}>
+        {isPublicWorkspace
+          ? `Current workspace has been published to the web as a public workspace.`
+          : `Invite others to join the Workspace or publish it to web`}
+      </div>
       <Button
         data-testid="share-menu-publish-to-web-button"
         onClick={() => {
@@ -51,4 +48,17 @@ export const ShareWorkspace: FC<ShareMenuProps> = props => {
       </Button>
     </div>
   );
+};
+
+export const ShareWorkspace: FC<ShareMenuProps> = props => {
+  if (props.workspace.flavour === WorkspaceFlavour.LOCAL) {
+    return (
+      <ShareLocalWorkspace {...(props as ShareMenuProps<LocalWorkspace>)} />
+    );
+  } else if (props.workspace.flavour === WorkspaceFlavour.AFFINE) {
+    return (
+      <ShareAffineWorkspace {...(props as ShareMenuProps<AffineWorkspace>)} />
+    );
+  }
+  throw new Error('Unreachable');
 };
