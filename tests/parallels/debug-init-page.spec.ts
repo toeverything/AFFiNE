dbaeeb0f0f82c8034663c3e7b7fe28e3c9ed0e4b
himself65 <himself65@outlook.com>
2023-04-13 15:36:34 -0500

    fix: test

--- tests/parallels/debug-init-page.spec.ts
+++ tests/parallels/debug-init-page.spec.ts
@@ -3,7 +3,7 @@ import { expect } from '@playwright/test';
 import { test } from '../libs/playwright';
 
 test.describe('Debug page broadcast', () => {
-  test('should broadcast a message to all debug pages', async ({ page }) => {
+  test('should have page0', async ({ page }) => {
     await page.goto(
       'http://localhost:8080/_debug/init-page?type=importMarkdown'
     );
