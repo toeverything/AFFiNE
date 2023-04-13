1162bffb30e30468c19b4b5f17ccf133d155955c
Himself65 <himself65@outlook.com>
2023-04-12 21:18:41 -0500

    build: support sentry replay (#1908)

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -5,4 +5,7 @@ const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [new Sentry.Replay()],
 });
