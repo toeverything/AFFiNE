4719712c9961dfb53a046c02276d10e6931e2725
himself65 <himself65@outlook.com>
2023-04-18 17:51:24 -0500

    test: simplify

--- apps/server/scripts/run-test.ts
+++ apps/server/scripts/run-test.ts
@@ -4,42 +4,52 @@ import { resolve } from 'node:path';
 import * as p from '@clack/prompts';
 import { spawn } from 'child_process';
 import { readdir } from 'fs/promises';
+import * as process from 'process';
 import { fileURLToPath } from 'url';
 
 const root = fileURLToPath(new URL('..', import.meta.url));
 const testDir = resolve(root, 'src', 'tests');
 const files = await readdir(testDir);
 
-const result = await p.group({
-  file: () =>
-    p.select({
-      message: 'Select a file to run',
-      options: files.map(file => ({
-        label: file,
-        value: file as any,
-      })),
-    }),
-});
-
-const target = resolve(testDir, result.file);
-
-spawn(
+const args = [
+  '--loader',
+  'ts-node/esm.mjs',
+  '--es-module-specifier-resolution',
   'node',
-  [
-    '--loader',
-    'ts-node/esm.mjs',
-    '--es-module-specifier-resolution',
-    'node',
-    '--test',
-    target,
-  ],
-  {
+  '--test',
+];
+
+if (process.argv[2] === 'all') {
+  spawn('node', [...args, resolve(testDir, '*')], {
     cwd: root,
     env: {
       PATH: process.env.PATH,
       NODE_ENV: 'test',
     },
-    stdio: 'pipe',
+    stdio: 'inherit',
     shell: true,
-  }
-);
+  });
+} else {
+  const result = await p.group({
+    file: () =>
+      p.select({
+        message: 'Select a file to run',
+        options: files.map(file => ({
+          label: file,
+          value: file as any,
+        })),
+      }),
+  });
+
+  const target = resolve(testDir, result.file);
+
+  spawn('node', [...args, target], {
+    cwd: root,
+    env: {
+      PATH: process.env.PATH,
+      NODE_ENV: 'test',
+    },
+    stdio: 'inherit',
+    shell: true,
+  });
+}
