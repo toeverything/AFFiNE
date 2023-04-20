a6090bf5b4b25c5fc859116e3ce8aa07f830ce73
himself65 <himself65@outlook.com>
2023-04-19 22:16:14 -0500

    feat: init affine blob storage

---
+++ packages/workspace/src/blob/index.ts
@@ -0,0 +1,105 @@
+import type { BlobStorage } from '@blocksuite/store';
+import { createIndexeddbStorage } from '@blocksuite/store';
+import { openDB } from 'idb';
+import type { DBSchema } from 'idb/build/entry';
+
+import type { createWorkspaceApis } from '../affine/api';
+
+type UploadingBlob = {
+  key: string;
+  arrayBuffer: ArrayBuffer;
+  type: string;
+};
+
+interface AffineBlob extends DBSchema {
+  uploading: {
+    key: string;
+    value: UploadingBlob;
+  };
+  // todo: migrate blob storage from `createIndexeddbStorage`
+}
+
+export const createAffineBlobStorage = (
+  workspaceId: string,
+  workspaceApis: ReturnType<typeof createWorkspaceApis>
+): BlobStorage => {
+  const storage = createIndexeddbStorage(workspaceId);
+  const dbPromise = openDB<AffineBlob>('affine-blob', 1, {
+    upgrade(db) {
+      db.createObjectStore('uploading', { keyPath: 'key' });
+    },
+  });
+  dbPromise.then(async db => {
+    const t = db.transaction('uploading', 'readwrite');
+    const uploading: Promise<unknown>[] = [];
+    for await (const cursor of t.store) {
+      const { key, arrayBuffer, type } = cursor.value;
+      uploading.push(
+        workspaceApis
+          .uploadBlob(workspaceId, arrayBuffer, type)
+          .then(() => t.store.delete(key))
+      );
+    }
+    await Promise.all(uploading);
+  });
+  return {
+    crud: {
+      get: async key => {
+        const blob = await storage.crud.get(key);
+        if (!blob) {
+          const buffer = await workspaceApis.getBlob(workspaceId, key);
+          return new Blob([buffer]);
+        } else {
+          return blob;
+        }
+      },
+      set: async (key, value) => {
+        const db = await dbPromise;
+        const t = db
+          .transaction('uploading', 'readwrite')
+          .objectStore('uploading');
+        let uploaded = false;
+        t.put({
+          key,
+          arrayBuffer: await value.arrayBuffer(),
+          type: value.type,
+        }).then(() => {
+          // delete the uploading blob after uploaded
+          if (uploaded) {
+            const t = db
+              .transaction('uploading', 'readwrite')
+              .objectStore('uploading');
+            t.delete(key);
+          }
+        });
+        await Promise.all([
+          storage.crud.set(key, value),
+          workspaceApis
+            .uploadBlob(workspaceId, await value.arrayBuffer(), value.type)
+            .then(async () => {
+              uploaded = true;
+              const t = db
+                .transaction('uploading', 'readwrite')
+                .objectStore('uploading');
+              // delete the uploading blob after uploaded
+              if (await t.get(key)) {
+                await t.delete(key);
+              }
+            }),
+        ]);
+        return key;
+      },
+      delete: async (key: string) => {
+        await Promise.all([
+          storage.crud.delete(key),
+          // we don't support deleting a blob?
+          // workspaceApis.deleteBlob(workspaceId, key)
+        ]);
+      },
+      list: async () => {
+        const blobs = await storage.crud.list();
+        return [...blobs];
+      },
+    },
+  };
+};
