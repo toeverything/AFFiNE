8a0d5bdf3ad860bab1ce3fefb954a9abd339372e
JimmFly <yangjinfei001@gmail.com>
2023-04-20 14:23:54 +0800

    feat: improve Empty ui component

--- packages/component/src/ui/empty/style.ts
+++ packages/component/src/ui/empty/style.ts
@@ -1,14 +1,18 @@
+import type { CSSProperties } from 'react';
+
 import { displayFlex, styled } from '../../styles';
 
-export const StyledEmptyContainer = styled('div')(() => {
-  return {
-    height: '100%',
-    ...displayFlex('center', 'center'),
-    flexDirection: 'column',
-
-    '.empty-img': {
-      width: '320px',
-      height: '280px',
-    },
-  };
-});
+export const StyledEmptyContainer = styled('div')<{ style?: CSSProperties }>(
+  ({ style }) => {
+    return {
+      height: '100%',
+      ...displayFlex('center', 'center'),
+      flexDirection: 'column',
+      svg: {
+        width: style?.width ?? '320px',
+        height: style?.height ?? '280px',
+        fontSize: style?.fontSize ?? 'inherit',
+      },
+    };
+  }
+);
