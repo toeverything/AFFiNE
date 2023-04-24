c0a1f0d28c636ee5883f64c7df4d58684c5d56cc
himself65 <himself65@outlook.com>
2023-04-24 13:13:01 -0500

    fix: test

--- scripts/setup/lit.ts
+++ scripts/setup/lit.ts
@@ -2,12 +2,11 @@ import { vi } from 'vitest';
 
 if (typeof window !== 'undefined') {
   // Refs: https://github.com/jsdom/jsdom/blob/master/lib/jsdom/living/custom-elements/CustomElementRegistry-impl.js
-  Object.defineProperty(window, 'customElements', {
-    value: {
-      define: vi.fn(),
-      get: vi.fn(),
-      whenDefined: vi.fn(),
-      upgrade: vi.fn(),
-    },
-  });
+  const customElements = {
+    define: vi.fn(),
+    get: vi.fn(),
+    whenDefined: vi.fn(),
+    upgrade: vi.fn(),
+  };
+  vi.stubGlobal('customElements', customElements);
 }
