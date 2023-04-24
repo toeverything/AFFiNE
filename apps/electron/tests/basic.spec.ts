585fb7400b93d5624e4df9947e9d30e05b818577
himself65 <himself65@outlook.com>
2023-04-24 15:11:46 -0500

    fix: test

--- apps/electron/tests/basic.spec.ts
+++ apps/electron/tests/basic.spec.ts
@@ -1,18 +1,26 @@
-import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
+import type { ElectronApplication } from 'playwright';
 import { _electron as electron } from 'playwright';
-import { expect, test } from 'vitest';
+import { afterAll, beforeAll, expect, test } from 'vitest';
+let electronApp: ElectronApplication;
+
+beforeAll(async () => {
+  electronApp = await electron.launch({
+    args: [
+      fileURLToPath(new URL('../dist/layers/main/index.js', import.meta.url)),
+    ],
+    executablePath: fileURLToPath(
+      new URL('../node_modules/.bin/electron', import.meta.url)
+    ),
+  });
+});
+
+afterAll(async () => {
+  await electronApp.close();
+});
 
 test('new page', async () => {
-  const electronApp = await electron.launch({
-    args: [resolve(__dirname, '../dist/layers/main/index.js')],
-    executablePath: resolve(__dirname, '../node_modules/.bin/electron'),
-    env: {
-      ...(process.env as Record<string, string>),
-    },
-    colorScheme: 'light',
-  });
-
   const page = await electronApp.firstWindow();
   await page.getByTestId('new-page-button').click({
     delay: 100,
