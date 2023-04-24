f520dbe330d934afea82098dd14ddcda04556d6d
himself65 <himself65@outlook.com>
2023-04-24 15:44:49 -0500

    ci: fix

--- apps/electron/tests/basic.spec.ts
+++ apps/electron/tests/basic.spec.ts
@@ -1,9 +1,22 @@
+import crypto from 'node:crypto';
+import fs from 'node:fs';
+import path from 'node:path';
+import process from 'node:process';
 import { fileURLToPath } from 'node:url';
 
 import type { ElectronApplication } from 'playwright';
 import { _electron as electron } from 'playwright';
 import { afterAll, beforeAll, expect, test } from 'vitest';
+
+const istanbulTempDir = process.env.ISTANBUL_TEMP_DIR
+  ? path.resolve(process.env.ISTANBUL_TEMP_DIR)
+  : path.join(process.cwd(), '.nyc_output');
+const enableCoverage = !!process.env.CI || !!process.env.COVERAGE;
 let electronApp: ElectronApplication;
+
+function generateUUID() {
+  return crypto.randomUUID();
+}
 
 beforeAll(async () => {
   electronApp = await electron.launch({
@@ -14,9 +27,43 @@ beforeAll(async () => {
       new URL('../node_modules/.bin/electron', import.meta.url)
     ),
   });
+  const context = electronApp.context();
+
+  if (enableCoverage) {
+    await context.addInitScript(() =>
+      window.addEventListener('beforeunload', () =>
+        // @ts-expect-error
+        window.collectIstanbulCoverage(JSON.stringify(window.__coverage__))
+      )
+    );
+
+    await fs.promises.mkdir(istanbulTempDir, { recursive: true });
+    await context.exposeFunction(
+      'collectIstanbulCoverage',
+      (coverageJSON?: string) => {
+        if (coverageJSON)
+          fs.writeFileSync(
+            path.join(
+              istanbulTempDir,
+              `playwright_coverage_${generateUUID()}.json`
+            ),
+            coverageJSON
+          );
+      }
+    );
+  }
 });
 
 afterAll(async () => {
+  const context = electronApp.context();
+  if (enableCoverage) {
+    for (const page of context.pages()) {
+      await page.evaluate(() =>
+        // @ts-expect-error
+        window.collectIstanbulCoverage(JSON.stringify(window.__coverage__))
+      );
+    }
+  }
   await electronApp.close();
 });
 
