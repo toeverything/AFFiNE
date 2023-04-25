a807647639904decc83545cf8be6bd2acc14cac3
Himself65 <himself65@outlook.com>
2023-04-25 01:58:30 -0500

    fix(component): editor component style (#2120)

--- packages/component/.storybook/main.ts
+++ packages/component/.storybook/main.ts
@@ -20,6 +20,7 @@ export default {
   },
   async viteFinal(config, { configType }) {
     return mergeConfig(config, {
+      assetsInclude: ['**/*.md'],
       plugins: [
         vanillaExtractPlugin(),
         tsconfigPaths({
