d9b7b0f750dc217f27bfaa950e584243a888290c
himself65 <himself65@outlook.com>
2023-04-25 01:10:40 -0500

    fix: add click test

--- packages/component/src/stories/ChangeLog.stories.tsx
+++ packages/component/src/stories/ChangeLog.stories.tsx
@@ -1,4 +1,6 @@
 import type { StoryFn } from '@storybook/react';
+import { within } from '@storybook/testing-library';
+import { useState } from 'react';
 
 import { ChangeLog } from '../components/changeLog';
 
@@ -17,3 +19,30 @@ export const Default: StoryFn = () => (
     <ChangeLog onCloseWhatsNew={() => {}} />
   </div>
 );
+
+export const Close: StoryFn = () => {
+  const [closed, setIsClosed] = useState(false);
+  return (
+    <>
+      <div>Closed: {closed ? 'true' : 'false'}</div>
+      <div
+        style={{
+          width: '256px',
+          height: '100vh',
+        }}
+      >
+        <ChangeLog
+          onCloseWhatsNew={() => {
+            setIsClosed(true);
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+Close.play = async ({ canvasElement }) => {
+  const element = within(canvasElement);
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  await element.getByTestId('change-log-close-button').click();
+};
