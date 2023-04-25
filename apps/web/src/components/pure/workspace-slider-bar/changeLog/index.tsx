35a287cd9a681b6ff28c5b44ade2fae66b8f3ade
JimmFly <yangjinfei001@gmail.com>
2023-04-25 13:15:23 +0800

    chore: migrate changelog to component package

--- apps/web/src/components/pure/workspace-slider-bar/changeLog/index.tsx
+++ apps/web/src/components/pure/workspace-slider-bar/changeLog/index.tsx
@@ -1,21 +1,15 @@
-import { IconButton } from '@affine/component';
-import { useTranslation } from '@affine/i18n';
-import { CloseIcon, NewIcon } from '@blocksuite/icons';
-import { useCallback, useState } from 'react';
+import ChangeLogComponent from '@affine/component/changeLog';
+import { useCallback } from 'react';
 
 import {
   useGuideHidden,
   useGuideHiddenUntilNextUpdate,
 } from '../../../../hooks/use-is-first-load';
-import { StyledChangeLog, StyledChangeLogWrapper } from '../shared-styles';
-import { StyledLink } from '../style';
 
 export const ChangeLog = () => {
   const [guideHidden, setGuideHidden] = useGuideHidden();
   const [guideHiddenUntilNextUpdate, setGuideHiddenUntilNextUpdate] =
     useGuideHiddenUntilNextUpdate();
-  const [isClose, setIsClose] = useState(false);
-  const { t } = useTranslation();
   const onCloseWhatsNew = useCallback(() => {
     setTimeout(() => {
       setGuideHiddenUntilNextUpdate({
@@ -33,28 +27,7 @@ export const ChangeLog = () => {
   if (guideHiddenUntilNextUpdate.changeLog) {
     return <></>;
   }
-  return (
-    <StyledChangeLogWrapper isClose={isClose}>
-      <StyledChangeLog data-testid="change-log" isClose={isClose}>
-        <StyledLink
-          href="https://github.com/toeverything/AFFiNE/releases"
-          target="_blank"
-        >
-          <NewIcon />
-          {t("Discover what's new!")}
-        </StyledLink>
-        <IconButton
-          onClick={() => {
-            setIsClose(true);
-            onCloseWhatsNew();
-          }}
-          data-testid="change-log-close-button"
-        >
-          <CloseIcon />
-        </IconButton>
-      </StyledChangeLog>
-    </StyledChangeLogWrapper>
-  );
+  return <ChangeLogComponent onCloseWhatsNew={onCloseWhatsNew} />;
 };
 
 export default ChangeLog;
