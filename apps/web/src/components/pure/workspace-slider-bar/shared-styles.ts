3f1293ca3c1f5e47be2f0dc256deacf5378269f0
JimmFly <yangjinfei001@gmail.com>
2023-04-25 14:24:46 +0800

    chore: add changeLog to storybook (#2118)
    Co-authored-by: himself65 <himself65@outlook.com>

--- apps/web/src/components/pure/workspace-slider-bar/shared-styles.ts
+++ apps/web/src/components/pure/workspace-slider-bar/shared-styles.ts
@@ -1,10 +1,4 @@
-import {
-  alpha,
-  displayFlex,
-  keyframes,
-  styled,
-  textEllipsis,
-} from '@affine/component';
+import { alpha, displayFlex, styled, textEllipsis } from '@affine/component';
 
 export const StyledListItem = styled('div')<{
   active?: boolean;
@@ -120,105 +114,6 @@ export const StyledCollapseItem = styled('div')<{
             visibility: 'visible',
           },
         },
-  };
-});
-
-const slideIn = keyframes({
-  '0%': {
-    height: '0px',
-  },
-  '50%': {
-    height: '36px',
-  },
-  '100%': {
-    height: '32px',
-  },
-});
-const slideIn2 = keyframes({
-  '0%': {
-    transform: 'translateX(100%)',
-  },
-  '50%': {
-    transform: 'translateX(100%)',
-  },
-  '80%': {
-    transform: 'translateX(-10%)',
-  },
-  '100%': {
-    transform: 'translateX(0%)',
-  },
-});
-
-const slideOut = keyframes({
-  '0%': {
-    height: '32px',
-  },
-  '60%': {
-    height: '32px',
-  },
-  '80%': {
-    height: '32px',
-  },
-  '100%': {
-    height: '0px',
-  },
-});
-const slideOut2 = keyframes({
-  '0%': {
-    transform: 'translateX(0%)',
-  },
-  '100%': {
-    transform: 'translateX(100%)',
-  },
-});
-
-export const StyledChangeLog = styled('div')<{
-  isClose?: boolean;
-}>(({ isClose }) => {
-  return {
-    width: '110%',
-    height: '32px',
-    ...displayFlex('flex-start', 'center'),
-    color: 'var(--affine-primary-color)',
-    backgroundColor: 'var(--affine-hover-color)',
-    border: '1px solid var(--affine-primary-color)',
-    borderRight: 'none',
-    marginLeft: '8px',
-    paddingLeft: '8px',
-    borderRadius: '16px 0 0 16px',
-    cursor: 'pointer',
-    zIndex: 1001,
-    position: 'absolute',
-    userSelect: 'none',
-    transition: 'all 0.3s',
-    animation: isClose
-      ? `${slideOut2} .3s ease-in-out forwards`
-      : `${slideIn2} 1s ease-in-out forwards`,
-    '> svg, a > svg': {
-      fontSize: '20px',
-      marginRight: '12px',
-      color: 'var(--affine-primary-color)',
-    },
-    button: {
-      marginRight: '12%',
-    },
-  };
-});
-export const StyledChangeLogWrapper = styled('div')<{
-  isClose?: boolean;
-}>(({ isClose }) => {
-  return {
-    width: 'calc(100% + 4px)',
-    height: '0px',
-    animation: isClose
-      ? `${slideOut} .3s ease-in-out forwards`
-      : `${slideIn} 1s ease-in-out forwards`,
-    ...displayFlex('flex-start', 'center'),
-    marginBottom: '4px',
-    position: 'relative',
-    userSelect: 'none',
-    transition: 'all 0.3s',
-    overflow: 'hidden',
   };
 });
 
---
+++ packages/component/src/components/changeLog/index.tsx
@@ -0,0 +1,66 @@
+import { useTranslation } from '@affine/i18n';
+import { CloseIcon, NewIcon } from '@blocksuite/icons';
+import clsx from 'clsx';
+import { useState } from 'react';
+
+import { IconButton } from '../..';
+import {
+  changeLogSlideInStyle,
+  changeLogSlideOutStyle,
+  changeLogWrapperSlideInStyle,
+  changeLogWrapperSlideOutStyle,
+  iconButtonStyle,
+  iconStyle,
+  linkStyle,
+} from './index.css';
+
+type ChangeLogProps = {
+  onCloseWhatsNew: () => void;
+};
+export const ChangeLog = (props: ChangeLogProps) => {
+  const { onCloseWhatsNew } = props;
+  const [isClose, setIsClose] = useState(false);
+  const { t } = useTranslation();
+  const handleClose = () => {
+    setIsClose(true);
+    onCloseWhatsNew();
+  };
+  return (
+    <div
+      className={clsx(changeLogWrapperSlideInStyle, {
+        [changeLogWrapperSlideOutStyle]: isClose,
+      })}
+    >
+      <div
+        data-testid="change-log"
+        className={clsx(changeLogSlideInStyle, {
+          [changeLogSlideOutStyle]: isClose,
+        })}
+      >
+        <div
+          className={linkStyle}
+          onClick={() => {
+            window.open(
+              'https://github.com/toeverything/AFFiNE/releases',
+              '_blank'
+            );
+          }}
+        >
+          <NewIcon className={iconStyle} />
+          {t("Discover what's new!")}
+        </div>
+        <IconButton
+          className={iconButtonStyle}
+          onClick={() => {
+            handleClose();
+          }}
+          data-testid="change-log-close-button"
+        >
+          <CloseIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default ChangeLog;
