606f6652ac2821fae9c6f0854d9ab2561491ea4a
Himself65 <himself65@outlook.com>
2023-04-27 00:23:34 -0500

    chore: bump version (#2162)

--- packages/component/src/ui/popper/Popper.tsx
+++ packages/component/src/ui/popper/Popper.tsx
@@ -1,5 +1,5 @@
 import ClickAwayListener from '@mui/base/ClickAwayListener';
-import PopperUnstyled from '@mui/base/PopperUnstyled';
+import PopperUnstyled from '@mui/base/Popper';
 import Grow from '@mui/material/Grow';
 import type { CSSProperties, PointerEvent } from 'react';
 import {
