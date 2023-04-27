c118584a149fb8120c4d37f868d9c1add96638e6
himself65 <himself65@outlook.com>
2023-04-26 19:08:51 -0500

    fix: css

--- packages/component/src/components/app-sidebar/resize-indicator/index.css.ts
+++ packages/component/src/components/app-sidebar/resize-indicator/index.css.ts
@@ -9,6 +9,12 @@ export const spacerStyle = style({
   top: 0,
   bottom: 0,
   cursor: 'col-resize',
+  '@media': {
+    '(max-width: 600px)': {
+      // do not allow resizing on mobile
+      display: 'none',
+    },
+  },
   selectors: {
     '&[data-resizing="true"]': {
       transition: 'width .3s, min-width .3s, max-width .3s',
