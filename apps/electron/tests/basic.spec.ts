09c563bfc570fa42a0c53c951ba9a198060dba0e
himself65 <himself65@outlook.com>
2023-04-28 01:11:19 -0500

    feat(electron): enable disable cloud alert

--- apps/electron/tests/basic.spec.ts
+++ apps/electron/tests/basic.spec.ts
@@ -21,3 +21,21 @@ test('new page', async () => {
   expect(flavour).toBe('local');
   await electronApp.close();
 });
+
+test('affine cloud disabled', async () => {
+  const electronApp = await electron.launch({
+    args: [resolve(__dirname, '..')],
+    executablePath: resolve(__dirname, '../node_modules/.bin/electron'),
+  });
+  const page = await electronApp.firstWindow();
+  await page.getByTestId('new-page-button').click({
+    delay: 100,
+  });
+  await page.waitForSelector('v-line');
+  await page.getByTestId('current-workspace').click();
+  await page.getByTestId('sign-in-button').click();
+  await page.getByTestId('disable-affine-cloud-modal').waitFor({
+    state: 'visible',
+  });
+  await electronApp.close();
+});
