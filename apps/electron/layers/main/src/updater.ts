15aaef5e536998be67ac8e1936519b99d481bbc9
m1911star <lhlxtl@gmail.com>
2023-05-03 17:11:43 +0800

    add update info

--- apps/electron/layers/main/src/updater.ts
+++ apps/electron/layers/main/src/updater.ts
@@ -1,9 +1,9 @@
+import { dialog } from 'electron';
 import { autoUpdater } from 'electron-updater';
 
 import { isMacOS } from '../../utils';
 const buildType = (process.env.BUILD_TYPE || 'canary').trim().toLowerCase();
-
-const mode = process.env.NODE_ENV || 'development';
+const mode = process.env.NODE_ENV;
 const isDev = mode === 'development';
 
 autoUpdater.autoDownload = false;
@@ -21,7 +21,32 @@ autoUpdater.setFeedURL({
 export const registerUpdater = async () => {
   if (isMacOS()) {
     autoUpdater.on('update-available', e => {
-      console.log('update-available', e);
+      dialog
+        .showMessageBox({
+          type: 'info',
+          title: 'Found Updates',
+          message: `version ${e.version} available, do you want update now?`,
+          buttons: ['Yes', 'No'],
+        })
+        .then(msg => {
+          if (msg.response === 0) {
+            autoUpdater.downloadUpdate();
+          }
+        });
+    });
+
+    autoUpdater.on('update-downloaded', () => {
+      dialog
+        .showMessageBox({
+          title: 'Install Updates',
+          message: 'Updates downloaded, application will be quit for update...',
+        })
+        .then(() => {
+          setImmediate(() => autoUpdater.quitAndInstall());
+        });
+    });
+    autoUpdater.on('error', e => {
+      console.log(e.message);
     });
     autoUpdater.forceDevUpdateConfig = isDev;
     await autoUpdater.checkForUpdatesAndNotify();
