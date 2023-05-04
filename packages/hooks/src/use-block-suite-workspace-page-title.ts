0048c993fdbf7e62b3e5dc951b6958ac953374c9
himself65 <himself65@outlook.com>
2023-05-04 02:32:46 -0500

    fix: code

--- packages/hooks/src/use-block-suite-workspace-page-title.ts
+++ packages/hooks/src/use-block-suite-workspace-page-title.ts
@@ -5,25 +5,29 @@ import { atom, useAtomValue } from 'jotai';
 
 const weakMap = new WeakMap<Workspace, Map<string, Atom<string>>>();
 
-function getAtom(w: Workspace, pageId: string) {
+function getAtom(w: Workspace, pageId: string): Atom<string> {
   if (!weakMap.has(w)) {
     weakMap.set(w, new Map());
   }
   const map = weakMap.get(w);
   assertExists(map);
-  const baseAtom = atom<string>(w.getPage(pageId)?.meta.title || 'Untitled');
-  baseAtom.onMount = set => {
-    const disposable = w.meta.pageMetasUpdated.on(() => {
-      const page = w.getPage(pageId);
-      assertExists(page);
-      set(page?.meta.title || 'Untitled');
-    });
-    return () => {
-      disposable.dispose();
+  if (!map.has(pageId)) {
+    const baseAtom = atom<string>(w.getPage(pageId)?.meta.title || 'Untitled');
+    baseAtom.onMount = set => {
+      const disposable = w.meta.pageMetasUpdated.on(() => {
+        const page = w.getPage(pageId);
+        assertExists(page);
+        set(page?.meta.title || 'Untitled');
+      });
+      return () => {
+        disposable.dispose();
+      };
     };
-  };
-  map.set(pageId, baseAtom);
-  return baseAtom;
+    map.set(pageId, baseAtom);
+    return baseAtom;
+  } else {
+    return map.get(pageId) as Atom<string>;
+  }
 }
 
 export function useBlockSuiteWorkspacePageTitle(
