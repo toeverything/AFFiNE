5bcda744709b01e570a50425ebbf0c9a30e465d8
Doma <leishenghao@126.com>
2023-05-10 15:19:57 +0800

    feat(app): app menu item and hotkey for creating new page

--- apps/electron/layers/main/src/events/index.ts
+++ apps/electron/layers/main/src/events/index.ts
@@ -1,7 +1,9 @@
 export * from './register';
 
+import { applicationMenuSubjects } from './application-menu';
 import { dbSubjects } from './db';
 
 export const subjects = {
   db: dbSubjects,
+  applicationMenu: applicationMenuSubjects,
 };
