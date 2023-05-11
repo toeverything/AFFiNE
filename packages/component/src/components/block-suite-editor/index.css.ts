d84f6e1969d73d7ebfea1a94c28e9ec378df9f47
himself65 <himself65@outlook.com>
2023-05-11 00:23:31 -0500

    fix: ui

--- packages/component/src/components/block-suite-editor/index.css.ts
+++ packages/component/src/components/block-suite-editor/index.css.ts
@@ -3,3 +3,8 @@ import { style } from '@vanilla-extract/css';
 export const blockSuiteEditorStyle = style({
   padding: '0 1rem',
 });
+
+export const blockSuiteEditorHeaderStyle = style({
+  marginTop: '40px',
+  marginBottom: '40px',
+});
