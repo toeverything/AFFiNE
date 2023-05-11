de2215d45ebed9d8d114178f41bb057360f0af8a
Peng Xiao <pengxiao@outlook.com>
2023-05-12 03:19:02 +0800

    fix: noise size

--- packages/component/src/components/workspace/index.css.ts
+++ packages/component/src/components/workspace/index.css.ts
@@ -18,7 +18,8 @@ export const appStyle = style({
       position: 'absolute',
       inset: 0,
       opacity: 'var(--affine-noise-opacity)',
-      background: `url("data:image/svg+xml,%3Csvg viewBox='0 0 1024 1024' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.25' numOctaves='10' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
+      backgroundSize: '25%',
+      backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 1024 1024' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.25' numOctaves='10' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
     },
   },
   vars: {
@@ -35,7 +36,7 @@ export const appStyle = style({
 
 globalStyle(`html[data-theme="light"] ${appStyle}`, {
   vars: {
-    '--affine-noise-opacity': '0.2',
+    '--affine-noise-opacity': '0.25',
   },
 });
 
