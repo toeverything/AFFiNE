a6bb4c52e0af2f7b9b8147b19c3404b821074289
himself65 <himself65@outlook.com>
2023-05-11 21:11:48 -0500

    test: fix

--- apps/electron/tests/basic.spec.ts
+++ apps/electron/tests/basic.spec.ts
@@ -14,8 +14,11 @@ test('new page', async ({ page, workspace }) => {
 test('app sidebar router forward/back', async ({ page }) => {
   await page.getByTestId('help-island').click();
   await page.getByTestId('easy-guide').click();
+  await page.getByTestId('onboarding-modal-next-button').click();
+  await page.getByTestId('onboarding-modal-close-button').click();
   {
     // create pages
+    await page.waitForTimeout(500);
     await page.getByTestId('new-page-button').click({
       delay: 100,
     });
@@ -24,6 +27,7 @@ test('app sidebar router forward/back', async ({ page }) => {
     await page.type('.affine-default-page-block-title', 'test1', {
       delay: 100,
     });
+    await page.waitForTimeout(500);
     await page.getByTestId('new-page-button').click({
       delay: 100,
     });
@@ -32,6 +36,7 @@ test('app sidebar router forward/back', async ({ page }) => {
     await page.type('.affine-default-page-block-title', 'test2', {
       delay: 100,
     });
+    await page.waitForTimeout(500);
     await page.getByTestId('new-page-button').click({
       delay: 100,
     });
@@ -47,24 +52,21 @@ test('app sidebar router forward/back', async ({ page }) => {
       .textContent()) as string;
     expect(title.trim()).toBe('test3');
   }
-  await page.click('[data-testid="app-sidebar-arrow-button-back"]', {
-    delay: 100,
-  });
-  await page.click('[data-testid="app-sidebar-arrow-button-back"]', {
-    delay: 100,
-  });
+
+  await page.click('[data-testid="app-sidebar-arrow-button-back"]');
+  await page.waitForTimeout(1000);
+  await page.click('[data-testid="app-sidebar-arrow-button-back"]');
+  await page.waitForTimeout(1000);
   {
     const title = (await page
       .locator('.affine-default-page-block-title')
       .textContent()) as string;
     expect(title.trim()).toBe('test1');
   }
-  await page.click('[data-testid="app-sidebar-arrow-button-forward"]', {
-    delay: 100,
-  });
-  await page.click('[data-testid="app-sidebar-arrow-button-forward"]', {
-    delay: 100,
-  });
+  await page.click('[data-testid="app-sidebar-arrow-button-forward"]');
+  await page.waitForTimeout(1000);
+  await page.click('[data-testid="app-sidebar-arrow-button-forward"]');
+  await page.waitForTimeout(1000);
   {
     const title = (await page
       .locator('.affine-default-page-block-title')
