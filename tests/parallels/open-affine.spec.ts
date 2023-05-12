b7a01409d3dc00a64150a13f619f1b4c2363d261
Peng Xiao <pengxiao@outlook.com>
2023-05-12 21:06:21 +0800

    fix: remove obsolete tests

--- tests/parallels/open-affine.spec.ts
+++ tests/parallels/open-affine.spec.ts
@@ -26,33 +26,6 @@ test('Open last workspace when back to affine', async ({ page }) => {
   expect(currentWorkspaceName).toEqual('New Workspace 2');
 });
 
-test('Open affine in first time after updated', async ({ page }) => {
-  await openHomePage(page);
-  const changeLogItem = page.locator('[data-testid=change-log]');
-  await expect(changeLogItem).toBeVisible();
-  const closeButton = page.locator('[data-testid=change-log-close-button]');
-  await closeButton.click();
-  await expect(changeLogItem).not.toBeVisible();
-  await page.goto('http://localhost:8080');
-  const currentChangeLogItem = page.locator('[data-testid=change-log]');
-  await expect(currentChangeLogItem).not.toBeVisible();
-});
-test('Click right-bottom corner change log icon', async ({ page }) => {
-  await openHomePage(page);
-  await waitMarkdownImported(page);
-  await page.locator('[data-testid=help-island]').click();
-  const editorRightBottomChangeLog = page.locator(
-    '[data-testid=right-bottom-change-log-icon]'
-  );
-  await page.waitForTimeout(50);
-  expect(await editorRightBottomChangeLog.isVisible()).toEqual(true);
-  await page.getByTestId('all-pages').click();
-  const normalRightBottomChangeLog = page.locator(
-    '[data-testid=right-bottom-change-log-icon]'
-  );
-  expect(await normalRightBottomChangeLog.isVisible()).toEqual(true);
-});
-
 test('Download client tip', async ({ page }) => {
   await openHomePage(page);
   const downloadClientTipItem = page.locator(
