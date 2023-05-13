toeverything/AFFiNE

commit c1a65b6b7684770083d97cba39350c1d31959e43

feat(component): init app sidebar (#2135)

apps/web/src/components/root-app-sidebar/index.tsx
@@ -1,0 +1,216 @@
+import {
+  AppSidebar,
+  appSidebarOpenAtom,
+  ResizeIndicator,
+} from '@affine/component/app-sidebar';
+import { config } from '@affine/env';
+import { useTranslation } from '@affine/i18n';
+import { WorkspaceFlavour } from '@affine/workspace/type';
+import {
+  DeleteTemporarilyIcon,
+  FolderIcon,
+  PlusIcon,
+  SearchIcon,
+  SettingsIcon,
+  ShareIcon,
+} from '@blocksuite/icons';
+import type { Page } from '@blocksuite/store';
+import { useAtomValue } from 'jotai';
+import type { ReactElement, UIEvent } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+
+import type { AllWorkspace } from '../../shared';
+import ChangeLog from '../pure/workspace-slider-bar/changeLog';
+import Favorite from '../pure/workspace-slider-bar/favorite';
+import { StyledListItem } from '../pure/workspace-slider-bar/shared-styles';
+import {
+  StyledLink,
+  StyledNewPageButton,
+  StyledScrollWrapper,
+  StyledSliderBarInnerWrapper,
+} from '../pure/workspace-slider-bar/style';
+import { WorkspaceSelector } from '../pure/workspace-slider-bar/WorkspaceSelector';
+
+export type RootAppSidebarProps = {
+  isPublicWorkspace: boolean;
+  onOpenQuickSearchModal: () => void;
+  onOpenWorkspaceListModal: () => void;
+  currentWorkspace: AllWorkspace | null;
+  currentPageId: string | null;
+  openPage: (pageId: string) => void;
+  createPage: () => Page;
+  currentPath: string;
+  paths: {
+    all: (workspaceId: string) => string;
+    favorite: (workspaceId: string) => string;
+    trash: (workspaceId: string) => string;
+    setting: (workspaceId: string) => string;
+    shared: (workspaceId: string) => string;
+  };
+};
+
+/**
+ * This is for the whole affine app sidebar.
+ * This component wraps the app sidebar in `@affine/component` with logic and data.
+ *
+ * @todo(himself65): rewrite all styled component into @vanilla-extract/css
+ */
+export const RootAppSidebar = ({
+  currentWorkspace,
+  currentPageId,
+  openPage,
+  createPage,
+  currentPath,
+  paths,
+  onOpenQuickSearchModal,
+  onOpenWorkspaceListModal,
+}: RootAppSidebarProps): ReactElement => {
+  const currentWorkspaceId = currentWorkspace?.id || null;
+  const blockSuiteWorkspace = currentWorkspace?.blockSuiteWorkspace;
+  const { t } = useTranslation();
+  const [isScrollAtTop, setIsScrollAtTop] = useState(true);
+  const onClickNewPage = useCallback(async () => {
+    const page = await createPage();
+    openPage(page.id);
+  }, [createPage, openPage]);
+  const sidebarOpen = useAtomValue(appSidebarOpenAtom);
+  useEffect(() => {
+    if (environment.isDesktop) {
+      window.apis?.onSidebarVisibilityChange(sidebarOpen);
+    }
+  }, [sidebarOpen]);
+  const ref = useRef<HTMLElement>(null);
+  return (
+    <>
+      <AppSidebar
+        ref={ref}
+        footer={
+          <StyledNewPageButton
+            data-testid="new-page-button"
+            onClick={onClickNewPage}
+          >
+            <PlusIcon /> {t('New Page')}
+          </StyledNewPageButton>
+        }
+      >
+        <StyledSliderBarInnerWrapper data-testid="sliderBar-inner">
+          <WorkspaceSelector
+            currentWorkspace={currentWorkspace}
+            onClick={onOpenWorkspaceListModal}
+          />
+          <ChangeLog />
+          <StyledListItem
+            data-testid="slider-bar-quick-search-button"
+            onClick={useCallback(() => {
+              onOpenQuickSearchModal();
+            }, [onOpenQuickSearchModal])}
+          >
+            <SearchIcon />
+            {t('Quick search')}
+          </StyledListItem>
+          <StyledListItem
+            active={
+              currentPath ===
+              (currentWorkspaceId && paths.setting(currentWorkspaceId))
+            }
+            data-testid="slider-bar-workspace-setting-button"
+            style={{
+              marginBottom: '16px',
+            }}
+          >
+            <StyledLink
+              href={{
+                pathname:
+                  currentWorkspaceId && paths.setting(currentWorkspaceId),
+              }}
+            >
+              <SettingsIcon />
+              <div>{t('Workspace Settings')}</div>
+            </StyledLink>
+          </StyledListItem>
+          <StyledListItem
+            active={
+              currentPath ===
+              (currentWorkspaceId && paths.all(currentWorkspaceId))
+            }
+          >
+            <StyledLink
+              href={{
+                pathname: currentWorkspaceId && paths.all(currentWorkspaceId),
+              }}
+            >
+              <FolderIcon />
+              <span data-testid="all-pages">{t('All pages')}</span>
+            </StyledLink>
+          </StyledListItem>
+          <StyledScrollWrapper
+            showTopBorder={!isScrollAtTop}
+            onScroll={(e: UIEvent<HTMLDivElement>) => {
+              (e.target as HTMLDivElement).scrollTop === 0
+                ? setIsScrollAtTop(true)
+                : setIsScrollAtTop(false);
+            }}
+          >
+            {blockSuiteWorkspace && (
+              <Favorite
+                currentPath={currentPath}
+                paths={paths}
+                currentPageId={currentPageId}
+                openPage={openPage}
+                currentWorkspace={currentWorkspace}
+              />
+            )}
+          </StyledScrollWrapper>
+          <div style={{ height: 16 }}></div>
+          {config.enableLegacyCloud &&
+            (currentWorkspace?.flavour === WorkspaceFlavour.AFFINE &&
+            currentWorkspace.public ? (
+              <StyledListItem>
+                <StyledLink
+                  href={{
+                    pathname:
+                      currentWorkspaceId && paths.setting(currentWorkspaceId),
+                  }}
+                >
+                  <ShareIcon />
+                  <span data-testid="Published-to-web">Published to web</span>
+                </StyledLink>
+              </StyledListItem>
+            ) : (
+              <StyledListItem
+                active={
+                  currentPath ===
+                  (currentWorkspaceId && paths.shared(currentWorkspaceId))
+                }
+              >
+                <StyledLink
+                  href={{
+                    pathname:
+                      currentWorkspaceId && paths.shared(currentWorkspaceId),
+                  }}
+                >
+                  <ShareIcon />
+                  <span data-testid="shared-pages">{t('Shared Pages')}</span>
+                </StyledLink>
+              </StyledListItem>
+            ))}
+          <StyledListItem
+            active={
+              currentPath ===
+              (currentWorkspaceId && paths.trash(currentWorkspaceId))
+            }
+          >
+            <StyledLink
+              href={{
+                pathname: currentWorkspaceId && paths.trash(currentWorkspaceId),
+              }}
+            >
+              <DeleteTemporarilyIcon /> {t('Trash')}
+            </StyledLink>
+          </StyledListItem>
+        </StyledSliderBarInnerWrapper>
+      </AppSidebar>
+      <ResizeIndicator targetElement={ref} />
+    </>
+  );
+};

apps/web/src/components/affine/sidebar-switch/index.tsx
@@ -1,6 +1,8 @@
 import { Tooltip } from '@affine/component';
+import { appSidebarOpenAtom } from '@affine/component/app-sidebar';
 import { getEnvironment } from '@affine/env';
 import { useTranslation } from '@affine/i18n';
+import { useAtom } from 'jotai';
 import { useCallback, useEffect, useState } from 'react';
 
 import {
@@ -8,7 +10,6 @@ import {
   useGuideHiddenUntilNextUpdate,
   useUpdateTipsOnVersionChange,
 } from '../../../hooks/use-is-first-load';
-import { useSidebarStatus } from '../../../hooks/use-sidebar-status';
 import { SidebarSwitchIcon } from './icons';
 import { StyledSidebarSwitch } from './style';
 type SidebarSwitchProps = {
@@ -24,7 +25,7 @@ export const SidebarSwitch = ({
   ...props
 }: SidebarSwitchProps) => {
   useUpdateTipsOnVersionChange();
-  const [open, setOpen] = useSidebarStatus();
+  const [open, setOpen] = useAtom(appSidebarOpenAtom);
   const [tooltipVisible, setTooltipVisible] = useState(false);
   const [guideHidden, setGuideHidden] = useGuideHidden();
   const [guideHiddenUntilNextUpdate, setGuideHiddenUntilNextUpdate] =
@@ -56,9 +57,9 @@ export const SidebarSwitch = ({
         visible={visible}
         disabled={!visible}
         onClick={useCallback(() => {
-          setOpen(!open);
+          setOpen(open => !open);
           setTooltipVisible(false);
-          if (guideHiddenUntilNextUpdate['quickSearchTips'] === false) {
+          if (!guideHiddenUntilNextUpdate['quickSearchTips']) {
             setGuideHiddenUntilNextUpdate({
               ...guideHiddenUntilNextUpdate,
               quickSearchTips: true,
@@ -70,7 +71,6 @@ export const SidebarSwitch = ({
         }, [
           guideHidden,
           guideHiddenUntilNextUpdate,
-          open,
           setGuideHidden,
           setGuideHiddenUntilNextUpdate,
           setOpen,

apps/web/src/components/pure/workspace-slider-bar/index.tsx
@@ -1,47 +1,6 @@
-import { config } from '@affine/env';
-import { useTranslation } from '@affine/i18n';
-import { WorkspaceFlavour } from '@affine/workspace/type';
-import {
-  DeleteTemporarilyIcon,
-  FolderIcon,
-  PlusIcon,
-  SearchIcon,
-  SettingsIcon,
-  ShareIcon,
-} from '@blocksuite/icons';
 import type { Page, PageMeta } from '@blocksuite/store';
-import type React from 'react';
-import type { UIEvent } from 'react';
-import { lazy, Suspense, useCallback, useEffect, useState } from 'react';
 
-import {
-  useSidebarFloating,
-  useSidebarResizing,
-  useSidebarStatus,
-  useSidebarWidth,
-} from '../../../hooks/use-sidebar-status';
 import type { AllWorkspace } from '../../../shared';
-import { ChangeLog } from './changeLog';
-import Favorite from './favorite';
-import { RouteNavigation } from './RouteNavigation';
-import { StyledListItem } from './shared-styles';
-import {
-  StyledLink,
-  StyledNewPageButton,
-  StyledScrollWrapper,
-  StyledSidebarHeader,
-  StyledSliderBar,
-  StyledSliderBarInnerWrapper,
-  StyledSliderBarWrapper,
-  StyledSliderModalBackground,
-} from './style';
-import { WorkspaceSelector } from './WorkspaceSelector';
-
-const SidebarSwitch = lazy(() =>
-  import('../../affine/sidebar-switch').then(module => ({
-    default: module.SidebarSwitch,
-  }))
-);
 
 export type FavoriteListProps = {
   currentPageId: string | null;
@@ -67,204 +26,3 @@ export type WorkSpaceSliderBarProps = {
     shared: (workspaceId: string) => string;
   };
 };
-
-export const WorkSpaceSliderBar: React.FC<WorkSpaceSliderBarProps> = ({
-  isPublicWorkspace,
-  currentWorkspace,
-  currentPageId,
-  openPage,
-  createPage,
-  currentPath,
-  paths,
-  onOpenQuickSearchModal,
-  onOpenWorkspaceListModal,
-}) => {
-  const currentWorkspaceId = currentWorkspace?.id || null;
-  const blockSuiteWorkspace = currentWorkspace?.blockSuiteWorkspace;
-  const { t } = useTranslation();
-  const [sidebarOpen, setSidebarOpen] = useSidebarStatus();
-  const onClickNewPage = useCallback(async () => {
-    const page = await createPage();
-    openPage(page.id);
-  }, [createPage, openPage]);
-  const floatingSlider = useSidebarFloating();
-  const [sliderWidth] = useSidebarWidth();
-  const [isResizing] = useSidebarResizing();
-  const [isScrollAtTop, setIsScrollAtTop] = useState(true);
-  const show = isPublicWorkspace ? false : sidebarOpen;
-  const actualWidth = floatingSlider ? 'calc(10vw + 400px)' : sliderWidth;
-
-  useEffect(() => {
-    if (environment.isDesktop) {
-      window.apis?.onSidebarVisibilityChange(sidebarOpen);
-    }
-  }, [sidebarOpen]);
-
-  useEffect(() => {
-    const keydown = (e: KeyboardEvent) => {
-      if ((e.key === '/' && e.metaKey) || (e.key === '/' && e.ctrlKey)) {
-        setSidebarOpen(!sidebarOpen);
-      }
-    };
-    document.addEventListener('keydown', keydown, { capture: true });
-    return () =>
-      document.removeEventListener('keydown', keydown, { capture: true });
-  }, [sidebarOpen, setSidebarOpen]);
-
-  return (
-    <>
-      <StyledSliderBarWrapper
-        resizing={isResizing}
-        floating={floatingSlider}
-        show={show}
-        style={{ width: actualWidth }}
-        data-testid="sliderBar-root"
-      >
-        <StyledSliderBar>
-          <StyledSidebarHeader>
-            <RouteNavigation />
-            <Suspense>
-              <SidebarSwitch
-                visible={sidebarOpen}
-                tooltipContent={t('Collapse sidebar')}
-                data-testid="sliderBar-arrowButton-collapse"
-              />
-            </Suspense>
-          </StyledSidebarHeader>
-
-          <StyledSliderBarInnerWrapper data-testid="sliderBar-inner">
-            <WorkspaceSelector
-              currentWorkspace={currentWorkspace}
-              onClick={onOpenWorkspaceListModal}
-            />
-            <ChangeLog />
-            <StyledListItem
-              data-testid="slider-bar-quick-search-button"
-              onClick={useCallback(() => {
-                onOpenQuickSearchModal();
-              }, [onOpenQuickSearchModal])}
-            >
-              <SearchIcon />
-              {t('Quick search')}
-            </StyledListItem>
-            <StyledListItem
-              active={
-                currentPath ===
-                (currentWorkspaceId && paths.setting(currentWorkspaceId))
-              }
-              data-testid="slider-bar-workspace-setting-button"
-              style={{
-                marginBottom: '16px',
-              }}
-            >
-              <StyledLink
-                href={{
-                  pathname:
-                    currentWorkspaceId && paths.setting(currentWorkspaceId),
-                }}
-              >
-                <SettingsIcon />
-                <div>{t('Workspace Settings')}</div>
-              </StyledLink>
-            </StyledListItem>
-            <StyledListItem
-              active={
-                currentPath ===
-                (currentWorkspaceId && paths.all(currentWorkspaceId))
-              }
-            >
-              <StyledLink
-                href={{
-                  pathname: currentWorkspaceId && paths.all(currentWorkspaceId),
-                }}
-              >
-                <FolderIcon />
-                <span data-testid="all-pages">{t('All pages')}</span>
-              </StyledLink>
-            </StyledListItem>
-            <StyledScrollWrapper
-              showTopBorder={!isScrollAtTop}
-              onScroll={(e: UIEvent<HTMLDivElement>) => {
-                (e.target as HTMLDivElement).scrollTop === 0
-                  ? setIsScrollAtTop(true)
-                  : setIsScrollAtTop(false);
-              }}
-            >
-              {blockSuiteWorkspace && (
-                <Favorite
-                  currentPath={currentPath}
-                  paths={paths}
-                  currentPageId={currentPageId}
-                  openPage={openPage}
-                  currentWorkspace={currentWorkspace}
-                />
-              )}
-            </StyledScrollWrapper>
-            <div style={{ height: 16 }}></div>
-            {config.enableLegacyCloud &&
-              (currentWorkspace?.flavour === WorkspaceFlavour.AFFINE &&
-              currentWorkspace.public ? (
-                <StyledListItem>
-                  <StyledLink
-                    href={{
-                      pathname:
-                        currentWorkspaceId && paths.setting(currentWorkspaceId),
-                    }}
-                  >
-                    <ShareIcon />
-                    <span data-testid="Published-to-web">Published to web</span>
-                  </StyledLink>
-                </StyledListItem>
-              ) : (
-                <StyledListItem
-                  active={
-                    currentPath ===
-                    (currentWorkspaceId && paths.shared(currentWorkspaceId))
-                  }
-                >
-                  <StyledLink
-                    href={{
-                      pathname:
-                        currentWorkspaceId && paths.shared(currentWorkspaceId),
-                    }}
-                  >
-                    <ShareIcon />
-                    <span data-testid="shared-pages">{t('Shared Pages')}</span>
-                  </StyledLink>
-                </StyledListItem>
-              ))}
-            <StyledListItem
-              active={
-                currentPath ===
-                (currentWorkspaceId && paths.trash(currentWorkspaceId))
-              }
-            >
-              <StyledLink
-                href={{
-                  pathname:
-                    currentWorkspaceId && paths.trash(currentWorkspaceId),
-                }}
-              >
-                <DeleteTemporarilyIcon /> {t('Trash')}
-              </StyledLink>
-            </StyledListItem>
-          </StyledSliderBarInnerWrapper>
-
-          <StyledNewPageButton
-            data-testid="new-page-button"
-            onClick={onClickNewPage}
-          >
-            <PlusIcon /> {t('New Page')}
-          </StyledNewPageButton>
-        </StyledSliderBar>
-      </StyledSliderBarWrapper>
-      <StyledSliderModalBackground
-        data-testid="sliderBar-modalBackground"
-        active={floatingSlider && sidebarOpen}
-        onClick={() => setSidebarOpen(false)}
-      />
-    </>
-  );
-};
-
-export default WorkSpaceSliderBar;

packages/component/src/components/app-sidebar/index.tsx
@@ -1,0 +1,108 @@
+import { getEnvironment } from '@affine/env';
+import {
+  ArrowLeftSmallIcon,
+  ArrowRightSmallIcon,
+  SidebarIcon,
+} from '@blocksuite/icons';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
+import { useAtom, useAtomValue } from 'jotai';
+import type { PropsWithChildren, ReactElement } from 'react';
+import type { ReactNode } from 'react';
+import { forwardRef, useCallback, useImperativeHandle, useRef } from 'react';
+
+import { IconButton } from '../../ui/button/IconButton';
+import {
+  navBodyStyle,
+  navFooterStyle,
+  navHeaderStyle,
+  navStyle,
+  navWidthVar,
+  sidebarButtonStyle,
+  sidebarFloatMaskStyle,
+} from './index.css';
+import { appSidebarOpenAtom, appSidebarWidthAtom } from './index.jotai';
+
+export { appSidebarOpenAtom };
+
+export type AppSidebarProps = PropsWithChildren<{
+  footer?: ReactNode | undefined;
+}>;
+
+export const AppSidebar = forwardRef<HTMLElement, AppSidebarProps>(
+  function AppSidebar(props, forwardedRef): ReactElement {
+    const ref = useRef<HTMLElement>(null);
+    const [open, setOpen] = useAtom(appSidebarOpenAtom);
+
+    const appSidebarWidth = useAtomValue(appSidebarWidthAtom);
+
+    const handleSidebarOpen = useCallback(() => {
+      setOpen(open => !open);
+    }, [setOpen]);
+
+    useImperativeHandle(forwardedRef, () => ref.current as HTMLElement);
+
+    const environment = getEnvironment();
+    const isMacosDesktop = environment.isDesktop && environment.isMacOs;
+    return (
+      <>
+        <nav
+          className={navStyle}
+          ref={ref}
+          style={assignInlineVars({
+            [navWidthVar]: `${appSidebarWidth}px`,
+          })}
+          data-testid="app-sidebar"
+          data-open={open}
+          data-is-macos-electron={isMacosDesktop}
+        >
+          <div
+            className={navHeaderStyle}
+            data-is-macos-electron={isMacosDesktop}
+          >
+            {isMacosDesktop && (
+              <>
+                <IconButton
+                  size="middle"
+                  onClick={() => {
+                    window.history.back();
+                  }}
+                >
+                  <ArrowLeftSmallIcon />
+                </IconButton>
+                <IconButton
+                  size="middle"
+                  onClick={() => {
+                    window.history.forward();
+                  }}
+                  style={{ marginLeft: '32px' }}
+                >
+                  <ArrowRightSmallIcon />
+                </IconButton>
+              </>
+            )}
+            <IconButton
+              data-testid="app-sidebar-arrow-button-collapse"
+              className={sidebarButtonStyle}
+              onClick={handleSidebarOpen}
+            >
+              <SidebarIcon width={24} height={24} />
+            </IconButton>
+          </div>
+          <div className={navBodyStyle}>{props.children}</div>
+          <div className={navFooterStyle}>{props.footer}</div>
+        </nav>
+        <div
+          data-testid="app-sidebar-float-mask"
+          data-open={open}
+          className={sidebarFloatMaskStyle}
+          onClick={useCallback(() => {
+            setOpen(false);
+          }, [setOpen])}
+        />
+      </>
+    );
+  }
+);
+
+export type { ResizeIndicatorProps } from './resize-indicator';
+export { ResizeIndicator } from './resize-indicator';

packages/component/src/components/app-sidebar/resize-indicator/index.tsx
@@ -1,0 +1,86 @@
+import type { Instance } from '@popperjs/core';
+import { createPopper } from '@popperjs/core';
+import { useAtom, useAtomValue, useSetAtom } from 'jotai';
+import type { ReactElement, RefObject } from 'react';
+import {
+  useCallback,
+  useDeferredValue,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+import { appSidebarOpenAtom, appSidebarWidthAtom } from '../index.jotai';
+import { spacerStyle } from './index.css';
+
+export type ResizeIndicatorProps = {
+  targetElement: RefObject<HTMLElement>;
+};
+
+export const ResizeIndicator = (props: ResizeIndicatorProps): ReactElement => {
+  const ref = useRef<HTMLDivElement>(null);
+  const popperRef = useRef<Instance | null>(null);
+  const setWidth = useSetAtom(appSidebarWidthAtom);
+  const [sidebarOpen, setSidebarOpen] = useAtom(appSidebarOpenAtom);
+  const [isResizing, setIsResizing] = useState(false);
+  useEffect(() => {
+    if (ref.current) {
+      if (props.targetElement.current) {
+        popperRef.current = createPopper(
+          props.targetElement.current,
+          ref.current,
+          {
+            placement: 'right',
+          }
+        );
+      }
+    }
+  }, [props.targetElement]);
+
+  const sidebarWidth = useDeferredValue(useAtomValue(appSidebarWidthAtom));
+  useEffect(() => {
+    if (popperRef.current) {
+      popperRef.current.update();
+    }
+  }, [sidebarWidth]);
+
+  const onResizeStart = useCallback(() => {
+    let resized = false;
+
+    function onMouseMove(e: MouseEvent) {
+      e.preventDefault();
+      const newWidth = Math.min(480, Math.max(e.clientX, 256));
+      setWidth(newWidth);
+      setIsResizing(true);
+      resized = true;
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener(
+      'mouseup',
+      () => {
+        // if not resized, toggle sidebar
+        if (!resized) {
+          setSidebarOpen(o => !o);
+        }
+        if (popperRef.current) {
+          popperRef.current.update();
+        }
+        setIsResizing(false);
+        document.removeEventListener('mousemove', onMouseMove);
+      },
+      { once: true }
+    );
+  }, [setSidebarOpen, setWidth]);
+
+  return (
+    <div
+      ref={ref}
+      className={spacerStyle}
+      data-testid="app-sidebar-resizer"
+      data-resizing={isResizing}
+      data-open={sidebarOpen}
+      onMouseDown={onResizeStart}
+    />
+  );
+};

apps/web/src/atoms/first-load.ts
@@ -1,19 +1,14 @@
-import { atomWithSyncStorage } from '@affine/jotai';
+import { atomWithStorage } from 'jotai/utils';
 
 export type Visibility = Record<string, boolean>;
 
 const DEFAULT_VALUE = '0.0.0';
 
-export const lastVersionAtom = atomWithSyncStorage(
-  'lastVersion',
-  DEFAULT_VALUE
-);
-export const guideHiddenAtom = atomWithSyncStorage<Visibility>(
-  'guideHidden',
-  {}
-);
+export const lastVersionAtom = atomWithStorage('lastVersion', DEFAULT_VALUE);
 
-export const guideHiddenUntilNextUpdateAtom = atomWithSyncStorage<Visibility>(
+export const guideHiddenAtom = atomWithStorage<Visibility>('guideHidden', {});
+
+export const guideHiddenUntilNextUpdateAtom = atomWithStorage<Visibility>(
   'guideHiddenUntilNextUpdate',
   {}
 );

apps/web/src/components/blocksuite/workspace-header/header.tsx
@@ -1,7 +1,9 @@
+import { appSidebarOpenAtom } from '@affine/component/app-sidebar';
 import { useTranslation } from '@affine/i18n';
 import { WorkspaceFlavour } from '@affine/workspace/type';
 import { CloseIcon } from '@blocksuite/icons';
 import type { Page } from '@blocksuite/store';
+import { useAtom } from 'jotai';
 import type { FC, HTMLAttributes, PropsWithChildren } from 'react';
 import {
   forwardRef,
@@ -12,10 +14,6 @@ import {
   useState,
 } from 'react';
 
-import {
-  useSidebarFloating,
-  useSidebarStatus,
-} from '../../../hooks/use-sidebar-status';
 import type { AffineOfficialWorkspace } from '../../../shared';
 import { EditorOptionMenu } from './header-right-items/EditorOptionMenu';
 import EditPage from './header-right-items/EditPage';
@@ -142,17 +140,11 @@ export const Header = forwardRef<
   useEffect(() => {
     setShowWarning(shouldShowWarning());
   }, []);
-  const [open] = useSidebarStatus();
-  const sidebarFloating = useSidebarFloating();
+  const [open] = useAtom(appSidebarOpenAtom);
   const { t } = useTranslation();
 
   return (
-    <StyledHeaderContainer
-      sidebarFloating={sidebarFloating && open}
-      ref={ref}
-      hasWarning={showWarning}
-      {...props}
-    >
+    <StyledHeaderContainer ref={ref} hasWarning={showWarning} {...props}>
       <BrowserWarning
         show={showWarning}
         onClose={() => {

apps/web/src/components/blocksuite/workspace-header/styles.ts
@@ -7,8 +7,7 @@ import {
 
 export const StyledHeaderContainer = styled('div')<{
   hasWarning: boolean;
-  sidebarFloating: boolean;
-}>(({ theme, hasWarning, sidebarFloating }) => {
+}>(({ hasWarning }) => {
   return {
     height: hasWarning ? '96px' : '52px',
     flexShrink: 0,
@@ -16,10 +15,6 @@ export const StyledHeaderContainer = styled('div')<{
     top: 0,
     background: 'var(--affine-background-primary-color)',
     zIndex: 1,
-    WebkitAppRegion: sidebarFloating ? '' : 'drag',
-    button: {
-      WebkitAppRegion: 'no-drag',
-    },
   };
 });
 export const StyledHeader = styled('div')<{ hasWarning: boolean }>(

commit 1d74f850c3cc93c864148ec0ed1a9df8716e0017

fix: add back CMD+/ shortcut to toggle sidebar
regression in https://github.com/toeverything/AFFiNE/pull/2135/files#diff-d3f4898bf43f12206d516293b25f76c9175a503e0ebb9dee705b4a77fc8bb28d

apps/web/src/components/root-app-sidebar/index.tsx
@@ -20,7 +20,7 @@ import {
   ShareIcon,
 } from '@blocksuite/icons';
 import type { Page } from '@blocksuite/store';
-import { useAtomValue } from 'jotai';
+import { useAtom, useAtomValue } from 'jotai';
 import type { ReactElement } from 'react';
 import type React from 'react';
 import { useCallback, useEffect } from 'react';
@@ -88,12 +88,23 @@ export const RootAppSidebar = ({
     const page = await createPage();
     openPage(page.id);
   }, [createPage, openPage]);
-  const sidebarOpen = useAtomValue(appSidebarOpenAtom);
+  const [sidebarOpen, setSidebarOpen] = useAtom(appSidebarOpenAtom);
   useEffect(() => {
     if (environment.isDesktop && typeof sidebarOpen === 'boolean') {
       window.apis?.ui.handleSidebarVisibilityChange(sidebarOpen);
     }
   }, [sidebarOpen]);
+
+  useEffect(() => {
+    const keydown = (e: KeyboardEvent) => {
+      if ((e.key === '/' && e.metaKey) || (e.key === '/' && e.ctrlKey)) {
+        setSidebarOpen(!sidebarOpen);
+      }
+    };
+    document.addEventListener('keydown', keydown, { capture: true });
+    return () =>
+      document.removeEventListener('keydown', keydown, { capture: true });
+  }, [sidebarOpen, setSidebarOpen]);
 
   const clientUpdateAvailable = useAtomValue(updateAvailableAtom);
 