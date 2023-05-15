06951319a6d3e86d23ed41aae9a604e825aa841f
JimmFly <yangjinfei001@gmail.com>
2023-05-15 23:41:10 +0800

    chore: remove quick search tips (#2375)

--- apps/web/src/components/blocksuite/workspace-header/index.tsx
+++ apps/web/src/components/blocksuite/workspace-header/index.tsx
@@ -1,20 +1,15 @@
-import type { PopperProps } from '@affine/component';
-import { QuickSearchTips } from '@affine/component';
-import { getEnvironment } from '@affine/env';
-import { ArrowDownSmallIcon } from '@blocksuite/icons';
 import { assertExists } from '@blocksuite/store';
 import { useBlockSuitePageMeta } from '@toeverything/hooks/use-block-suite-page-meta';
-import { useAtom, useSetAtom } from 'jotai';
+import { useSetAtom } from 'jotai';
 import type {
   FC,
   HTMLAttributes,
   PropsWithChildren,
   ReactElement,
 } from 'react';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 import { openQuickSearchModalAtom } from '../../../atoms';
-import { guideQuickSearchTipsAtom } from '../../../atoms/guide';
 import { QuickSearchButton } from '../../pure/quick-search-button';
 import { EditorModeSwitch } from './editor-mode-switch';
 import type { BaseHeaderProps } from './header';
@@ -22,11 +17,6 @@ import { Header } from './header';
 import * as styles from './styles.css';
 
 export type WorkspaceHeaderProps = BaseHeaderProps;
-
-const isMac = () => {
-  const env = getEnvironment();
-  return env.isBrowser && env.isMacOs;
-};
 
 export const WorkspaceHeader: FC<
   PropsWithChildren<WorkspaceHeaderProps> & HTMLAttributes<HTMLDivElement>
@@ -40,56 +30,6 @@ export const WorkspaceHeader: FC<
   const headerRef = useRef<HTMLDivElement>(null);
   assertExists(pageMeta);
   const title = pageMeta.title;
-
-  const popperRef: PopperProps['popperRef'] = useRef(null);
-
-  const [showQuickSearchTips, setShowQuickSearchTips] = useAtom(
-    guideQuickSearchTipsAtom
-  );
-
-  useEffect(() => {
-    if (!headerRef.current) {
-      return;
-    }
-    const resizeObserver = new ResizeObserver(() => {
-      if (showQuickSearchTips || !popperRef.current) {
-        return;
-      }
-      popperRef.current.update();
-    });
-    resizeObserver.observe(headerRef.current);
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, [showQuickSearchTips]);
-
-  const TipsContent = (
-    <div className={styles.quickSearchTipContent}>
-      <div>
-        Click button
-        {
-          <span
-            style={{
-              fontSize: '24px',
-              verticalAlign: 'middle',
-            }}
-          >
-            <ArrowDownSmallIcon />
-          </span>
-        }
-        or use
-        {isMac() ? ' ⌘ + K' : ' Ctrl + K'} to activate Quick Search. Then you
-        can search keywords or quickly open recently viewed pages.
-      </div>
-      <div
-        className={styles.quickSearchTipButton}
-        data-testid="quick-search-got-it"
-        onClick={() => setShowQuickSearchTips(false)}
-      >
-        Got it
-      </div>
-    </div>
-  );
 
   return (
     <Header ref={headerRef} {...props}>
@@ -107,22 +47,14 @@ export const WorkspaceHeader: FC<
               />
             </div>
             <div className={styles.title}>{title || 'Untitled'}</div>
-            <QuickSearchTips
-              data-testid="quick-search-tips"
-              content={TipsContent}
-              placement="bottom"
-              popperRef={popperRef}
-              open={showQuickSearchTips}
-              offset={[0, -5]}
-            >
-              <div className={styles.searchArrowWrapper}>
-                <QuickSearchButton
-                  onClick={() => {
-                    setOpenQuickSearch(true);
-                  }}
-                />
-              </div>
-            </QuickSearchTips>
+
+            <div className={styles.searchArrowWrapper}>
+              <QuickSearchButton
+                onClick={() => {
+                  setOpenQuickSearch(true);
+                }}
+              />
+            </div>
           </div>
         </div>
       )}
