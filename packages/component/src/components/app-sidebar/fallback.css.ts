d5c3d1b86ac97ea296a769aaf051c21468d627e6
Himself65 <himself65@outlook.com>
2023-05-17 15:49:55 -0700

    fix: sidebar fallback ui position (#2424)

--- packages/component/src/components/app-sidebar/fallback.css.ts
+++ packages/component/src/components/app-sidebar/fallback.css.ts
@@ -1,14 +1,15 @@
 import { style } from '@vanilla-extract/css';
 
 export const fallbackStyle = style({
-  margin: '0 2px',
+  margin: '12px 16px',
   height: '100%',
 });
 
 export const fallbackHeaderStyle = style({
-  height: '58px',
+  height: '56px',
   width: '100%',
   display: 'flex',
+  alignItems: 'center',
   flexDirection: 'row',
   gap: '8px',
 });
