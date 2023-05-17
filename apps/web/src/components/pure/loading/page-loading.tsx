0b771c610c9882d7d3a7fab0cd9c204fd682aaf7
himself65 <himself65@outlook.com>
2023-05-16 21:41:18 -0700

    fix: hydration error

--- apps/web/src/components/pure/loading/page-loading.tsx
+++ apps/web/src/components/pure/loading/page-loading.tsx
@@ -1,4 +1,3 @@
-import { styled } from '@affine/component';
 import { AffineLoading } from '@affine/component/affine-loading';
 import { memo, Suspense } from 'react';
 
@@ -17,34 +16,9 @@ export const Loading = memo(function Loading() {
   );
 });
 
-// Used for the full page loading
-const StyledLoadingContainer = styled('div')(() => {
-  return {
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: '#6880FF',
-    flexDirection: 'column',
-    h1: {
-      fontSize: '2em',
-      marginTop: '15px',
-      fontWeight: '600',
-    },
-  };
-});
-
+/**
+ * @deprecated use skeleton instead
+ */
 export const PageLoading = () => {
-  // We disable the loading on desktop, because want it looks faster.
-  //  This is a design requirement.
-  if (environment.isDesktop) {
-    return null;
-  }
-  return (
-    <StyledLoadingContainer>
-      <Loading />
-    </StyledLoadingContainer>
-  );
+  return null;
 };
-
-export default PageLoading;
