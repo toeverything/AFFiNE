75c54f0af5c2160f2010b076d684354a16ab232d
Peng Xiao <pengxiao@outlook.com>
2023-05-18 13:18:40 +0800

    feat: fav page references (#2422)
    Co-authored-by: Himself65 <himself65@outlook.com>

--- apps/web/src/components/pure/workspace-slider-bar/favorite/favorite-list.tsx
+++ apps/web/src/components/pure/workspace-slider-bar/favorite/favorite-list.tsx
@@ -1,40 +1,103 @@
 import { MenuLinkItem } from '@affine/component/app-sidebar';
 import { EdgelessIcon, PageIcon } from '@blocksuite/icons';
+import type { PageMeta, Workspace } from '@blocksuite/store';
 import { useBlockSuitePageMeta } from '@toeverything/hooks/use-block-suite-page-meta';
+import { useBlockSuitePageReferences } from '@toeverything/hooks/use-block-suite-page-references';
 import { useAtomValue } from 'jotai';
 import { useRouter } from 'next/router';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
 import { workspacePreferredModeAtom } from '../../../../atoms';
 import type { FavoriteListProps } from '../index';
 import EmptyItem from './empty-item';
-export const FavoriteList = ({ currentWorkspace }: FavoriteListProps) => {
+
+interface FavoriteMenuItemProps {
+  workspace: Workspace;
+  pageId: string;
+  metaMapping: Record<string, PageMeta>;
+  parentIds: Set<string>;
+}
+
+function FavoriteMenuItem({
+  workspace,
+  pageId,
+  metaMapping,
+  parentIds,
+}: FavoriteMenuItemProps) {
   const router = useRouter();
   const record = useAtomValue(workspacePreferredModeAtom);
-  const pageMeta = useBlockSuitePageMeta(currentWorkspace.blockSuiteWorkspace);
-  const workspaceId = currentWorkspace.id;
+  const active = router.query.pageId === pageId;
+  const icon = record[pageId] === 'edgeless' ? <EdgelessIcon /> : <PageIcon />;
+  const references = useBlockSuitePageReferences(workspace, pageId);
+  const referencesToShow = useMemo(() => {
+    return [...new Set(references.filter(ref => !parentIds.has(ref)))];
+  }, [references, parentIds]);
+  const [collapsed, setCollapsed] = useState(true);
+  const collapsible = referencesToShow.length > 0 && parentIds.size === 0;
+  const showReferences = collapsible ? !collapsed : referencesToShow.length > 0;
+  const nestedItem = parentIds.size > 0;
+  return (
+    <>
+      <MenuLinkItem
+        style={{
+          marginLeft: nestedItem ? '12px' : undefined,
+          width: nestedItem ? 'calc(100% - 12px)' : undefined,
+        }}
+        data-type="favorite-list-item"
+        data-testid={`favorite-list-item-${pageId}`}
+        active={active}
+        href={`/workspace/${workspace.id}/${pageId}`}
+        icon={icon}
+        collapsed={collapsible ? collapsed : undefined}
+        onCollapsedChange={setCollapsed}
+      >
+        <span>{metaMapping[pageId]?.title || 'Untitled'}</span>
+      </MenuLinkItem>
+      {showReferences &&
+        referencesToShow.map(ref => {
+          return (
+            <FavoriteMenuItem
+              key={ref}
+              workspace={workspace}
+              pageId={ref}
+              metaMapping={metaMapping}
+              parentIds={new Set([...parentIds, pageId])}
+            />
+          );
+        })}
+    </>
+  );
+}
+
+export const FavoriteList = ({ currentWorkspace }: FavoriteListProps) => {
+  const metas = useBlockSuitePageMeta(currentWorkspace.blockSuiteWorkspace);
 
   const favoriteList = useMemo(
-    () => pageMeta.filter(p => p.favorite && !p.trash),
-    [pageMeta]
+    () => metas.filter(p => p.favorite && !p.trash),
+    [metas]
+  );
+
+  const metaMapping = useMemo(
+    () =>
+      metas.reduce((acc, meta) => {
+        acc[meta.id] = meta;
+        return acc;
+      }, {} as Record<string, PageMeta>),
+    [metas]
   );
 
   return (
     <>
       {favoriteList.map((pageMeta, index) => {
-        const active = router.query.pageId === pageMeta.id;
-        const icon =
-          record[pageMeta.id] === 'edgeless' ? <EdgelessIcon /> : <PageIcon />;
         return (
-          <MenuLinkItem
+          <FavoriteMenuItem
             key={`${pageMeta}-${index}`}
-            data-testid={`favorite-list-item-${pageMeta.id}`}
-            active={active}
-            href={`/workspace/${workspaceId}/${pageMeta.id}`}
-            icon={icon}
-          >
-            <span>{pageMeta.title || 'Untitled'}</span>
-          </MenuLinkItem>
+            metaMapping={metaMapping}
+            pageId={pageMeta.id}
+            // memo?
+            parentIds={new Set()}
+            workspace={currentWorkspace.blockSuiteWorkspace}
+          />
         );
       })}
       {favoriteList.length === 0 && <EmptyItem />}
--- packages/component/src/components/app-sidebar/menu-item/index.css.ts
+++ packages/component/src/components/app-sidebar/menu-item/index.css.ts
@@ -8,7 +8,7 @@ export const root = style({
   minHeight: '30px',
   userSelect: 'none',
   cursor: 'pointer',
-  padding: '0 12px',
+  padding: '0 8px 0 12px',
   fontSize: 'var(--affine-font-sm)',
   selectors: {
     '&:hover': {
@@ -27,6 +27,11 @@ export const root = style({
         // make this a variable?
         'linear-gradient(0deg, rgba(0, 0, 0, 0.04), rgba(0, 0, 0, 0.04)), rgba(0, 0, 0, 0.04);',
     },
+    '&[data-collapsible="true"]': {
+      width: 'calc(100% + 8px)',
+      transform: 'translateX(-8px)',
+      paddingLeft: '8px',
+    },
   },
 });
 
@@ -37,9 +42,47 @@ export const content = style({
 });
 
 export const icon = style({
-  marginRight: '14px',
   color: 'var(--affine-icon-color)',
   fontSize: '20px',
+});
+
+export const collapsedIconContainer = style({
+  width: '12px',
+  height: '12px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: '2px',
+  transition: 'transform 0.2s',
+  selectors: {
+    '&[data-collapsed="true"]': {
+      transform: 'rotate(-90deg)',
+    },
+    '&:hover': {
+      background: 'var(--affine-hover-color)',
+    },
+  },
+});
+
+export const iconsContainer = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  width: '28px',
+  selectors: {
+    '&[data-collapsible="true"]': {
+      width: '40px',
+    },
+  },
+});
+
+export const collapsedIcon = style({
+  transition: 'transform 0.2s ease-in-out',
+  selectors: {
+    '&[data-collapsed="true"]': {
+      transform: 'rotate(-90deg)',
+    },
+  },
 });
 
 export const spacer = style({
