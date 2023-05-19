ef2d135e9bdde7535a252934f618b24551531074
Peng Xiao <pengxiao@outlook.com>
2023-05-19 15:07:07 +0800

    fix: optimize app updater (#2452)

--- apps/web/src/components/pure/workspace-slider-bar/favorite/styles.css.ts
+++ apps/web/src/components/pure/workspace-slider-bar/favorite/styles.css.ts
@@ -1,4 +1,4 @@
-import { style } from '@vanilla-extract/css';
+import { keyframes, style } from '@vanilla-extract/css';
 
 export const label = style({
   selectors: {
@@ -16,6 +16,36 @@ export const favItemWrapper = style({
     '&[data-nested="true"]': {
       marginLeft: '12px',
       width: 'calc(100% - 12px)',
+    },
+  },
+});
+
+const slideDown = keyframes({
+  '0%': {
+    height: '0px',
+  },
+  '100%': {
+    height: 'var(--radix-collapsible-content-height)',
+  },
+});
+
+const slideUp = keyframes({
+  '0%': {
+    height: 'var(--radix-collapsible-content-height)',
+  },
+  '100%': {
+    height: '0px',
+  },
+});
+
+export const collapsibleContent = style({
+  overflow: 'hidden',
+  selectors: {
+    '&[data-state="open"]': {
+      animation: `${slideDown} 0.2s ease-out`,
+    },
+    '&[data-state="closed"]': {
+      animation: `${slideUp} 0.2s ease-out`,
     },
   },
 });
