2e23a4830bf5bf338a9a710b98afccb0ce16dac7
LongYinan <lynweklm@gmail.com>
2023-05-22 12:53:55 +0800

    ci: add circular import detect (#2475)
    Co-authored-by: himself65 <himself65@outlook.com>

--- packages/component/src/components/page-list/all-pages-body.tsx
+++ packages/component/src/components/page-list/all-pages-body.tsx
@@ -1,12 +1,12 @@
 import { TableBody, TableCell } from '@affine/component';
-import { OperationCell } from '@affine/component/page-list';
 import { useAFFiNEI18N } from '@affine/i18n/hooks';
 import { useMediaQuery, useTheme } from '@mui/material';
 
-import type { ListData } from './all-page';
 import { FavoriteTag } from './components/favorite-tag';
 import { TitleCell } from './components/title-cell';
+import { OperationCell } from './operation-cell';
 import { StyledTableRow } from './styles';
+import type { ListData } from './type';
 
 export const AllPagesBody = ({
   isPublicWorkspace,
