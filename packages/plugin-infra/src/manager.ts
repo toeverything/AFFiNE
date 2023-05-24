a1ce729df0e3422ae1edc4f3c5b961901b020828
himself65 <himself65@outlook.com>
2023-05-24 23:29:45 +0800

    fix: code

--- packages/plugin-infra/src/manager.ts
+++ packages/plugin-infra/src/manager.ts
@@ -3,28 +3,17 @@ import { rootStore } from '@affine/workspace/atom';
 import { atom } from 'jotai';
 
 import type { AffinePlugin, Definition } from './type';
-import type { AffinePluginContext } from './type';
 import type { Loader, PluginUIAdapter } from './type';
 
 // todo: for now every plugin is enabled by default
 export const affinePluginsAtom = atom<Record<string, AffinePlugin<string>>>({});
-export const affineRightItemsAtom = atom<HTMLElement[]>([]);
 
 const pluginLogger = new DebugLogger('affine:plugin');
-
-function createPluginContext(): AffinePluginContext {
-  return {
-    toast: _text => {
-      // todo
-    },
-  };
-}
 
 export function definePlugin<ID extends string>(
   definition: Definition<ID>,
   uiAdapterLoader?: Loader<Partial<PluginUIAdapter>>
 ) {
-  const _context = createPluginContext();
   const basePlugin = {
     definition,
     uiAdapter: {},
@@ -56,13 +45,3 @@ export function definePlugin<ID extends string>(
     }
   }
 }
-
-// rootStore.sub(affinePluginsAtom, () => {
-//   const plugins = Object.values(rootStore.get(affinePluginsAtom))
-//   const items = plugins.filter(plugin => plugin.uiAdapter.headerItem != null).map(plugin => {
-//     const headerItem = plugin.uiAdapter
-//       .headerItem as PluginUIAdapter['headerItem'];
-//     return headerItem()
-//   });
-//   rootStore.set(affineRightItemsAtom, [])
-// })
