7dcbe64d4edd4a5c45665c579fb967b2187aeec2
Whitewater <me@waterwater.moe>
2023-05-25 22:23:51 -0700

    feat: group all page by date (#2532)
    Co-authored-by: Himself65 <himself65@outlook.com>

--- packages/component/src/components/page-list/utils.tsx
+++ packages/component/src/components/page-list/utils.tsx
@@ -1,11 +1,51 @@
-const isToday = (date: Date) => {
+export function isToday(date: Date): boolean {
   const today = new Date();
   return (
     date.getDate() == today.getDate() &&
     date.getMonth() == today.getMonth() &&
     date.getFullYear() == today.getFullYear()
   );
-};
+}
+
+export function isYesterday(date: Date): boolean {
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  return (
+    date.getFullYear() === yesterday.getFullYear() &&
+    date.getMonth() === yesterday.getMonth() &&
+    date.getDate() === yesterday.getDate()
+  );
+}
+
+export function isLastWeek(date: Date): boolean {
+  const today = new Date();
+  const lastWeek = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() - 7
+  );
+  return date >= lastWeek && date < today;
+}
+
+export function isLastMonth(date: Date): boolean {
+  const today = new Date();
+  const lastMonth = new Date(
+    today.getFullYear(),
+    today.getMonth() - 1,
+    today.getDate()
+  );
+  return date >= lastMonth && date < today;
+}
+
+export function isLastYear(date: Date): boolean {
+  const today = new Date();
+  const lastYear = new Date(
+    today.getFullYear() - 1,
+    today.getMonth(),
+    today.getDate()
+  );
+  return date >= lastYear && date < today;
+}
 
 export const formatDate = (date: Date): string => {
   // yyyy-mm-dd MM-DD HH:mm
