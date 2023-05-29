cec93ab9ebf2cced41e59983eb6c2c2ae11cc5a2
himself65 <himself65@outlook.com>
2023-05-29 23:47:21 +0800

    feat: add test

--- packages/component/src/components/page-list/__tests__/use-all-page-setting.spec.ts
+++ packages/component/src/components/page-list/__tests__/use-all-page-setting.spec.ts
@@ -4,11 +4,12 @@
 import 'fake-indexeddb/auto';
 
 import { renderHook } from '@testing-library/react';
+import { RESET } from 'jotai/utils';
 import { expect, test } from 'vitest';
 
 import { useAllPageSetting } from '../use-all-page-setting';
 
-test('useAllPageSetting', () => {
+test('useAllPageSetting', async () => {
   const settingHook = renderHook(() => useAllPageSetting());
   const prevView = settingHook.result.current.currentView;
   expect(settingHook.result.current.savedViews).toEqual([]);
@@ -40,4 +41,12 @@ test('useAllPageSetting', () => {
       args: [],
     },
   ]);
+  settingHook.result.current.setCurrentView(RESET);
+  await settingHook.result.current.createView({
+    ...settingHook.result.current.currentView,
+    id: '1',
+  });
+  settingHook.rerender();
+  expect(settingHook.result.current.savedViews.length).toBe(1);
+  expect(settingHook.result.current.savedViews[0].id).toBe('1');
 });
