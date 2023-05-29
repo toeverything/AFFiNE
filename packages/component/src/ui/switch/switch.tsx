f595aef81bc47eaca005ce713053436d5f4c5352
JimmFly <yangjinfei001@gmail.com>
2023-05-29 15:33:36 +0800

    chore: migrate to vanilla-extract

--- packages/component/src/ui/switch/switch.tsx
+++ packages/component/src/ui/switch/switch.tsx
@@ -1,57 +1,8 @@
 // components/switch.tsx
+import clsx from 'clsx';
 import { useState } from 'react';
 
-import { styled } from '../../styles';
-
-const StyledLabel = styled('label')(() => {
-  return {
-    display: 'flex',
-    alignItems: 'center',
-    gap: '10px',
-    cursor: 'pointer',
-  };
-});
-const StyledInput = styled('input')(() => {
-  return {
-    opacity: 0,
-    position: 'absolute',
-
-    '&:checked': {
-      '& + span': {
-        background: 'var(--affine-primary-color)',
-        '&:before': {
-          background: 'var(--affine-toggle-circle-background-color)',
-          transform: 'translate(100%,-50%)',
-        },
-      },
-    },
-  };
-});
-const StyledSwitch = styled('span')(() => {
-  return {
-    position: 'relative',
-    width: '46px',
-    height: '26px',
-    background: 'var(--affine-toggle-disable-background-color)',
-    borderRadius: '37px',
-    transition: '300ms all',
-    border: '1px solid var(--affine-black-10)',
-    boxShadow: 'var(--affine-toggle-circle-shadow)',
-    '&:before': {
-      transition: 'all .2s cubic-bezier(0.27, 0.2, 0.25, 1.51)',
-      content: '""',
-      position: 'absolute',
-      width: '20px',
-      height: '20px',
-      borderRadius: '50%',
-      top: '50%',
-      border: '1px solid var(--affine-black-10)',
-      background: 'var(--affine-white)',
-      transform: 'translate(1px, -50%)',
-      boxShadow: 'var(--affine-toggle-circle-shadow)',
-    },
-  };
-});
+import * as styles from './index.css';
 
 type SwitchProps = {
   checked?: boolean;
@@ -70,14 +21,19 @@ export const Switch = (props: SwitchProps) => {
   };
 
   return (
-    <StyledLabel>
+    <label className={clsx(styles.labelStyle)}>
       {children}
-      <StyledInput
+      <input
+        className={clsx(styles.inputStyle)}
         type="checkbox"
         checked={isChecked}
         onChange={handleChange}
       />
-      <StyledSwitch />
-    </StyledLabel>
+      <span
+        className={clsx(styles.switchStyle, {
+          [styles.switchCheckedStyle]: isChecked,
+        })}
+      />
+    </label>
   );
 };
