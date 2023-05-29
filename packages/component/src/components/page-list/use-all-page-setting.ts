a0afed093129143a649a3889d7e2fbb5e9f6726f
himself65 <himself65@outlook.com>
2023-05-29 17:06:17 +0800

    feat: save page view setting

--- packages/component/src/components/page-list/use-all-page-setting.ts
+++ packages/component/src/components/page-list/use-all-page-setting.ts
@@ -1,61 +1,61 @@
-import { useState } from 'react';
+import type { DBSchema} from 'idb';
+import {openDB } from 'idb'
+import { useCallback, useState } from "react";
+import useSWR from "swr";
+import { NIL }from 'uuid'
 
 import { evalFilterList } from './filter';
 import type { Filter, VariableMap } from './filter/vars';
 
 export type View = {
+  id: string;
   name: string;
   filterList: Filter[];
 };
-export type AllPageSetting = {
-  mainView: View;
-  currentView?: number;
-  savedViews: View[];
-};
-export const useAllPageSetting = () => {
-  const [setting, changeSetting] = useState<AllPageSetting>({
-    mainView: {
-      name: 'default',
-      filterList: [],
-    },
-    savedViews: [],
-  });
 
-  const changeView = (view: View, i?: number) =>
-    changeSetting(setting => {
-      if (i != null) {
-        return {
-          ...setting,
-          savedViews: setting.savedViews.map((v, index) =>
-            i === index ? view : v
-          ),
-        };
-      } else {
-        return {
-          ...setting,
-          mainView: view,
-        };
-      }
-    });
-  const createView = (view: View) =>
-    changeSetting(setting => ({
-      ...setting,
-      currentView: setting.savedViews.length,
-      savedViews: [...setting.savedViews, view],
-    }));
-  const selectView = (i?: number) =>
-    changeSetting(setting => ({ ...setting, currentView: i }));
-  const currentView =
-    setting.currentView != null
-      ? setting.savedViews[setting.currentView]
-      : setting.mainView;
+export interface PageViewDBV1 extends DBSchema {
+  view: {
+    key: View['id']
+    value: View
+  }
+}
+
+const pageViewDBPromise = openDB<PageViewDBV1>('page-view', 1, {
+  upgrade (database) {
+    database.createObjectStore('view', {
+      keyPath: 'id'
+    })
+  }
+})
+
+export const useAllPageSetting = () => {
+  const { data: savedViews, mutate } = useSWR(['affine', 'page-view'], {
+    fetcher: async () => {
+      const db = await pageViewDBPromise
+      const t = db.transaction('view').objectStore('view')
+      return t.getAll()
+    },
+    suspense: true,
+    fallbackData: []
+  })
+
+  const [currentView, setCurrentView] = useState<View>(() => ({
+    name: 'default',
+    id: NIL,
+    filterList: [],
+  }));
+
+  const createView = (view: View) => useCallback(async () => {
+    view
+  }, [])
+
   return {
     currentView,
-    currentViewIndex: setting.currentView,
-    viewList: setting.savedViews,
-    selectView,
+    savedViews,
+
+    // actions
     createView,
-    changeView,
+    setCurrentView,
   };
 };
 export const filterByView = (view: View, varMap: VariableMap) =>
