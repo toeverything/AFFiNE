3eef11416ae4d19e1ddd78ad4f337206deb3d8c7
Himself65 <himself65@outlook.com>
2023-05-31 14:54:59 +0800

    refactor: remove deprecated atoms (#2615)

--- apps/web/src/hooks/current/use-current-mode.ts
+++ apps/web/src/hooks/current/use-current-mode.ts
@@ -1,9 +1,10 @@
+import { rootCurrentPageIdAtom } from '@affine/workspace/atom';
 import { atom, useAtomValue } from 'jotai';
 
-import { currentPageIdAtom, workspacePreferredModeAtom } from '../../atoms';
+import { workspacePreferredModeAtom } from '../../atoms';
 
 const currentModeAtom = atom<'page' | 'edgeless'>(get => {
-  const pageId = get(currentPageIdAtom);
+  const pageId = get(rootCurrentPageIdAtom);
   const record = get(workspacePreferredModeAtom);
   if (pageId) return record[pageId] ?? 'page';
   else {
