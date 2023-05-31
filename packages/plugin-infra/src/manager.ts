454f1887cf601aaca63bb8227887aff0fe748e8d
Himself65 <himself65@outlook.com>
2023-05-31 17:08:03 +0800

    feat: add `@affine/bookmark-block` plugin (#2618)

--- packages/plugin-infra/src/manager.ts
+++ packages/plugin-infra/src/manager.ts
@@ -4,27 +4,53 @@ import { atom } from 'jotai';
 
 import type { AffinePlugin, Definition } from './type';
 import type { Loader, PluginUIAdapter } from './type';
+import type { PluginBlockSuiteAdapter } from './type';
 
 // todo: for now every plugin is enabled by default
 export const affinePluginsAtom = atom<Record<string, AffinePlugin<string>>>({});
 
 const pluginLogger = new DebugLogger('affine:plugin');
-import { config } from '@affine/env';
+
 export function definePlugin<ID extends string>(
   definition: Definition<ID>,
-  uiAdapterLoader?: Loader<Partial<PluginUIAdapter>>
+  uiAdapterLoader?: Loader<Partial<PluginUIAdapter>>,
+  blockSuiteAdapter?: Loader<Partial<PluginBlockSuiteAdapter>>
 ) {
-  if (!config.enablePlugin) {
-    return;
-  }
   const basePlugin = {
     definition,
     uiAdapter: {},
+    blockSuiteAdapter: {},
   };
+
   rootStore.set(affinePluginsAtom, plugins => ({
     ...plugins,
     [definition.id]: basePlugin,
   }));
+
+  if (blockSuiteAdapter) {
+    const updateAdapter = (adapter: Partial<PluginBlockSuiteAdapter>) => {
+      rootStore.set(affinePluginsAtom, plugins => ({
+        ...plugins,
+        [definition.id]: {
+          ...basePlugin,
+          blockSuiteAdapter: adapter,
+        },
+      }));
+    };
+
+    blockSuiteAdapter
+      .load()
+      .then(({ default: adapter }) => updateAdapter(adapter));
+
+    if (import.meta.webpackHot) {
+      blockSuiteAdapter.hotModuleReload(async _ => {
+        const adapter = (await _).default;
+        updateAdapter(adapter);
+        pluginLogger.info('[HMR] Plugin', definition.id, 'hot reloaded.');
+      });
+    }
+  }
+
   if (uiAdapterLoader) {
     const updateAdapter = (adapter: Partial<PluginUIAdapter>) => {
       rootStore.set(affinePluginsAtom, plugins => ({
@@ -39,6 +65,7 @@ export function definePlugin<ID extends string>(
     uiAdapterLoader
       .load()
       .then(({ default: adapter }) => updateAdapter(adapter));
+
     if (import.meta.webpackHot) {
       uiAdapterLoader.hotModuleReload(async _ => {
         const adapter = (await _).default;
