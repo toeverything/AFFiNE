2ec8ad5110b6435af37f97d4ec7033279df7995f
himself65 <himself65@outlook.com>
2023-06-02 15:34:10 +0800

    fix: build

--- scripts/setup/build-plugins.ts
+++ scripts/setup/build-plugins.ts
@@ -1,14 +1,14 @@
-import { spawnSync } from 'node:child_process';
 import { resolve } from 'node:path';
 
 import { fileURLToPath } from 'url';
+import { build } from 'vite';
 import { beforeAll } from 'vitest';
 
 export const rootDir = fileURLToPath(new URL('../..', import.meta.url));
 
 beforeAll(async () => {
-  spawnSync('yarn', ['build'], {
-    cwd: resolve(rootDir, './plugins/bookmark-block'),
-    stdio: 'inherit',
-  });
+  const { default: config } = await import(
+    resolve(rootDir, './plugins/bookmark-block/vite.config.ts')
+  );
+  await build(config);
 });
