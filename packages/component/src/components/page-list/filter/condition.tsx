280540c44e19b41d69dfdf9c9b3885706ad54ba3
3720 <zuozijian1994@gmail.com>
2023-06-04 23:34:16 +0800

    test: add some e2e tests for all pages filter (#2674)
    (cherry picked from commit b95808a052570147104c97bbd41ffc854d4e42ef)

--- packages/component/src/components/page-list/filter/condition.tsx
+++ packages/component/src/components/page-list/filter/condition.tsx
@@ -68,7 +68,7 @@ const FunctionSelect = ({
     return filterMatcher.allMatchedData(type);
   }, [value.left.name]);
   return (
-    <div>
+    <div data-testid="filter-name-select">
       {list.map(v => (
         <MenuItem
           onClick={() => {
@@ -101,7 +101,7 @@ export const Arg = ({
     return null;
   }
   return (
-    <div style={{ marginLeft: 4 }}>
+    <div data-testid="filter-arg" style={{ marginLeft: 4 }}>
       {data.render({ type, value, onChange })}
     </div>
   );
