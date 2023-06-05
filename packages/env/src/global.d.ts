bc3ce5ae07f3dfb9675f741158725c69c7a0a390
himself65 <himself65@outlook.com>
2023-06-05 16:24:43 +0800

    fix: type

--- packages/env/src/global.d.ts
+++ packages/env/src/global.d.ts
@@ -1,20 +1,4 @@
 interface Window {
-  affine: {
-    ipcRenderer: {
-      send(channel: string, ...args: any[]): void;
-      invoke(channel: string, ...args: any[]): Promise<any>;
-      on(
-        channel: string,
-        listener: (event: unknown, ...args: any[]) => void
-      ): this;
-      once(
-        channel: string,
-        listener: (event: unknown, ...args: any[]) => void
-      ): this;
-      removeListener(channel: string, listener: (...args: any[]) => void): this;
-    };
-  };
-
   appInfo: {
     electron: bool;
   };
--- packages/hooks/src/use-affine-ipc-renderer.ts
+++ packages/hooks/src/use-affine-ipc-renderer.ts
@@ -1,5 +1,26 @@
-/// <reference types="@affine/env" />
 import { useCallback, useEffect, useRef } from 'react';
+
+declare global {
+  interface IPCRenderer {
+    send(channel: string, ...args: any[]): void;
+    invoke(channel: string, ...args: any[]): Promise<any>;
+    on(
+      channel: string,
+      listener: (event: unknown, ...args: any[]) => void
+    ): this;
+    once(
+      channel: string,
+      listener: (event: unknown, ...args: any[]) => void
+    ): this;
+    removeListener(channel: string, listener: (...args: any[]) => void): this;
+  }
+
+  interface Window {
+    affine: {
+      ipcRenderer: IPCRenderer;
+    };
+  }
+}
 
 /**
  * Unsafe
