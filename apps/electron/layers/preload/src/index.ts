8f6db00402afc97b2ca86f91920aa5ca17a7bf9c
Himself65 <himself65@outlook.com>
2023-06-06 11:43:34 +0800

    feat: init `window.affine` (#2682)

--- apps/electron/layers/preload/src/index.ts
+++ apps/electron/layers/preload/src/index.ts
@@ -1,18 +1,1 @@
-/**
- * @module preload
- */
-
-import { contextBridge } from 'electron';
-
-import * as affineApis from './affine-apis';
-
-/**
- * The "Main World" is the JavaScript context that your main renderer code runs in.
- * By default, the page you load in your renderer executes code in this world.
- *
- * @see https://www.electronjs.org/docs/api/context-bridge
- */
-
-contextBridge.exposeInMainWorld('apis', affineApis.apis);
-contextBridge.exposeInMainWorld('events', affineApis.events);
-contextBridge.exposeInMainWorld('appInfo', affineApis.appInfo);
+import './bootstrap';
