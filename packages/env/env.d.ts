aa9e4db4c2695361c292c4fb02143d6513c4585a
himself65 <himself65@outlook.com>
2023-06-06 11:17:22 +0800

    fix: type

--- packages/env/env.d.ts
+++ packages/env/env.d.ts
@@ -1,4 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import '../hooks/src/use-block-suite-page-meta.ts';
-// eslint-disable-next-line @typescript-eslint/no-restricted-imports
-import './src/global.d.ts';
