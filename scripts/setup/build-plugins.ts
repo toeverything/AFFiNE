f857b3a7108a47a476435237dc1aefc46e71a93d
himself65 <himself65@outlook.com>
2023-06-07 14:03:22 +0800

    fix: build

--- scripts/setup/build-plugins.ts
+++ scripts/setup/build-plugins.ts
@@ -1,6 +1,16 @@
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { build } from 'vite';
 import { beforeAll } from 'vitest';
 
+const rootDir = fileURLToPath(new URL('../..', import.meta.url));
+
 beforeAll(async () => {
+  console.log('Build plugin infra');
+  await build({
+    configFile: resolve(rootDir, './packages/plugin-infra/vite.config.ts'),
+  });
   console.log('Build plugins');
-  await import('../esbuild/build-plugins.mjs');
+  await import('../../apps/electron/scripts/plugins/build-plugins.mjs');
 });
