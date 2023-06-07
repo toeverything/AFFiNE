10e6fea3326c4d7ce926821e164c4eb57f26175f
himself65 <himself65@outlook.com>
2023-06-07 15:35:44 +0800

    fix: code

--- apps/electron/scripts/setup/build-layers.ts
+++ apps/electron/scripts/setup/build-layers.ts
@@ -1,7 +1,13 @@
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import * as esbuild from 'esbuild';
 import { beforeAll } from 'vitest';
 
 import { config } from '../common.mjs';
+
+const rootDir = fileURLToPath(new URL('../..', import.meta.url));
+const pluginOutputDir = resolve(rootDir, './dist/plugins');
 
 beforeAll(async () => {
   const common = config();
@@ -14,6 +20,7 @@ beforeAll(async () => {
     ...common.main,
     define: {
       ...common.main.define,
+      'process.env.PLUGIN_DIR': JSON.stringify(pluginOutputDir),
       'process.env.NODE_ENV': `"development"`,
       'process.env.BUILD_TYPE': `"canary"`,
     },
