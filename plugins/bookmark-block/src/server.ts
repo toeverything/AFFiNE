3872df4153afee2e4886e9407d03710281189ae3
QiShaoXuan <qishaoxuan777@gmail.com>
2023-06-07 16:14:40 +0800

    feat: test link-preview

--- plugins/bookmark-block/src/server.ts
+++ plugins/bookmark-block/src/server.ts
@@ -1,7 +1,21 @@
+import { getLinkPreview } from 'link-preview-js';
+
 import { getMetaData } from './server/get-meta-data';
 
 export default {
   getBookmarkDataByLink: async (_: unknown, url: string) => {
+    const data1 = await getLinkPreview(
+      'https://twitter.com/HimeWorks/status/1299973665916936192',
+      {
+        timeout: 6000,
+        headers: {
+          'user-agent': 'googlebot',
+        },
+        followRedirects: 'follow',
+      }
+    );
+    console.log('data1', data1);
+
     return getMetaData(url, {
       shouldReGetHTML: metaData => {
         return !metaData.title && !metaData.description;
