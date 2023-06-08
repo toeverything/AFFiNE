336ff5220ffc03b1569113dd1cd4a797420560b2
Qi <474021214@qq.com>
2023-06-08 17:51:45 +0800

    feat: replace electron to puppeteer (#2700)
    Co-authored-by: himself65 <himself65@outlook.com>
    (cherry picked from commit fda89b05e7b48b476d53dbcaea95f1868658e82a)

--- plugins/bookmark-block/src/server.ts
+++ plugins/bookmark-block/src/server.ts
@@ -1,11 +1,56 @@
-import { getMetaData } from './server/get-meta-data';
+import { getLinkPreview } from 'link-preview-js';
+
+type MetaData = {
+  title?: string;
+  description?: string;
+  icon?: string;
+  image?: string;
+  [x: string]: string | string[] | undefined;
+};
+
+export interface PreviewType {
+  url: string;
+  title: string;
+  siteName: string | undefined;
+  description: string | undefined;
+  mediaType: string;
+  contentType: string | undefined;
+  images: string[];
+  videos: {
+    url: string | undefined;
+    secureUrl: string | null | undefined;
+    type: string | null | undefined;
+    width: string | undefined;
+    height: string | undefined;
+  }[];
+  favicons: string[];
+}
 
 export default {
-  getBookmarkDataByLink: async (_: unknown, url: string) => {
-    return getMetaData(url, {
-      shouldReGetHTML: metaData => {
-        return !metaData.title && !metaData.description;
+  getBookmarkDataByLink: async (_: unknown, url: string): Promise<MetaData> => {
+    const previewData = (await getLinkPreview(url, {
+      timeout: 6000,
+      headers: {
+        'user-agent': 'googlebot',
       },
-    });
+      followRedirects: 'follow',
+    }).catch(() => {
+      return {
+        title: '',
+        siteName: '',
+        description: '',
+        images: [],
+        videos: [],
+        contentType: `text/html`,
+        favicons: [],
+      };
+    })) as PreviewType;
+
+    return {
+      title: previewData.title,
+      description: previewData.description,
+      icon: previewData.favicons[0],
+      image: previewData.images[0],
+    };
   },
 };
