d6145e2607c7f7e79088e492a928dce8856c7f58
himself65 <himself65@outlook.com>
2023-06-08 16:25:15 +0800

    fix: code

--- apps/web/src/hooks/current/use-current-mode.ts
+++ apps/web/src/hooks/current/use-current-mode.ts
@@ -1,12 +1,14 @@
 import { rootCurrentPageIdAtom } from '@affine/workspace/atom';
-import { assertExists } from '@blocksuite/store';
 import { atom, useAtomValue } from 'jotai';
 
 import { pageSettingFamily } from '../../atoms';
 
 const currentModeAtom = atom<'page' | 'edgeless'>(get => {
   const pageId = get(rootCurrentPageIdAtom);
-  assertExists(pageId);
+  // fixme(himself65): pageId should not be null
+  if (!pageId) {
+    return 'page';
+  }
   return get(pageSettingFamily(pageId))?.mode ?? 'page';
 });
 
