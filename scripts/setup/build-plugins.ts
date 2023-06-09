3d2909e0e4ceabaa6e0031769637a24242d6399f
LongYinan <lynweklm@gmail.com>
2023-06-09 11:23:07 +0800

    Fix script import in vitest

--- scripts/setup/build-plugins.ts
+++ scripts/setup/build-plugins.ts
@@ -3,5 +3,5 @@ import { beforeAll } from 'vitest';
 beforeAll(async () => {
   console.log('Build plugins');
   // @ts-expect-error
-  await import('@affine/electron/scripts/plugins/build-plugins.mjs');
+  await import('../../apps/electron/scripts/plugins/build-plugins.mjs');
 });
