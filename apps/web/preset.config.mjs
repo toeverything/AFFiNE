afe16a306cbee61e358a222f139923b1e0fad244
himself65 <himself65@outlook.com>
2023-06-10 00:06:44 +0800

    fix: build

--- apps/web/preset.config.mjs
+++ apps/web/preset.config.mjs
@@ -11,38 +11,39 @@ export const blockSuiteFeatureFlags = {
   enable_drag_handle: true,
   enable_surface: true,
   enable_linked_page: true,
-  enable_bookmark_operation: process.env.ENABLE_BOOKMARK_OPERATION === 'true',
+  enable_bookmark_operation:
+    process.env.NX_ENABLE_BOOKMARK_OPERATION === 'true',
 };
 
 /**
  * @type {import('@affine/env').BuildFlags}
  */
 export const buildFlags = {
-  enablePlugin: process.env.ENABLE_PLUGIN === 'true',
-  enableAllPageFilter:
-    !!process.env.VERCEL ||
-    (process.env.ENABLE_ALL_PAGE_FILTER
-      ? process.env.ENABLE_ALL_PAGE_FILTER === 'true'
-      : false),
-  enableImagePreviewModal: process.env.ENABLE_IMAGE_PREVIEW_MODAL
-    ? process.env.ENABLE_IMAGE_PREVIEW_MODAL === 'true'
+  enablePlugin: process.env.NX_ENABLE_PLUGIN === 'true',
+  enableAllPageFilter: process.env.NX_ENABLE_ALL_PAGE_FILTER
+    ? process.env.NX_ENABLE_ALL_PAGE_FILTER === 'true'
+    : false,
+  enableImagePreviewModal: process.env.NX_ENABLE_IMAGE_PREVIEW_MODAL
+    ? process.env.NX_ENABLE_IMAGE_PREVIEW_MODAL === 'true'
     : true,
-  enableTestProperties: process.env.ENABLE_TEST_PROPERTIES
-    ? process.env.ENABLE_TEST_PROPERTIES === 'true'
+  enableTestProperties: process.env.NX_ENABLE_TEST_PROPERTIES
+    ? process.env.NX_ENABLE_TEST_PROPERTIES === 'true'
     : true,
-  enableLegacyCloud: process.env.ENABLE_LEGACY_PROVIDER
-    ? process.env.ENABLE_LEGACY_PROVIDER === 'true'
+  enableLegacyCloud: process.env.NX_ENABLE_LEGACY_PROVIDER
+    ? process.env.NX_ENABLE_LEGACY_PROVIDER === 'true'
     : true,
   enableBroadCastChannelProvider: Boolean(
-    process.env.ENABLE_BC_PROVIDER ?? '1'
+    process.env.NX_ENABLE_BC_PROVIDER ?? '1'
   ),
   enableDebugPage: Boolean(
-    process.env.ENABLE_DEBUG_PAGE ?? process.env.NODE_ENV === 'development'
+    process.env.NX_ENABLE_DEBUG_PAGE ??
+      process.env.NX_NODE_ENV === 'development'
   ),
   changelogUrl:
-    process.env.CHANGELOG_URL ?? 'http://affine.pro/blog/whats-new-affine-0601',
+    process.env.NX_CHANGELOG_URL ??
+    'http://affine.pro/blog/whats-new-affine-0601',
   enablePreloading:
-    process.env.ENABLE_PRELOADING === undefined
+    process.env.NX_ENABLE_PRELOADING === undefined
       ? true
-      : process.env.ENABLE_PRELOADING === 'true',
+      : process.env.NX_ENABLE_PRELOADING === 'true',
 };
