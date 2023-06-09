75eefaf9cdfbaee89a80b37cdf0e91c355897dcb
himself65 <himself65@outlook.com>
2023-06-09 14:51:06 +0800

    fix: entry

--- apps/electron/layers/main/src/ui/index.ts
+++ apps/electron/layers/main/src/ui/index.ts
@@ -1,16 +1,8 @@
-import { join } from 'node:path';
-
 import { app, BrowserWindow, nativeTheme } from 'electron';
 
 import type { NamespaceHandlers } from '../type';
 import { isMacOS } from '../utils';
 import { getGoogleOauthCode } from './google-auth';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const handlers = require(join(
-  process.env.PLUGIN_DIR ?? '../../plugins',
-  './bookmark-block/server'
-)).default as NamespaceHandlers;
 
 export const uiHandlers = {
   handleThemeChange: async (_, theme: (typeof nativeTheme)['themeSource']) => {
@@ -47,5 +39,4 @@ export const uiHandlers = {
   getGoogleOauthCode: async () => {
     return getGoogleOauthCode();
   },
-  ...handlers,
 } satisfies NamespaceHandlers;
