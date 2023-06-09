9402c80133908fa909b18930de720c3912194e4c
Himself65 <himself65@outlook.com>
2023-06-09 11:42:54 +0800

    chore: bump `typescript` to `5.1.3` (#2735)
    Co-authored-by: LongYinan <lynweklm@gmail.com>
    (cherry picked from commit c5a295a87b65e6bfe3810a83389da0d2fc4a54c9)

--- scripts/setup/build-plugins.ts
+++ scripts/setup/build-plugins.ts
@@ -2,5 +2,6 @@ import { beforeAll } from 'vitest';
 
 beforeAll(async () => {
   console.log('Build plugins');
+  // @ts-expect-error
   await import('../../apps/electron/scripts/plugins/build-plugins.mjs');
 });
