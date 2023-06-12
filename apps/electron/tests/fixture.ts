5ef716246befaa64abdf65fc027f4c18f09bb3eb
Peng Xiao <pengxiao@outlook.com>
2023-06-12 17:04:15 +0800

    fix: flaky tests

--- apps/electron/tests/fixture.ts
+++ apps/electron/tests/fixture.ts
@@ -80,8 +80,21 @@ export const test = base.extend<{
     // a random id to avoid conflicts between tests
     const id = generateUUID();
     const ext = process.platform === 'win32' ? '.cmd' : '';
+    const dist = resolve(__dirname, '..', 'dist');
+    const clonedDist = resolve(__dirname, '../e2e-dist', 'dist-' + id);
+    await fs.copy(dist, clonedDist);
+    const packageJson = await fs.readJSON(
+      resolve(__dirname, '..', 'package.json')
+    );
+    // overwrite the app name
+    packageJson.name = 'affine-test-' + id;
+    // overwrite the path to the main script
+    packageJson.main = './main.js';
+    // write to the cloned dist
+    await fs.writeJSON(resolve(clonedDist, 'package.json'), packageJson);
+
     const electronApp = await electron.launch({
-      args: [resolve(__dirname, '..'), '--app-name', 'affine-test-' + id],
+      args: [clonedDist],
       executablePath: resolve(
         __dirname,
         '..',
