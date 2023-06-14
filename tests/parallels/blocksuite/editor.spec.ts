be57068944b642743f3597a1e53a0802146ba980
JimmFly <yangjinfei001@gmail.com>
2023-06-14 23:11:59 +0800

    test: add basic test for link page and database

---
+++ tests/parallels/blocksuite/editor.spec.ts
@@ -0,0 +1,68 @@
+import { test } from '@affine-test/kit/playwright';
+import { expect } from '@playwright/test';
+
+import { openHomePage } from '../../libs/load-page';
+import { newPage, waitEditorLoad } from '../../libs/page-logic';
+
+test('database is useable', async ({ page }) => {
+  await openHomePage(page);
+  await waitEditorLoad(page);
+  await newPage(page);
+  await page.keyboard.insertText('test title');
+  await page.keyboard.press('Enter');
+  const title = page.locator('.affine-default-page-block-title');
+  expect(await title.innerText()).toBe('test title');
+  await page.keyboard.press('/', { delay: 50 });
+  await page.keyboard.press('d');
+  await page.keyboard.press('a');
+  await page.keyboard.press('t');
+  await page.keyboard.press('a');
+  await page.keyboard.press('b');
+  await page.keyboard.press('a');
+  await page.keyboard.press('Enter');
+  const database = page.locator('.affine-database-table');
+  expect(database).toBeVisible();
+  await page.reload();
+  await waitEditorLoad(page);
+  await newPage(page);
+  await page.keyboard.insertText('test title2');
+  await page.keyboard.press('Enter');
+  const title2 = page.locator('.affine-default-page-block-title');
+  expect(await title2.innerText()).toBe('test title2');
+  await page.keyboard.press('/', { delay: 50 });
+  await page.keyboard.press('d');
+  await page.keyboard.press('a');
+  await page.keyboard.press('t');
+  await page.keyboard.press('a');
+  await page.keyboard.press('b');
+  await page.keyboard.press('a');
+  await page.keyboard.press('Enter');
+  const database2 = page.locator('.affine-database-table');
+  expect(database2).toBeVisible();
+});
+
+test('link page is useable', async ({ page }) => {
+  await openHomePage(page);
+  await waitEditorLoad(page);
+  await newPage(page);
+  await page.keyboard.insertText('page1');
+  await page.keyboard.press('Enter');
+  const title = page.locator('.affine-default-page-block-title');
+  expect(await title.innerText()).toBe('page1');
+  await newPage(page);
+  await page.keyboard.insertText('page2');
+  await page.keyboard.press('Enter');
+  const title2 = page.locator('.affine-default-page-block-title');
+  expect(await title2.innerText()).toBe('page2');
+  await page.keyboard.press('@', { delay: 50 });
+  await page.keyboard.press('p');
+  await page.keyboard.press('a');
+  await page.keyboard.press('g');
+  await page.keyboard.press('e');
+  await page.keyboard.press('1');
+  await page.keyboard.press('Enter');
+  const link = page.locator('.affine-reference');
+  expect(link).toBeVisible();
+  await page.click('.affine-reference');
+  expect(title.innerText()).toBe('page1');
+});
