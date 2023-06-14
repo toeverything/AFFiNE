ad32ed5dd5b816b0f73ab0b41ccdd1c24f1a1752
danielchim <kahungchim@gmail.com>
2023-06-14 12:20:29 +0800

    feat: image-preview (#2720)
    Co-authored-by: Himself65 <himself65@outlook.com>

---
+++ packages/component/src/components/image-preview-modal/hooks/use-zoom.tsx
@@ -0,0 +1,194 @@
+import type { MouseEvent as ReactMouseEvent, RefObject } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+
+interface UseZoomControlsProps {
+  zoomRef: RefObject<HTMLDivElement>;
+  imageRef: RefObject<HTMLImageElement>;
+}
+
+export const useZoomControls = ({
+  zoomRef,
+  imageRef,
+}: UseZoomControlsProps) => {
+  const [currentScale, setCurrentScale] = useState<number>(0.5);
+  const [isZoomedBigger, setIsZoomedBigger] = useState<boolean>(false);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [mouseX, setMouseX] = useState<number>(0);
+  const [mouseY, setMouseY] = useState<number>(0);
+  const [dragBeforeX, setDragBeforeX] = useState<number>(0);
+  const [dragBeforeY, setDragBeforeY] = useState<number>(0);
+  const [imagePos, setImagePos] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0,
+  });
+
+  const zoomIn = useCallback(() => {
+    const image = imageRef.current;
+
+    if (image && currentScale < 2) {
+      const newScale = currentScale + 0.1;
+      setCurrentScale(newScale);
+      image.style.width = `${image.naturalWidth * newScale}px`;
+      image.style.height = `${image.naturalHeight * newScale}px`;
+    }
+  }, [imageRef, currentScale]);
+
+  const zoomOut = useCallback(() => {
+    const image = imageRef.current;
+    if (image && currentScale > 0.5) {
+      const newScale = currentScale - 0.1;
+      setCurrentScale(newScale);
+      image.style.width = `${image.naturalWidth * newScale}px`;
+      image.style.height = `${image.naturalHeight * newScale}px`;
+      if (!isZoomedBigger) {
+        image.style.transform = `translate(0px, 0px)`;
+      }
+    }
+  }, [imageRef, currentScale, isZoomedBigger]);
+
+  const resetZoom = useCallback(() => {
+    const image = imageRef.current;
+    if (image) {
+      const newScale = 0.5;
+      setCurrentScale(newScale);
+      image.style.width = `${image.naturalWidth * newScale}px`;
+      image.style.height = `${image.naturalHeight * newScale}px`;
+      image.style.transform = `translate(0px, 0px)`;
+      setImagePos({ x: 0, y: 0 });
+    }
+  }, [imageRef]);
+
+  const handleDragStart = useCallback(
+    (event: ReactMouseEvent) => {
+      event?.preventDefault();
+      setIsDragging(true);
+      const image = imageRef.current;
+      if (image && isZoomedBigger) {
+        image.style.cursor = 'grab';
+        const rect = image.getBoundingClientRect();
+        setDragBeforeX(rect.left);
+        setDragBeforeY(rect.top);
+        setMouseX(event.clientX);
+        setMouseY(event.clientY);
+      }
+    },
+    [imageRef, isZoomedBigger]
+  );
+
+  const handleDrag = useCallback(
+    (event: ReactMouseEvent) => {
+      event?.preventDefault();
+      const image = imageRef.current;
+
+      if (isDragging && image && isZoomedBigger) {
+        image.style.cursor = 'grabbing';
+        const currentX = imagePos.x;
+        const currentY = imagePos.y;
+        const newPosX = currentX + event.clientX - mouseX;
+        const newPosY = currentY + event.clientY - mouseY;
+        image.style.transform = `translate(${newPosX}px, ${newPosY}px)`;
+      }
+    },
+    [
+      imagePos.x,
+      imagePos.y,
+      imageRef,
+      isDragging,
+      isZoomedBigger,
+      mouseX,
+      mouseY,
+    ]
+  );
+
+  const dragEndImpl = useCallback(() => {
+    setIsDragging(false);
+
+    const image = imageRef.current;
+    if (image && isZoomedBigger && isDragging) {
+      image.style.cursor = 'pointer';
+      const rect = image.getBoundingClientRect();
+      const newPos = { x: rect.left, y: rect.top };
+      const currentX = imagePos.x;
+      const currentY = imagePos.y;
+      const newPosX = currentX + newPos.x - dragBeforeX;
+      const newPosY = currentY + newPos.y - dragBeforeY;
+      setImagePos({ x: newPosX, y: newPosY });
+    }
+  }, [
+    dragBeforeX,
+    dragBeforeY,
+    imagePos.x,
+    imagePos.y,
+    imageRef,
+    isDragging,
+    isZoomedBigger,
+  ]);
+
+  const handleDragEnd = useCallback(
+    (event: ReactMouseEvent) => {
+      event.preventDefault();
+      dragEndImpl();
+    },
+    [dragEndImpl]
+  );
+
+  const handleMouseUp = useCallback(() => {
+    if (isDragging) {
+      dragEndImpl();
+    }
+  }, [isDragging, dragEndImpl]);
+
+  const checkZoomSize = useCallback(() => {
+    const { current: zoomArea } = zoomRef;
+    if (zoomArea) {
+      const image = zoomArea.querySelector('img');
+      if (image) {
+        const zoomedWidth = image.naturalWidth * currentScale;
+        const zoomedHeight = image.naturalHeight * currentScale;
+        const containerWidth = window.innerWidth;
+        const containerHeight = window.innerHeight;
+        setIsZoomedBigger(
+          zoomedWidth > containerWidth || zoomedHeight > containerHeight
+        );
+      }
+    }
+  }, [currentScale, zoomRef]);
+
+  useEffect(() => {
+    const handleScroll = (event: WheelEvent) => {
+      const { deltaY } = event;
+      if (deltaY > 0) {
+        zoomOut();
+      } else if (deltaY < 0) {
+        zoomIn();
+      }
+    };
+
+    const handleResize = () => {
+      checkZoomSize();
+    };
+
+    checkZoomSize();
+
+    window.addEventListener('wheel', handleScroll, { passive: false });
+    window.addEventListener('resize', handleResize);
+    window.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [zoomIn, zoomOut, checkZoomSize, handleMouseUp]);
+
+  return {
+    zoomIn,
+    zoomOut,
+    resetZoom,
+    isZoomedBigger,
+    currentScale,
+    handleDragStart,
+    handleDrag,
+    handleDragEnd,
+  };
+};
--- packages/component/src/components/image-preview-modal/index.css.ts
+++ packages/component/src/components/image-preview-modal/index.css.ts
@@ -11,7 +11,8 @@ export const imagePreviewModalStyle = style({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  background: 'var(--affine-background-modal-color)',
+  // background: 'var(--affine-background-modal-color)',
+  background: 'rgba(0,0,0,0.75)',
 });
 
 export const imagePreviewModalCloseButtonStyle = style({
@@ -20,11 +21,9 @@ export const imagePreviewModalCloseButtonStyle = style({
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
-
   height: '36px',
   width: '36px',
   borderRadius: '10px',
-
   top: '0.5rem',
   right: '0.5rem',
   background: 'var(--affine-white)',
@@ -33,37 +32,97 @@ export const imagePreviewModalCloseButtonStyle = style({
   cursor: 'pointer',
   color: 'var(--affine-icon-color)',
   transition: 'background 0.2s ease-in-out',
+  zIndex: 1,
 });
 
 export const imagePreviewModalGoStyle = style({
-  height: '50%',
   color: 'var(--affine-white)',
   position: 'absolute',
   fontSize: '60px',
   lineHeight: '60px',
   fontWeight: 'bold',
-  display: 'flex',
-  alignItems: 'center',
   opacity: '0.2',
   padding: '0 15px',
   cursor: 'pointer',
 });
 
+export const imageNavigationControlStyle = style({
+  display: 'flex',
+  height: '100%',
+  zIndex: 0,
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
 export const imagePreviewModalContainerStyle = style({
-  position: 'absolute',
-  top: '20%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  zIndex: 1,
+  '@media': {
+    'screen and (max-width: 768px)': {
+      alignItems: 'center',
+    },
+  },
 });
 
-export const imagePreviewModalImageStyle = style({
-  background: 'transparent',
-  maxWidth: '686px',
-  objectFit: 'contain',
-  objectPosition: 'center',
-  borderRadius: '4px',
+export const imagePreviewModalCenterStyle = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 });
 
-export const imagePreviewModalActionsStyle = style({
-  position: 'absolute',
+export const imagePreviewModalCaptionStyle = style({
+  color: 'var(--affine-white)',
+  marginTop: '24px',
+  '@media': {
+    'screen and (max-width: 768px)': {
+      textAlign: 'center',
+    },
+  },
+});
+
+export const imagePreviewActionBarStyle = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '16px 0',
+  backgroundColor: 'var(--affine-white)',
+  borderRadius: '8px',
+  boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)',
+  maxWidth: 'max-content',
+});
+
+export const groupStyle = style({
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: 'var(--affine-white)',
+  borderLeft: '1px solid #E3E2E4',
+});
+
+export const buttonStyle = style({
+  paddingLeft: '10px',
+  paddingRight: '10px',
+});
+
+export const scaleIndicatorStyle = style({
+  margin: '0 8px',
+});
+
+export const imageBottomContainerStyle = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  position: 'fixed',
   bottom: '28px',
-  background: 'var(--affine-white)',
+  zIndex: baseTheme.zIndexModal + 1,
+});
+
+export const captionStyle = style({
+  maxWidth: '686px',
+  color: 'var(--affine-white)',
+  background: 'rgba(0,0,0,0.75)',
+  padding: '10px',
+  marginBottom: '21px',
 });
