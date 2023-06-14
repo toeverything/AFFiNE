c3b35a03eeceb1bc3c2088e6364fd628f6093d1f
himself65 <himself65@outlook.com>
2023-06-14 23:14:28 +0800

    fix(electron): bookmark plugin wound not work

--- apps/electron/src/main/plugin.ts
+++ apps/electron/src/main/plugin.ts
@@ -1,6 +1,7 @@
 import { join, resolve } from 'node:path';
 import { Worker } from 'node:worker_threads';
 
+import { logger } from '@affine/electron/main/logger';
 import { AsyncCall } from 'async-call-rpc';
 import { ipcMain } from 'electron';
 
@@ -13,24 +14,31 @@ declare global {
   var asyncCall: Record<string, (...args: any) => PromiseLike<any>>;
 }
 
-export async function registerPlugin() {
+export function registerPlugin() {
   const pluginWorkerPath = join(__dirname, './workers/plugin.worker.js');
   const asyncCall = AsyncCall<
     Record<string, (...args: any) => PromiseLike<any>>
   >(
-    {},
+    {
+      log: (...args: any[]) => {
+        logger.log('Plugin Worker', ...args);
+      },
+    },
     {
       channel: new MessageEventChannel(new Worker(pluginWorkerPath)),
     }
   );
   globalThis.asyncCall = asyncCall;
-  await import('@toeverything/plugin-infra/manager').then(
-    ({ rootStore, affinePluginsAtom }) => {
+  logger.info('import plugin manager');
+  import('@toeverything/plugin-infra/manager')
+    .then(({ rootStore, affinePluginsAtom }) => {
+      logger.info('import plugin manager');
       const bookmarkPluginPath = join(
         process.env.PLUGIN_DIR ?? resolve(__dirname, './plugins'),
         './bookmark-block/index.mjs'
       );
-      import('file://' + bookmarkPluginPath);
+      logger.info('bookmark plugin path:', bookmarkPluginPath);
+      import(bookmarkPluginPath);
       let dispose: () => void = () => {
         // noop
       };
@@ -38,7 +46,9 @@ export async function registerPlugin() {
         dispose();
         const plugins = rootStore.get(affinePluginsAtom);
         Object.values(plugins).forEach(plugin => {
+          logger.info('register plugin', plugin.definition.id);
           plugin.definition.commands.forEach(command => {
+            logger.info('register plugin command', command);
             ipcMain.handle(command, (event, ...args) =>
               asyncCall[command](...args)
             );
@@ -47,11 +57,14 @@ export async function registerPlugin() {
         dispose = () => {
           Object.values(plugins).forEach(plugin => {
             plugin.definition.commands.forEach(command => {
+              logger.info('unregister plugin command', command);
               ipcMain.removeHandler(command);
             });
           });
         };
       });
-    }
-  );
+    })
+    .catch(error => {
+      logger.error('import plugin manager error', error);
+    });
 }
--- apps/electron/src/main/workers/plugin.worker.ts
+++ apps/electron/src/main/workers/plugin.worker.ts
@@ -11,9 +11,19 @@ if (!parentPort) {
   throw new Error('parentPort is undefined');
 }
 
-AsyncCall(commandProxy, {
+const mainThread = AsyncCall<{
+  log: (...args: any[]) => Promise<void>;
+}>(commandProxy, {
   channel: new MessageEventChannel(parentPort),
 });
+
+globalThis.console.log = mainThread.log;
+globalThis.console.error = mainThread.log;
+globalThis.console.info = mainThread.log;
+globalThis.console.debug = mainThread.log;
+globalThis.console.warn = mainThread.log;
+
+console.log('import plugin infra');
 
 import('@toeverything/plugin-infra/manager')
   .then(({ rootStore, affinePluginsAtom }) => {
@@ -22,20 +32,34 @@ import('@toeverything/plugin-infra/manager')
       './bookmark-block/index.mjs'
     );
 
-    import('file://' + bookmarkPluginPath);
+    console.log('import bookmark plugin', bookmarkPluginPath);
+
+    import(bookmarkPluginPath).catch(console.log);
     rootStore.sub(affinePluginsAtom, () => {
       const plugins = rootStore.get(affinePluginsAtom);
       Object.values(plugins).forEach(plugin => {
+        console.log('handle plugin', plugin.definition.id);
         if (plugin.serverAdapter) {
-          plugin.serverAdapter({
-            registerCommand: (command, fn) => {
-              console.log('register command', command);
-              commandProxy[command] = fn;
-            },
-            unregisterCommand: command => {
-              delete commandProxy[command];
-            },
-          });
+          try {
+            plugin.serverAdapter({
+              registerCommand: (command, fn) => {
+                console.log('register command', command);
+                commandProxy[command] = fn;
+              },
+              unregisterCommand: command => {
+                console.log('unregister command', command);
+                delete commandProxy[command];
+              },
+            });
+          } catch (e) {
+            console.log(
+              'error when handle plugin',
+              plugin.definition.id,
+              `${e}`
+            );
+          }
+        } else {
+          console.log('no server adapter, skipping.');
         }
       });
     });
