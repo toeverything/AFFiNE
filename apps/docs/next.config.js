3dfead4a2a76531125a18032082f6aba5cb28e14
himself65 <himself65@outlook.com>
2023-06-21 15:58:22 +0800

    feat(docs): add `@affine/docs` package

---
+++ apps/docs/next.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+});
+
+module.exports = withNextra();
---
+++ apps/docs/theme.config.jsx
@@ -0,0 +1,6 @@
+export default {
+  logo: <span>AFFiNE Documentation</span>,
+  project: {
+    link: 'https://github.com/toeverything/AFFiNE',
+  },
+};
