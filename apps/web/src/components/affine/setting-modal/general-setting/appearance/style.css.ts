6c857770df22991b6aa6141e4e5e33df4662ff95
QiShaoXuan <qishaoxuan777@gmail.com>
2023-06-26 16:55:56 +0800

    fix: error style

--- apps/web/src/components/affine/setting-modal/general-setting/appearance/style.css.ts
+++ apps/web/src/components/affine/setting-modal/general-setting/appearance/style.css.ts
@@ -3,7 +3,7 @@ import { style } from '@vanilla-extract/css';
 export const settingWrapper = style({
   flexGrow: 1,
   display: 'flex',
-  width: '50%',
+  justifyContent: 'flex-end',
   minWidth: '150px',
   maxWidth: '250px',
 });
