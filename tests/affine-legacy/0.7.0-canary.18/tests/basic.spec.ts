20fd9b65743b4e00ec2b7f8e53c7d3c2b1baae9e
Alex Yang <himself65@outlook.com>
2023-06-28 03:10:08 +0800

    feat: upload 0.7.0-canary.18 static output (#2883)

---
+++ tests/affine-legacy/0.7.0-canary.18/tests/basic.spec.ts
@@ -0,0 +1,22 @@
+import { resolve } from 'node:path';
+
+import { test } from '@playwright/test';
+
+test('init page', async ({ page }) => {
+  await page.goto('http://localhost:8081/');
+  await page.waitForSelector('v-line');
+
+  const currentWorkspaceId: string = await page.evaluate(
+    () => (globalThis.currentWorkspace as any).id
+  );
+
+  const downloadPromise = page.waitForEvent('download');
+  await page.evaluate(() => {
+    const workspace = (globalThis.currentWorkspace as any).blockSuiteWorkspace;
+    workspace.exportYDoc();
+  });
+
+  const download = await downloadPromise;
+  const output = resolve(__dirname, 'fixtures', currentWorkspaceId + '.ydoc');
+  await download.saveAs(output);
+});
