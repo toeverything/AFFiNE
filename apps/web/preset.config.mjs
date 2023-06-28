5119ccdd936f93ffb915d44abb33a5dd82dfb43e
Alex Yang <himself65@outlook.com>
2023-06-28 14:57:16 +0800

    build: unify build flags

--- apps/web/preset.config.mjs
+++ apps/web/preset.config.mjs
@@ -11,13 +11,47 @@ export const blockSuiteFeatureFlags = {
   enable_drag_handle: true,
   enable_surface: true,
   enable_linked_page: true,
-  enable_bookmark_operation: process.env.ENABLE_BOOKMARK_OPERATION === 'true',
+  enable_bookmark_operation: false,
 };
 
 /**
- * @type {import('@affine/env').BuildFlags}
+ * @type {Record<string, import('@affine/env').BuildFlags>}
  */
-export const buildFlags = {
+const buildPreset = {
+  stable: {
+    enableAllPageFilter: true,
+    enablePlugin: false,
+    enableTestProperties: false,
+    enableBroadcastChannelProvider: true,
+    enableDebugPage: true,
+    enableLegacyCloud: false,
+    changelogUrl: 'https://affine.pro/blog/what-is-new-affine-0620',
+    enablePreloading: true,
+    enableNewSettingModal: false,
+  },
+  beta: {},
+  canary: {
+    enableAllPageFilter: true,
+    enablePlugin: true,
+    enableTestProperties: true,
+    enableBroadcastChannelProvider: true,
+    enableDebugPage: true,
+    enableLegacyCloud: false,
+    changelogUrl: 'https://affine.pro/blog/what-is-new-affine-0620',
+    enablePreloading: true,
+    enableNewSettingModal: true,
+  },
+};
+
+// beta and internal versions are the same as stable
+buildPreset.beta = buildPreset.stable;
+buildPreset.canary = buildPreset.stable;
+
+const currentBuild = process.env.BUILD_ENV || 'stable';
+
+const currentBuildPreset = buildPreset[currentBuild];
+
+const environmentPreset = {
   enablePlugin: process.env.ENABLE_PLUGIN === 'true',
   enableAllPageFilter:
     !!process.env.VERCEL ||
@@ -48,3 +82,16 @@ export const buildFlags = {
       ? true
       : process.env.ENABLE_PRELOADING === 'true',
 };
+
+/**
+ * @type {import('@affine/env').BuildFlags}
+ */
+const buildFlags = {
+  ...currentBuildPreset,
+  // environment preset will overwrite current build preset
+  // this environment variable is for debug proposes only
+  // do not put them into CI
+  ...environmentPreset,
+};
+
+export { buildFlags };
