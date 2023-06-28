fe47fe092c0a8475a312fb161662f8625f09af82
regischen <regischen@toeverything.info>
2023-06-28 12:26:08 +0800

    test: ifx

--- packages/env/src/__tests__/subdoc-migration.spec.ts
+++ packages/env/src/__tests__/subdoc-migration.spec.ts
@@ -38,6 +38,11 @@ describe('subdoc', () => {
       if (id === 'xyWNqindHH') {
         return;
       }
+      if(blocks[id]['sys:flavour'] === 'affine:surface'){
+        if(!blocks[id]['prop:elements']){
+          blocks[id]['prop:elements'] =  data[id]['prop:elements'];
+        }
+      }
       expect(data[id]).toEqual(blocks[id]);
     });
   });
