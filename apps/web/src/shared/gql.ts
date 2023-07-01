55d86f4ba9c10c5ca62171aa82253485800c25ce
Alex Yang <himself65@outlook.com>
2023-07-01 12:07:13 +0800

    chore: bump version

--- apps/web/src/shared/gql.ts
+++ apps/web/src/shared/gql.ts
@@ -79,6 +79,7 @@ export function useMutation<Mutation extends GraphQLQuery>(
 ): SWRMutationResponse<
   QueryResponse<Mutation>,
   GraphQLError | GraphQLError[],
+  string,
   QueryVariables<Mutation>
 >;
 export function useMutation<Mutation extends GraphQLQuery>(
@@ -94,6 +95,7 @@ export function useMutation<Mutation extends GraphQLQuery>(
 ): SWRMutationResponse<
   QueryResponse<Mutation>,
   GraphQLError | GraphQLError[],
+  string,
   QueryVariables<Mutation>
 >;
 export function useMutation(
