063498dbf9aa2e9757938b12413869dfa5e75a63
danielchim <kahungchim@gmail.com>
2023-07-02 23:28:10 +0800

    test: zoom in/out button test

--- tests/parallels/image-preview.spec.ts
+++ tests/parallels/image-preview.spec.ts
@@ -120,7 +120,7 @@ test('image able to zoom in and out with mouse scroll', async ({ page }) => {
       .getAttribute('data-blob-id')) as string;
     expect(blobId).toBeTruthy();
   }
-  const locator = page.getByTestId('image-preview-modal');
+  const locator = page.getByTestId('image-content');
   const naturalWidth = await locator.evaluate(
     (img: HTMLImageElement) => img.naturalWidth
   );
@@ -148,6 +148,48 @@ test('image able to zoom in and out with mouse scroll', async ({ page }) => {
   await page.mouse.wheel(0, -100);
   await page.mouse.wheel(0, -100);
   await page.waitForTimeout(1000);
+  imageBoundary = await locator.boundingBox();
+  imageWidth = await imageBoundary?.width;
+  if (imageWidth) {
+    expect((imageWidth / naturalWidth).toFixed(2)).toBe('0.84');
+  }
+});
+
+test('image able to zoom in and out with button click', async ({ page }) => {
+  await openHomePage(page);
+  await waitEditorLoad(page);
+  await newPage(page);
+  let blobId: string;
+  {
+    const title = await getBlockSuiteEditorTitle(page);
+    await title.click();
+    await page.keyboard.press('Enter');
+    await importImage(page, 'http://localhost:8081/large-image.png');
+    await page.locator('img').first().dblclick();
+    await page.waitForTimeout(500);
+    blobId = (await page
+      .locator('img')
+      .nth(1)
+      .getAttribute('data-blob-id')) as string;
+    expect(blobId).toBeTruthy();
+  }
+  const locator = page.getByTestId('image-content');
+  expect(locator.isVisible()).toBeTruthy();
+  const naturalWidth = await locator.evaluate(
+    (img: HTMLImageElement) => img.naturalWidth
+  );
+
+  // zooom in
+  await page.getByTestId('zoom-in-button').dblclick();
+  await page.waitForTimeout(1000);
+  let imageBoundary = await locator.boundingBox();
+  let imageWidth = await imageBoundary?.width;
+  if (imageWidth) {
+    expect((imageWidth / naturalWidth).toFixed(2)).toBe('1.04');
+  }
+
+  // zooom out
+  await page.getByTestId('zoom-out-button').dblclick();
   imageBoundary = await locator.boundingBox();
   imageWidth = await imageBoundary?.width;
   if (imageWidth) {
