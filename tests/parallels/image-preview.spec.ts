0289980f17a6e10032560fd69dfbd2b5ae929e03
danielchim <kahungchim@gmail.com>
2023-07-02 23:19:24 +0800

    test: scroll to zoom in/out

--- tests/parallels/image-preview.spec.ts
+++ tests/parallels/image-preview.spec.ts
@@ -12,7 +12,7 @@ async function importImage(page: Page, url: string) {
   await page.evaluate(
     ([url]) => {
       const clipData = {
-        'text/html': `<img src=${url} />`,
+        'text/html': `<img alt={'Sample image'} src=${url} />`,
       };
       const e = new ClipboardEvent('paste', {
         clipboardData: new DataTransfer(),
@@ -99,5 +99,58 @@ test('image go left and right', async ({ page }) => {
       .first()
       .getAttribute('data-blob-id')) as string;
     expect(newBlobId).toBe(blobId);
+  }
+});
+
+test('image able to zoom in and out with mouse scroll', async ({ page }) => {
+  await openHomePage(page);
+  await waitEditorLoad(page);
+  await newPage(page);
+  let blobId: string;
+  {
+    const title = await getBlockSuiteEditorTitle(page);
+    await title.click();
+    await page.keyboard.press('Enter');
+    await importImage(page, 'http://localhost:8081/large-image.png');
+    await page.locator('img').first().dblclick();
+    await page.waitForTimeout(500);
+    blobId = (await page
+      .locator('img')
+      .nth(1)
+      .getAttribute('data-blob-id')) as string;
+    expect(blobId).toBeTruthy();
+  }
+  const locator = page.getByTestId('image-preview-modal');
+  const naturalWidth = await locator.evaluate(
+    (img: HTMLImageElement) => img.naturalWidth
+  );
+  expect(locator.isVisible()).toBeTruthy();
+  const { width, height } = await page.evaluate(() => ({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  }));
+
+  // zooom in
+  await page.mouse.move(width / 2, height / 2);
+  await page.mouse.wheel(0, 100);
+  await page.mouse.wheel(0, 100);
+  await page.mouse.wheel(0, 100);
+  await page.waitForTimeout(1000);
+  let imageBoundary = await locator.boundingBox();
+  let imageWidth = await imageBoundary?.width;
+  if (imageWidth) {
+    expect((imageWidth / naturalWidth).toFixed(2)).toBe('0.54');
+  }
+
+  // zooom in
+  await page.mouse.move(width / 2, height / 2);
+  await page.mouse.wheel(0, -100);
+  await page.mouse.wheel(0, -100);
+  await page.mouse.wheel(0, -100);
+  await page.waitForTimeout(1000);
+  imageBoundary = await locator.boundingBox();
+  imageWidth = await imageBoundary?.width;
+  if (imageWidth) {
+    expect((imageWidth / naturalWidth).toFixed(2)).toBe('0.84');
   }
 });
