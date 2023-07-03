32692bd54acf0bd96764336587a28f8dbccbb9b9
danielchim <kahungchim@gmail.com>
2023-07-04 00:23:53 +0800

    feat: page mode shortcut (#2985)

--- apps/web/src/components/blocksuite/workspace-header/editor-mode-switch/index.tsx
+++ apps/web/src/components/blocksuite/workspace-header/editor-mode-switch/index.tsx
@@ -1,8 +1,10 @@
+import { Tooltip } from '@affine/component';
 import { useAFFiNEI18N } from '@affine/i18n/hooks';
 import { assertExists } from '@blocksuite/store';
 import { useBlockSuitePageMeta } from '@toeverything/hooks/use-block-suite-page-meta';
 import { useAtom } from 'jotai';
 import type { CSSProperties } from 'react';
+import { useEffect } from 'react';
 
 import { pageSettingFamily } from '../../../../atoms';
 import type { BlockSuiteWorkspace } from '../../../../shared';
@@ -30,39 +32,59 @@ export const EditorModeSwitch = ({
   const t = useAFFiNEI18N();
   assertExists(pageMeta);
   const { trash } = pageMeta;
-
-  return (
-    <StyledEditorModeSwitch
-      style={style}
-      switchLeft={currentMode === 'page'}
-      showAlone={trash}
-    >
-      <PageSwitchItem
-        data-testid="switch-page-mode-button"
-        active={currentMode === 'page'}
-        hide={trash && currentMode !== 'page'}
-        onClick={() => {
-          setSetting(setting => {
-            if (setting?.mode !== 'page') {
-              toast(t['com.affine.pageMode']());
-            }
+  useEffect(() => {
+    const keydown = (e: KeyboardEvent) => {
+      if ((e.key === 's' && e.metaKey) || (e.key === 's' && e.altKey)) {
+        e.preventDefault();
+        setSetting(setting => {
+          if (setting?.mode !== 'page') {
+            toast(t['com.affine.pageMode']());
             return { ...setting, mode: 'page' };
-          });
-        }}
-      />
-      <EdgelessSwitchItem
-        data-testid="switch-edgeless-mode-button"
-        active={currentMode === 'edgeless'}
-        hide={trash && currentMode !== 'edgeless'}
-        onClick={() => {
-          setSetting(setting => {
-            if (setting?.mode !== 'edgeless') {
-              toast(t['com.affine.pageMode']());
-            }
+          } else {
+            toast(t['com.affine.edgelessMode']());
             return { ...setting, mode: 'edgeless' };
-          });
-        }}
-      />
-    </StyledEditorModeSwitch>
+          }
+        });
+      }
+    };
+    document.addEventListener('keydown', keydown, { capture: true });
+    return () =>
+      document.removeEventListener('keydown', keydown, { capture: true });
+  }, [setSetting, t]);
+  return (
+    <Tooltip content={'Switch ⌘ + S'}>
+      <StyledEditorModeSwitch
+        style={style}
+        switchLeft={currentMode === 'page'}
+        showAlone={trash}
+      >
+        <PageSwitchItem
+          data-testid="switch-page-mode-button"
+          active={currentMode === 'page'}
+          hide={trash && currentMode !== 'page'}
+          onClick={() => {
+            setSetting(setting => {
+              if (setting?.mode !== 'page') {
+                toast(t['com.affine.pageMode']());
+              }
+              return { ...setting, mode: 'page' };
+            });
+          }}
+        />
+        <EdgelessSwitchItem
+          data-testid="switch-edgeless-mode-button"
+          active={currentMode === 'edgeless'}
+          hide={trash && currentMode !== 'edgeless'}
+          onClick={() => {
+            setSetting(setting => {
+              if (setting?.mode !== 'edgeless') {
+                toast(t['com.affine.edgelessMode']());
+              }
+              return { ...setting, mode: 'edgeless' };
+            });
+          }}
+        />
+      </StyledEditorModeSwitch>
+    </Tooltip>
   );
 };
