5eae76de0364e932ed43da25716261a1eb3ff9a9
Alex Yang <himself65@outlook.com>
2023-07-05 22:59:14 +0800

    fix: error wrapper

--- apps/web/src/components/cloud/provider.tsx
+++ apps/web/src/components/cloud/provider.tsx
@@ -1,11 +1,46 @@
+import { pushNotificationAtom } from '@affine/component/notification-center';
+import { assertExists } from '@blocksuite/global/utils';
+import { GraphQLError } from 'graphql/index';
+import { useSetAtom } from 'jotai';
 import { SessionProvider } from 'next-auth/react';
 import type { PropsWithChildren, ReactElement } from 'react';
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import type { SWRConfiguration } from 'swr';
 import { SWRConfig } from 'swr';
 
 const cloudConfig: SWRConfiguration = {
   suspense: true,
+  use: [
+    useSWRNext => (key, fetcher, config) => {
+      const pushNotification = useSetAtom(pushNotificationAtom);
+      const fetcherWrapper = useCallback(
+        async (...args: any[]) => {
+          assertExists(fetcher);
+          const d = fetcher(...args);
+          if (d instanceof Promise) {
+            return d.catch(e => {
+              if (
+                e instanceof GraphQLError ||
+                (Array.isArray(e) && e[0] instanceof GraphQLError)
+              ) {
+                const graphQLError = e instanceof GraphQLError ? e : e[0];
+                pushNotification({
+                  title: 'GraphQL Error',
+                  message: graphQLError.toString(),
+                  key: Date.now().toString(),
+                  type: 'error',
+                });
+              }
+              return e;
+            });
+          }
+          return d;
+        },
+        [fetcher]
+      );
+      return useSWRNext(key, fetcher ? fetcherWrapper : fetcher, config);
+    },
+  ],
 };
 
 export const Provider = (props: PropsWithChildren): ReactElement => {
