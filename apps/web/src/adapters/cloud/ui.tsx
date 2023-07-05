07a5053bac4b2ca023d58b79a0b3fd4e38971dfd
Alex Yang <himself65@outlook.com>
2023-07-05 16:49:37 +0800

    fix: ui

--- apps/web/src/adapters/cloud/ui.tsx
+++ apps/web/src/adapters/cloud/ui.tsx
@@ -1,8 +1,17 @@
+import { initEmptyPage } from '@affine/env/blocksuite';
+import { PageNotFoundError } from '@affine/env/constant';
 import type {
   WorkspaceFlavour,
   WorkspaceUISchema,
 } from '@affine/env/workspace';
 import { lazy } from 'react';
+
+import {
+  BlockSuitePageList,
+  NewWorkspaceSettingDetail,
+  PageDetailEditor,
+  WorkspaceHeader,
+} from '../shared';
 
 const Provider = lazy(() =>
   import('../../components/cloud/provider').then(({ Provider }) => ({
@@ -16,15 +25,50 @@ const LoginCard = lazy(() =>
   }))
 );
 
-const unimplemented = () => {
-  throw new Error('Cloud API Not implemented');
-};
-
 export const UI = {
   Provider,
-  Header: unimplemented,
-  PageDetail: unimplemented,
-  PageList: unimplemented,
-  NewSettingsDetail: unimplemented,
   LoginCard,
+  Header: WorkspaceHeader,
+  PageDetail: ({ currentWorkspace, currentPageId, onLoadEditor }) => {
+    const page = currentWorkspace.blockSuiteWorkspace.getPage(currentPageId);
+    if (!page) {
+      throw new PageNotFoundError(
+        currentWorkspace.blockSuiteWorkspace,
+        currentPageId
+      );
+    }
+    return (
+      <>
+        <PageDetailEditor
+          pageId={currentPageId}
+          onInit={initEmptyPage}
+          onLoad={onLoadEditor}
+          workspace={currentWorkspace}
+        />
+      </>
+    );
+  },
+  PageList: ({ blockSuiteWorkspace, onOpenPage, collection }) => {
+    return (
+      <BlockSuitePageList
+        listType="all"
+        collection={collection}
+        onOpenPage={onOpenPage}
+        blockSuiteWorkspace={blockSuiteWorkspace}
+      />
+    );
+  },
+  NewSettingsDetail: ({
+    currentWorkspace,
+    onDeleteWorkspace,
+    onTransformWorkspace,
+  }) => {
+    return (
+      <NewWorkspaceSettingDetail
+        onDeleteWorkspace={onDeleteWorkspace}
+        workspace={currentWorkspace}
+        onTransferWorkspace={onTransformWorkspace}
+      />
+    );
+  },
 } satisfies WorkspaceUISchema<WorkspaceFlavour.AFFINE_CLOUD>;
