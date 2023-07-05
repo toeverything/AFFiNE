191b2d79dd9ef53bc33d923770b192a707fd64ea
Alex Yang <himself65@outlook.com>
2023-07-05 21:51:05 +0800

    fix: provider

--- apps/web/src/components/cloud/provider.tsx
+++ apps/web/src/components/cloud/provider.tsx
@@ -1,9 +1,31 @@
 import { SessionProvider } from 'next-auth/react';
 import type { PropsWithChildren, ReactElement } from 'react';
+import { useMemo } from 'react';
+import type { SWRConfiguration } from 'swr';
+import { SWRConfig } from 'swr';
+
+const cloudConfig: SWRConfiguration = {
+  suspense: true,
+};
 
 export const Provider = (props: PropsWithChildren): ReactElement => {
   if (!runtimeConfig.enableCloud) {
     return <>{props.children}</>;
   }
-  return <SessionProvider>{props.children}</SessionProvider>;
+  return (
+    <SWRConfig
+      value={
+        // This is a safe conditional hook
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        useMemo(
+          () => ({
+            ...cloudConfig,
+          }),
+          []
+        )
+      }
+    >
+      <SessionProvider>{props.children}</SessionProvider>
+    </SWRConfig>
+  );
 };
