a6ce3ee29ad0241f045d558ea3f868b7473f5aa4
Alex Yang <himself65@outlook.com>
2023-07-05 15:41:14 +0800

    fix: use localhost

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -154,15 +154,15 @@ if (process.env.NODE_ENV === 'development') {
   nextConfig.rewrites = async () => [
     {
       source: '/api/auth/:path*/:path2*',
-      destination: 'http://127.0.0.1:3010/api/auth/:path*/:path2*',
+      destination: 'http://localhost:3010/api/auth/:path*/:path2*',
     },
     {
       source: '/graphql',
-      destination: 'http://127.0.0.1:3010/graphql',
+      destination: 'http://localhost:3010/graphql',
     },
     {
       source: '/socket.io/:path*',
-      destination: 'http://127.0.0.1:3010/socket.io/:path*',
+      destination: 'http://localhost:3010/socket.io/:path*',
     },
   ];
 }
