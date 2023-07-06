607415207a11fe91c5ffcc14cf015c10a17f6cd8
Alex Yang <himself65@outlook.com>
2023-07-06 17:33:00 +0800

    fix: rename

--- packages/workspace/src/blob/local-static-storage.ts
+++ packages/workspace/src/blob/local-static-storage.ts
@@ -4,10 +4,8 @@ export const createStaticStorage = (): BlobStorage => {
   return {
     crud: {
       get: async (key: string) => {
-        if (key.startsWith('$local$')) {
-          const id = key.slice('$local$'.length);
-          const path = `/static/${id}`;
-          const response = await fetch(path);
+        if (key.startsWith('/static/')) {
+          const response = await fetch(key);
           return response.blob();
         }
         return null;
