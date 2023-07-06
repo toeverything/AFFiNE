85b9d12efad7862254195f491e0c8ed22087739f
Alex Yang <himself65@outlook.com>
2023-07-06 19:03:22 +0800

    fix(web): setting panel refresh

--- apps/web/src/adapters/local/index.tsx
+++ apps/web/src/adapters/local/index.tsx
@@ -113,6 +113,7 @@ export const LocalAdapter: WorkspaceAdapter<WorkspaceFlavour.LOCAL> = {
         <NewWorkspaceSettingDetail
           onDeleteWorkspace={onDeleteWorkspace}
           workspace={currentWorkspace}
+          key={currentWorkspace.id}
           onTransferWorkspace={onTransformWorkspace}
         />
       );
