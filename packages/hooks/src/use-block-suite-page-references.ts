51ec111259d59b44ec9cd3793130f4bc35b00e38
Alex Yang <himself65@outlook.com>
2023-07-09 12:45:46 +0800

    refactor(hooks): reduce null types

--- packages/hooks/src/use-block-suite-page-references.ts
+++ packages/hooks/src/use-block-suite-page-references.ts
@@ -1,9 +1,10 @@
+import { assertExists } from '@blocksuite/global/utils';
 import type { Page, Workspace } from '@blocksuite/store';
-import { atom, useAtomValue } from 'jotai';
-import { atomFamily } from 'jotai/utils';
+import { type Atom, atom, useAtomValue } from 'jotai';
 
 import { useBlockSuiteWorkspacePage } from './use-block-suite-workspace-page';
 
+const weakMap = new WeakMap<Page, Atom<string[]>>();
 function getPageReferences(page: Page): string[] {
   // todo: is there a way to use page indexer to get all references?
   return page
@@ -13,26 +14,27 @@ function getPageReferences(page: Page): string[] {
     .filter(Boolean);
 }
 
-const pageReferencesAtomFamily = atomFamily((page: Page | null) => {
-  if (page === null) {
-    return atom([]);
-  }
-  const baseAtom = atom<string[]>(getPageReferences(page));
-  baseAtom.onMount = set => {
-    const dispose = page.slots.yUpdated.on(() => {
-      set(getPageReferences(page));
-    });
-    return () => {
-      dispose.dispose();
+const getPageReferencesAtom = (page: Page) => {
+  if (!weakMap.has(page)) {
+    const baseAtom = atom<string[]>(getPageReferences(page));
+    baseAtom.onMount = set => {
+      const dispose = page.slots.yUpdated.on(() => {
+        set(getPageReferences(page));
+      });
+      return () => {
+        dispose.dispose();
+      };
     };
-  };
-  return baseAtom;
-});
+    weakMap.set(page, baseAtom);
+  }
+  return weakMap.get(page) as Atom<string[]>;
+};
 
 export function useBlockSuitePageReferences(
   blockSuiteWorkspace: Workspace,
   pageId: string
 ): string[] {
   const page = useBlockSuiteWorkspacePage(blockSuiteWorkspace, pageId);
-  return useAtomValue(pageReferencesAtomFamily(page));
+  assertExists(page);
+  return useAtomValue(getPageReferencesAtom(page));
 }
