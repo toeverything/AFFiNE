21a35719dea6632a673e7fa6134a9509b160730e
Alex Yang <himself65@outlook.com>
2023-07-10 15:58:31 +0800

    feat: init blob storage api

---
+++ packages/workspace/src/blob/cloud-blob-storage.ts
@@ -0,0 +1,23 @@
+import { fetcher } from '@affine/workspace/affine/gql';
+import { assertExists } from '@blocksuite/global/utils';
+import type { BlobStorage } from '@blocksuite/store';
+
+export const createCloudBlobStorage = (): BlobStorage => {
+  assertExists(fetcher);
+  return {
+    crud: {
+      get: async () => {
+        throw new Error('Not implemented');
+      },
+      set: async () => {
+        throw new Error('Not implemented');
+      },
+      list: async () => {
+        throw new Error('Not implemented');
+      },
+      delete: async () => {
+        throw new Error('Not implemented');
+      },
+    },
+  };
+};
