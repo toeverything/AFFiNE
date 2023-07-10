d3ff04537835cdc253bca9f091b8b9b496e3bf00
Alex Yang <himself65@outlook.com>
2023-07-10 16:25:29 +0800

    fix: blob upload

--- packages/workspace/src/blob/cloud-blob-storage.ts
+++ packages/workspace/src/blob/cloud-blob-storage.ts
@@ -1,23 +1,46 @@
-import { assertExists } from '@blocksuite/global/utils';
+import {
+  deleteBlobMutation,
+  listBlobsQuery,
+  setBlobMutation,
+} from '@affine/graphql';
 import type { BlobStorage } from '@blocksuite/store';
 
 import { fetcher } from '../affine/gql';
 
-export const createCloudBlobStorage = (): BlobStorage => {
-  assertExists(fetcher);
+export const createCloudBlobStorage = (workspaceId: string): BlobStorage => {
   return {
     crud: {
       get: async () => {
         throw new Error('Not implemented');
       },
-      set: async () => {
-        throw new Error('Not implemented');
+      set: async (key, value) => {
+        const result = await fetcher({
+          query: setBlobMutation,
+          variables: {
+            workspaceId,
+            blob: new File([value], key),
+          },
+        });
+        console.assert(result.setBlob === key, 'Blob hash mismatch');
+        return key;
       },
       list: async () => {
-        throw new Error('Not implemented');
+        const result = await fetcher({
+          query: listBlobsQuery,
+          variables: {
+            workspaceId,
+          },
+        });
+        return result.listBlobs;
       },
-      delete: async () => {
-        throw new Error('Not implemented');
+      delete: async (key: string) => {
+        await fetcher({
+          query: deleteBlobMutation,
+          variables: {
+            workspaceId,
+            hash: key,
+          },
+        });
       },
     },
   };
