b4a0d26477df00ca84f6b26352efbcc071afb69b
danielchim <kahungchim@gmail.com>
2023-07-10 16:43:42 +0800

    feat: following up question integration

--- plugins/copilot/src/core/components/following-up/index.tsx
+++ plugins/copilot/src/core/components/following-up/index.tsx
@@ -1,5 +1,7 @@
+import { useAtom, useSetAtom } from 'jotai';
 import type { ReactElement } from 'react';
 
+import { isAskingAtom, useChatAtoms } from '../../hooks';
 import { followingUpStyle, questionStyle } from './index.css';
 
 export type FollowingUpProps = {
@@ -7,10 +9,27 @@ export type FollowingUpProps = {
 };
 
 export const FollowingUp = (props: FollowingUpProps): ReactElement => {
+  const { conversationAtom, followingUpAtoms } = useChatAtoms();
+  const call = useSetAtom(conversationAtom);
+  const generateFollowingUp = useSetAtom(followingUpAtoms.generateChatAtom);
+  const [isAsking, setIsasking] = useAtom(isAskingAtom);
+
+  const handleClick = async (question: string) => {
+    if (!isAsking) {
+      setIsasking(true);
+      await call(question);
+      await generateFollowingUp();
+      setIsasking(false);
+    }
+  };
   return (
     <div className={followingUpStyle}>
       {props.questions.map((question, index) => (
-        <div className={questionStyle} key={index}>
+        <div
+          className={questionStyle}
+          key={index}
+          onClick={() => handleClick(question)}
+        >
           {question}
         </div>
       ))}
