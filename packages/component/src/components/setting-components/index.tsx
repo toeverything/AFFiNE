2f72d7edab3ac959deabdb1b77506e1f34a9cde8
QiShaoXuan <qishaoxuan777@gmail.com>
2023-07-11 17:28:50 +0800

    feat: add suspense to setting workspace list

--- packages/component/src/components/setting-components/index.tsx
+++ packages/component/src/components/setting-components/index.tsx
@@ -1,4 +1,5 @@
 export { SettingModal, type SettingModalProps } from './modal';
 export { SettingHeader } from './setting-header';
 export { SettingRow } from './setting-row';
+export * from './workspace-list-skeleton';
 export { SettingWrapper } from './wrapper';
