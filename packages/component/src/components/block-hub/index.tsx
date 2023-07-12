1bc8d6a0b814941a8c51f67152a44989f24b77b3
Alex Yang <himself65@outlook.com>
2023-07-13 02:02:00 +0800

    fix: block hub might not work (#3199)

--- packages/component/src/components/block-hub/index.tsx
+++ packages/component/src/components/block-hub/index.tsx
@@ -2,7 +2,7 @@ import type { BlockHub } from '@blocksuite/blocks';
 import type { Atom } from 'jotai';
 import { useAtomValue } from 'jotai';
 import type { HTMLAttributes, ReactElement } from 'react';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export interface BlockHubProps extends HTMLAttributes<HTMLDivElement> {
   blockHubAtom: Atom<Readonly<BlockHub> | null>;
@@ -11,15 +11,17 @@ export interface BlockHubProps extends HTMLAttributes<HTMLDivElement> {
 export const BlockHubWrapper = (props: BlockHubProps): ReactElement => {
   const blockHub = useAtomValue(props.blockHubAtom);
   const ref = useRef<HTMLDivElement>(null);
-  if (ref.current) {
-    const div = ref.current;
-    if (!blockHub) {
-      if (div.hasChildNodes()) {
-        div.removeChild(div.firstChild as ChildNode);
+  useEffect(() => {
+    console.log('ref.current', blockHub, ref);
+    if (ref.current) {
+      const div = ref.current;
+      if (blockHub) {
+        if (div.hasChildNodes()) {
+          div.removeChild(div.firstChild as ChildNode);
+        }
+        div.appendChild(blockHub);
       }
-    } else {
-      div.appendChild(blockHub);
     }
-  }
+  }, [blockHub]);
   return <div ref={ref} data-testid="block-hub" />;
 };
