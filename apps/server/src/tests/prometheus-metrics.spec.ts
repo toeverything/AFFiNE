a95cea3f6744855b86f1e383276e19a64cad5421
x1a0t <thorseraq@gmail.com>
2023-07-14 02:02:48 +0800

    fix: test

--- apps/server/src/tests/prometheus-metrics.spec.ts
+++ apps/server/src/tests/prometheus-metrics.spec.ts
@@ -1,5 +1,5 @@
 import { ok } from 'node:assert';
-import { beforeEach, test } from 'node:test';
+import { beforeEach, describe, test } from 'node:test';
 
 import { Test } from '@nestjs/testing';
 import { register } from 'prom-client';
@@ -17,40 +17,44 @@ beforeEach(async () => {
   metrics = module.get(Metrics);
 });
 
-test('should be able to increment counter', async () => {
-  metrics.socketIOCounter(1, { event: 'client-handshake' });
-  const socketIOCounterMetric = await register.getSingleMetric(
-    'socket_io_counter'
-  );
-  ok(socketIOCounterMetric);
+describe('MetricsModule', () => {
+  test('should be able to increment counter', async () => {
+    metrics.socketIOCounter(1, { event: 'client-handshake' });
+    const socketIOCounterMetric = await register.getSingleMetric(
+      'socket_io_counter'
+    );
+    ok(socketIOCounterMetric);
 
-  ok(
-    JSON.stringify((await socketIOCounterMetric.get()).values) ===
-      '[{"value":1,"labels":{"event":"client-handshake"}}]'
-  );
-});
+    ok(
+      JSON.stringify((await socketIOCounterMetric.get()).values) ===
+        '[{"value":1,"labels":{"event":"client-handshake"}}]'
+    );
+  });
 
-test('should be able to timer', async () => {
-  const endTimer = metrics.socketIOTimer({ event: 'client-handshake' });
-  await new Promise(resolve => setTimeout(resolve, 50));
-  endTimer();
+  test('should be able to timer', async () => {
+    const endTimer = metrics.socketIOTimer({ event: 'client-handshake' });
+    await new Promise(resolve => setTimeout(resolve, 50));
+    endTimer();
 
-  const endTimer2 = metrics.socketIOTimer({ event: 'client-handshake' });
-  await new Promise(resolve => setTimeout(resolve, 100));
-  endTimer2();
+    const endTimer2 = metrics.socketIOTimer({ event: 'client-handshake' });
+    await new Promise(resolve => setTimeout(resolve, 100));
+    endTimer2();
 
-  const socketIOTimerMetric = await register.getSingleMetric('socket_io_timer');
-  ok(socketIOTimerMetric);
+    const socketIOTimerMetric = await register.getSingleMetric(
+      'socket_io_timer'
+    );
+    ok(socketIOTimerMetric);
 
-  const observations = (await socketIOTimerMetric.get()).values;
+    const observations = (await socketIOTimerMetric.get()).values;
 
-  for (const observation of observations) {
-    if (
-      observation.labels.event === 'client-handshake' &&
-      'quantile' in observation.labels
-    ) {
-      ok(observation.value >= 0.05);
-      ok(observation.value <= 0.15);
+    for (const observation of observations) {
+      if (
+        observation.labels.event === 'client-handshake' &&
+        'quantile' in observation.labels
+      ) {
+        ok(observation.value >= 0.05);
+        ok(observation.value <= 0.15);
+      }
     }
-  }
+  });
 });
