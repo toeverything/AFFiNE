64cf297399b53ea0bd1a319f3d216b530d4b9045
Alex Yang <himself65@outlook.com>
2023-07-13 20:14:30 +0800

    refactor(web): move current atoms into plugin infra (#3220)

--- apps/web/src/hooks/current/use-current-mode.ts
+++ apps/web/src/hooks/current/use-current-mode.ts
@@ -1,10 +1,10 @@
-import { rootCurrentPageIdAtom } from '@affine/workspace/atom';
+import { currentPageIdAtom } from '@toeverything/plugin-infra/manager';
 import { atom, useAtomValue } from 'jotai';
 
 import { pageSettingFamily } from '../../atoms';
 
 const currentModeAtom = atom<'page' | 'edgeless'>(get => {
-  const pageId = get(rootCurrentPageIdAtom);
+  const pageId = get(currentPageIdAtom);
   // fixme(himself65): pageId should not be null
   if (!pageId) {
     return 'page';
