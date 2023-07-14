071dd0a4c076290461cee6c0e408eacead7cad10
Alex Yang <himself65@outlook.com>
2023-07-14 13:05:40 +0800

    fix: side effect

--- packages/workspace/src/atom.ts
+++ packages/workspace/src/atom.ts
@@ -71,27 +71,6 @@ const rootWorkspacesMetadataPromiseAtom = atom<
   if (maybeMetadata !== null) {
     return maybeMetadata;
   }
-  const createFirst = (): RootWorkspaceMetadataV2[] => {
-    if (signal.aborted) {
-      return [];
-    }
-
-    const Plugins = Object.values(WorkspaceAdapters).sort(
-      (a, b) => a.loadPriority - b.loadPriority
-    );
-
-    return Plugins.flatMap(Plugin => {
-      return Plugin.Events['app:init']?.().map(
-        id =>
-          ({
-            id,
-            flavour: Plugin.flavour,
-            // new workspace should all support sub-doc feature
-            version: WorkspaceVersion.SubDoc,
-          }) satisfies RootWorkspaceMetadataV2
-      );
-    }).filter((ids): ids is RootWorkspaceMetadataV2 => !!ids);
-  };
 
   if (environment.isServer) {
     // return a promise in SSR to avoid the hydration mismatch
@@ -157,17 +136,6 @@ const rootWorkspacesMetadataPromiseAtom = atom<
         } catch (e) {
           console.error('list data error:', e);
         }
-      }
-    }
-    // step 3: create initial workspaces
-    {
-      if (
-        metadata.length === 0 &&
-        localStorage.getItem('is-first-open') === null
-      ) {
-        metadata.push(...createFirst());
-        console.info('create first workspace', metadata);
-        localStorage.setItem('is-first-open', 'false');
       }
     }
     const metadataMap = new Map(metadata.map(x => [x.id, x]));
