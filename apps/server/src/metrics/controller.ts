b3a029fda30ad81b8a8c1e0b4a695ee56a0cf10b
x1a0t <thorseraq@gmail.com>
2023-07-17 10:17:37 +0800

    feat: add metrics for prisma client

--- apps/server/src/metrics/controller.ts
+++ apps/server/src/metrics/controller.ts
@@ -2,11 +2,17 @@ import { Controller, Get, Res } from '@nestjs/common';
 import type { Response } from 'express';
 import { register } from 'prom-client';
 
+import { PrismaService } from '../prisma';
+
 @Controller()
 export class MetricsController {
+  constructor(private readonly prisma: PrismaService) {}
+
   @Get('/metrics')
   async index(@Res() res: Response): Promise<void> {
     res.header('Content-Type', register.contentType);
-    res.send(await register.metrics());
+    const prismaMetrics = await this.prisma.$metrics.prometheus();
+    const appMetrics = await register.metrics();
+    res.send(appMetrics + prismaMetrics);
   }
 }
