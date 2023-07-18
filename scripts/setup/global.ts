2468f7a70bb7abae489d0f53cc930723af416fed
Alex Yang <himself65@outlook.com>
2023-07-18 21:24:51 +0800

    fix: code

--- scripts/setup/global.ts
+++ scripts/setup/global.ts
@@ -1,3 +1,10 @@
 import { setupGlobal } from '@affine/env/global';
 
+import { getRuntimeConfig } from '../../apps/core/.webpack/runtime-config';
+
+getRuntimeConfig({
+  distribution: 'browser',
+  mode: 'development',
+  channel: 'canary',
+});
 setupGlobal();
