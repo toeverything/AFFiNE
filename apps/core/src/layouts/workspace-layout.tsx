1a3361439102a8760a09b4da81fe27762da7c3d2
Alex Yang <himself65@outlook.com>
2023-07-21 11:32:16 +0800

    fix: goto not found page

--- apps/core/src/layouts/workspace-layout.tsx
+++ apps/core/src/layouts/workspace-layout.tsx
@@ -135,12 +135,19 @@ export const CurrentWorkspaceContext = ({
 
 export const WorkspaceLayout: FC<PropsWithChildren> =
   function WorkspacesSuspense({ children }) {
+    const { jumpToIndex } = useNavigateHelper();
     const currentWorkspaceId = useAtomValue(currentWorkspaceIdAtom);
     const jotaiWorkspaces = useAtomValue(rootWorkspacesMetadataAtom);
     const meta = useMemo(
       () => jotaiWorkspaces.find(x => x.id === currentWorkspaceId),
       [currentWorkspaceId, jotaiWorkspaces]
     );
+
+    useEffect(() => {
+      if (!meta) {
+        jumpToIndex();
+      }
+    }, [jumpToIndex, meta]);
 
     if (!meta) {
       return <WorkspaceFallback />;
