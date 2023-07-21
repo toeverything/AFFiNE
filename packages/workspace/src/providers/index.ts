ad0bec0e91030f2b62d69d12a66bb93be7e772eb
Peng Xiao <pengxiao@outlook.com>
2023-07-21 15:28:53 +0800

    fix: do not need to download every doc on connect

--- packages/workspace/src/providers/index.ts
+++ packages/workspace/src/providers/index.ts
@@ -61,7 +61,6 @@ const createIndexedDBBackgroundProvider: DocProviderCreator = (
   };
 };
 
-const cache: WeakMap<Doc, Uint8Array> = new WeakMap();
 const indexedDBDownloadOrigin = 'indexeddb-download-provider';
 
 const createIndexedDBDownloadProvider: DocProviderCreator = (
@@ -74,18 +73,11 @@ const createIndexedDBDownloadProvider: DocProviderCreator = (
     _resolve = resolve;
     _reject = reject;
   });
-  async function downloadBinaryRecursively(doc: Doc) {
-    if (cache.has(doc)) {
-      const binary = cache.get(doc) as Uint8Array;
+  async function downloadAndApply(doc: Doc) {
+    const binary = await downloadBinary(doc.guid);
+    if (binary) {
       Y.applyUpdate(doc, binary, indexedDBDownloadOrigin);
-    } else {
-      const binary = await downloadBinary(doc.guid);
-      if (binary) {
-        Y.applyUpdate(doc, binary, indexedDBDownloadOrigin);
-        cache.set(doc, binary);
-      }
     }
-    await Promise.all([...doc.subdocs].map(downloadBinaryRecursively));
   }
   return {
     flavour: 'local-indexeddb',
@@ -98,7 +90,7 @@ const createIndexedDBDownloadProvider: DocProviderCreator = (
     },
     sync: () => {
       logger.info('sync indexeddb provider', id);
-      downloadBinaryRecursively(doc).then(_resolve).catch(_reject);
+      downloadAndApply(doc).then(_resolve).catch(_reject);
     },
   };
 };
