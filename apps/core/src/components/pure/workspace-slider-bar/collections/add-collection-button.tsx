ad736d7d609def281e5a4bc5040e5b3da09c3f20
JimmFly <yangjinfei001@gmail.com>
2023-07-26 11:40:21 +0800

    feat: optimize performance

--- apps/core/src/components/pure/workspace-slider-bar/collections/add-collection-button.tsx
+++ apps/core/src/components/pure/workspace-slider-bar/collections/add-collection-button.tsx
@@ -7,7 +7,7 @@ import { useAFFiNEI18N } from '@affine/i18n/hooks';
 import { PlusIcon } from '@blocksuite/icons';
 import type { Workspace } from '@blocksuite/store';
 import { uuidv4 } from '@blocksuite/store';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import { useGetPageInfoById } from '../../../../hooks/use-get-page-info';
 
@@ -22,12 +22,16 @@ export const AddCollectionButton = ({
   const setting = useCollectionManager(workspace.id);
   const t = useAFFiNEI18N();
   const [show, showUpdateCollection] = useState(false);
-  const defaultCollection = {
-    id: uuidv4(),
-    name: '',
-    filterList: [],
-    workspaceId: workspace.id,
-  };
+  const defaultCollection = useMemo(
+    () => ({
+      id: uuidv4(),
+      name: '',
+      pinned: true,
+      filterList: [],
+      workspaceId: workspace.id,
+    }),
+    [workspace.id]
+  );
   return (
     <>
       <IconButton
