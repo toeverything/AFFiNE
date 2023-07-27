4de063de988f9569ea1468699b79a7bc9408ece8
JimmFly <yangjinfei001@gmail.com>
2023-07-28 04:37:34 +0800

    style: adjust collection modal style (#3407)

--- packages/component/src/components/page-list/view/create-collection.tsx
+++ packages/component/src/components/page-list/view/create-collection.tsx
@@ -201,6 +201,7 @@ export const EditCollection = ({
             borderRadius: 8,
             padding: 18,
             marginTop: 20,
+            minHeight: '200px',
           }}
         >
           <div className={styles.filterTitle}>Filters</div>
