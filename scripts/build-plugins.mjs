dc669fbc71e5b514fa203c1e07ade47328b7d1cb
Alex Yang <himself65@outlook.com>
2023-07-28 22:37:01 -0700

    fix: script

--- scripts/build-plugins.mjs
+++ scripts/build-plugins.mjs
@@ -3,8 +3,13 @@ import { spawn } from 'node:child_process';
 const builtInPlugins = ['bookmark', 'hello-world', 'copilot', 'image-preview'];
 
 for (const plugin of builtInPlugins) {
-  spawn('yarn', ['-T', 'run', 'build-plugin', '--plugin', plugin], {
+  const cp = spawn('yarn', ['-T', 'run', 'dev-plugin', '--plugin', plugin], {
     stdio: 'inherit',
     shell: true,
+  });
+  cp.on('exit', code => {
+    if (code !== 0) {
+      process.exit(code);
+    }
   });
 }
