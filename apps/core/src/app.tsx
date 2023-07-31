452c780d40d9b7169ada498ffde15e3d8fdb5dae
Alex Yang <himself65@outlook.com>
2023-07-31 02:21:12 -0700

    refactor(i18n): language setup (#3484)

--- apps/core/src/app.tsx
+++ apps/core/src/app.tsx
@@ -5,8 +5,9 @@ import { AffineContext } from '@affine/component/context';
 import { WorkspaceFallback } from '@affine/component/workspace';
 import { createI18n, setUpLanguage } from '@affine/i18n';
 import { CacheProvider } from '@emotion/react';
+import { use } from 'foxact/use';
 import type { PropsWithChildren, ReactElement } from 'react';
-import { lazy, memo, Suspense, useEffect } from 'react';
+import { lazy, memo, Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './router';
@@ -32,14 +33,21 @@ const future = {
   v7_startTransition: true,
 } as const;
 
-export const App = memo(function App() {
-  useEffect(() => {
+const languageLoadingPromise = new Promise<void>(resolve => {
+  if (environment.isBrowser) {
     document.documentElement.lang = i18n.language;
-    // todo(himself65): this is a hack, we should use a better way to set the language
-    setUpLanguage(i18n)?.catch(error => {
-      console.error(error);
-    });
-  }, []);
+    setUpLanguage(i18n)
+      .then(() => resolve())
+      .catch(error => {
+        console.error(error);
+      });
+  } else {
+    resolve();
+  }
+});
+
+export const App = memo(function App() {
+  use(languageLoadingPromise);
   return (
     <CacheProvider value={cache}>
       <AffineContext>
