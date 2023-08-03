ce903dd3ba219d4b3ed848e56b5154cd0be3739b
Alex Yang <himself65@outlook.com>
2023-08-02 22:33:15 -0700

    fix: bundle

--- packages/infra/vite.config.ts
+++ packages/infra/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       entry: {
         index: resolve(root, 'src/index.ts'),
         atom: resolve(root, 'src/atom.ts'),
+        type: resolve(root, 'src/type.ts'),
         'core/event-emitter': resolve(root, 'src/core/event-emitter.ts'),
         'preload/electron': resolve(root, 'src/preload/electron.ts'),
         '__internal__/workspace': resolve(
