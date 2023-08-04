94c9f197a488027f4a2170e33acf513f747157ed
Peng Xiao <pengxiao@outlook.com>
2023-08-04 23:03:58 +0800

    fix: redirect api requests through file:// protocol

--- apps/electron/src/main/config.ts
+++ apps/electron/src/main/config.ts
@@ -14,3 +14,12 @@ export const buildType = ReleaseTypeSchema.parse(envBuildType);
 
 export const mode = process.env.NODE_ENV;
 export const isDev = mode === 'development';
+
+const API_URL_MAPPING = {
+  stable: `https://affine.fail`,
+  beta: `https://affine.fail`,
+  canary: `https://affine.fail`,
+  internal: `https://affine.fail`,
+}
+
+export const CLOUD_API_URL = API_URL_MAPPING[buildType];
