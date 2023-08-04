20e19690f7c15f5eb96c9b0a109c5237126ea8f9
Alex Yang <himself65@outlook.com>
2023-08-04 15:46:10 -0700

    fix: code

--- packages/env/src/global.ts
+++ packages/env/src/global.ts
@@ -1,60 +1,10 @@
 /// <reference types="@blocksuite/global" />
 import { assertEquals } from '@blocksuite/global/utils';
-import type {
-  DBHandlerManager,
-  DebugHandlerManager,
-  DialogHandlerManager,
-  ExportHandlerManager,
-  UIHandlerManager,
-  UnwrapManagerHandlerToClientSide,
-  UpdaterHandlerManager,
-  WorkspaceHandlerManager,
-} from '@toeverything/infra';
 import { z } from 'zod';
 
 import { isBrowser, isDesktop, isServer } from './constant.js';
 import { isValidIPAddress } from './is-valid-ip-address.js';
 import { UaHelper } from './ua-helper.js';
-
-declare global {
-  interface Window {
-    appInfo: {
-      electron: boolean;
-    };
-    apis: {
-      db: UnwrapManagerHandlerToClientSide<DBHandlerManager>;
-      debug: UnwrapManagerHandlerToClientSide<DebugHandlerManager>;
-      dialog: UnwrapManagerHandlerToClientSide<DialogHandlerManager>;
-      export: UnwrapManagerHandlerToClientSide<ExportHandlerManager>;
-      ui: UnwrapManagerHandlerToClientSide<UIHandlerManager>;
-      updater: UnwrapManagerHandlerToClientSide<UpdaterHandlerManager>;
-      workspace: UnwrapManagerHandlerToClientSide<WorkspaceHandlerManager>;
-    };
-    events: any;
-  }
-
-  interface WindowEventMap {
-    'migration-done': CustomEvent;
-  }
-
-  // eslint-disable-next-line no-var
-  var $migrationDone: boolean;
-  // eslint-disable-next-line no-var
-  var platform: Platform | undefined;
-  // eslint-disable-next-line no-var
-  var environment: Environment;
-  // eslint-disable-next-line no-var
-  var runtimeConfig: RuntimeConfig;
-  // eslint-disable-next-line no-var
-  var $AFFINE_SETUP: boolean | undefined;
-  // eslint-disable-next-line no-var
-  var editorVersion: string | undefined;
-  // eslint-disable-next-line no-var
-  var prefixUrl: string;
-  // eslint-disable-next-line no-var
-  var websocketPrefixUrl: string;
-}
-
 export const blockSuiteFeatureFlags = z.object({
   enable_database: z.boolean(),
   enable_drag_handle: z.boolean(),
