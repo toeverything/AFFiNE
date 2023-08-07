a32f5ad36b1b8dde3b82749e316d8aed05bb77ea
Peng Xiao <pengxiao@outlook.com>
2023-08-07 16:17:48 +0800

    feat: handle open url schema

--- apps/electron/src/main/deep-link.ts
+++ apps/electron/src/main/deep-link.ts
@@ -1,7 +1,9 @@
 import type { App } from 'electron';
+import { BrowserView } from 'electron';
 
 import { buildType, isDev } from './config';
 import { logger } from './logger';
+import { restoreOrCreateWindow } from './main-window';
 
 let protocol = buildType === 'stable' ? 'affine' : `affine-${buildType}`;
 if (isDev) {
@@ -13,11 +15,37 @@ export function setupDeepLink(app: App) {
   app.on('open-url', (event, url) => {
     if (url.startsWith(`${protocol}://`)) {
       event.preventDefault();
-      handleAffineUrl(url);
+      handleAffineUrl(url).catch(e => {
+        logger.error('failed to handle affine url', e);
+      });
     }
   });
 }
 
-function handleAffineUrl(url: string) {
+async function handleAffineUrl(url: string) {
   logger.info('open affine url', url);
+  const urlObj = new URL(url);
+  if (urlObj.hostname === 'open-url') {
+    const window = await restoreOrCreateWindow();
+    const urlToOpen = urlObj.search.slice(1);
+    if (urlToOpen) {
+      const view = new BrowserView();
+      window.addBrowserView(view);
+      const bounds = window.getBounds();
+      view.setAutoResize({ width: true, height: true });
+      view.setBounds({
+        x: 0,
+        y: 0,
+        width: bounds.width,
+        height: bounds.height,
+      });
+      view.webContents.loadURL(urlToOpen).catch(e => {
+        logger.error('failed to load url', e);
+      });
+      // close view on main webview reload
+      window.webContents.on('did-finish-load', () => {
+        window.removeBrowserView(view);
+      });
+    }
+  }
 }
