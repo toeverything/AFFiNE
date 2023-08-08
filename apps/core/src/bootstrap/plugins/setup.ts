c339f12421a810dd1f3ab5d1d0d24ea236af1301
QiShaoXuan <qishaoxuan777@gmail.com>
2023-08-08 10:59:19 +0800

    fix: plugin import error

--- apps/core/src/bootstrap/plugins/setup.ts
+++ apps/core/src/bootstrap/plugins/setup.ts
@@ -7,6 +7,7 @@ import * as BlockSuiteGlobalUtils from '@blocksuite/global/utils';
 import { assertExists } from '@blocksuite/global/utils';
 import { DisposableGroup } from '@blocksuite/global/utils';
 import * as Icons from '@blocksuite/icons';
+import * as Button from '@toeverything/components/button';
 import {
   contentLayoutAtom,
   currentPageAtom,
@@ -50,6 +51,10 @@ const setupRootImportsMap = () => {
   _rootImportsMap.set(
     '@affine/component',
     new Map(Object.entries(AFFiNEComponent))
+  );
+  _rootImportsMap.set(
+    '@toeverything/components/button',
+    new Map(Object.entries(Button))
   );
   _rootImportsMap.set(
     '@blocksuite/blocks/std',
