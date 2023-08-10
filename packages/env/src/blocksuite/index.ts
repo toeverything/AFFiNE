c18a5e90636705d55c69bb2df202d0e34145110f
Alex Yang <himself65@outlook.com>
2023-08-10 11:20:02 -0400

    feat: init new preloading pages

--- packages/env/src/blocksuite/index.ts
+++ packages/env/src/blocksuite/index.ts
@@ -1,12 +1,8 @@
 import type { Page } from '@blocksuite/store';
 
-export async function initPageWithPreloading(page: Page) {
-  const workspace = page.workspace;
-  const data = await import('@affine/templates/v1/preloading.json');
-  await page.waitForLoaded();
-  await workspace.importPageSnapshot(data, page.id);
-}
-
+/**
+ * @deprecated
+ */
 export async function initEmptyPage(page: Page) {
   await page.waitForLoaded();
   const pageBlockId = page.addBlock('affine:page', {
---
+++ packages/infra/src/blocksuite/index.ts
@@ -0,0 +1,34 @@
+import type { Workspace } from '@blocksuite/store';
+
+export async function buildShowcaseWorkspace(workspace: Workspace) {
+  const data = [
+    ['F1SX6cgNxy-hello-world', import('@affine/templates/v1/preloading.json')],
+    ['gc5FeppNDv', import('@affine/templates/v1/getting-started.json')],
+    ['3R9X-gMh3m', import('@affine/templates/v1/template-galleries.json')],
+    ['z_v6LOqNpp', import('@affine/templates/v1/personal-home.json')],
+    ['0N0WzwmtK_', import('@affine/templates/v1/working-home.json')],
+    [
+      '6gexHy-jto',
+      import('@affine/templates/v1/personal-project-management.json'),
+    ],
+    ['nQd2Bdvoqz', import('@affine/templates/v1/travel-plan.json')],
+    [
+      'bj_cuI1tN7',
+      import('@affine/templates/v1/personal-knowledge-management.json'),
+    ],
+    [
+      'fFoDX2J1Z5',
+      import('@affine/templates/v1/annual-performance-review.json'),
+    ],
+    ['PqZ7MLlL_9', import('@affine/templates/v1/brief-event-planning.json')],
+    ['A4wBRdQZN0', import('@affine/templates/v1/meeting-summary.json')],
+    ['kBB4lzhm7C', import('@affine/templates/v1/okr-template.json')],
+  ] as const;
+  await Promise.all(
+    data.map(async ([id, promise]) => {
+      const { default: template } = await promise;
+      console.log(`Importing ${id}...`, template);
+      await workspace.importPageSnapshot(template, id);
+    })
+  );
+}
