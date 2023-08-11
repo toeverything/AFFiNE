e21d6a6f807fed73507633ea22c465a1e1ad008b
JimmFly <yangjinfei001@gmail.com>
2023-08-11 16:37:46 +0800

    test: fix test

--- apps/electron/e2e/workspace.spec.ts
+++ apps/electron/e2e/workspace.spec.ts
@@ -58,14 +58,13 @@ test('export then add', async ({ page, appInfo, workspace }) => {
 
   // goto workspace setting
   await page.getByTestId('workspace-list-item').click();
-
-  await page.waitForTimeout(500);
+  const input = page.getByTestId('workspace-name-input');
+  await expect(input).toBeVisible();
 
   // change workspace name
-  await page.getByTestId('workspace-name-input').fill(newWorkspaceName);
+  await input.fill(newWorkspaceName);
   await page.getByTestId('save-workspace-name').click();
   await page.waitForSelector('text="Update workspace name success"');
-  await page.waitForTimeout(500);
 
   const tmpPath = path.join(appInfo.sessionData, w.id + '-tmp.db');
 
@@ -78,7 +77,7 @@ test('export then add', async ({ page, appInfo, workspace }) => {
 
   await page.getByTestId('export-affine-backup').click();
   await page.waitForSelector('text="Export success"');
-
+  await page.waitForTimeout(1000);
   expect(await fs.exists(tmpPath)).toBe(true);
 
   await page.getByTestId('modal-close-button').click();
