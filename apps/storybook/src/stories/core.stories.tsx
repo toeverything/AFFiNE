a7d8973dca824732c89112ef1cc5abcb320fe28c
Alex Yang <himself65@outlook.com>
2023-08-15 15:03:31 -0500

    fix: storybook

--- apps/storybook/src/stories/core.stories.tsx
+++ apps/storybook/src/stories/core.stories.tsx
@@ -1,4 +1,5 @@
 import { routes } from '@affine/core/router';
+import type { StoryContext, StoryFn } from '@storybook/react';
 import { Outlet, useLocation } from 'react-router-dom';
 import {
   reactRouterOutlets,
@@ -6,17 +7,22 @@ import {
   withRouter,
 } from 'storybook-addon-react-router-v6';
 
+const withCleanLocalStorage = (Story: StoryFn, context: StoryContext) => {
+  localStorage.clear();
+  return <Story {...context.args} />;
+};
+
 export default {
   title: 'Preview/Core',
 };
 
-export const Index = () => {
+export const Index: StoryFn = () => {
   const location = useLocation();
   // fixme: `key` is a hack to force the storybook to re-render the outlet
   return <Outlet key={location.pathname} />;
 };
 
-Index.decorators = [withRouter];
+Index.decorators = [withRouter, withCleanLocalStorage];
 Index.parameters = {
   reactRouter: reactRouterParameters({
     routing: reactRouterOutlets(routes),
