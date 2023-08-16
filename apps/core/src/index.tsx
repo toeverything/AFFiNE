6db65b6e486c7e5d843077d22cc20b1ede8624dd
Alex Yang <himself65@outlook.com>
2023-08-16 17:38:53 -0500

    fix: bootstrap in app

--- apps/core/src/index.tsx
+++ apps/core/src/index.tsx
@@ -4,9 +4,15 @@ import { getCurrentStore } from '@toeverything/infra/atom';
 import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { bootstrapPluginSystem } from './bootstrap/register-plugins';
+
 async function main() {
   const { setup } = await import('./bootstrap/setup');
-  await setup(getCurrentStore());
+  const rootStore = getCurrentStore();
+  await setup(rootStore);
+  bootstrapPluginSystem(rootStore).catch(err => {
+    console.error('Failed to bootstrap plugin system', err);
+  });
   const { App } = await import('./app');
   const root = document.getElementById('app');
   assertExists(root);
