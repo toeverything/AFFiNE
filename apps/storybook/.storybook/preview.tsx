babff00fae14934327e79d78c757f17d37cc2d8c
Alex Yang <himself65@outlook.com>
2023-08-19 12:30:24 -0500

    feat(storybook): avoid refresh (#3841)

--- apps/storybook/.storybook/preview.tsx
+++ apps/storybook/.storybook/preview.tsx
@@ -58,11 +58,15 @@ const withContextDecorator: Decorator = (Story, context) => {
   const { data: store } = useSWR(
     context.id,
     async () => {
+      if (storeMap.has(context.id)) {
+        return storeMap.get(context.id);
+      }
       localStorage.clear();
       const store = createStore();
       _setCurrentStore(store);
       await setup(store);
       await bootstrapPluginSystem(store);
+      storeMap.set(context.id, store);
       return store;
     },
     {
