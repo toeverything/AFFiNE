fe48a3e113f6ccbb44b9cbab30e6a102fc9a91cf
Alex Yang <himself65@outlook.com>
2023-08-19 12:09:27 -0500

    fix: dynamic import

--- apps/core/src/bootstrap/plugins/setup.ts
+++ apps/core/src/bootstrap/plugins/setup.ts
@@ -28,6 +28,7 @@ import { createFetch } from './endowments/fercher';
 import { createTimers } from './endowments/timer';
 import { setupImportsMap } from './setup-imports-map';
 
+// DO NOT REMOVE INVISIBLE CHARACTERS
 const dynamicImportKey = '$h‍_import';
 
 const permissionLogger = new DebugLogger('plugins:permission');
