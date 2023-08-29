c4b5dcd175ef6159349d8a6c72ca8aa2c7baeabd
LongYinan <lynweklm@gmail.com>
2023-08-29 12:03:17 +0800

    Add send emails logging

--- apps/server/src/modules/auth/next-auth-options.ts
+++ apps/server/src/modules/auth/next-auth-options.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from 'node:crypto';
 
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { BadRequestException, FactoryProvider } from '@nestjs/common';
+import { BadRequestException, FactoryProvider, Logger } from '@nestjs/common';
 import { verify } from '@node-rs/argon2';
 import { Algorithm, sign, verify as jwtVerify } from '@node-rs/jsonwebtoken';
 import { NextAuthOptions } from 'next-auth';
@@ -35,6 +35,7 @@ function wrapUrlWithSchema(url: string, schema: string | null) {
 export const NextAuthOptionsProvider: FactoryProvider<NextAuthOptions> = {
   provide: NextAuthOptionsProvide,
   useFactory(config: Config, prisma: PrismaService, mailer: MailService) {
+    const logger = new Logger('NextAuth');
     const prismaAdapter = PrismaAdapter(prisma);
     // createUser exists in the adapter
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -97,6 +98,9 @@ export const NextAuthOptionsProvider: FactoryProvider<NextAuthOptions> = {
               text: text({ url: wrappedUrl, host }),
               html: html({ url: wrappedUrl, host }),
             });
+            logger.debug(
+              `send verification email success: ${result.accepted.join(', ')}`
+            );
             const failed = result.rejected
               .concat(result.pending)
               .filter(Boolean);
