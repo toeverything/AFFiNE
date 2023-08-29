3b7f20f8fb2a4616c5d5630cac98860b73040f6a
QiShaoXuan <qishaoxuan777@gmail.com>
2023-08-29 22:05:17 +0800

    feat: modify comment

--- packages/component/src/ui/menu/index.ts
+++ packages/component/src/ui/menu/index.ts
@@ -1,3 +1,4 @@
-// @deprecated Use @toeverything/components/menu instead, this component only used in bookmark plugin, since it support set anchor as Range
+// @deprecated
+// Use @toeverything/components/menu instead, this component only used in bookmark plugin, since it support set anchor as Range
 export * from './menu-item';
 export * from './pure-menu';
