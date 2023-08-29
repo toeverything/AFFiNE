393a45bba5fa73e8c800095959e7be52f31cc077
Alex Yang <himself65@outlook.com>
2023-08-29 15:24:49 -0500

    test: update test

--- tests/affine-cloud/e2e/basic.spec.ts
+++ tests/affine-cloud/e2e/basic.spec.ts
@@ -3,9 +3,9 @@ import {
   createRandomUser,
   deleteUser,
   getLoginCookie,
+  loginUser,
 } from '@affine-test/kit/utils/cloud';
 import { waitEditorLoad } from '@affine-test/kit/utils/page-logic';
-import { clickSideBarCurrentWorkspaceBanner } from '@affine-test/kit/utils/sidebar';
 import { expect } from '@playwright/test';
 
 let user: {
@@ -30,29 +30,15 @@ test('server exist', async () => {
 });
 
 test('enable cloud success', async ({ page, context }) => {
-  await page.goto('http://localhost:8080');
-  await page.waitForSelector('v-line');
-
-  await clickSideBarCurrentWorkspaceBanner(page);
-  await page.getByTestId('cloud-signin-button').click({
-    delay: 200,
+  await loginUser(page, user, {
+    beforeLogin: async () => {
+      expect(await getLoginCookie(context)).toBeUndefined();
+    },
+    afterLogin: async () => {
+      expect(await getLoginCookie(context)).toBeTruthy();
+      await page.reload();
+      await waitEditorLoad(page);
+      expect(await getLoginCookie(context)).toBeTruthy();
+    },
   });
-  await page.getByPlaceholder('Enter your email address').type(user.email, {
-    delay: 50,
-  });
-  await page.getByTestId('continue-login-button').click({
-    delay: 200,
-  });
-  await page.getByTestId('sign-in-with-password').click({
-    delay: 200,
-  });
-  await page.getByTestId('password-input').type('123456', {
-    delay: 50,
-  });
-  expect(await getLoginCookie(context)).toBeUndefined();
-  await page.getByTestId('sign-in-button').click();
-  await page.waitForTimeout(1000);
-  await page.reload();
-  await waitEditorLoad(page);
-  expect(await getLoginCookie(context)).toBeTruthy();
 });
