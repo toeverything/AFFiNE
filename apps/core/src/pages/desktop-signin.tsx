463750e996a0bde23adb05f1c754a315008d4aed
Alex Yang <himself65@outlook.com>
2023-08-30 21:30:36 -0500

    fix: api

--- apps/core/src/pages/desktop-signin.tsx
+++ apps/core/src/pages/desktop-signin.tsx
@@ -1,35 +1,31 @@
 import { signIn, type SignInResponse } from 'next-auth/react';
-import { useEffect, useMemo } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import type { LoaderFunction } from 'react-router-dom';
 import { z } from 'zod';
 
 const supportedProvider = z.enum(['google']);
 
-let wip = false;
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const searchParams = url.searchParams;
+  const provider = searchParams.get('provider');
+  const callback_url = searchParams.get('callback_url');
+  if (!callback_url) {
+    return null;
+  }
+  const maybeProvider = supportedProvider.safeParse(provider);
+  if (maybeProvider.success) {
+    const provider = maybeProvider.data;
+    await signIn(provider, {
+      callbackUrl: callback_url,
+    }).catch((res: SignInResponse | undefined) => {
+      if (res?.error) {
+        console.error(res.error);
+      }
+    });
+  }
+  return null;
+};
 
 export const Component = () => {
-  const [params] = useSearchParams();
-  const provider = useMemo(() => {
-    const maybeProvider = supportedProvider.safeParse(params.get('provider'));
-    return maybeProvider.success ? maybeProvider.data : null;
-  }, [params]);
-
-  const callback_url = params.get('callback_url');
-
-  useEffect(() => {
-    if (wip) {
-      return;
-    }
-    if (provider && callback_url) {
-      wip = true;
-      signIn(provider, {
-        callbackUrl: callback_url,
-      }).catch((res: SignInResponse | undefined) => {
-        if (res?.error) {
-          console.error(res.error);
-        }
-      });
-    }
-  }, [provider, callback_url]);
   return null;
 };
