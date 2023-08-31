97e55bf0dc2205e72d644a240d082923b90c8b11
DarkSky <darksky2048@gmail.com>
2023-08-31 12:23:01 +0800

    feat: blob size test

--- apps/server/src/tests/workspace-blobs.spec.ts
+++ apps/server/src/tests/workspace-blobs.spec.ts
@@ -50,6 +50,33 @@ describe('Workspace Module - Blobs', () => {
     await app.close();
   });
 
+  it('should set blobs', async () => {
+    const u1 = await signUp(app, 'u1', 'u1@affine.pro', '1');
+
+    const workspace = await createWorkspace(app, u1.token.token);
+
+    const buffer1 = Buffer.from([0, 0]);
+    const hash1 = await setBlob(app, u1.token.token, workspace.id, buffer1);
+    const buffer2 = Buffer.from([0, 1]);
+    const hash2 = await setBlob(app, u1.token.token, workspace.id, buffer2);
+
+    const server = app.getHttpServer();
+
+    const response1 = await request(server)
+      .get(`/api/workspaces/${workspace.id}/blobs/${hash1}`)
+      .auth(u1.token.token, { type: 'bearer' })
+      .buffer();
+
+    deepEqual(response1.body, buffer1, 'failed to get blob');
+
+    const response2 = await request(server)
+      .get(`/api/workspaces/${workspace.id}/blobs/${hash2}`)
+      .auth(u1.token.token, { type: 'bearer' })
+      .buffer();
+
+    deepEqual(response2.body, buffer2, 'failed to get blob');
+  });
+
   it('should list blobs', async () => {
     const u1 = await signUp(app, 'u1', 'u1@affine.pro', '1');
 
@@ -66,24 +93,19 @@ describe('Workspace Module - Blobs', () => {
     ok(ret.length === 2, 'failed to list blobs');
     ok(ret[0] === hash1, 'failed to list blobs');
     ok(ret[1] === hash2, 'failed to list blobs');
+  });
+
+  it('should calc blobs size', async () => {
+    const u1 = await signUp(app, 'u1', 'u1@affine.pro', '1');
+
+    const workspace = await createWorkspace(app, u1.token.token);
+
+    const buffer1 = Buffer.from([0, 0]);
+    await setBlob(app, u1.token.token, workspace.id, buffer1);
+    const buffer2 = Buffer.from([0, 1]);
+    await setBlob(app, u1.token.token, workspace.id, buffer2);
 
     const size = await collectBlobSizes(app, u1.token.token, workspace.id);
     ok(size === 4, 'failed to collect blob sizes');
-
-    const server = app.getHttpServer();
-
-    const token = u1.token.token;
-    const response1 = await request(server)
-      .get(`/api/workspaces/${workspace.id}/blobs/${hash1}`)
-      .auth(token, { type: 'bearer' })
-      .buffer();
-
-    deepEqual(response1.body, buffer1, 'failed to get blob');
-
-    const response2 = await request(server)
-      .get(`/api/workspaces/${workspace.id}/blobs/${hash2}`)
-      .auth(token, { type: 'bearer' })
-      .buffer();
-    deepEqual(response2.body, buffer2, 'failed to get blob');
   });
 });
