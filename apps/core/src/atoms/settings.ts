92963e391187fe6024515569b08c643fb7ff3019
JimmFly <yangjinfei001@gmail.com>
2023-08-31 11:27:40 +0800

    fix: use is desktop

--- apps/core/src/atoms/settings.ts
+++ apps/core/src/atoms/settings.ts
@@ -49,7 +49,7 @@ export const fontStyleOptions = [
 }[];
 
 const appSettingBaseAtom = atomWithStorage<AppSetting>('affine-settings', {
-  clientBorder: !environment.isBrowser && globalThis.platform !== 'win32',
+  clientBorder: environment.isDesktop && globalThis.platform !== 'win32',
   fullWidthLayout: false,
   windowFrameStyle: 'frameless',
   fontStyle: 'Sans',
