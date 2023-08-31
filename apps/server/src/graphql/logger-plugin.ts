0590bfacdab87b92e0334d885838f1f2759d0c8d
x1a0t <thorseraq@gmail.com>
2023-08-31 14:49:55 +0800

    remove graphql log

--- apps/server/src/graphql/logger-plugin.ts
+++ apps/server/src/graphql/logger-plugin.ts
@@ -7,7 +7,6 @@ import { Plugin } from '@nestjs/apollo';
 import { Logger } from '@nestjs/common';
 import { Response } from 'express';
 
-import { OPERATION_NAME, REQUEST_ID } from '../constants';
 import { Metrics } from '../metrics/metrics';
 import { ReqContext } from '../types';
 
@@ -22,18 +21,9 @@ export class GQLLoggerPlugin implements ApolloServerPlugin {
   ): Promise<GraphQLRequestListener<GraphQLRequestContext<ReqContext>>> {
     const res = reqContext.contextValue.req.res as Response;
     const operation = reqContext.request.operationName;
-    const headers = reqContext.request.http?.headers;
-    const requestId = headers
-      ? headers.get(`${REQUEST_ID}`)
-      : 'Unknown Request ID';
-    const operationName = headers
-      ? headers.get(`${OPERATION_NAME}`)
-      : 'Unknown Operation Name';
 
     this.metrics.gqlRequest(1, { operation });
     const timer = this.metrics.gqlTimer({ operation });
-
-    const requestInfo = `${REQUEST_ID}: ${requestId}, ${OPERATION_NAME}: ${operationName}`;
 
     return Promise.resolve({
       willSendResponse: () => {
@@ -42,7 +32,6 @@ export class GQLLoggerPlugin implements ApolloServerPlugin {
           'Server-Timing',
           `gql;dur=${costInMilliseconds};desc="GraphQL"`
         );
-        this.logger.log(requestInfo);
         return Promise.resolve();
       },
       didEncounterErrors: () => {
@@ -52,7 +41,6 @@ export class GQLLoggerPlugin implements ApolloServerPlugin {
           'Server-Timing',
           `gql;dur=${costInMilliseconds};desc="GraphQL ${operation}"`
         );
-        this.logger.error(`${requestInfo}, query: ${reqContext.request.query}`);
         return Promise.resolve();
       },
     });
