254bd33a8fb0b77639fb2dd1c01cb0b6d3694a31
x1a0t <thorseraq@gmail.com>
2023-08-31 11:55:40 +0800

    feat: exception logger

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from './config';
 import { MetricsModule } from './metrics';
-import { BusinessModules } from './modules';
+import { BusinessModules, Providers } from './modules';
 import { PrismaModule } from './prisma';
 import { StorageModule } from './storage';
 
@@ -15,6 +15,7 @@ import { StorageModule } from './storage';
     MetricsModule,
     ...BusinessModules,
   ],
+  providers: Providers,
   controllers: [AppController],
 })
 export class AppModule {}
