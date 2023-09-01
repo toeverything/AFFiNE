d4a83c1c6fa79fc37050987add6f26b83a5ebf65
X1a0t <405028157@qq.com>
2023-09-01 12:05:35 +0800

    feat: exception logger (#4059)

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from './config';
 import { MetricsModule } from './metrics';
-import { BusinessModules } from './modules';
+import { BusinessModules, Providers } from './modules';
 import { PrismaModule } from './prisma';
 import { StorageModule } from './storage';
 import { RateLimiterModule } from './throttler';
@@ -17,6 +17,7 @@ import { RateLimiterModule } from './throttler';
     RateLimiterModule,
     ...BusinessModules,
   ],
+  providers: Providers,
   controllers: [AppController],
 })
 export class AppModule {}
