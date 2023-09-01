8cead1ab58a9b2ebc03b045aa24a74de91aef41e
DarkSky <darksky2048@gmail.com>
2023-09-01 20:27:15 +0800

    feat: add user level blob quota

--- apps/server/src/config/def.ts
+++ apps/server/src/config/def.ts
@@ -186,6 +186,11 @@ export interface AFFiNEConfig {
     fs: {
       path: string;
     };
+    /**
+     * Free user storage quota
+     * @default 10 * 1024 * 1024 (10GB)
+     */
+    quota: number;
   };
 
   /**
