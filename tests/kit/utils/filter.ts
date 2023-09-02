afe700ed42000ce4067fff033caf768b21cc02bb
Alex Yang <himself65@outlook.com>
2023-09-02 00:56:02 -0500

    fix: lint

--- tests/kit/utils/filter.ts
+++ tests/kit/utils/filter.ts
@@ -1,7 +1,7 @@
 import type { Page } from '@playwright/test';
 import { expect } from '@playwright/test';
 
-import { clickNewPageButton,getBlockSuiteEditorTitle } from './page-logic';
+import { clickNewPageButton, getBlockSuiteEditorTitle } from './page-logic';
 
 const monthNames = [
   'Jan',
