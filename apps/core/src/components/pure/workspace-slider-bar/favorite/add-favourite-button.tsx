d4259d77566e05789db5803baf32279e195da580
Alex Yang <himself65@outlook.com>
2023-09-04 09:58:09 -0700

    fix: type

--- apps/core/src/components/pure/workspace-slider-bar/favorite/add-favourite-button.tsx
+++ apps/core/src/components/pure/workspace-slider-bar/favorite/add-favourite-button.tsx
@@ -1,4 +1,3 @@
-import { assertExists } from '@blocksuite/global/utils';
 import { PlusIcon } from '@blocksuite/icons';
 import type { Workspace } from '@blocksuite/store';
 import { IconButton } from '@toeverything/components/button';
@@ -7,12 +6,15 @@ import { useCallback } from 'react';
 
 import { usePageHelper } from '../../../blocksuite/block-suite-page-list/utils';
 
-export const AddFavouriteButton = ({ workspace }: { workspace: Workspace }) => {
+type AddFavouriteButtonProps = {
+  workspace: Workspace;
+};
+
+export const AddFavouriteButton = ({ workspace }: AddFavouriteButtonProps) => {
   const { createPage } = usePageHelper(workspace);
   const { setPageMeta } = usePageMetaHelper(workspace);
   const handleAddFavorite = useCallback(async () => {
     const id = createPage();
-    assertExists(id);
     setPageMeta(id, { favorite: true });
   }, [createPage, setPageMeta]);
 
