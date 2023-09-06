c9b7f2f2b605c75c40a122f2cde6366cfc760fbc
x1a0t <thorseraq@gmail.com>
2023-09-06 14:13:57 +0800

    fix: logger filter should pass graphql context

---
+++ apps/server/src/tests/exception-logger.e2e.ts
@@ -0,0 +1,85 @@
+import { Controller, Get, INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import test from 'ava';
+// @ts-expect-error graphql-upload is not typed
+import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.mjs';
+import request from 'supertest';
+
+import { AppModule } from '../app';
+import { ExceptionLogger } from '../middleware/exception-logger';
+import { PrismaService } from '../prisma';
+
+const gql = '/graphql';
+const rest = '/rest';
+
+let app: INestApplication;
+
+class FakePrisma {
+  get workspace() {
+    return {
+      async findUnique() {
+        throw Error('exception from graphql');
+      },
+    };
+  }
+}
+
+@Controller('rest')
+export class MockController {
+  @Get()
+  test(): string {
+    throw new Error('exception from rest api');
+  }
+}
+
+test.beforeEach(async () => {
+  const module = await Test.createTestingModule({
+    imports: [AppModule],
+    controllers: [MockController],
+  })
+    .overrideProvider(PrismaService)
+    .useClass(FakePrisma)
+    .compile();
+  app = module.createNestApplication({
+    cors: true,
+    bodyParser: true,
+  });
+  app.useGlobalFilters(new ExceptionLogger());
+  app.use(
+    graphqlUploadExpress({
+      maxFileSize: 10 * 1024 * 1024,
+      maxFiles: 5,
+    })
+  );
+  await app.init();
+});
+
+test.afterEach(async () => {
+  await app.close();
+});
+
+test('should get response', async t => {
+  const id = 'workspace';
+
+  const response = await request(app.getHttpServer())
+    .post(gql)
+    .send({
+      name: 'getPublicWorkspace',
+      query: `
+          query getPublicWorkspace($id: String!) {
+            publicWorkspace(id: $id) {
+              id
+            }
+          }
+        `,
+      variables: { id },
+    });
+
+  t.is(response.status, 200);
+});
+
+test('should get response from rest api', async t => {
+  const response = await request(app.getHttpServer()).get(rest);
+
+  t.is(response.status, 500);
+});
