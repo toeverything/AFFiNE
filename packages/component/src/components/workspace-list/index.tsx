fd05bb3e3950cfebd77fd67f4951ab389e5cd9b0
Peng Xiao <pengxiao@outlook.com>
2023-09-07 23:20:48 +0800

    fix: better transition (#4267)

--- packages/component/src/components/workspace-list/index.tsx
+++ packages/component/src/components/workspace-list/index.tsx
@@ -14,9 +14,9 @@ import {
   restrictToParentElement,
   restrictToVerticalAxis,
 } from '@dnd-kit/modifiers';
-import { SortableContext, useSortable } from '@dnd-kit/sortable';
+import { arrayMove, SortableContext, useSortable } from '@dnd-kit/sortable';
 import type { CSSProperties } from 'react';
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { WorkspaceCard } from '../../components/card/workspace-card';
 import { workspaceItemStyle } from './index.css';
@@ -35,18 +35,20 @@ interface SortableWorkspaceItemProps extends Omit<WorkspaceListProps, 'items'> {
 }
 
 const SortableWorkspaceItem = (props: SortableWorkspaceItemProps) => {
-  const { setNodeRef, attributes, listeners, transform } = useSortable({
-    id: props.item.id,
-  });
+  const { setNodeRef, attributes, listeners, transform, transition } =
+    useSortable({
+      id: props.item.id,
+    });
   const style: CSSProperties = useMemo(
     () => ({
       transform: transform
         ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
         : undefined,
+      transition,
       pointerEvents: props.disabled ? 'none' : undefined,
       opacity: props.disabled ? 0.6 : undefined,
     }),
-    [props.disabled, transform]
+    [props.disabled, transform, transition]
   );
   return (
     <div
@@ -77,14 +79,33 @@ export const WorkspaceList = (props: WorkspaceListProps) => {
       },
     })
   );
+  const workspaceList = props.items;
+  const [optimisticList, setOptimisticList] = useState(workspaceList);
+
+  useEffect(() => {
+    setOptimisticList(workspaceList);
+  }, [workspaceList]);
+
+  const onDragEnd = useCallback(
+    (event: DragEndEvent) => {
+      const { active, over } = event;
+      if (active.id !== over?.id) {
+        setOptimisticList(workspaceList => {
+          const oldIndex = workspaceList.findIndex(w => w.id === active.id);
+          const newIndex = workspaceList.findIndex(w => w.id === over?.id);
+          const newList = arrayMove(workspaceList, oldIndex, newIndex);
+          return newList;
+        });
+        props.onDragEnd(event);
+      }
+    },
+    [props]
+  );
+
   return (
-    <DndContext
-      sensors={sensors}
-      onDragEnd={props.onDragEnd}
-      modifiers={modifiers}
-    >
-      <SortableContext items={props.items}>
-        {props.items.map(item => (
+    <DndContext sensors={sensors} onDragEnd={onDragEnd} modifiers={modifiers}>
+      <SortableContext items={optimisticList}>
+        {optimisticList.map(item => (
           <SortableWorkspaceItem {...props} item={item} key={item.id} />
         ))}
       </SortableContext>
