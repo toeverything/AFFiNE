dd82304d21476c7508ca877973f0536d28764ea6
DarkSky <darksky2048@gmail.com>
2023-09-08 14:01:24 +0800

    chore: bump default api rate limit to 120/m

--- apps/server/src/config/def.ts
+++ apps/server/src/config/def.ts
@@ -205,7 +205,7 @@ export interface AFFiNEConfig {
     ttl: number;
     /**
      * How many requests can be made in the given time frame
-     * @default 60
+     * @default 120
      * @env THROTTLE_LIMIT
      */
     limit: number;
