6d28d0042272bf720e93cbf8f2966fbee545630d
Alex Yang <himself65@outlook.com>
2023-09-12 14:48:31 -0700

    fix(core): session provider

--- apps/core/src/providers/session-provider.tsx
+++ apps/core/src/providers/session-provider.tsx
@@ -7,12 +7,17 @@ import { refreshRootMetadataAtom } from '@affine/workspace/atom';
 import { useAtom, useSetAtom } from 'jotai';
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { SessionProvider, useSession } from 'next-auth/react';
-import { type PropsWithChildren, startTransition, useRef } from 'react';
+import {
+  type PropsWithChildren,
+  startTransition,
+  useEffect,
+  useRef,
+} from 'react';
 
 import { sessionAtom } from '../atoms/cloud-user';
 import { useOnceSignedInEvents } from '../atoms/event';
 
-const SessionDefence = (props: PropsWithChildren) => {
+const useRefreshHook = () => {
   const session = useSession();
   const prevSession = useRef<ReturnType<typeof useSession>>();
   const [sessionInAtom, setSession] = useAtom(sessionAtom);
@@ -21,40 +26,46 @@ const SessionDefence = (props: PropsWithChildren) => {
   const onceSignedInEvents = useOnceSignedInEvents();
   const t = useAFFiNEI18N();
 
-  if (sessionInAtom !== session && session.status === 'authenticated') {
-    setSession(session);
-  }
-
-  if (prevSession.current !== session && session.status !== 'loading') {
-    // unauthenticated -> authenticated
-    if (
-      prevSession.current?.status === 'unauthenticated' &&
-      session.status === 'authenticated'
-    ) {
-      startTransition(() => {
-        onceSignedInEvents().then(() => {
-          refreshMetadata();
-        });
-      });
-      pushNotification({
-        title: t['com.affine.auth.has.signed'](),
-        message: t['com.affine.auth.has.signed.message'](),
-        type: 'success',
-      });
-
-      if (isDesktop) {
-        window.affine.ipcRenderer.send('affine:login');
-      }
+  useEffect(() => {
+    if (sessionInAtom !== session && session.status === 'authenticated') {
+      setSession(session);
     }
-    prevSession.current = session;
-  }
-  return props.children;
+
+    if (prevSession.current !== session && session.status !== 'loading') {
+      // unauthenticated -> authenticated
+      if (
+        prevSession.current?.status === 'unauthenticated' &&
+        session.status === 'authenticated'
+      ) {
+        startTransition(() => {
+          onceSignedInEvents().then(() => {
+            refreshMetadata();
+          });
+        });
+        pushNotification({
+          title: t['com.affine.auth.has.signed'](),
+          message: t['com.affine.auth.has.signed.message'](),
+          type: 'success',
+        });
+
+        if (isDesktop) {
+          window.affine.ipcRenderer.send('affine:login');
+        }
+      }
+      prevSession.current = session;
+    }
+  }, [
+    onceSignedInEvents,
+    pushNotification,
+    refreshMetadata,
+    session,
+    sessionInAtom,
+    setSession,
+    t,
+  ]);
 };
 
 export const CloudSessionProvider = ({ children }: PropsWithChildren) => {
-  return (
-    <SessionProvider refetchOnWindowFocus>
-      <SessionDefence>{children}</SessionDefence>
-    </SessionProvider>
-  );
+  useRefreshHook();
+  return <SessionProvider refetchOnWindowFocus>{children}</SessionProvider>;
 };
