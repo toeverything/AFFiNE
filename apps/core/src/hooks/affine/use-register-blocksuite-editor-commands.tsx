6336c3a7887cd2d1962757bbdab6d23eecc78fb5
JimmFly <yangjinfei001@gmail.com>
2023-09-27 14:10:03 +0800

    refactor(core): move editor commands hooks to core

--- apps/core/src/hooks/affine/use-register-blocksuite-editor-commands.tsx
+++ apps/core/src/hooks/affine/use-register-blocksuite-editor-commands.tsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 
 export function useRegisterBlocksuiteEditorCommands() {
   const t = useAFFiNEI18N();
+
   useEffect(() => {
     const unsubs: Array<() => void> = [];
     const getEdgeless = () => {
