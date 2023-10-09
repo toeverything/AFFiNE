70c9de252c1724d703b5e686f651c966d226c35b
Alex Yang <himself65@outlook.com>
2023-10-09 13:07:24 -0500

    fix(infra): create template workspace with isolated nanoid

--- packages/env/src/constant.ts
+++ packages/env/src/constant.ts
@@ -17,7 +17,6 @@ export const isDesktop = isBrowser && !!window.appInfo?.electron;
 //#endregion
 export const DEFAULT_WORKSPACE_NAME = 'Demo Workspace';
 export const UNTITLED_WORKSPACE_NAME = 'Untitled';
-export const DEFAULT_HELLO_WORLD_PAGE_ID_SUFFIX = 'hello-world';
 
 export const DEFAULT_SORT_KEY = 'updatedDate';
 export const MessageCode = {
