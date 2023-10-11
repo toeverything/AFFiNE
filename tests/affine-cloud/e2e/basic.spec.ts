28ba5dc20b6d3a6dc0ce80f1d0df8a51d9408215
Alex Yang <himself65@outlook.com>
2023-10-11 16:30:37 -0500

    test: fix migration

--- tests/affine-cloud/e2e/basic.spec.ts
+++ tests/affine-cloud/e2e/basic.spec.ts
@@ -100,8 +100,8 @@ test.describe('basic', () => {
       timeout: 60000,
     });
     await page.goto(
-      // page '0773e198-5de0-45d4-a35e-de22ea72b96b' has edgeless mode
-      `${coreUrl}/workspace/${workspaceId}/0773e198-5de0-45d4-a35e-de22ea72b96b`
+      // page 'F1SX6cgNxy' has edgeless mode
+      `${coreUrl}/workspace/${workspaceId}/F1SX6cgNxy`
     );
     await waitForEditorLoad(page);
     await clickEdgelessModeButton(page);
