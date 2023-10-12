232fda968998b16d1b396ac90575842a16c800b0
JimmFly <yangjinfei001@gmail.com>
2023-10-12 21:21:47 +0800

    refactor(core): optimize sign out modal performance and readability

--- apps/core/src/components/affine/sign-out-modal/index.tsx
+++ apps/core/src/components/affine/sign-out-modal/index.tsx
@@ -3,25 +3,39 @@ import {
   ConfirmModal,
   type ConfirmModalProps,
 } from '@toeverything/components/modal';
+import { useMemo } from 'react';
+
+type SignOutConfirmModalI18NKeys =
+  | 'title'
+  | 'description'
+  | 'cancel'
+  | 'confirm';
 
 export const SignOutModal = ({ ...props }: ConfirmModalProps) => {
+  const { title, description, cancelText, confirmButtonOptions = {} } = props;
   const t = useAFFiNEI18N();
+
+  const defaultTexts = useMemo(() => {
+    const getDefaultText = (key: SignOutConfirmModalI18NKeys) => {
+      return t[`com.affine.auth.sign-out.confirm-modal.${key}`]();
+    };
+    return {
+      title: getDefaultText('title'),
+      description: getDefaultText('description'),
+      cancelText: getDefaultText('cancel'),
+      children: getDefaultText('confirm'),
+    };
+  }, [t]);
+
   return (
     <ConfirmModal
-      title={props.title ?? t['com.affine.auth.sign-out.confirm-modal.title']()}
-      description={
-        props.description ??
-        t['com.affine.auth.sign-out.confirm-modal.description']()
-      }
-      cancelText={
-        props.cancelText ?? t['com.affine.auth.sign-out.confirm-modal.cancel']()
-      }
+      title={title ?? defaultTexts.title}
+      description={description ?? defaultTexts.description}
+      cancelText={cancelText ?? defaultTexts.cancelText}
       confirmButtonOptions={{
         type: 'error',
         ['data-testid' as string]: 'confirm-sign-out-button',
-        children:
-          props.confirmButtonOptions?.children ??
-          t['com.affine.auth.sign-out.confirm-modal.confirm'](),
+        children: confirmButtonOptions.children ?? defaultTexts.children,
       }}
       contentOptions={{
         ['data-testid' as string]: 'confirm-sign-out-modal',
