1d06114f00534734b10e8409bb8aeb99a47bb499
Alex Yang <himself65@outlook.com>
2023-10-16 16:25:09 -0500

    build(y-indexeddb): fix output package.json (#4640)

--- packages/y-indexeddb/vite.config.ts
+++ packages/y-indexeddb/vite.config.ts
@@ -8,16 +8,21 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   build: {
-    terserOptions: {
-      ecma: 2020,
-    },
+    minify: 'esbuild',
     sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       fileName: 'index',
       name: 'ToEverythingIndexedDBProvider',
+      formats: ['es', 'cjs', 'umd'],
     },
     rollupOptions: {
+      output: {
+        globals: {
+          idb: 'idb',
+          yjs: 'yjs',
+        },
+      },
       external: ['idb', 'yjs'],
     },
   },
