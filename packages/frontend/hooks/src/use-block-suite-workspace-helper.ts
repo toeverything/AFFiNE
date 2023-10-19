9958baa843a780bd67324af10ed6fb0705b20bba
Alex Yang <himself65@outlook.com>
2023-10-18 22:14:41 -0500

    fix(hooks): use uuid as pageId (#4658)

--- packages/frontend/hooks/src/use-block-suite-workspace-helper.ts
+++ packages/frontend/hooks/src/use-block-suite-workspace-helper.ts
@@ -1,10 +1,14 @@
 import type { Page, Workspace } from '@blocksuite/store';
 import { useMemo } from 'react';
+import { v4 as uuid } from 'uuid';
 
 export function useBlockSuiteWorkspaceHelper(blockSuiteWorkspace: Workspace) {
   return useMemo(
     () => ({
       createPage: (pageId?: string): Page => {
+        if (!pageId) {
+          pageId = uuid();
+        }
         return blockSuiteWorkspace.createPage({ id: pageId });
       },
     }),
