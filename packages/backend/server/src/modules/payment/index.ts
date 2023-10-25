9d108662a118a97fc94077a06653f71b8e0e8e4c
forehalo <forehalo@gmail.com>
2023-10-25 15:26:04 +0800

    feat(server): auto attach early access coupon

--- packages/backend/server/src/modules/payment/index.ts
+++ packages/backend/server/src/modules/payment/index.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 
+import { UsersModule } from '../users';
 import { SubscriptionResolver, UserSubscriptionResolver } from './resolver';
+import { ScheduleManager } from './schedule';
 import { SubscriptionService } from './service';
 import { StripeProvider } from './stripe';
 import { StripeWebhook } from './webhook';
 
 @Module({
+  imports: [UsersModule],
   providers: [
+    ScheduleManager,
     StripeProvider,
     SubscriptionService,
     SubscriptionResolver,
