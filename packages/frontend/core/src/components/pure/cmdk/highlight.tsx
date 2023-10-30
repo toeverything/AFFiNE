690c6d24879dadfc118300bd2b9e9e019800b87e
JimmFly <447268514@qq.com>
2023-10-30 16:02:36 +0800

    refactor(core): support fuzzy highlighting

--- packages/frontend/core/src/components/pure/cmdk/highlight.tsx
+++ packages/frontend/core/src/components/pure/cmdk/highlight.tsx
@@ -1,13 +1,6 @@
-import { escapeRegExp } from 'lodash-es';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 
-import {
-  highlightContainer,
-  highlightKeyword,
-  highlightText,
-  labelContent,
-  labelTitle,
-} from './highlight.css';
+import * as styles from './highlight.css';
 
 type SearchResultLabel = {
   title: string;
@@ -24,36 +17,85 @@ type HighlightLabelProps = {
   highlight: string;
 };
 
+function useHighlight(text: string, query: string) {
+  return useMemo(() => {
+    const highlights = [];
+
+    const lowerCaseText = text.toLowerCase();
+    let startIndex = lowerCaseText.indexOf(query);
+
+    if (startIndex !== -1) {
+      // Full match found, highlight it
+      if (startIndex > 0) {
+        highlights.push({
+          text: text.substring(0, startIndex),
+          highlight: false,
+        });
+      }
+
+      highlights.push({
+        text: text.substring(startIndex, startIndex + query.length),
+        highlight: true,
+      });
+
+      if (startIndex + query.length < text.length) {
+        highlights.push({
+          text: text.substring(startIndex + query.length),
+          highlight: false,
+        });
+      }
+    } else {
+      // No full match found, so we proceed to partial matching
+      startIndex = 0;
+      for (const char of query) {
+        const pos = text.toLowerCase().indexOf(char, startIndex);
+        if (pos !== -1) {
+          if (pos > startIndex) {
+            highlights.push({
+              text: text.substring(startIndex, pos),
+              highlight: false,
+            });
+          }
+          highlights.push({
+            text: text.substring(pos, pos + 1),
+            highlight: true,
+          });
+          startIndex = pos + 1;
+        }
+      }
+      if (startIndex < text.length) {
+        highlights.push({
+          text: text.substring(startIndex),
+          highlight: false,
+        });
+      }
+    }
+    return highlights;
+  }, [text, query]);
+}
+
 export const Highlight = memo(function Highlight({
   text = '',
   highlight = '',
 }: HighlightProps) {
-  //Regex is used to ignore case
-  const regex = highlight.trim()
-    ? new RegExp(`(${escapeRegExp(highlight)})`, 'ig')
-    : null;
+  // Use regular expression to replace all line breaks and carriage returns in the text
+  const cleanedText = text.replace(/\r?\n|\r/g, '');
 
-  if (!regex) {
-    return <span>{text}</span>;
-  }
-
-  //Replacing \n part
-  text = text.replace(/(\r\n|\n|\r)/gm, '');
-  const parts = text.split(regex);
+  const highlights = useHighlight(cleanedText, highlight.toLowerCase());
 
   return (
-    <div className={highlightContainer}>
-      {parts.map((part, i) => {
-        if (regex.test(part)) {
+    <div className={styles.highlightContainer}>
+      {highlights.map((part, i) => {
+        if (part.highlight) {
           return (
-            <span key={i} className={highlightKeyword}>
-              {part}
+            <span key={i} className={styles.highlightKeyword}>
+              {part.text}
             </span>
           );
         } else {
           return (
-            <span key={i} className={highlightText}>
-              {part}
+            <span key={i} className={styles.highlightText}>
+              {part.text}
             </span>
           );
         }
@@ -68,11 +110,11 @@ export const HighlightLabel = memo(function HighlightLabel({
 }: HighlightLabelProps) {
   return (
     <div>
-      <div className={labelTitle}>
+      <div className={styles.labelTitle}>
         <Highlight text={label.title} highlight={highlight} />
       </div>
       {label.subTitle ? (
-        <div className={labelContent}>
+        <div className={styles.labelContent}>
           <Highlight text={label.subTitle} highlight={highlight} />
         </div>
       ) : null}
