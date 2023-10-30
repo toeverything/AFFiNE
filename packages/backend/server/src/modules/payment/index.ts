7e153165179fe7db2e6963489851d0d5062b8205
DarkSky <darksky2048@gmail.com>
2023-10-30 18:50:35 +0800

    feat: use new  early access check api

--- packages/backend/server/src/modules/payment/index.ts
+++ packages/backend/server/src/modules/payment/index.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { UsersModule } from '../users';
+import { QuotaModule } from '../quota';
 import { SubscriptionResolver, UserSubscriptionResolver } from './resolver';
 import { ScheduleManager } from './schedule';
 import { SubscriptionService } from './service';
@@ -8,7 +8,7 @@ import { StripeProvider } from './stripe';
 import { StripeWebhook } from './webhook';
 
 @Module({
-  imports: [UsersModule],
+  imports: [QuotaModule],
   providers: [
     ScheduleManager,
     StripeProvider,
