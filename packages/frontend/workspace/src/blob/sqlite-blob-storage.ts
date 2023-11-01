90be580f3ebf91297800e53abdb8705aea49f0ed
zhe he <zhe-he@qq.com>
2023-11-01 16:34:01 +0800

    Update sqlite-blob-storage.ts

--- packages/frontend/workspace/src/blob/sqlite-blob-storage.ts
+++ packages/frontend/workspace/src/blob/sqlite-blob-storage.ts
@@ -4,13 +4,15 @@ import type { BlobStorage } from '@blocksuite/store';
 export const createSQLiteStorage = (workspaceId: string): BlobStorage => {
   const apis = window.apis;
   assertExists(apis);
+  const typeMap = new Map();
   return {
     crud: {
       get: async (key: string) => {
         const buffer = await apis.db.getBlob(workspaceId, key);
-        return buffer ? new Blob([buffer]) : null;
+        return buffer ? new Blob([buffer], { type: typeMap.get(key) ?? buffer.type }) : null;
       },
       set: async (key: string, value: Blob) => {
+        typeMap.set(key, value.type);
         await apis.db.addBlob(
           workspaceId,
           key,
@@ -19,6 +21,7 @@ export const createSQLiteStorage = (workspaceId: string): BlobStorage => {
         return key;
       },
       delete: async (key: string) => {
+        typeMap.delete(key);
         return apis.db.deleteBlob(workspaceId, key);
       },
       list: async () => {
