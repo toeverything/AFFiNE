78ec5adb1d001e2d9820d13dd67c6c55cc056d6a
Peng Xiao <pengxiao@outlook.com>
2023-11-02 21:13:59 +0800

    fix(components): rewrite useVirtuosoItems hook

--- packages/frontend/component/src/components/page-list/virtualized-page-list.tsx
+++ packages/frontend/component/src/components/page-list/virtualized-page-list.tsx
@@ -1,6 +1,5 @@
 import type { PageMeta } from '@blocksuite/store';
 import clsx from 'clsx';
-import { atom } from 'jotai';
 import { selectAtom } from 'jotai/utils';
 import {
   forwardRef,
@@ -96,62 +95,58 @@ const PageListHeading = () => {
 };
 
 const useVirtuosoItems = () => {
-  const virtuosoItemsAtom = useMemo(
-    () =>
-      atom(get => {
-        const groups = get(pageGroupsAtom);
-        const groupCollapsedState = get(pageGroupCollapseStateAtom);
-        const items: VirtuosoItem[] = [];
+  const groups = useAtomValue(pageGroupsAtom);
+  const groupCollapsedState = useAtomValue(pageGroupCollapseStateAtom);
 
-        // 1.
-        // always put sticky header at the top
-        // the visibility of sticky header is inside of PageListTableHeader
+  return useMemo(() => {
+    const items: VirtuosoItem[] = [];
+
+    // 1.
+    // always put sticky header at the top
+    // the visibility of sticky header is inside of PageListTableHeader
+    items.push({
+      type: 'sticky-header',
+    });
+
+    // 2.
+    // iterate groups and add page items
+    for (const group of groups) {
+      // skip empty group header since it will cause issue in virtuoso ("Zero-sized element")
+      if (group.label) {
         items.push({
-          type: 'sticky-header',
+          type: 'page-group-header',
+          data: group,
         });
-
-        // 2.
-        // iterate groups and add page items
-        for (const group of groups) {
-          // skip empty group header since it will cause issue in virtuoso ("Zero-sized element")
-          if (group.label) {
+      }
+      // do not render items if the group is collapsed
+      if (!groupCollapsedState[group.id]) {
+        for (const item of group.items) {
+          items.push({
+            type: 'page-item',
+            data: item,
+          });
+          // add a spacer between items (4px), unless it's the last item
+          if (item !== group.items[group.items.length - 1]) {
             items.push({
-              type: 'page-group-header',
-              data: group,
+              type: 'page-item-spacer',
+              data: {
+                height: 4,
+              },
             });
           }
-          // do not render items if the group is collapsed
-          if (!groupCollapsedState[group.id]) {
-            for (const item of group.items) {
-              items.push({
-                type: 'page-item',
-                data: item,
-              });
-              // add a spacer between items (4px), unless it's the last item
-              if (item !== group.items[group.items.length - 1]) {
-                items.push({
-                  type: 'page-item-spacer',
-                  data: {
-                    height: 4,
-                  },
-                });
-              }
-            }
-          }
-
-          // add a spacer between groups (16px)
-          items.push({
-            type: 'page-item-spacer',
-            data: {
-              height: 16,
-            },
-          });
         }
-        return items;
-      }),
-    []
-  );
-  return useAtomValue(virtuosoItemsAtom);
+      }
+
+      // add a spacer between groups (16px)
+      items.push({
+        type: 'page-item-spacer',
+        data: {
+          height: 16,
+        },
+      });
+    }
+    return items;
+  }, [groupCollapsedState, groups]);
 };
 
 const itemContentRenderer = (_index: number, data: VirtuosoItem) => {
