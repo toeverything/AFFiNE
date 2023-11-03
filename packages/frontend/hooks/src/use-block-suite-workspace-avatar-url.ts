2cd298ed69b594289d5c25d09eaf632d9ceb61fd
JimmFly <447268514@qq.com>
2023-11-03 17:36:46 +0800

    feat(core): add files size limit to avatar

--- packages/frontend/hooks/src/use-block-suite-workspace-avatar-url.ts
+++ packages/frontend/hooks/src/use-block-suite-workspace-avatar-url.ts
@@ -3,6 +3,47 @@ import type { Workspace } from '@blocksuite/store';
 import { useCallback, useEffect, useState } from 'react';
 import useSWR from 'swr';
 
+export function validateImage(file: File) {
+  return new Promise((resolve, reject) => {
+    // check file size
+    const sizeInMB = file.size / (1024 * 1024);
+    if (sizeInMB > 10) {
+      reject('File is too large, please select a file less than 10MB');
+      return;
+    }
+
+    const img = new Image();
+    const url = URL.createObjectURL(file);
+
+    img.onload = () => {
+      // check image dimensions
+      if (img.width > 4000 || img.height > 4000) {
+        img.src = '';
+        URL.revokeObjectURL(url);
+        reject('Image dimensions are too large. Maximum size is 4K');
+        return;
+      }
+      img
+        .decode()
+        .then(() => {
+          URL.revokeObjectURL(url);
+          resolve('Image is valid.');
+        })
+        .catch(error => {
+          URL.revokeObjectURL(url);
+          reject('Image could not be decoded: ' + error);
+        });
+    };
+
+    img.onerror = error => {
+      URL.revokeObjectURL(url);
+      reject('Image could not be loaded: ' + error);
+    };
+
+    img.src = url;
+  });
+}
+
 export function useBlockSuiteWorkspaceAvatarUrl(
   blockSuiteWorkspace: Workspace
 ) {
@@ -23,21 +64,30 @@ export function useBlockSuiteWorkspaceAvatarUrl(
     suspense: true,
     fallbackData: null,
   });
+
   const setAvatar = useCallback(
-    async (file: File | null) => {
+    async (file: File | null): Promise<boolean> => {
       assertExists(blockSuiteWorkspace);
       if (!file) {
         blockSuiteWorkspace.meta.setAvatar('');
-        return;
+        return false;
       }
-      const blob = new Blob([file], { type: file.type });
-      const blobs = blockSuiteWorkspace.blobs;
-      const blobId = await blobs.set(blob);
-      blockSuiteWorkspace.meta.setAvatar(blobId);
-      await mutate(blobId);
+      try {
+        await validateImage(file);
+        const blob = new Blob([file], { type: file.type });
+        const blobs = blockSuiteWorkspace.blobs;
+        const blobId = await blobs.set(blob);
+        blockSuiteWorkspace.meta.setAvatar(blobId);
+        await mutate(blobId);
+        return true;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
     },
     [blockSuiteWorkspace, mutate]
   );
+
   useEffect(() => {
     if (blockSuiteWorkspace) {
       const dispose = blockSuiteWorkspace.meta.commonFieldsUpdated.on(() => {
