86b8c2738e415dce99a3b170b964e43a3cbbf542
Peng Xiao <pengxiao@outlook.com>
2023-11-09 16:35:10 +0800

    fix(core): svg blob syncing issue

--- packages/frontend/workspace/src/blob/cloud-blob-storage.ts
+++ packages/frontend/workspace/src/blob/cloud-blob-storage.ts
@@ -9,6 +9,7 @@ import { fetcher } from '@affine/workspace/affine/gql';
 import type { BlobStorage } from '@blocksuite/store';
 
 import { predefinedStaticFiles } from './local-static-storage';
+import { isSvgBuffer } from './util';
 
 export const createCloudBlobStorage = (workspaceId: string): BlobStorage => {
   return {
@@ -17,16 +18,24 @@ export const createCloudBlobStorage = (workspaceId: string): BlobStorage => {
         const suffix = predefinedStaticFiles.includes(key)
           ? `/static/${key}`
           : `/api/workspaces/${workspaceId}/blobs/${key}`;
-        return fetchWithTraceReport(
+
+        const buffer = await fetchWithTraceReport(
           runtimeConfig.serverUrlPrefix + suffix
         ).then(res => {
           if (!res.ok) {
             // status not in the range 200-299
             return null;
           }
-          // todo: shall we add svg type here if it is missing?
-          return res.blob();
+          return res.arrayBuffer();
         });
+        if (!buffer) {
+          return null;
+        }
+        const isSVG = isSvgBuffer(new Uint8Array(buffer));
+        // for svg blob, we need to explicitly set the type to image/svg+xml
+        return isSVG
+          ? new Blob([buffer], { type: 'image/svg+xml' })
+          : new Blob([buffer]);
       },
       set: async (key, value) => {
         const {
