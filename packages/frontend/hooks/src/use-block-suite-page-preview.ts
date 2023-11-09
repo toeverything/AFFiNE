5a6781affec53ff58a70dcea2732a290f132666c
lawvs <18554747+lawvs@users.noreply.github.com>
2023-11-09 17:34:23 +0800

    fix: get page preview based on block order

--- packages/frontend/hooks/src/use-block-suite-page-preview.ts
+++ packages/frontend/hooks/src/use-block-suite-page-preview.ts
@@ -1,20 +1,49 @@
-import type { ParagraphBlockModel } from '@blocksuite/blocks/models';
 import type { Page } from '@blocksuite/store';
 import type { Atom } from 'jotai';
 import { atom } from 'jotai';
 
+const MAX_PREVIEW_LENGTH = 150;
+const MAX_SEARCH_BLOCK_COUNT = 30;
+
 const weakMap = new WeakMap<Page, Atom<string>>();
 
 export const getPagePreviewText = (page: Page) => {
-  // TODO this is incorrect, since the order of blocks is not guaranteed
-  const paragraphBlocks = page.getBlockByFlavour(
-    'affine:paragraph'
-  ) as ParagraphBlockModel[];
-  const text = paragraphBlocks
-    .slice(0, 10)
-    .map(block => block.text.toString())
-    .join('\n');
-  return text.slice(0, 300);
+  const pageRoot = page.root;
+  if (!pageRoot) {
+    return '';
+  }
+  const preview: string[] = [];
+  // DFS
+  const queue = [pageRoot];
+  let previewLenNeeded = MAX_PREVIEW_LENGTH;
+  let count = MAX_SEARCH_BLOCK_COUNT;
+  while (queue.length && previewLenNeeded > 0 && count-- > 0) {
+    const block = queue.shift();
+    if (!block) {
+      console.error('Unexpected empty block');
+      break;
+    }
+    if (block.children) {
+      queue.unshift(...block.children);
+    }
+    if (block.role !== 'content') {
+      continue;
+    }
+    if (block.text) {
+      const text = block.text.toString();
+      if (!text.length) {
+        continue;
+      }
+      previewLenNeeded -= text.length;
+      preview.push(text);
+    } else {
+      // image/attachment/bookmark
+      const type = block.flavour.split('affine:')[1] ?? null;
+      previewLenNeeded -= type.length + 2;
+      type && preview.push(`[${type}]`);
+    }
+  }
+  return preview.join(' ');
 };
 
 const emptyAtom = atom<string>('');
