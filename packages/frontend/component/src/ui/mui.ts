4fedd183b796afc91ff843d73d539593f94ecab7
Cats Juice <wp1298554944@hotmail.com>
2023-11-13 15:29:48 +0800

    feat(component): remove MuiAvatar, MuiBreadcrumbs

--- packages/frontend/component/src/ui/mui.ts
+++ packages/frontend/component/src/ui/mui.ts
@@ -1,15 +1,6 @@
-import MuiAvatar from '@mui/material/Avatar';
-import MuiBreadcrumbs from '@mui/material/Breadcrumbs';
 import MuiCollapse from '@mui/material/Collapse';
 import MuiFade from '@mui/material/Fade';
 import MuiGrow from '@mui/material/Grow';
 import MuiSkeleton from '@mui/material/Skeleton';
 
-export {
-  MuiAvatar,
-  MuiBreadcrumbs,
-  MuiCollapse,
-  MuiFade,
-  MuiGrow,
-  MuiSkeleton,
-};
+export { MuiCollapse, MuiFade, MuiGrow, MuiSkeleton };
