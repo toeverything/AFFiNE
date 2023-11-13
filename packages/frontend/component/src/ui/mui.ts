c244493094843b042b171026047c19fa78c9a19b
Cats Juice <wp1298554944@hotmail.com>
2023-11-13 17:22:36 +0800

    feat(component): remove MuiCollapse

--- packages/frontend/component/src/ui/mui.ts
+++ packages/frontend/component/src/ui/mui.ts
@@ -1,6 +1,5 @@
-import MuiCollapse from '@mui/material/Collapse';
 import MuiFade from '@mui/material/Fade';
 import MuiGrow from '@mui/material/Grow';
 import MuiSkeleton from '@mui/material/Skeleton';
 
-export { MuiCollapse, MuiFade, MuiGrow, MuiSkeleton };
+export { MuiFade, MuiGrow, MuiSkeleton };
