a5ee603a28c1d24d166ab6c64a6fe8f4fa6a6e6a
Cats Juice <wp1298554944@hotmail.com>
2023-11-13 18:16:57 +0800

    feat(component): remove MuiFade

--- packages/frontend/component/src/ui/mui.ts
+++ packages/frontend/component/src/ui/mui.ts
@@ -1,5 +1,4 @@
-import MuiFade from '@mui/material/Fade';
 import MuiGrow from '@mui/material/Grow';
 import MuiSkeleton from '@mui/material/Skeleton';
 
-export { MuiFade, MuiGrow, MuiSkeleton };
+export { MuiGrow, MuiSkeleton };
