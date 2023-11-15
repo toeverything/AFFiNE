5c8ac8ca5242d55e30fb742b2be300eb4fd36c1b
Peng Xiao <584378+pengx17@users.noreply.github.com>
2023-11-15 10:41:20 +0000

    fix(component): lottie icon color hack (#4953)

--- packages/frontend/component/src/ui/lottie/collections-icon.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const root = style({
-  width: '1em',
-  height: '1em',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-});
