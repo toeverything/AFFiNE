62c0efcfd1246913bd26d8fc0a944f08fa0634d6
LongYinan <lynweklm@gmail.com>
2023-11-17 16:50:48 +0800

    fix(core): handle the getSession network error properly (#4909)
    If network offline or API error happens, the `session` returned by the `useSession` hook will be null, so we can't assume it is not null.
    
    There should be following changes:
    1. create a page in ErrorBoundary to let the user refetch the session.
    2. The `SessionProvider` stop to pull the new session once the session is null, we need to figure out a way to pull the new session when the network is back or the user click the refetch button.

---
+++ packages/frontend/core/src/components/affine/affine-error-boundary.css.ts
@@ -0,0 +1,41 @@
+import { style } from '@vanilla-extract/css';
+
+export const errorLayout = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  width: '100%',
+});
+
+export const errorDetailStyle = style({
+  display: 'flex',
+  flexDirection: 'column',
+  maxWidth: '420px',
+});
+
+export const errorTitle = style({
+  fontSize: '36px',
+  lineHeight: '44px',
+  fontWeight: 700,
+});
+
+export const errorImage = style({
+  height: '178px',
+  maxWidth: '400px',
+  flexGrow: 1,
+});
+
+export const errorDescription = style({
+  marginTop: '24px',
+});
+
+export const errorRetryButton = style({
+  marginTop: '24px',
+  width: '94px',
+});
+
+export const errorDivider = style({
+  width: '20px',
+  height: '100%',
+});
---
+++ packages/frontend/core/src/components/affine/affine-error-boundary.tsx
@@ -0,0 +1,176 @@
+import type {
+  QueryParamError,
+  Unreachable,
+  WorkspaceNotFoundError,
+} from '@affine/env/constant';
+import { PageNotFoundError } from '@affine/env/constant';
+import { rootWorkspacesMetadataAtom } from '@affine/workspace/atom';
+import { Button } from '@toeverything/components/button';
+import {
+  currentPageIdAtom,
+  currentWorkspaceIdAtom,
+  getCurrentStore,
+} from '@toeverything/infra/atom';
+import { useAtomValue } from 'jotai/react';
+import { Provider } from 'jotai/react';
+import type { ErrorInfo, ReactElement, ReactNode } from 'react';
+import type React from 'react';
+import { Component, useEffect } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+
+import {
+  RecoverableError,
+  SessionFetchErrorRightAfterLoginOrSignUp,
+} from '../../unexpected-application-state/errors';
+import {
+  errorDescription,
+  errorDetailStyle,
+  errorDivider,
+  errorImage,
+  errorLayout,
+  errorRetryButton,
+  errorTitle,
+} from './affine-error-boundary.css';
+import errorBackground from './error-status.assets.svg';
+
+export type AffineErrorBoundaryProps = React.PropsWithChildren;
+
+type AffineError =
+  | QueryParamError
+  | Unreachable
+  | WorkspaceNotFoundError
+  | PageNotFoundError
+  | Error
+  | SessionFetchErrorRightAfterLoginOrSignUp;
+
+interface AffineErrorBoundaryState {
+  error: AffineError | null;
+  canRetryRecoveredError: boolean;
+}
+
+export const DumpInfo = () => {
+  const location = useLocation();
+  const metadata = useAtomValue(rootWorkspacesMetadataAtom);
+  const currentWorkspaceId = useAtomValue(currentWorkspaceIdAtom);
+  const currentPageId = useAtomValue(currentPageIdAtom);
+  const path = location.pathname;
+  const query = useParams();
+  useEffect(() => {
+    console.info('DumpInfo', {
+      path,
+      query,
+      currentWorkspaceId,
+      currentPageId,
+      metadata,
+    });
+  }, [path, query, currentWorkspaceId, currentPageId, metadata]);
+  return null;
+};
+
+export class AffineErrorBoundary extends Component<
+  AffineErrorBoundaryProps,
+  AffineErrorBoundaryState
+> {
+  override state: AffineErrorBoundaryState = {
+    error: null,
+    canRetryRecoveredError: true,
+  };
+
+  private readonly handleRecoverableRetry = () => {
+    if (this.state.error instanceof RecoverableError) {
+      if (this.state.error.canRetry()) {
+        this.state.error.retry();
+        this.setState({
+          error: this.state.error,
+          canRetryRecoveredError: this.state.error.canRetry(),
+        });
+      } else {
+        document.location.reload();
+      }
+    }
+  };
+
+  static getDerivedStateFromError(
+    error: AffineError
+  ): AffineErrorBoundaryState {
+    return {
+      error,
+      canRetryRecoveredError:
+        error instanceof RecoverableError ? error.canRetry() : true,
+    };
+  }
+
+  override componentDidCatch(error: AffineError, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  override render(): ReactNode {
+    if (this.state.error) {
+      let errorDetail: ReactElement | null = null;
+      const error = this.state.error;
+      if (error instanceof PageNotFoundError) {
+        errorDetail = (
+          <>
+            <h1>Sorry.. there was an error</h1>
+            <>
+              <span> Page error </span>
+              <span>
+                Cannot find page {error.pageId} in workspace{' '}
+                {error.workspace.id}
+              </span>
+            </>
+          </>
+        );
+      } else if (error instanceof SessionFetchErrorRightAfterLoginOrSignUp) {
+        const retryButtonDesc = this.state.canRetryRecoveredError
+          ? 'Refetch'
+          : 'Reload';
+        errorDetail = (
+          <>
+            <h1 className={errorTitle}>Sorry.. there was an error</h1>
+            <span className={errorDescription}> Fetching session failed </span>
+            <span className={errorDescription}>
+              If you are still experiencing this issue, please{' '}
+              <a
+                style={{ color: 'var(--affine-primary-color)' }}
+                href="https://community.affine.pro"
+                target="__blank"
+              >
+                contact us through the community.
+              </a>
+            </span>
+            <Button
+              className={errorRetryButton}
+              onClick={this.handleRecoverableRetry}
+              type="primary"
+            >
+              {retryButtonDesc}
+            </Button>
+          </>
+        );
+      } else {
+        errorDetail = (
+          <>
+            <h1>Sorry.. there was an error</h1>
+            {error.message ?? error.toString()}
+          </>
+        );
+      }
+      return (
+        <div className={errorLayout}>
+          <div className={errorDetailStyle}>{errorDetail}</div>
+          <span className={errorDivider} />
+          <div
+            className={errorImage}
+            style={{ backgroundImage: `url(${errorBackground})` }}
+          />
+          <Provider key="JotaiProvider" store={getCurrentStore()}>
+            <DumpInfo />
+          </Provider>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
