a226eb8d5f2deb578bca4b2f860bed6105039cb7
Joooye_34 <Joooye1991@gmail.com>
2023-11-29 20:31:35 +0800

    fix(core): expose catched editor load error (#5133)

--- packages/frontend/core/src/components/affine/affine-error-boundary/error-fallbacks/any-error-fallback.tsx
+++ packages/frontend/core/src/components/affine/affine-error-boundary/error-fallbacks/any-error-fallback.tsx
@@ -1,17 +1,25 @@
 import { useAFFiNEI18N } from '@affine/i18n/hooks';
-import type { FC } from 'react';
+import { type FC, useCallback } from 'react';
 
 import { ErrorDetail } from '../error-basic/error-detail';
 import type { FallbackProps } from '../error-basic/fallback-creator';
 
+/**
+ * TODO: Support reload and retry two reset actions in page error and area error.
+ */
 export const AnyErrorFallback: FC<FallbackProps> = props => {
-  const { error, resetError } = props;
+  const { error } = props;
   const t = useAFFiNEI18N();
+
+  const reloadPage = useCallback(() => {
+    document.location.reload();
+  }, []);
 
   return (
     <ErrorDetail
       title={t['com.affine.error.unexpected-error.title']()}
-      resetError={resetError}
+      resetError={reloadPage}
+      buttonText={t['com.affine.error.reload']()}
       description={error.message ?? error.toString()}
     />
   );
