579828a700987f362723523253a69c339b779920
DarkSky <25152247+darkskygit@users.noreply.github.com>
2023-12-13 21:58:22 +0800

    fix: use secure websocket (#5297)

--- packages/frontend/workspace/src/providers/utils/affine-io.ts
+++ packages/frontend/workspace/src/providers/utils/affine-io.ts
@@ -7,9 +7,16 @@ export function getIoManager(): Manager {
   if (ioManager) {
     return ioManager;
   }
-  ioManager = new Manager('/', {
-    autoConnect: false,
-    transports: ['websocket'],
-  });
+  const { protocol, hostname, port } = window.location;
+  ioManager = new Manager(
+    `${protocol === 'https:' ? 'wss' : 'ws'}://${hostname}${
+      port ? `:${port}` : ''
+    }/`,
+    {
+      autoConnect: false,
+      transports: ['websocket'],
+      secure: location.protocol === 'https:',
+    }
+  );
   return ioManager;
 }
