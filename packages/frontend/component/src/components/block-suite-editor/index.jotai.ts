8e9ccb8b5faea5175acf97ec6f640a7d3bdba6fb
donteatfriedrice <huisheng.chen7788@outlook.com>
2023-12-13 11:26:49 +0800

    fix: lint

--- packages/frontend/component/src/components/block-suite-editor/index.jotai.ts
+++ packages/frontend/component/src/components/block-suite-editor/index.jotai.ts
@@ -1,4 +1,4 @@
-import type { EditorContainer } from "@blocksuite/presets";
-import { atom } from "jotai";
+import type { EditorContainer } from '@blocksuite/presets';
+import { atom } from 'jotai';
 
 export const editorContainerAtom = atom<EditorContainer | null>(null);
