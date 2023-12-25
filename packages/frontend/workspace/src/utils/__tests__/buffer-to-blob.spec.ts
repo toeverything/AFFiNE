56f05803827106bdad531e467682217b903e26aa
EYHN <cneyhn@gmail.com>
2023-12-25 06:58:11 +0000

    fix(workspace): fix svg file with xml header (#5388)

--- packages/frontend/workspace/src/utils/__tests__/buffer-to-blob.spec.ts
+++ packages/frontend/workspace/src/utils/__tests__/buffer-to-blob.spec.ts
@@ -9,5 +9,10 @@ describe('isSvgBuffer', () => {
     expect(isSvgBuffer(Buffer.from('<svg></svg>'))).toBe(true);
     expect(isSvgBuffer(Buffer.from(' \n\r\t<svg></svg>'))).toBe(true);
     expect(isSvgBuffer(Buffer.from('<123>'))).toBe(false);
+    expect(
+      isSvgBuffer(
+        Buffer.from('<?xml version="1.0" encoding="UTF-8"?><svg></svg>')
+      )
+    ).toBe(true);
   });
 });
