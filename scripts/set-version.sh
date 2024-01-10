dd671c8764b31fbd3f38f9b2ca46312d72cd592d
Joooye_34 <Joooye1991@gmail.com>
2024-01-10 10:57:52 +0800

    build: change space replacement when set version to fix client upgrade always problem (#5554)

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 for DIR_ITEM in $(yarn workspaces list --json | jq -r '.location'); do
-  DIR=$(echo -n "$DIR_ITEM" | sed -e 's/[[:blank:]]*$//' -e 's/[\n\r]*$//')
+  DIR=$(echo -n "$DIR_ITEM" | tr -d '\r\n')
   if [ -f "$DIR/package.json" ]; then
     echo "Setting version for $DIR"
     jq ".version = \"$1\"" "$DIR"/package.json > tmp.json && mv tmp.json "$DIR"/package.json
+  else
+    echo "No package.json found in: $DIR"
   fi
 done
 
