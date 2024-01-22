45cc75a814f1957f64ac8b2a4337e79ddee3b075
Cats Juice <wp1298554944@hotmail.com>
2024-01-22 10:25:33 +0000

    ci(component): deploy ui storybook (#5655)

--- packages/frontend/component/.storybook/main.ts
+++ packages/frontend/component/.storybook/main.ts
@@ -34,7 +34,6 @@ export default {
         }),
       ],
       define: {
-        'process.on': '(() => void 0)',
         'process.env': {},
         'process.env.COVERAGE': JSON.stringify(!!process.env.COVERAGE),
         'process.env.SHOULD_REPORT_TRACE': `${Boolean(
