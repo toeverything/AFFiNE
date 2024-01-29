04ab6edf0214425d876c419fbf697fcdbd66ea54
Yifeng Wang <doodlewind@toeverything.info>
2024-01-29 16:22:11 +0800

    feat: bump blocksuite

--- scripts/bump-blocksuite.sh
+++ scripts/bump-blocksuite.sh
@@ -3,10 +3,11 @@
 LATEST_NIGHTLY=$(npm view @blocksuite/presets@nightly version | cut -d ':' -f 2 | tr -d '[:space:]')
 echo "Bump to latest BlockSuite nightly version: $LATEST_NIGHTLY"
 
-yarn up "@blocksuite/store@${LATEST_NIGHTLY}"
-yarn up "@blocksuite/blocks@${LATEST_NIGHTLY}"
-yarn up "@blocksuite/presets@${LATEST_NIGHTLY}"
-yarn up "@blocksuite/global@${LATEST_NIGHTLY}"
-yarn up "@blocksuite/block-std@${LATEST_NIGHTLY}"
-yarn up "@blocksuite/lit@${LATEST_NIGHTLY}"
-yarn up "@blocksuite/inline@${LATEST_NIGHTLY}"
+yarn up \
+  "@blocksuite/store@${LATEST_NIGHTLY}" \
+  "@blocksuite/blocks@${LATEST_NIGHTLY}" \
+  "@blocksuite/presets@${LATEST_NIGHTLY}" \
+  "@blocksuite/global@${LATEST_NIGHTLY}" \
+  "@blocksuite/block-std@${LATEST_NIGHTLY}" \
+  "@blocksuite/lit@${LATEST_NIGHTLY}" \
+  "@blocksuite/inline@${LATEST_NIGHTLY}"
