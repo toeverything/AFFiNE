25e8b8306f5ba206f19b287643c31cd8d86d6cb7
LongYinan <lynweklm@gmail.com>
2024-01-29 07:18:25 +0000

    fix: path issue on Windows (#5725)

--- packages/frontend/templates/build.mjs
+++ packages/frontend/templates/build.mjs
@@ -1,31 +1,32 @@
 import fs from 'node:fs';
-import path from 'node:path';
+import path, { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import * as glob from 'glob';
 
 // purpose: bundle all json files into one json file in onboarding folder
-const __dirname = new URL('.', import.meta.url).pathname;
+const __dirname = join(fileURLToPath(import.meta.url), '..');
 
 const jsonFiles = glob.sync('./*.json', {
-  cwd: path.resolve(__dirname, 'onboarding'),
+  cwd: path.join(__dirname, 'onboarding'),
 });
 
 const imports = jsonFiles
-  .map((fileName, index) => {
-    return `import json_${index} from './onboarding/${fileName}';`;
-  })
+  .map(
+    (fileName, index) => `import json_${index} from './onboarding/${fileName}';`
+  )
   .join('\n');
 
 const exports = `export const onboarding = {
-  ${jsonFiles
-    .map((fileName, index) => {
-      return `'${fileName}': json_${index}`;
-    })
-    .join(',\n')}
+${jsonFiles
+  .map((fileName, index) => {
+    return `  '${fileName}': json_${index}`;
+  })
+  .join(',\n')}
 }`;
 
 const template = `/* eslint-disable simple-import-sort/imports */
 // Auto generated, do not edit manually
 ${imports}\n\n${exports}`;
 
-fs.writeFileSync(path.resolve(__dirname, 'templates.gen.ts'), template);
+fs.writeFileSync(path.join(__dirname, 'templates.gen.ts'), template);
