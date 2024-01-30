b3a8e6298439304fdbfa80aee87996e99b717902
EYHN <cneyhn@gmail.com>
2024-01-30 06:31:15 +0000

    feat(infra): standard storage service (#5563)

--- packages/common/infra/src/index.ts
+++ packages/common/infra/src/index.ts
@@ -2,3 +2,6 @@ export * from './app-config-storage';
 export * from './atom';
 export * from './blocksuite';
 export * from './command';
+export * from './di';
+export * from './livedata';
+export * from './storage';
