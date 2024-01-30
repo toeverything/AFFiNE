88cd83fed1c2cc8fe65151a3075ca4e79e0c567f
EYHN <cneyhn@gmail.com>
2024-01-30 06:31:18 +0000

    feat(infra): standard lifecycle service (#5564)

--- packages/common/infra/src/index.ts
+++ packages/common/infra/src/index.ts
@@ -5,3 +5,17 @@ export * from './command';
 export * from './di';
 export * from './livedata';
 export * from './storage';
+
+import type { ServiceCollection } from './di';
+import { CleanupService } from './lifecycle';
+import { GlobalCache, GlobalState, MemoryMemento } from './storage';
+
+export function configureInfraServices(services: ServiceCollection) {
+  services.add(CleanupService);
+}
+
+export function configureTestingInfraServices(services: ServiceCollection) {
+  configureInfraServices(services);
+  services.addImpl(GlobalCache, MemoryMemento);
+  services.addImpl(GlobalState, MemoryMemento);
+}
