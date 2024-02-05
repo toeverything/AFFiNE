0870262b8a948d97376384bf963803e1dd6ad4d9
EYHN <cneyhn@gmail.com>
2024-02-05 10:56:46 +0800

    fix(workspace): fix sync handshake (hot-fix) (#5797)

--- packages/frontend/workspace-impl/src/cloud/sync.ts
+++ packages/frontend/workspace-impl/src/cloud/sync.ts
@@ -2,6 +2,14 @@ import { DebugLogger } from '@affine/debug';
 import { fetchWithTraceReport } from '@affine/graphql';
 import { type SyncStorage } from '@toeverything/infra';
 import type { CleanupService } from '@toeverything/infra/lifecycle';
+import {
+  BehaviorSubject,
+  distinctUntilChanged,
+  filter,
+  lastValueFrom,
+  take,
+  timeout,
+} from 'rxjs';
 
 import { getIoManager } from '../utils/affine-io';
 import { base64ToUint8Array, uint8ArrayToBase64 } from '../utils/base64';
@@ -15,21 +23,60 @@ export class AffineSyncStorage implements SyncStorage {
 
   socket = getIoManager().socket('/');
 
+  connected = new BehaviorSubject(false);
+  handshook = new BehaviorSubject(false);
+
   constructor(
     private readonly workspaceId: string,
     cleanupService: CleanupService
   ) {
-    this.socket.on('connect', this.handleConnect);
+    const handleConnect = () => {
+      this.connected.next(true);
+    };
+    this.socket.on('connect', handleConnect);
+    this.connected.next(this.socket.connected);
 
-    if (this.socket.connected) {
-      this.socket.emit('client-handshake-sync', this.workspaceId);
-    } else {
-      this.socket.connect();
-    }
+    const handleDisconnect = () => {
+      this.connected.next(false);
+      this.handshook.next(false);
+    };
+    this.socket.on('disconnect', handleDisconnect);
 
-    cleanupService.add(() => {
-      this.cleanup();
+    this.connected.pipe(distinctUntilChanged()).subscribe(connected => {
+      if (connected) {
+        this.socket
+          .timeout(this.SEND_TIMEOUT)
+          .emitWithAck('client-handshake-sync', this.workspaceId)
+          .then(() => {
+            this.handshook.next(true);
+          })
+          .catch(err => {
+            logger.error('client-handshake-sync error', {
+              workspaceId: this.workspaceId,
+              error: err,
+            });
+          });
+      }
+      cleanupService.add(() => {
+        this.cleanup();
+      });
     });
+
+    this.disconnect = () => {
+      this.socket.emit('client-leave-sync', this.workspaceId);
+      this.socket.off('connect', handleConnect);
+      this.socket.off('disconnect', handleDisconnect);
+    };
+  }
+
+  async waitForHandshake() {
+    await lastValueFrom(
+      this.handshook.pipe(
+        filter(v => v),
+        timeout({ first: this.SEND_TIMEOUT }),
+        take(1)
+      )
+    );
   }
 
   handleConnect = () => {
@@ -40,6 +87,7 @@ export class AffineSyncStorage implements SyncStorage {
     docId: string,
     state: Uint8Array
   ): Promise<{ data: Uint8Array; state?: Uint8Array } | null> {
+    await this.waitForHandshake();
     const stateVector = state ? await uint8ArrayToBase64(state) : undefined;
 
     logger.debug('doc-load-v2', {
@@ -83,6 +131,8 @@ export class AffineSyncStorage implements SyncStorage {
   }
 
   async push(docId: string, update: Uint8Array) {
+    await this.waitForHandshake();
+
     logger.debug('client-update-v2', {
       workspaceId: this.workspaceId,
       guid: docId,
@@ -148,6 +198,7 @@ export class AffineSyncStorage implements SyncStorage {
     this.socket.emit('client-leave-sync', this.workspaceId);
     this.socket.off('connect', this.handleConnect);
   }
+  disconnect: () => void;
 }
 
 export class AffineStaticSyncStorage implements SyncStorage {
