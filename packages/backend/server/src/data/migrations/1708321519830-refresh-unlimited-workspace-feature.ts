f36431d908b5dbedcce45b619062fe16cf4d9cb0
DarkSky <darksky2048@gmail.com>
2024-02-19 14:49:04 +0800

    feat: upsert latest version

--- packages/backend/server/src/data/migrations/1708321519830-refresh-unlimited-workspace-feature.ts
+++ packages/backend/server/src/data/migrations/1708321519830-refresh-unlimited-workspace-feature.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-import { Features } from '../../core/features';
-import { upsertFeature } from './utils/user-features';
+import { FeatureType } from '../../core/features';
+import { upsertLatestFeatureVersion } from './utils/user-features';
 
 export class RefreshUnlimitedWorkspaceFeature1708321519830 {
   // do the migration
   static async up(db: PrismaClient) {
     // add unlimited workspace feature
-    const feature = Features[3];
-    await upsertFeature(db, feature);
+    await upsertLatestFeatureVersion(db, FeatureType.UnlimitedWorkspace);
   }
 
   // revert the migration
