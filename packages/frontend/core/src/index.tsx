nboucaud/playbook

feat(core): orm demo

packages/frontend/core/src/index.tsx
@@ -1,5 +1,6 @@
 import './polyfill/intl-segmenter';
 import './polyfill/request-idle-callback';
+import './orm/index';
 
 import { assertExists } from '@blocksuite/global/utils';
 import { StrictMode } from 'react';

toeverything/AFFiNE

packages/frontend/core/src/index.tsx
@@ -1,5 +1,6 @@
 import './polyfill/intl-segmenter';
 import './polyfill/request-idle-callback';
+import './orm/test';
 
 import { assertExists } from '@blocksuite/global/utils';
 import { StrictMode } from 'react';