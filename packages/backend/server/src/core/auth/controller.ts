350110e0909ccd6e5351c241bf5c06d67170c32e
DarkSky <darksky2048@gmail.com>
2024-03-11 16:52:42 +0800

    chore: cleanup rebase error

--- packages/backend/server/src/core/auth/controller.ts
+++ packages/backend/server/src/core/auth/controller.ts
@@ -1,5 +1,3 @@
-import { randomUUID } from 'node:crypto';
-
 import {
   BadRequestException,
   Body,
@@ -178,16 +176,6 @@ export class AuthController {
 
     return {
       users: await this.auth.getUserList(token),
-    };
-  }
-
-  @Public()
-  @Get('/challenge')
-  async challenge() {
-    // TODO: impl in following PR
-    return {
-      challenge: randomUUID(),
-      resource: randomUUID(),
     };
   }
 }
