96b0a57417efd664159f6f7fbcffcc602d9904ba
DarkSky <darksky2048@gmail.com>
2024-03-22 21:05:45 +0800

    feat: init controller

---
+++ packages/backend/server/src/plugins/copilot/controller.ts
@@ -0,0 +1,98 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Param,
+  Post,
+  Query,
+  Req,
+  Sse,
+} from '@nestjs/common';
+import {
+  concatMap,
+  connect,
+  EMPTY,
+  from,
+  map,
+  merge,
+  Observable,
+  switchMap,
+  toArray,
+} from 'rxjs';
+
+import { Public } from '../../core/auth';
+import { CurrentUser } from '../../core/auth/current-user';
+import { ProviderService } from './providers';
+import { type ChatSessionOptions, ChatSessionService } from './session';
+import { CopilotCapability } from './types';
+
+export interface ChatEvent {
+  data: string;
+  id?: string;
+}
+
+@Controller('/api/copilot')
+export class CopilotController {
+  constructor(
+    private readonly chatSession: ChatSessionService,
+    private readonly provider: ProviderService
+  ) {}
+
+  @Public()
+  @Post('/chat')
+  async createSession(
+    @CurrentUser() user: CurrentUser | undefined,
+    @Body() options: ChatSessionOptions
+  ): Promise<{ session: string }> {
+    const session = await this.chatSession.create({
+      ...options,
+      user: user?.id,
+    });
+    return { session };
+  }
+
+  @Public()
+  @Sse('/chat/:sessionId')
+  async chat(
+    @CurrentUser() user: CurrentUser | undefined,
+    @Req() req: Request,
+    @Param('sessionId') sessionId: string,
+    @Query('message') content: string
+  ): Promise<Observable<ChatEvent>> {
+    const provider = this.provider.getProviderByCapability(
+      CopilotCapability.TextToText
+    );
+    if (!provider) {
+      throw new InternalServerErrorException('No provider available');
+    }
+    const session = await this.chatSession.get(sessionId);
+    if (!session) {
+      throw new BadRequestException('Session not found');
+    }
+    session.push({ role: 'user', content: decodeURIComponent(content) });
+
+    return from(
+      provider.generateTextStream(session.finish(), session.model, {
+        signal: req.signal,
+        user: user?.id,
+      })
+    ).pipe(
+      connect(shared =>
+        merge(
+          // actual chat event stream
+          shared.pipe(map(data => ({ id: sessionId, data }))),
+          // save the generated text to the session
+          shared.pipe(
+            toArray(),
+            concatMap(values => {
+              session.push({ role: 'assistant', content: values.join('') });
+              return from(session.save());
+            }),
+            switchMap(() => EMPTY)
+          )
+        )
+      )
+    );
+  }
+}
