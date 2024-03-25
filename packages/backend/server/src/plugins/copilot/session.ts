d56448e35aced500530f09f755d5f69b9db5ad49
DarkSky <darksky2048@gmail.com>
2024-03-25 21:02:39 +0800

    feat: store session

--- packages/backend/server/src/plugins/copilot/session.ts
+++ packages/backend/server/src/plugins/copilot/session.ts
@@ -1,22 +1,34 @@
 import { randomUUID } from 'node:crypto';
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { encoding_for_model, Tiktoken, TiktokenModel } from 'tiktoken';
 
 import { SessionCache } from '../../fundamentals';
 import { PromptService } from './prompt';
-import { ChatMessage } from './types';
+import { ChatMessage, ChatMessageSchema } from './types';
 
 const CHAT_SESSION_KEY = 'chat-session';
 const CHAT_SESSION_TTL = 3600 * 12 * 1000; // 12 hours
 
-export type ChatSessionState = {
-  sessionId: string;
+export interface ChatSessionOptions {
+  // connect ids
+  userId: string;
+  workspaceId: string;
+  docId: string;
   promptName: string;
+  // options
+  action: boolean;
   model: TiktokenModel;
+}
+
+export interface ChatSessionState extends ChatSessionOptions {
+  // connect ids
+  sessionId: string;
+  // states
   prompt: ChatMessage[];
   messages: ChatMessage[];
-};
+}
 
 export class ChatSession implements AsyncDisposable {
   private readonly encoder: Tiktoken;
@@ -77,43 +89,96 @@ export class ChatSession implements AsyncDisposable {
   }
 }
 
-export interface ChatSessionOptions {
-  promptName: string;
-  model: TiktokenModel;
-  user?: string;
-}
-
 @Injectable()
 export class ChatSessionService {
+  private readonly logger = new Logger(ChatSessionService.name);
   constructor(
+    private readonly db: PrismaClient,
     private readonly cache: SessionCache,
     private readonly prompt: PromptService
   ) {}
 
-  private async setState(state: ChatSessionState): Promise<void> {
-    await this.cache.set(`${CHAT_SESSION_KEY}:${state.sessionId}`, state, {
-      ttl: CHAT_SESSION_TTL,
+  private async setSession(state: ChatSessionState): Promise<void> {
+    await this.db.aiSession.upsert({
+      where: {
+        id: state.sessionId,
+      },
+      update: {
+        messages: {
+          create: state.messages.map((m, idx) => ({ idx, ...m })),
+        },
+      },
+      create: {
+        id: state.sessionId,
+        action: state.action,
+        model: state.model,
+        messages: { create: state.messages },
+        // connect
+        user: { connect: { id: state.userId } },
+        workspace: { connect: { id: state.workspaceId } },
+        doc: {
+          connect: {
+            id_workspaceId: { id: state.docId, workspaceId: state.workspaceId },
+          },
+        },
+        promptName: state.promptName,
+      },
     });
+  }
+
+  private async getSession(
+    sessionId: string
+  ): Promise<ChatSessionState | undefined> {
+    return await this.db.aiSession
+      .findUnique({
+        where: { id: sessionId },
+      })
+      .then(async session => {
+        if (!session) return;
+        const messages = ChatMessageSchema.array().safeParse(session.messages);
+
+        return {
+          sessionId: session.id,
+          userId: session.userId,
+          workspaceId: session.workspaceId,
+          docId: session.docId,
+          promptName: session.promptName,
+          action: session.action,
+          model: session.model as TiktokenModel,
+          prompt: await this.prompt.get(session.promptName),
+          messages: messages.success ? messages.data : [],
+        };
+      });
+  }
+
+  private async setState(state: ChatSessionState): Promise<void> {
+    await Promise.all([
+      this.cache.set(`${CHAT_SESSION_KEY}:${state.sessionId}`, state, {
+        ttl: CHAT_SESSION_TTL,
+      }),
+      this.setSession(state),
+    ]);
   }
 
   private async getState(
     sessionId: string
   ): Promise<ChatSessionState | undefined> {
-    return await this.cache.get<ChatSessionState>(
+    const state = await this.cache.get<ChatSessionState>(
       `${CHAT_SESSION_KEY}:${sessionId}`
     );
+    if (state) return state;
+    return await this.getSession(sessionId);
   }
 
   async create(options: ChatSessionOptions): Promise<string> {
     const sessionId = randomUUID();
     const prompt = await this.prompt.get(options.promptName);
     if (!prompt.length) {
-      throw new Error(`Prompt not found: ${options.promptName}`);
+      this.logger.warn(`Prompt not found: ${options.promptName}`);
     }
     await this.setState({
+      ...options,
       sessionId,
-      model: options.model,
-      promptName: options.promptName,
       prompt,
       messages: [],
     });
