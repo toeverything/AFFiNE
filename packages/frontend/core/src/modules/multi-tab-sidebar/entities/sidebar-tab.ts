39efad774681736a8fb9fec5931a75b507d3efa5
regischen <regischen@toeverything.info>
2024-04-10 15:42:36 +0800

    feat: add chat panel

--- packages/frontend/core/src/modules/multi-tab-sidebar/entities/sidebar-tab.ts
+++ packages/frontend/core/src/modules/multi-tab-sidebar/entities/sidebar-tab.ts
@@ -1,6 +1,6 @@
 import type { AffineEditorContainer } from '@blocksuite/presets';
 
-export type SidebarTabName = 'outline' | 'frame' | 'copilot' | 'journal';
+export type SidebarTabName = 'outline' | 'frame' | 'chat' | 'journal';
 
 export interface SidebarTabProps {
   editor: AffineEditorContainer | null;
