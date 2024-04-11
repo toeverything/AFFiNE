7a1eb63d425879910ff4ed7efedb2d35fcadd8a3
JimmFly <447268514@qq.com>
2024-04-11 10:30:45 +0000

    fix(core): unexpected routing jump behavior (#6524)
    https://github.com/toeverything/AFFiNE/assets/102217452/b4dba402-b07d-4f8e-a118-a35eb6e7d317

--- packages/frontend/core/src/components/page-list/collections/collection-list-item.tsx
+++ packages/frontend/core/src/components/page-list/collections/collection-list-item.tsx
@@ -1,10 +1,10 @@
 import { Checkbox } from '@affine/component';
 import { getDNDId } from '@affine/core/hooks/affine/use-global-dnd-helper';
+import { WorkbenchLink } from '@affine/core/modules/workbench';
 import { useAFFiNEI18N } from '@affine/i18n/hooks';
 import { useDraggable } from '@dnd-kit/core';
 import type { PropsWithChildren } from 'react';
 import { useCallback, useMemo } from 'react';
-import { Link } from 'react-router-dom';
 
 import { selectionStateAtom, useAtom } from '../scoped-atoms';
 import type {
@@ -219,9 +219,9 @@ function CollectionListItemWrapper({
 
   if (to) {
     return (
-      <Link {...commonProps} to={to}>
+      <WorkbenchLink {...commonProps} to={to}>
         {children}
-      </Link>
+      </WorkbenchLink>
     );
   } else {
     return <div {...commonProps}>{children}</div>;
