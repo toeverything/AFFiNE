ea3f42791823f46d9e610a9eade17f8b9580c1f7
darkskygit <darksky.graphite@toeverything.info>
2024-04-25 08:33:20 +0000

    fix: large transaction (#6695)

--- packages/backend/server/src/data/migrations/utils/user-quotas.ts
+++ packages/backend/server/src/data/migrations/utils/user-quotas.ts
@@ -13,56 +13,62 @@ export async function upgradeQuotaVersion(
   // add new quota
   await upsertFeature(db, quota);
   // migrate all users that using old quota to new quota
-  await db.$transaction(async tx => {
-    const latestQuotaVersion = await tx.features.findFirstOrThrow({
-      where: { feature: quota.feature },
-      orderBy: { version: 'desc' },
-      select: { id: true },
-    });
+  await db.$transaction(
+    async tx => {
+      const latestQuotaVersion = await tx.features.findFirstOrThrow({
+        where: { feature: quota.feature },
+        orderBy: { version: 'desc' },
+        select: { id: true },
+      });
 
-    // find all users that have old free plan
-    const userIds = await tx.user.findMany({
-      where: {
-        features: {
-          some: {
-            feature: {
-              type: FeatureKind.Quota,
-              feature: quota.feature,
-              version: { lt: quota.version },
+      // find all users that have old free plan
+      const userIds = await tx.user.findMany({
+        where: {
+          features: {
+            some: {
+              feature: {
+                type: FeatureKind.Quota,
+                feature: quota.feature,
+                version: { lt: quota.version },
+              },
+              activated: true,
             },
-            activated: true,
           },
         },
-      },
-      select: { id: true },
-    });
+        select: { id: true },
+      });
 
-    // deactivate all old quota for the user
-    await tx.userFeatures.updateMany({
-      where: {
-        id: undefined,
-        userId: {
-          in: userIds.map(({ id }) => id),
+      // deactivate all old quota for the user
+      await tx.userFeatures.updateMany({
+        where: {
+          id: undefined,
+          userId: {
+            in: userIds.map(({ id }) => id),
+          },
+          feature: {
+            type: FeatureKind.Quota,
+          },
+          activated: true,
         },
-        feature: {
-          type: FeatureKind.Quota,
+        data: {
+          activated: false,
         },
-        activated: true,
-      },
-      data: {
-        activated: false,
-      },
-    });
+      });
 
-    await tx.userFeatures.createMany({
-      data: userIds.map(({ id: userId }) => ({
-        userId,
-        featureId: latestQuotaVersion.id,
-        reason,
-        activated: true,
-      })),
-    });
-  });
+      await tx.userFeatures.createMany({
+        data: userIds.map(({ id: userId }) => ({
+          userId,
+          featureId: latestQuotaVersion.id,
+          reason,
+          activated: true,
+        })),
+      });
+    },
+    {
+      maxWait: 10000,
+      timeout: 20000,
+    }
+  );
 }
 
 export async function upsertLatestQuotaVersion(
