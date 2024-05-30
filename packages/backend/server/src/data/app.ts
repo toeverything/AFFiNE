622239fd41076c9456ebd860e8006b52eddab422
forehalo <forehalo@gmail.com>
2024-05-30 09:11:34 +0000

    fix(server): runtime config overrides won't affect static startup (#7110)

--- packages/backend/server/src/data/app.ts
+++ packages/backend/server/src/data/app.ts
@@ -1,25 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { AppModule as BusinessAppModule } from '../app.module';
-import { ConfigModule } from '../fundamentals/config';
 import { CreateCommand, NameQuestion } from './commands/create';
 import { RevertCommand, RunCommand } from './commands/run';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      doc: {
-        manager: {
-          enableUpdateAutoMerging: false,
-        },
-      },
-      metrics: {
-        enabled: false,
-        customerIo: {},
-      },
-    }),
-    BusinessAppModule,
-  ],
+  imports: [BusinessAppModule],
   providers: [NameQuestion, CreateCommand, RunCommand, RevertCommand],
 })
 export class CliAppModule {}
--- packages/backend/server/src/data/index.ts
+++ packages/backend/server/src/data/index.ts
@@ -4,6 +4,8 @@ import { Logger } from '@nestjs/common';
 import { CommandFactory } from 'nest-commander';
 
 async function bootstrap() {
+  AFFiNE.metrics.enabled = false;
+  AFFiNE.doc.manager.enableUpdateAutoMerging = false;
   const { CliAppModule } = await import('./app');
   await CommandFactory.run(CliAppModule, new Logger()).catch(e => {
     console.error(e);
