8c4a42f0e6a33ecaf752c5ae8898773816e160df
Peng Xiao <pengxiao@outlook.com>
2024-07-01 19:31:36 +0800

    fix: cmd-k keybinding not show when editor input is active (#7398)

--- packages/frontend/core/src/commands/registry/command.ts
+++ packages/frontend/core/src/commands/registry/command.ts
@@ -30,6 +30,7 @@ export type CommandCategory =
 
 export interface KeybindingOptions {
   binding: string;
+  capture?: boolean;
   // some keybindings are already registered in blocksuite
   // we can skip the registration of these keybindings __FOR NOW__
   skipRegister?: boolean;
