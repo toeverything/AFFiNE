89edb2282c2ea29de797e884aeb2db84b2d2cd0c
Flrande <flrande.ch@gmail.com>
2024-07-01 18:41:42 +0800

    feat: bump

--- packages/frontend/core/src/components/affine/setting-modal/general-setting/experimental-features/index.tsx
+++ packages/frontend/core/src/components/affine/setting-modal/general-setting/experimental-features/index.tsx
@@ -126,7 +126,6 @@ const blocksuiteFeatureFlags: Partial<Record<keyof BlockSuiteFlags, string>> = {
   enable_expand_database_block: 'Enable Expand Database Block',
   enable_database_statistics: 'Enable Database Block Statistics',
   enable_block_query: 'Enable Todo Block Query',
-  enable_edgeless_text: 'Enable New Edgeless Text',
   enable_ai_onboarding: 'Enable AI Onboarding',
 };
 
