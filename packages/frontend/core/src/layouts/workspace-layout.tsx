2e437d7abe11a5a3d08d338ee6d72ee7f60338dd
Deyvidy <deyvidy-lua@hotmail.com>
2024-07-09 13:32:55 -0300

    Hide AI tour

--- packages/frontend/core/src/layouts/workspace-layout.tsx
+++ packages/frontend/core/src/layouts/workspace-layout.tsx
@@ -41,7 +41,6 @@ import { Map as YMap } from 'yjs';
 
 import { openSettingModalAtom } from '../atoms';
 import { AIProvider } from '../blocksuite/presets/ai';
-import { WorkspaceAIOnboarding } from '../components/affine/ai-onboarding';
 import { AppContainer } from '../components/affine/app-container';
 import { SyncAwareness } from '../components/affine/awareness';
 import { appSidebarResizingAtom } from '../components/app-sidebar';
@@ -81,7 +80,6 @@ export const WorkspaceLayout = function WorkspaceLayout({
       <CurrentWorkspaceModals />
       <WorkspaceLayoutInner>{children}</WorkspaceLayoutInner>
       {/* should show after workspace loaded */}
-      <WorkspaceAIOnboarding />
     </SWRConfigProvider>
   );
 };
