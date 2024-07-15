a28c91844ee6d20ddc72e6c1f57ef55cc1f7f2be
Chen <99816898+donteatfriedrice@users.noreply.github.com>
2024-07-15 17:16:00 +0800

    chore: use AffineSchemas

--- packages/common/infra/src/modules/workspace/global-schema.ts
+++ packages/common/infra/src/modules/workspace/global-schema.ts
@@ -1,17 +1,6 @@
 import { AffineSchemas } from '@blocksuite/blocks/schemas';
-import { AIChatBlockSchema } from '@blocksuite/presets';
 import { Schema } from '@blocksuite/store';
 
 export const globalBlockSuiteSchema = new Schema();
 
-const schemas = [...AffineSchemas, AIChatBlockSchema];
-const surfaceBlockSchema = schemas.find(
-  schema => schema.model.flavour === 'affine:surface'
-);
-
-if (surfaceBlockSchema) {
-  const AIChatBlockFlavour = AIChatBlockSchema.model.flavour;
-  surfaceBlockSchema.model.children?.push(AIChatBlockFlavour);
-}
-
-globalBlockSuiteSchema.register(schemas);
+globalBlockSuiteSchema.register(AffineSchemas);
