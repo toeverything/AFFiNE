11a2dc7d7f8401cc3be8683054fada97177c17c5
CatsJuice <wp1298554944@hotmail.com>
2024-07-25 08:00:54 +0000

    feat(core): bump theme (#7587)
    close AF-1108

--- packages/frontend/component/src/theme/theme.css.ts
+++ packages/frontend/component/src/theme/theme.css.ts
@@ -1,20 +1,12 @@
-import {
-  cssVar,
-  darkCssVariables,
-  lightCssVariables,
-} from '@toeverything/theme';
+import { cssVar } from '@toeverything/theme';
 import { globalStyle } from '@vanilla-extract/css';
+
 globalStyle('body', {
   color: cssVar('textPrimaryColor'),
   fontFamily: cssVar('fontFamily'),
   fontSize: cssVar('fontBase'),
 });
-globalStyle('html', {
-  vars: lightCssVariables,
-});
-globalStyle('html[data-theme="dark"]', {
-  vars: darkCssVariables,
-});
+
 if (process.env.NODE_ENV === 'development') {
   globalStyle('.undefined', {
     border: '5px solid red !important',
