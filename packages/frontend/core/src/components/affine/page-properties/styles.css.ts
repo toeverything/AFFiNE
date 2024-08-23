316ebaf785a7060c9eb9cf1fe1fbcb5adaa37a22
DarkSky <darksky2048@gmail.com>
2024-08-23 12:38:50 +0800

    fix: user cell style

--- packages/frontend/core/src/components/affine/page-properties/styles.css.ts
+++ packages/frontend/core/src/components/affine/page-properties/styles.css.ts
@@ -353,9 +353,9 @@ export const propertyRowValueUserCell = style([
   propertyRowValueCell,
   {
     border: 'none',
-    height: '100%',
     overflow: 'hidden',
     columnGap: '0.5rem',
+    alignItems: 'center',
   },
 ]);
 
