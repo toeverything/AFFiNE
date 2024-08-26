4d75da61fd7ce10a025e6cacf45dc30aa6669b11
DarkSky <darksky2048@gmail.com>
2024-08-26 11:31:33 +0800

    chore: adjust onDelete behavior

--- packages/backend/server/migrations/20240826033024_editor_record/migration.sql
+++ packages/backend/server/migrations/20240826033024_editor_record/migration.sql
@@ -6,7 +6,7 @@ ALTER TABLE "snapshots" ADD COLUMN     "created_by" VARCHAR,
 ADD COLUMN     "updated_by" VARCHAR;
 
 -- AlterTable
-ALTER TABLE "updates" ADD COLUMN     "created_by" VARCHAR NOT NULL DEFAULT 'system';
+ALTER TABLE "updates" ADD COLUMN     "created_by" VARCHAR DEFAULT 'system';
 
 -- AddForeignKey
 ALTER TABLE "snapshots" ADD CONSTRAINT "snapshots_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -15,7 +15,7 @@ ALTER TABLE "snapshots" ADD CONSTRAINT "snapshots_created_by_fkey" FOREIGN KEY (
 ALTER TABLE "snapshots" ADD CONSTRAINT "snapshots_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "updates" ADD CONSTRAINT "updates_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "updates" ADD CONSTRAINT "updates_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "snapshot_histories" ADD CONSTRAINT "snapshot_histories_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "snapshot_histories" ADD CONSTRAINT "snapshot_histories_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
