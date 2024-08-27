3e810eb043e62811ba3ab2e021c6f4b92fb4fe70
EYHN <cneyhn@gmail.com>
2024-08-27 12:37:30 +0000

    fix(core): no share page in desktop (#7983)

--- packages/frontend/core/src/pages/workspace/index.tsx
+++ packages/frontend/core/src/pages/workspace/index.tsx
@@ -100,7 +100,7 @@ export const Component = (): ReactElement => {
   if (workspaceNotFound) {
     if (
       detailDocRoute /*  */ &&
-      environment.isBrowser /* only browser has share page */
+      !environment.isDesktop /* only browser has share page */
     ) {
       return (
         <SharePage
