2799e8cd430f0cd5cbc724778b2b5bf2e1c83629
EYHN <cneyhn@gmail.com>
2024-09-04 08:00:09 +0000

    fix(electron): fix ci (#8085)

--- packages/frontend/electron/vitest.config.ts
+++ packages/frontend/electron/vitest.config.ts
@@ -21,8 +21,8 @@ export default defineConfig({
     include: ['./test/**/*.spec.ts'],
     testTimeout: 5000,
     poolOptions: {
-      threads: {
-        singleThread: true,
+      forks: {
+        singleFork: true,
       },
     },
     coverage: {
