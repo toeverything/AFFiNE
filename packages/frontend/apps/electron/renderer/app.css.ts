38ecb30bdf1cb67a27aeab0b9ff7a2de679784e8
Peng Xiao <pengxiao@outlook.com>
2024-09-12 17:21:30 +0800

    fix(electron): app fallback style

---
+++ packages/frontend/apps/electron/renderer/app.css.ts
@@ -0,0 +1,5 @@
+import { style } from '@vanilla-extract/css';
+
+export const fallback = style({
+  paddingTop: 52,
+});
---
+++ packages/frontend/core/src/components/affine/app-container.css.ts
@@ -0,0 +1,5 @@
+import { style } from '@vanilla-extract/css';
+
+export const electronFallback = style({
+  paddingTop: 52,
+});
