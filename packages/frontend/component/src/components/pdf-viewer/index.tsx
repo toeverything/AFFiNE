9eb119c83412c63b4fcfe32e130e596668fdc129
Fangdun Tsai <fundon@pindash.io>
2024-10-21 20:44:36 +0800

    feat: init PDF Viewer

---
+++ packages/frontend/component/src/components/pdf-viewer/index.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement } from 'react';
+
+export const PDFViewer = (): ReactElement => {
+  return (
+    <>
+      <div>{'PDF Viewer'}</div>
+      <div>{'PDF ...'}</div>
+    </>
+  );
+};
