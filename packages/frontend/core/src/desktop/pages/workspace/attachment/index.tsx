946bba745cb508686c68932f107c3a94a963146e
Fangdun Tsai <fundon@pindash.io>
2024-10-21 20:34:49 +0800

    feat(core): PDF preview

---
+++ packages/frontend/core/src/desktop/pages/workspace/attachment/index.tsx
@@ -0,0 +1,14 @@
+import type { ReactElement } from 'react';
+
+export const AttachmentViewer = (): ReactElement => {
+  return (
+    <>
+      <div>{'Attachment Viewer'}</div>
+      <div>{'Attachment Viewer'}</div>
+    </>
+  );
+};
+
+export const Component = () => {
+  return <AttachmentViewer />;
+};
