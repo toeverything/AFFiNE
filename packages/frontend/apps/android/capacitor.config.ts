57bb028369f00d5cf112e4589a5cda822c37a1fa
LongYinan <lynweklm@gmail.com>
2024-10-22 18:30:23 +0800

    feat(android): android internal testing

--- packages/frontend/apps/android/capacitor.config.ts
+++ packages/frontend/apps/android/capacitor.config.ts
@@ -1,3 +1,5 @@
+import { join } from 'node:path';
+
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
@@ -6,6 +8,13 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   android: {
     path: 'App',
+    buildOptions: {
+      keystorePath: join(__dirname, 'affine.keystore'),
+      keystorePassword: process.env.AFFINE_ANDROID_KEYSTORE_PASSWORD,
+      keystoreAlias: 'key0',
+      keystoreAliasPassword: process.env.AFFINE_ANDROID_KEYSTORE_ALIAS_PASSWORD,
+      releaseType: 'AAB',
+    },
   },
 };
 
