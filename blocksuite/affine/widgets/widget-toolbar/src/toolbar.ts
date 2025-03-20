5b3c3de3616db3e5049e8ddab116aa728d651220
oleg <67059482+OlegDev1@users.noreply.github.com>
2025-03-20 22:46:39 +0000

    fix(editor): move attachment block toolbar to the left

--- blocksuite/affine/widgets/widget-toolbar/src/toolbar.ts
+++ blocksuite/affine/widgets/widget-toolbar/src/toolbar.ts
@@ -84,7 +84,7 @@ export class AffineToolbarWidget extends WidgetComponent {
     }
   `;
 
-  placement$ = signal<Placement>('top-start');
+  placement$ = signal<Placement>('top');
 
   sideOptions$ = signal<Partial<SideObject> | null>(null);
 
@@ -366,7 +366,7 @@ export class AffineToolbarWidget extends WidgetComponent {
 
           sideOptions$.value = null;
           flavour$.value = flavour;
-          placement$.value = flavour === 'affine:note' ? 'top' : 'top-start';
+          placement$.value = 'top-start';
           flags.refresh(Flag.Block);
         });
       })
