3eb575235503be9fc3345d3dc1e6e2f84f3070d2
Lakr <git@qaq.wiki>
2025-04-01 13:35:20 +0800

    feat: attach document to context

--- packages/frontend/apps/ios/App/Packages/Intelligents/Sources/Intelligents/Interface/IntelligentsChatController/IntelligentsChatController+Chat.swift
+++ packages/frontend/apps/ios/App/Packages/Intelligents/Sources/Intelligents/Interface/IntelligentsChatController/IntelligentsChatController+Chat.swift
@@ -238,11 +238,25 @@ private extension IntelligentsChatController {
       self.tableView.scrollLastCellToTop()
     }
 
+    var textContent = text
+    if !isDocumentAttachedToMessage {
+      // TODO: Update Use Indexer
+      isDocumentAttachedToMessage = true
+      textContent = """
+      <system_content_begin>
+      \(metadata[.content] ?? "-")
+      <system_content_end>
+      <user_message_begin>
+      \(text)
+      <user_message_end>
+      """
+    }
+
     let sem = DispatchSemaphore(value: 0)
     let sessionID = sessionID
     Intelligents.qlClient.perform(
       mutation: CreateCopilotMessageMutation(options: .init(
-        content: .init(stringLiteral: text),
+        content: .init(stringLiteral: textContent),
         sessionId: sessionID
       )),
       queue: .global()
