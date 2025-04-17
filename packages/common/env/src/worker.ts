4b667f9dd95d901d193166078f413fe527da2757
forehalo <forehalo@gmail.com>
2025-04-17 17:54:09 +0800

    core(core): wrong worker url prefix

--- packages/common/env/src/worker.ts
+++ packages/common/env/src/worker.ts
@@ -1,6 +1,7 @@
 export function getWorkerUrl(name: string) {
   return (
-    environment.publicPath +
+    // NOTE: worker can not use publicPath because it must obey the same-origin policy
+    (environment.subPath || '/') +
     'js/' +
     `${name}-${BUILD_CONFIG.appVersion}.worker.js`
   );
