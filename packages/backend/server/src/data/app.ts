e1de0365ce9362d91e15817210dfe98fc79bddf2
fengmk2 <fengmk2@gmail.com>
2025-04-21 13:07:27 +0800

    feat(server): create indexer tables on migration mk/create-indexer-tables-migration

--- packages/backend/server/src/data/app.ts
+++ packages/backend/server/src/data/app.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { FunctionalityModules } from '../app.module';
+import { IndexerModule } from '../plugins/indexer';
 import { CreateCommand, NameQuestion } from './commands/create';
 import { ImportConfigCommand } from './commands/import';
 import { RevertCommand, RunCommand } from './commands/run';
 
 @Module({
-  imports: FunctionalityModules,
+  imports: [...FunctionalityModules, IndexerModule],
   providers: [
     NameQuestion,
     CreateCommand,
