16ecd0a544b2e5445bf332c5a14732ced549d5b5
congzhou09 <yuancongzhou@yeah.net>
2025-04-29 10:26:27 +0800

    fix(server): read the .env file on time

---
+++ packages/backend/server/src/dotenv.ts
@@ -0,0 +1,2 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
--- packages/backend/server/src/prelude.ts
+++ packages/backend/server/src/prelude.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import './dotenv';
 import './env';
 
 import { existsSync, readFileSync } from 'node:fs';
