d6e9bdcc1cbf1abbe25db4e17a72ab1571f1a803
Cats Juice <wp1298554944@hotmail.com>
2025-05-22 09:44:10 +0800

    fix(core): correct new page button background

--- packages/frontend/core/src/components/page-list/components/new-page-button.css.ts
+++ packages/frontend/core/src/components/page-list/components/new-page-button.css.ts
@@ -1,5 +1,9 @@
 import { cssVar } from '@toeverything/theme';
+import { cssVarV2 } from '@toeverything/theme/v2';
 import { style } from '@vanilla-extract/css';
 export const menuContent = style({
   backgroundColor: cssVar('backgroundOverlayPanelColor'),
+});
+export const button = style({
+  backgroundColor: cssVarV2.button.secondary,
 });
