c27acc717a6965ad45d5a912cafd825c3c0b60e9
NorkzYT <richard@pcscorp.dev>
2025-06-11 09:45:05 -0400

    style(extract-emoji-icon.ts): format regular expression declaration for better readability

--- packages/frontend/core/src/utils/extract-emoji-icon.ts
+++ packages/frontend/core/src/utils/extract-emoji-icon.ts
@@ -1,5 +1,6 @@
 import Graphemer from 'graphemer';
-const emojiRe = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
+const emojiRe =
+  /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
 
 export function extractEmojiIcon(text: string) {
   emojiRe.lastIndex = 0;
