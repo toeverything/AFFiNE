ccd896f56fd47832d32476eb072e3fd49790618b
Lakr <git@qaq.wiki>
2025-06-13 15:04:22 +0800

    Clean Up

--- packages/frontend/apps/ios/App/Packages/Intelligents/Package.swift
+++ packages/frontend/apps/ios/App/Packages/Intelligents/Package.swift
@@ -7,26 +7,20 @@ let package = Package(
   name: "Intelligents",
   defaultLocalization: "en",
   platforms: [
-    .iOS(.v15),
-    .macCatalyst(.v15),
+    .iOS(.v17),
   ],
   products: [
     .library(name: "Intelligents", targets: ["Intelligents"]),
   ],
   dependencies: [
     .package(path: "../AffineGraphQL"),
-    .package(path: "../MarkdownView"),
-    .package(url: "https://github.com/apollographql/apollo-ios.git", from: "1.22.0"),
+    .package(url: "https://github.com/apollographql/apollo-ios.git", from: "1.18.0"),
     .package(url: "https://github.com/LaunchDarkly/swift-eventsource.git", from: "3.3.0"),
     .package(url: "https://github.com/apple/swift-collections", from: "1.2.0"),
-    .package(url: "https://github.com/Lakr233/ChidoriMenu", from: "3.0.0"),
   ],
   targets: [
     .target(name: "Intelligents", dependencies: [
       "AffineGraphQL",
-      "ChidoriMenu",
-      "MarkdownView",
-      "ChidoriMenu",
       .product(name: "Apollo", package: "apollo-ios"),
       .product(name: "LDSwiftEventSource", package: "swift-eventsource"),
       .product(name: "OrderedCollections", package: "swift-collections"),
