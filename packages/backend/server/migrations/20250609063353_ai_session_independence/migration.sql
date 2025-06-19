85078e14c44d5a54e48b23bd7f8470ed111bc7b6
DarkSky <darksky2048@gmail.com>
2025-06-19 19:33:11 +0800

    chore: format

--- packages/backend/server/migrations/20250609063353_ai_session_independence/migration.sql
+++ packages/backend/server/migrations/20250609063353_ai_session_independence/migration.sql
@@ -8,10 +8,10 @@ ALTER TABLE "ai_sessions_metadata" ADD COLUMN     "pinned" BOOLEAN NOT NULL DEFA
 ALTER TABLE "ai_sessions_metadata" ADD COLUMN     "prompt_action" VARCHAR(32) DEFAULT '';
 
 -- CreateIndex
-CREATE UNIQUE INDEX ai_session_unique_pinned_idx ON ai_sessions_metadata (user_id, workspace_id) WHERE pinned = true AND deleted_at IS NULL;
+CREATE UNIQUE INDEX "ai_session_unique_pinned_idx" ON "ai_sessions_metadata" (user_id, workspace_id) WHERE pinned = true AND deleted_at IS NULL;
 
 -- CreateIndex
-CREATE UNIQUE INDEX ai_session_unique_doc_session_idx ON ai_sessions_metadata (user_id, workspace_id, doc_id) WHERE prompt_action IS NULL AND parent_session_id IS NULL AND doc_id IS NOT NULL AND deleted_at IS NULL;
+CREATE UNIQUE INDEX "ai_session_unique_doc_session_idx" ON "ai_sessions_metadata" (user_id, workspace_id, doc_id) WHERE prompt_action IS NULL AND parent_session_id IS NULL AND doc_id IS NOT NULL AND deleted_at IS NULL;
 
 -- CreateIndex
 CREATE INDEX "ai_sessions_metadata_prompt_name_idx" ON "ai_sessions_metadata"("prompt_name");
