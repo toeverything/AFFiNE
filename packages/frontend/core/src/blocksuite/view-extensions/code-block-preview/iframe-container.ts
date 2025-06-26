cc2c614e8eda2ad961dcc2d8b4343def56acfe66
eyhn <cneyhn@gmail.com>
2025-06-26 10:38:10 +0800

    feat(editor): use affine container url in preview

--- packages/frontend/core/src/blocksuite/view-extensions/code-block-preview/iframe-container.ts
+++ packages/frontend/core/src/blocksuite/view-extensions/code-block-preview/iframe-container.ts
@@ -2,6 +2,19 @@ import type { CodeBlockModel } from '@blocksuite/affine/model';
 
 export function linkIframe(iframe: HTMLIFrameElement, model: CodeBlockModel) {
   const html = model.props.text.toString();
-  iframe.srcdoc = html;
-  iframe.sandbox.add('allow-scripts', 'allow-same-origin');
+  // force reload iframe
+  iframe.src = '';
+  iframe.src = 'https://affine.run/static/container.html';
+  iframe.sandbox.add(
+    'allow-pointer-lock',
+    'allow-popups',
+    'allow-forms',
+    'allow-popups-to-escape-sandbox',
+    'allow-downloads',
+    'allow-scripts',
+    'allow-same-origin'
+  );
+  iframe.onload = () => {
+    iframe.contentWindow?.postMessage(html, 'https://affine.run');
+  };
 }
