5d86466bbfcaeb10720b1be0d6584574e8461323
L-Sun <zover.v@gmail.com>
2025-07-07 17:42:40 +0800

    fix(editor): centerize iframe modal in mobile

--- blocksuite/affine/widgets/slash-menu/src/slash-menu-popover.ts
+++ blocksuite/affine/widgets/slash-menu/src/slash-menu-popover.ts
@@ -378,7 +378,7 @@ export class InnerSlashMenu extends WithDisposable(LitElement) {
       this._closeSubMenu();
     });
 
-    const subMenuElement = createLitPortal({
+    const { portal: subMenuElement } = createLitPortal({
       shadowDom: false,
       template: html`<inner-slash-menu
         .context=${this.context}
