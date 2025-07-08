d4317547dec1b2dde05629a176c8b19c2ead847d
DarkSky <darksky2048@gmail.com>
2025-07-08 17:36:56 +0800

    fix: session updated at

---
+++ packages/backend/server/src/data/migrations/1751966744168-correct-session-update-time.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+
+type SessionTime = {
+  sessionId: string;
+  _max: {
+    createdAt: Date;
+  };
+};
+
+export class CorrectSessionUpdateTime1751966744168 {
+  // do the migration
+  static async up(db: PrismaClient) {
+    const sessionTime = await db.aiSessionMessage.groupBy({
+      by: ['sessionId'],
+      _max: {
+        createdAt: true,
+      },
+    });
+
+    await db.$transaction(async tx => {
+      await Promise.all(
+        sessionTime
+          .filter((s): s is SessionTime => !!s._max.createdAt)
+          .map(s =>
+            tx.aiSession.update({
+              where: { id: s.sessionId },
+              data: { updatedAt: s._max.createdAt },
+            })
+          )
+      );
+    });
+  }
+
+  // revert the migration
+  static async down(_db: PrismaClient) {}
+}
