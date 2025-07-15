ea4b0d7fb8c4c2880f3669f161d73280ea7ee645
DarkSky <darksky2048@gmail.com>
2025-07-15 19:01:51 +0800

    feat: delay send mail if retry many times

--- packages/backend/server/src/base/utils/promise.ts
+++ packages/backend/server/src/base/utils/promise.ts
@@ -48,3 +48,7 @@ export function defer(dispose: () => Promise<void>) {
     [Symbol.asyncDispose]: dispose,
   };
 }
+
+export function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
