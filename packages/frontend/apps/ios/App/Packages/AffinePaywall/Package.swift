0208c6dcc6cc15a7af0aff5c5c5c8040eec7f29b
Lakr <git@qaq.wiki>
2025-09-26 17:56:55 +0800

    chore: fix ci build

--- packages/frontend/apps/ios/App/Packages/AffinePaywall/Package.swift
+++ packages/frontend/apps/ios/App/Packages/AffinePaywall/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
