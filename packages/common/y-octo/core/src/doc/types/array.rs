31a8675a0faca7bb3782563613b7ded5439482a8
Abimael10 <juanabimael@gmail.com>
2025-10-16 11:26:04 -0400

    Simplify Array::get method with functional approach

--- packages/common/y-octo/core/src/doc/types/array.rs
+++ packages/common/y-octo/core/src/doc/types/array.rs
@@ -36,15 +36,13 @@ impl Array {
   pub fn get(&self, index: u64) -> Option<Value> {
     let (item, offset) = self.get_item_at(index)?;
 
-    if let Some(item) = item.get() {
+    item.get().and_then(|item| {
       // TODO: rewrite to content.read(&mut [Any])
-      return match &item.content {
-        Content::Any(any) => return any.get(offset as usize).map(|any| Value::Any(any.clone())),
+      match &item.content {
+        Content::Any(any) => any.get(offset as usize).map(|any| Value::Any(any.clone())),
         _ => Some(Value::from(&item.content)),
-      };
-    }
-
-    None
+      }
+    })
   }
 
   pub fn iter(&self) -> ArrayIter {
