65cb5d02ecfa039da1a24f27876e690626f56459
hi <hi@qsu.io>
2025-12-17 16:15:27 +0800

    fix: fix typo in link shortcut key binding

--- packages/frontend/core/src/components/hooks/affine/use-shortcuts.ts
+++ packages/frontend/core/src/components/hooks/affine/use-shortcuts.ts
@@ -237,7 +237,7 @@ export const useWinPageKeyboardShortcuts = (): ShortcutMap => {
       [t('strikethrough')]: ['Ctrl', 'Shift', 'S'],
       [t('inlineCode')]: [' Ctrl', 'E'],
       [t('codeBlock')]: ['Ctrl', 'Alt', 'C'],
-      [t('link')]: ['Ctr', 'K'],
+      [t('link')]: ['Ctrl', 'K'],
       [t('quickSearch')]: ['Ctrl', 'K'],
       [t('bodyText')]: ['Ctrl', 'Shift', '0'],
       [tH('1')]: ['Ctrl', 'Shift', '1'],
