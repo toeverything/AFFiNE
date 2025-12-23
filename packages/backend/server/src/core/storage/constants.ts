41ca8def9489ccc07607bb3f9ba618da8fccb8a0
DarkSky <darksky2048@gmail.com>
2025-12-23 17:38:05 +0800

    fix: multipart size

--- packages/backend/server/src/core/storage/constants.ts
+++ packages/backend/server/src/core/storage/constants.ts
@@ -1,4 +1,4 @@
 import { OneMB } from '../../base';
 
-export const MULTIPART_PART_SIZE = 4 * OneMB;
-export const MULTIPART_THRESHOLD = 8 * OneMB;
+export const MULTIPART_PART_SIZE = 5 * OneMB;
+export const MULTIPART_THRESHOLD = 10 * OneMB;
