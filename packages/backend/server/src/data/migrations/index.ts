9572a6030fd99e2e4e0a09135d4bcce1d3a242b3
DarkSky <darksky2048@gmail.com>
2026-01-03 02:11:24 +0800

    feat: cleanup tables

--- packages/backend/server/src/data/migrations/index.ts
+++ packages/backend/server/src/data/migrations/index.ts
@@ -1,7 +1,5 @@
 export * from './1698398506533-guid';
 export * from './1703756315970-unamed-account';
 export * from './1721299086340-refresh-unnamed-user';
-export * from './1732861452428-migrate-invite-status';
-export * from './1733125339942-universal-subscription';
 export * from './1745211351719-create-indexer-tables';
 export * from './1751966744168-correct-session-update-time';
