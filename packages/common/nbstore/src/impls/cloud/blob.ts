6abcf5fe9370e5966e7d6c8154a910c10d721e70
DarkSky <darksky2048@gmail.com>
2026-01-04 00:23:51 +0800

    fix: blob redirect

--- packages/common/nbstore/src/impls/cloud/blob.ts
+++ packages/common/nbstore/src/impls/cloud/blob.ts
@@ -25,7 +25,8 @@ interface CloudBlobStorageOptions {
   id: string;
 }
 
-const SHOULD_MANUAL_REDIRECT = BUILD_CONFIG.isAndroid || BUILD_CONFIG.isIOS;
+const SHOULD_MANUAL_REDIRECT =
+  BUILD_CONFIG.isAndroid || BUILD_CONFIG.isIOS || BUILD_CONFIG.isElectron;
 const UPLOAD_REQUEST_TIMEOUT = 0;
 
 export class CloudBlobStorage extends BlobStorageBase {
