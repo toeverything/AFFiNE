5f578d56e4bdccecb461098221a2714a50af8bbc
DarkSky <darksky2048@gmail.com>
2026-07-12 01:10:40 +0800

    fix: test & lint

--- tools/utils/src/workspace.gen.ts
+++ tools/utils/src/workspace.gen.ts
@@ -1209,7 +1209,6 @@ export const PackageList = [
     location: 'packages/frontend/apps/electron',
     name: '@affine/electron',
     workspaceDependencies: [
-      'packages/common/auth',
       'tools/utils',
       'packages/frontend/i18n',
       'packages/frontend/native',
