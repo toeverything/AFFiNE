44698d6c578e3db7f8b631ef0bebb5d8f568038e
DarkSky <darksky2048@gmail.com>
2026-08-10 09:58:12 +0800

    feat: update schema & bump deps

---
+++ packages/frontend/apps/ios/App/Packages/AffineGraphQL/Sources/Schema/Objects/WorkspaceByokProbeStatusType.graphql.swift
@@ -0,0 +1,12 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public extension Objects {
+  static let WorkspaceByokProbeStatusType = ApolloAPI.Object(
+    typename: "WorkspaceByokProbeStatusType",
+    implementedInterfaces: [],
+    keyFields: nil
+  )
+}
